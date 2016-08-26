1f3808608811b2ef393db59fb1ac9ed6d969d992
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-08-26 13:15:40 +0300

    feat: support the npm fetch-* configs
    Use the retry package to support all the different
    retry strategy configurations that can be used with npm:
    
    fetch-retries
    fetch-retry-factor
    fetch-retry-mintimeout
    fetch-retry-maxtimeout
    
    More info about the configs: https://docs.npmjs.com/misc/config#fetch-retries
    
    close #317, PR #327

--- test/support/prepare.js
+++ test/support/prepare.js
@@ -5,10 +5,17 @@ const join = require('path').join
 const root = process.cwd()
 process.env.ROOT = root
 
+const tmpPath = join(root, '.tmp')
+mkdirp.sync(tmpPath)
+const npmrc = [
+  'store-path=./node_modules/.store'
+]
+fs.writeFileSync(join(tmpPath, '.npmrc'), npmrc, 'utf-8')
+
 module.exports = function prepare (pkg) {
-  const tmpPath = join(root, '.tmp', Math.random().toString())
-  mkdirp.sync(tmpPath)
+  const pkgTmpPath = join(tmpPath, Math.random().toString())
+  mkdirp.sync(pkgTmpPath)
   const json = JSON.stringify(pkg || {})
-  fs.writeFileSync(join(tmpPath, 'package.json'), json, 'utf-8')
-  process.chdir(tmpPath)
+  fs.writeFileSync(join(pkgTmpPath, 'package.json'), json, 'utf-8')
+  process.chdir(pkgTmpPath)
 }
