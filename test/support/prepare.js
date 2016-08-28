875656fc4e377a0dca8ab10694a3853c08df6597
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-08-28 16:53:56 +0300

    test: retry fetch more

--- test/support/prepare.js
+++ test/support/prepare.js
@@ -8,7 +8,9 @@ process.env.ROOT = root
 const tmpPath = join(root, '.tmp')
 mkdirp.sync(tmpPath)
 const npmrc = [
-  'store-path=./node_modules/.store'
+  'store-path=./node_modules/.store',
+  'fetch-retries=5',
+  'fetch-retry-maxtimeout=180000'
 ]
 fs.writeFileSync(join(tmpPath, '.npmrc'), npmrc, 'utf-8')
 
