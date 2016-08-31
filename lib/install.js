pnpm/pnpm

commit 0c247a1f4cc1959fe370109f488bb552c50ade20

Lock store when running commands
close #82

lib/install.js
@@ -3,6 +3,10 @@ var debug = require('debug')('pnpm:install')
 var npa = require('npm-package-arg')
 var getUuid = require('node-uuid')
 var fs = require('mz/fs')
+var thenify = require('thenify')
+var lock = thenify(require('lockfile').lock)
+var unlock = thenify(require('lockfile').unlock)
+var checkLock = thenify(require('lockfile').unlock)
 
 var join = require('path').join
 var dirname = require('path').dirname
@@ -184,7 +188,7 @@ function fetchToStore (ctx, paths, pkg, log) {
     .then(_ => log('download-queued'))
     .then(_ => mkdirp(ctx.store))
     .then(_ => mkdirp(join(paths.tmp, '_')))
-    .then(_ => fs.writeFile(join(paths.tmp, '.pnpm_inprogress'), '', 'utf-8'))
+    .then(_ => lock(join(paths.tmp, '.pnpm_inprogress')))
     .then(_ => fetch(join(paths.tmp, '_'), pkg.dist, log))
     .then(_ => pkg.dist.local ? fs.unlink(pkg.dist.tarball) : Promise.resolve())
 
@@ -222,7 +226,7 @@ function buildInStore (ctx, paths, pkg, log) {
     .then(_ => !ctx.ignoreScripts && postInstall(paths.tmp, fulldata, installLogger(log, pkg)))
 
     // move to .store/lodash@4.0.0; remove the stub done earlier
-    .then(_ => fs.unlink(join(paths.tmp, '.pnpm_inprogress')))
+    .then(_ => unlock(join(paths.tmp, '.pnpm_inprogress')))
     // we need to make sure that symlinkToModules for another project dependent
     // on this package will not get called inbetween `unlink` and `rename`
     // the easiest way to achieve this is to make them synchronous
@@ -291,11 +295,10 @@ function symlinkToModules (target, modules) {
 function make (path, isWorking, fn) {
   return fs.stat(path)
   .then(_ => {
-    return fs.stat(join(path, '.pnpm_inprogress'))
-    .then(_ => {
-      if (!isWorking) return obliterate(path).then(fn)
+    return checkLock(join(path, '.pnpm_inprogress'))
+    .then(isLocked => {
+      if (isLocked && !isWorking) return obliterate(path).then(fn)
     })
-    .catch(err => { if (err.code !== 'ENOENT') throw err })
   })
   .catch(err => {
     if (err.code !== 'ENOENT') throw err

lib/cmd/install.js
@@ -31,6 +31,11 @@ function installCmd (input, opts) {
     .then(_ => install())
     .then(_ => linkPeers(cmd.pkg, cmd.ctx.store, cmd.ctx.installs))
     .then(_ => mainPostInstall())
+    .then(_ => cmd.unlock())
+    .catch(err => {
+      cmd.unlock()
+      throw err
+    })
 
   function install () {
     installType = input && input.length ? 'named' : 'general'

lib/cmd/init_cmd.js
@@ -1,23 +1,32 @@
 var readPkgUp = require('read-pkg-up')
 var resolve = require('path').resolve
 var dirname = require('path').dirname
+var thenify = require('thenify')
+var lock = thenify(require('lockfile').lock)
+var unlock = thenify(require('lockfile').unlock)
 
 var logger = require('../logger')
 var storeJsonController = require('../fs/store_json_controller')
+var mkdirp = require('../fs/mkdirp')
 
 module.exports = function (opts) {
   var cmd = {
     ctx: {}
   }
+  var lockfile
   return readPkgUp()
     .then(_ => { cmd.pkg = _ })
     .then(_ => updateContext())
+    .then(_ => mkdirp(cmd.ctx.store))
+    .then(_ => lock(lockfile))
     .then(_ => cmd)
 
   function updateContext () {
     var root = cmd.pkg.path ? dirname(cmd.pkg.path) : process.cwd()
     cmd.ctx.root = root
     cmd.ctx.store = resolve(root, opts.store_path)
+    lockfile = resolve(cmd.ctx.store, 'lock')
+    cmd.unlock = () => unlock(lockfile)
     cmd.storeJson = storeJsonController(opts.store_path)
     Object.assign(cmd.ctx, cmd.storeJson.read())
     if (!opts.quiet) cmd.ctx.log = logger(opts.logger)

lib/cmd/uninstall.js
@@ -34,6 +34,11 @@ function uninstallCmd (input, opts) {
     }))
     .then(_ => Promise.all(input.map(dep => rimraf(join(cmd.ctx.root, 'node_modules', dep)))))
     .then(_ => saveType && removeDeps(cmd.pkg, input, saveType))
+    .then(_ => cmd.unlock())
+    .catch(err => {
+      cmd.unlock()
+      throw err
+    })
 
   function canBeUninstalled (pkgFullName) {
     return !cmd.ctx.dependents[pkgFullName] || !cmd.ctx.dependents[pkgFullName].length ||

test/index.js
@@ -484,6 +484,28 @@ test('keep dependencies used by others', function (t) {
   }, t.end)
 })
 
+test('fail when trying to install into the same store simultaneously', function (t) {
+  prepare()
+  install(['browserify'], { quiet: true })
+  install(['rimraf@2.5.1'], { quiet: true })
+    .then(_ => t.fail('the store should have been locked'))
+    .catch(err => {
+      t.ok(err, 'store is locked')
+      t.end()
+    })
+})
+
+test('fail when trying to install and uninstall from the same store simultaneously', function (t) {
+  prepare()
+  install(['browserify'], { quiet: true })
+  uninstall(['rimraf@2.5.1'], { quiet: true })
+    .then(_ => t.fail('the store should have been locked'))
+    .catch(err => {
+      t.ok(err, 'store is locked')
+      t.end()
+    })
+})
+
 function exists (path) {
   try {
     return fs.statSync(path)

commit 13661864a8e09933321ba9206f67eb7eab966786

fix: temp dir locking
This was broken by commit 0c247a1f4cc1959fe370109f488bb552c50ade20
The fix is just reverting the unnecessary changes that have broken
the tmp dir locking.

PR #339

lib/install.js
@@ -2,10 +2,6 @@
 const debug = require('debug')('pnpm:install')
 const npa = require('npm-package-arg')
 const fs = require('mz/fs')
-const thenify = require('thenify')
-const lock = thenify(require('lockfile').lock)
-const unlock = thenify(require('lockfile').unlock)
-const checkLock = thenify(require('lockfile').unlock)
 const sanitizeFilename = require('sanitize-filename')
 
 const join = require('path').join
@@ -195,7 +191,7 @@ function fetchToStore (ctx, paths, pkg, log) {
     .then(_ => log('download-queued'))
     .then(_ => mkdirp(ctx.store))
     .then(_ => mkdirp(join(paths.tmp, '_')))
-    .then(_ => lock(join(paths.tmp, '.pnpm_inprogress')))
+    .then(_ => fs.writeFile(join(paths.tmp, '.pnpm_inprogress'), '', 'utf-8'))
     .then(_ => fetch(join(paths.tmp, '_'), pkg.dist, {log, got: ctx.got}))
     .then(_ => pkg.dist.local && pkg.dist.remove ? fs.unlink(pkg.dist.tarball) : Promise.resolve())
 
@@ -234,7 +230,7 @@ function buildInStore (ctx, paths, pkg, log) {
     .then(_ => !ctx.ignoreScripts && postInstall(paths.tmp, fulldata, installLogger(log, pkg)))
 
     // move to .store/lodash@4.0.0; remove the stub done earlier
-    .then(_ => unlock(join(paths.tmp, '.pnpm_inprogress')))
+    .then(_ => fs.unlink(join(paths.tmp, '.pnpm_inprogress')))
     // we need to make sure that symlinkToModules for another project dependent
     // on this package will not get called inbetween `unlink` and `rename`
     // the easiest way to achieve this is to make them synchronous
@@ -303,10 +299,11 @@ function symlinkToModules (target, modules) {
 function make (path, isWorking, fn) {
   return fs.stat(path)
   .then(_ => {
-    return checkLock(join(path, '.pnpm_inprogress'))
-    .then(isLocked => {
-      if (isLocked && !isWorking) return obliterate(path).then(fn)
+    return fs.stat(join(path, '.pnpm_inprogress'))
+    .then(_ => {
+      if (!isWorking) return obliterate(path).then(fn)
     })
+    .catch(err => { if (err.code !== 'ENOENT') throw err })
   })
   .catch(err => {
     if (err.code !== 'ENOENT') throw err