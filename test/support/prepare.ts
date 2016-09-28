5d1b4fb203a199ef33f8baf31bb0a61a9532f5f8
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-09-28 17:05:40 +0300

    test: use the official registry for e2e tests

--- test/support/prepare.ts
+++ test/support/prepare.ts
@@ -1,17 +1,19 @@
 import mkdirp = require('mkdirp')
 import fs = require('fs')
 import path = require('path')
+import {stripIndent} from 'common-tags'
 
 const root = process.cwd()
 process.env.ROOT = root
 
 const tmpPath = path.join(root, '.tmp')
 mkdirp.sync(tmpPath)
-const npmrc = [
-  'store-path=./node_modules/.store',
-  'fetch-retries=5',
-  'fetch-retry-maxtimeout=180000'
-].join('')
+const npmrc = stripIndent` 
+  store-path = ./node_modules/.store
+  fetch-retries = 5
+  fetch-retry-maxtimeout = 180000
+  registry =  https://registry.npmjs.org/
+`
 fs.writeFileSync(path.join(tmpPath, '.npmrc'), npmrc, 'utf-8')
 
 export default function prepare (pkg?: Object) {
