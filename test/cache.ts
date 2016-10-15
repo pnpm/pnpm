5c1d2a9356cbd880293efae5fc8c318dfb98a3e0
zkochan <zoltan.kochan@gmail.com>
2016-10-15 13:19:26 +0300

    test: cache TTL

--- test/cache.ts
+++ test/cache.ts
@@ -1,9 +1,11 @@
-import {cleanCache, installPkgs} from '../src'
+import {cleanCache, installPkgs, install} from '../src'
 import globalPath from './support/globalPath'
+import {add as addDistTag} from './support/distTags'
 import tape = require('tape')
 import promisifyTape = require('tape-promise')
 import exists = require('exists-file')
 import path = require('path')
+import prepare from './support/prepare'
 
 const test = promisifyTape(tape)
 
@@ -17,4 +19,41 @@ test('cache clean removes cache', async function (t) {
   await cleanCache(globalPath)
 
   t.ok(!await exists(cache), 'cache is removed')
+})
+
+test('should fail to update when requests are cached', async function (t) {
+  prepare()
+
+  const latest = 'stable'
+  const cacheTTL = 60 * 60
+
+  await addDistTag('dep-of-pkg-with-1-dep', '1.0.0', latest)
+
+  await installPkgs(['pkg-with-1-dep'], {save: true, tag: latest, cacheTTL})
+
+  t.ok(await exists('node_modules/.store/dep-of-pkg-with-1-dep@1.0.0'), 'should install dep-of-pkg-with-1-dep@1.0.0')
+
+  await addDistTag('dep-of-pkg-with-1-dep', '1.1.0', latest)
+
+  await install({depth: 1, tag: latest, cacheTTL})
+
+  t.ok(await exists('node_modules/.store/dep-of-pkg-with-1-dep@1.0.0'), 'should not update to dep-of-pkg-with-1-dep@1.1.0')
+})
+
+test('should skip cahe even if it exists when cacheTTL = 0', async function (t) {
+  prepare()
+
+  const latest = 'stable'
+
+  await addDistTag('dep-of-pkg-with-1-dep', '1.0.0', latest)
+
+  await installPkgs(['pkg-with-1-dep'], {save: true, tag: latest, cacheTTL: 60 * 60})
+
+  t.ok(await exists('node_modules/.store/dep-of-pkg-with-1-dep@1.0.0'), 'should install dep-of-pkg-with-1-dep@1.0.0')
+
+  await addDistTag('dep-of-pkg-with-1-dep', '1.1.0', latest)
+
+  await install({depth: 1, tag: latest, cacheTTL: 0})
+
+  t.ok(await exists('node_modules/.store/dep-of-pkg-with-1-dep@1.1.0'), 'should update to dep-of-pkg-with-1-dep@1.1.0')
 })
