197b2dc54dd237b7bb3ab2b4ec9856d1141dd084
zkochan <zoltan.kochan@gmail.com>
2016-12-29 22:56:48 +0200

    refactor(git): use more human-friendly Git IDs

--- src/resolve/git.ts
+++ src/resolve/git.ts
@@ -9,7 +9,10 @@ export default async function resolveGithub (parsedSpec: PackageSpec, opts: Reso
   const repo = parts[0]
   const ref = parts[1] || 'master'
   return {
-    id: parsedSpec.spec.replace(/[\/:]/g, delimiter),
+    id: parsedSpec.spec
+      .replace(/^.*:\/\/(git@)?/, '')
+      .replace(/:/g, delimiter)
+      .replace(/\.git$/, ''),
     fetch: (target: string) => {
       return clone(repo, ref, target)
     }
