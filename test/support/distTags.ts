383e772921f4426df95adff23aecc6e068892978
zkochan <zoltan.kochan@gmail.com>
2017-01-15 18:35:48 +0200

    test: use verdaccio instead of sinopia
    Unfortunately sinopia is not maintained anymore.

--- test/support/distTags.ts
+++ test/support/distTags.ts
@@ -3,10 +3,10 @@ import RegClient = require('anonymous-npm-registry-client')
 export async function add (pkg: string, version: string, distTag: string) {
   const client = new RegClient()
 
-  // just to make sinopia cache the package
+  // just to make verdaccio cache the package
   await new Promise((resolve, reject) => client.distTags.fetch('http://localhost:4873', {package: pkg}, (err: Error) => err ? reject(err) : resolve()))
 
-  // the tag has to be removed first because in sinopia it is an array of versions
+  // the tag has to be removed first because in verdaccio it is an array of versions
   await new Promise((resolve, reject) => client.distTags.rm('http://localhost:4873', {package: pkg, distTag}, (err: Error) => err ? reject(err) : resolve()))
   await new Promise((resolve, reject) => client.distTags.add('http://localhost:4873', {package: pkg, version, distTag}, (err: Error) => err ? reject(err) : resolve()))
 }
