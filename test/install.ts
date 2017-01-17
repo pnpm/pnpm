49932fcd7383d7da58b23998043b11b646aaf43d
zkochan <zoltan.kochan@gmail.com>
2017-01-18 00:03:04 +0200

    revert: "test: temporarily skip GitHub tests"
    This reverts commit 84878f8750c9b1adac3684a6d56db509c6ba6f73.

--- test/install.ts
+++ test/install.ts
@@ -367,10 +367,6 @@ test('nested local dependency of a local dependency', async function (t) {
 })
 
 test('from a github repo', async function (t) {
-  if (isCI) {
-    t.skip('Skip till https://github.com/rstacruz/pnpm/issues/536 is not resolved')
-    return t.end()
-  }
   const project = prepare(t)
   await installPkgs(['kevva/is-negative'], testDefaults())
 
@@ -666,10 +662,6 @@ test('tarball local package', async function (t) {
 })
 
 test("don't fail when peer dependency is fetched from GitHub", t => {
-  if (isCI) {
-    t.skip('Skip till https://github.com/rstacruz/pnpm/issues/536 is not resolved')
-    return t.end()
-  }
   const project = prepare(t)
   return installPkgs(['test-pnpm-peer-deps'], testDefaults())
 })
