8900b3f25dcee443c0e2b189288c2b1a0dd6a0ee
Andrey Popp <8mayday@gmail.com>
2017-01-20 23:12:40 +0300

    fix: fixes for Node 4, style fixes

--- src/api/lock.ts
+++ src/api/lock.ts
@@ -6,23 +6,21 @@ async function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function lock(lockFilename: string, notFirstTime: boolean = false): Promise<{}> {
+async function lock(lockFilename: string, firstTime: boolean): Promise<{}> {
   const promise = new Promise((resolve, reject) => {
     lockfile.lock(
       lockFilename,
       {realpath: false},
       async (err: Error & {code: string}) => {
-        if (err) {
-          if (err.code === 'ELOCKED') {
-            if (!notFirstTime) {
-              logger.warn('waiting for another installation to complete...')
-            }
-            await delay(200)
-            await lock(lockFilename, true)
-            resolve()
-          } else {
-            reject(err)
+        if (err && err.code === 'ELOCKED') {
+          if (firstTime) {
+            logger.warn('waiting for another installation to complete...')
           }
+          await delay(200)
+          await lock(lockFilename, false)
+          resolve()
+        } else if (err) {
+          reject(err)
         } else {
           resolve()
         }
@@ -42,7 +40,7 @@ async function unlock(lockFilename: string): Promise<{}> {
 
 export default async function withLock<T> (storePath: string, fn: () => Promise<T>): Promise<T> {
   const lockFilename: string = path.resolve(storePath, 'lock')
-  await lock(lockFilename)
+  await lock(lockFilename, true)
   try {
     const result = await fn()
     await unlock(lockFilename)
