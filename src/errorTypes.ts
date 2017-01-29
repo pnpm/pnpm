c72052acb357bd901587c47521b4f74512c45ece
zkochan <zoltan.kochan@gmail.com>
2017-01-29 20:08:54 +0200

    fix(logging): pass additional info with a message
    bole sends only message, code and stack from errors, so a message
    object has to be used to send additional information.

--- src/errorTypes.ts
+++ src/errorTypes.ts
@@ -3,9 +3,7 @@ export type PnpmErrorCode = 'UNEXPECTED_STORE' | 'STORE_BREAKING_CHANGE' | 'MODU
 export class PnpmError extends Error {
   constructor (code: PnpmErrorCode, message: string) {
     super(message)
-    this._code = code
+    this.code = code
   }
-
-  private _code: PnpmErrorCode
-  get code () { return this._code }
+  code: PnpmErrorCode
 }
