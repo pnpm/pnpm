4f5a26adba779898243dbfbe8f902dd4adf86169
Zoltan Kochan <zoltan.kochan@gmail.com>
2017-02-06 12:16:45 +0200

    fix: don't fail when no package shasum is found
    For backward compatibility.

--- src/pkgIsUntouched.ts
+++ src/pkgIsUntouched.ts
@@ -3,6 +3,12 @@ import dirsum from './fs/dirsum'
 
 export default async function untouched (pkgDir: string): Promise<Boolean> {
   const realShasum = await dirsum(pkgDir)
-  const originalShasum = await fs.readFile(`${pkgDir}_shasum`, 'utf8')
+  let originalShasum: string | null = null
+  try {
+    originalShasum = await fs.readFile(`${pkgDir}_shasum`, 'utf8')
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
+    return false // for backward compatibility
+  }
   return realShasum === originalShasum
 }
