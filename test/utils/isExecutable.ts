c1a8c7132598d5b3be8f6c329c078129d845fc2c
zkochan <zoltan.kochan@gmail.com>
2017-02-20 21:39:53 +0200

    test: fix executable checking

--- test/utils/isExecutable.ts
+++ test/utils/isExecutable.ts
@@ -3,7 +3,18 @@ import fs = require('mz/fs')
 import {Test} from 'tape'
 import semver = require('semver')
 import {Stats} from 'fs'
+import isWindows = require('is-windows')
+
+const IS_WINDOWS = isWindows()
 
 export default async function isExecutable (t: Test, filePath: string) {
-  t.ok(isexe(filePath), filePath + ' is executable')
+  if (IS_WINDOWS) {
+    t.ok(isexe(filePath), `${filePath} is executable`)
+    return
+  }
+
+  const stat = await fs.stat(filePath)
+  t.equal(stat.mode, parseInt('100755', 8), `${filePath} is executable`)
+  t.ok(stat.isFile(), `${filePath} refers to a file`)
+  return
 }
