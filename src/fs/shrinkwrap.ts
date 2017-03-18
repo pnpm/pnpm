4b02b85f0a47cccec46eb22c2b961390dc28dea0
zkochan <zoltan.kochan@gmail.com>
2017-03-18 14:50:08 +0200

    feat: when resolution only a shasum, fold the property
    Close #664, PR #666

--- src/fs/shrinkwrap.ts
+++ src/fs/shrinkwrap.ts
@@ -36,8 +36,8 @@ export type ResolvedPackages = {
   [pkgId: string]: DependencyShrinkwrap,
 }
 
-export type DependencyShrinkwrap = {
-  resolution: Resolution,
+export type DependencyShrinkwrap = string | {
+  resolution: string | Resolution,
   dependencies?: ResolvedDependencies,
 }
 
@@ -153,6 +153,7 @@ function copyDependencyTree (shr: Shrinkwrap, registry: string): ResolvedPackage
       }
       const depShr = shr.packages[pkgId]
       resolvedPackages[pkgId] = depShr
+      if (typeof depShr === 'string') continue
       const newDependencies = R.keys(depShr.dependencies)
         .map((pkgName: string) => getPkgShortId(<string>(depShr.dependencies && depShr.dependencies[pkgName]), pkgName))
         .filter((newPkgId: string) => !resolvedPackages[newPkgId] && pkgIds.indexOf(newPkgId) === -1)
