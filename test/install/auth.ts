33ada9f9470dd59fcdeb5bb2cc74819d07bd6f7c
zkochan <zoltan.kochan@gmail.com>
2017-04-25 20:09:12 +0300

    test: when pkg meta is not on the domain on which the tarball

--- test/install/auth.ts
+++ test/install/auth.ts
@@ -35,3 +35,30 @@ test('a package that need authentication', async function (t: tape.Test) {
 
   t.ok(typeof m === 'function', 'needs-auth() is available')
 })
+
+test('a package that need authentication reuses authorization tokens for tarball fetching', async function (t: tape.Test) {
+  const project = prepare(t)
+
+  const client = new RegClient()
+
+  const data = await new Promise((resolve, reject) => {
+    client.adduser('http://localhost:4873', {
+      auth: {
+        username: 'foo',
+        password: 'bar',
+        email: 'foo@bar.com',
+      }
+    }, (err: Error, data: Object) => err ? reject(err) : resolve(data))
+  })
+
+  await installPkgs(['needs-auth'], testDefaults({
+    registry: 'http://127.0.0.1:4873',
+    rawNpmConfig: {
+      '//127.0.0.1:4873/:_authToken': data['token'],
+    },
+  }))
+
+  const m = project.requireModule('needs-auth')
+
+  t.ok(typeof m === 'function', 'needs-auth() is available')
+})
