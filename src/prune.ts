87a09792606cb6b0f3cda93e7b1b237072f6eac1
zkochan <zoltan.kochan@gmail.com>
2017-06-25 22:14:37 +0300

    fix: don't log warning on missing local dependency resolution

--- src/prune.ts
+++ src/prune.ts
@@ -86,6 +86,8 @@ function copyDependencySubTree (
 ) {
   for (let pkgId of pkgIds) {
     if (keypath.indexOf(pkgId) !== -1) continue
+    // local dependencies don't need to be resolved in shrinkwrap.yaml
+    if (pkgId.indexOf('file:') === 0) continue
     if (!shr.packages || !shr.packages[pkgId]) {
       logger.warn(`Cannot find resolution of ${pkgId} in shrinkwrap file`)
       continue
