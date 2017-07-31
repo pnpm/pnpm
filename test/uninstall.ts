a6f9229ee5b28cf230212b508be67ed33e380078
zkochan <zoltan.kochan@gmail.com>
2017-08-01 02:06:17 +0300

    fix: uninstall bins of global packages
    Close #749

--- test/uninstall.ts
+++ test/uninstall.ts
@@ -5,12 +5,16 @@ import readPkg = require('read-pkg')
 import {
   prepare,
   testDefaults,
+  execPnpm,
 } from './utils'
 import {
   installPkgs,
 } from 'supi'
 import thenify = require('thenify')
 import pnpmCli = require('../src/bin/pnpm')
+import path = require('path')
+import isWindows = require('is-windows')
+import exists = require('path-exists')
 
 test('uninstall package and remove from appropriate property', async function (t: tape.Test) {
   const project = prepare(t)
@@ -30,4 +34,24 @@ test('uninstall package and remove from appropriate property', async function (t
 
   const pkgJson = await readPkg()
   t.equal(pkgJson.optionalDependencies, undefined, 'is-negative has been removed from optionalDependencies')
+})
+
+test('uninstall global package with its bin files', async (t: tape.Test) => {
+  prepare(t)
+  process.chdir('..')
+
+  const global = path.resolve('global')
+  const globalBin = isWindows() ? global : path.join(global, 'bin')
+
+  process.env.NPM_CONFIG_PREFIX = global
+
+  await execPnpm('install', '-g', 'sh-hello-world@1.0.1')
+
+  let stat = await exists(path.resolve(globalBin, 'sh-hello-world'))
+  t.ok(stat, 'sh-hello-world is in .bin')
+
+  await execPnpm('uninstall', '-g', 'sh-hello-world')
+
+  stat = await exists(path.resolve(globalBin, 'sh-hello-world'))
+  t.notOk(stat, 'sh-hello-world is removed from .bin')
 })
