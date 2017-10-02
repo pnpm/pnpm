fd47cbaaa1f534372f0db8a77a7427e2c3d14f3c
zkochan <zoltan.kochan@gmail.com>
2017-10-03 01:44:57 +0300

    fix: decrease shrinkwrapVersion to 3

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,8 @@ import path = require('path')
 
 export const SHRINKWRAP_FILENAME = 'shrinkwrap.yaml'
 export const PRIVATE_SHRINKWRAP_FILENAME = path.join('node_modules', '.shrinkwrap.yaml')
-export const SHRINKWRAP_VERSION = 3.1
+
+// Although .0 versions are supported, a bump to 3.1 would be a breaking change
+// because comver version support was added after releasing version 3.
+// From version 4.0 everything should be fine.
+export const SHRINKWRAP_VERSION = 3
--- test/read.ts
+++ test/read.ts
@@ -9,13 +9,13 @@ test('read()', async t => {
     const shr = await read(path.join('fixtures', '1'), {
       ignoreIncompatible: false,
     })
-    t.equal(shr!.shrinkwrapVersion, 3.1, 'converted version to shrinkwrapVersion')
+    t.equal(shr!.shrinkwrapVersion, 3, 'converted version to shrinkwrapVersion')
   }
   {
     const shr = await read(path.join('fixtures', '2'), {
       ignoreIncompatible: false,
     })
-    t.equal(shr!.shrinkwrapVersion, 3.1)
+    t.equal(shr!.shrinkwrapVersion, 3)
   }
   t.end()
 })
@@ -25,13 +25,13 @@ test('readPrivate()', async t => {
     const shr = await readPrivate(path.join('fixtures', '1'), {
       ignoreIncompatible: false,
     })
-    t.equal(shr!.shrinkwrapVersion, 3.1, 'converted version to shrinkwrapVersion')
+    t.equal(shr!.shrinkwrapVersion, 3, 'converted version to shrinkwrapVersion')
   }
   {
     const shr = await readPrivate(path.join('fixtures', '2'), {
       ignoreIncompatible: false,
     })
-    t.equal(shr!.shrinkwrapVersion, 3.1)
+    t.equal(shr!.shrinkwrapVersion, 3)
   }
   t.end()
 })
