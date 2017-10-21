f799affa0edb0b7143d975167aad5b9897749903
zkochan <zoltan.kochan@gmail.com>
2017-10-21 15:02:25 +0300

    chore: use pnpx during releasing pnpm

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -5,19 +5,19 @@ set -u
 node .scripts/check
 npm run tsc
 mv node_modules _node_modules
-npx npm@4 cache clear
+pnpx npm@4 cache clear
 
 # Regular version release
-npx npm@4 i --production --ignore-scripts
-npx npm@4 shrinkwrap
-npx npm@4 publish --ignore-scripts --tag next
+pnpx npm@4 i --production --ignore-scripts
+pnpx npm@4 shrinkwrap
+pnpx npm@4 publish --ignore-scripts --tag next
 rm npm-shrinkwrap.json
 
 # Bundled version release
 node .scripts/addBundleDependencies
 rm -rf node_modules
-npx npm@4 i --production --legacy-bundling --ignore-scripts
-npx npm@4 publish --ignore-scripts --tag next
+pnpx npm@4 i --production --legacy-bundling --ignore-scripts
+pnpx npm@4 publish --ignore-scripts --tag next
 rm -rf node_modules
 mv _node_modules node_modules
 node .scripts/removeBundleDependencies
