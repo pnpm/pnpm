78968aa8e9010e35bf0dcf04e0ad3cf5b076f133
Zoltan Kochan <z@kochan.io>
2017-12-04 23:28:30 +0200

    refactor: move most of the logic to @pnpm/package-requester
    BREAKING CHANGE:
    
    most of the API functions/types removed

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,15 @@
 import getRegistryName = require('encode-registry') // TODO: remove this. BREAKING CHANGE
-import {
-  FetchFunction,
-  FetchOptions,
-} from './combineFetchers'
-import createFetcher, {FetchedPackage, PackageContentInfo} from './fetch'
 import pkgIdToFilename from './fs/pkgIdToFilename'
 import {read, save, Store} from './fs/storeController'
 import pkgIsUntouched from './pkgIsUntouched'
-import {
-  DirectoryResolution,
-  Resolution,
-  ResolveFunction,
-  ResolveOptions,
-} from './resolvers'
 import resolveStore from './resolveStore'
 
 export {
   pkgIdToFilename,
-  createFetcher,
-  PackageContentInfo,
-  FetchedPackage,
-  DirectoryResolution,
-  Resolution,
   Store,
   read,
   save,
   getRegistryName,
   pkgIsUntouched,
   resolveStore,
-  ResolveFunction,
-  ResolveOptions,
-  FetchFunction,
-  FetchOptions,
 }
-
-export {
-  ProgressLog,
-  Log,
-} from './loggers'
--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,6 @@ import * as packageStore from 'package-store'
 import test = require('tape')
 
 test('public API', t => {
-  t.equal(typeof packageStore.createFetcher, 'function')
   t.equal(typeof packageStore.getRegistryName, 'function')
   t.equal(typeof packageStore.pkgIdToFilename, 'function')
   t.equal(typeof packageStore.pkgIsUntouched, 'function')
