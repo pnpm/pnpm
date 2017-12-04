8c9b25f8517ba86ba31344c6fe4d509d72cb69e6
Zoltan Kochan <z@kochan.io>
2017-12-05 00:19:39 +0200

    feat: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import packageRequester from './packageRequester'
 
+export {
+  FetchedPackage,
+  PackageContentInfo,
+} from './packageRequester'
+
 export default packageRequester
 
 export {
   ProgressLog,
   Log,
 } from './loggers'
+
+export * from './fetchTypes'
+export * from './resolveTypes'
