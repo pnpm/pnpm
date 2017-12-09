542153404104312e795e4740c48ba66c5b848b3f
Zoltan Kochan <z@kochan.io>
2017-12-09 18:52:20 +0200

    feat: create
    ref https://github.com/pnpm/pnpm/issues/832

---
+++ src/client.ts
@@ -0,0 +1,111 @@
+import {
+  RequestPackageFunction,
+  Resolution,
+} from '@pnpm/package-requester'
+import JsonSocket = require('json-socket')
+import net = require('net')
+import uuid = require('uuid')
+
+export default function (
+  opts: {
+    port: number,
+    hostname?: string,
+  },
+): Promise<RequestPackageFunction> {
+  const socket = new JsonSocket(new net.Socket());
+  socket.connect(opts.port, opts.hostname || '127.0.0.1')
+
+  return new Promise((resolve, reject) => {
+    socket.on('connect', () => {
+      const waiters = createWaiters()
+
+      socket.on('message', (message) => {
+        waiters.resolve(message.action, message.body)
+      })
+
+      const result = requestPackage.bind(null, socket, waiters)
+      result['end'] = () => socket.end() // tslint:disable-line
+      resolve(result)
+    })
+  })
+}
+
+function createWaiters () {
+  const waiters = {}
+  return {
+    add (id: string) {
+      waiters[id] = deffered()
+      return waiters[id].promise
+    },
+    resolve (id: string, obj: object) {
+      if (waiters[id]) {
+        waiters[id].resolve(obj)
+      }
+    },
+  }
+}
+
+// tslint:disable-next-line
+function noop () {}
+
+function deffered<T> (): {
+  promise: Promise<T>,
+  resolve: (v: T) => void,
+  reject: (err: Error) => void,
+} {
+  let pResolve: (v: T) => void = noop
+  let pReject: (err: Error) => void = noop
+  const promise = new Promise<T>((resolve, reject) => {
+    pResolve = resolve
+    pReject = reject
+  })
+  return {
+    promise,
+    reject: pReject,
+    resolve: pResolve,
+  }
+}
+
+function requestPackage (
+  socket: JsonSocket,
+  waiters: object,
+  wantedDependency: {
+    alias?: string,
+    pref: string,
+  },
+  options: {
+    downloadPriority: number,
+    loggedPkg: {
+      rawSpec: string,
+      name?: string,
+      dependentId?: string,
+    },
+    offline: boolean,
+    currentPkgId?: string,
+    prefix: string,
+    registry: string,
+    shrinkwrapResolution?: Resolution,
+    update?: boolean,
+    verifyStoreIntegrity: boolean,
+  },
+) {
+  const msgId = uuid.v4()
+
+  const fetchingManifest = waiters['add'](`manifestResponse:${msgId}`) // tslint:disable-line
+  const fetchingFiles = waiters['add'](`packageFilesResponse:${msgId}`) // tslint:disable-line
+  const response = waiters['add'](`packageResponse:${msgId}`) // tslint:disable-line
+    .then((packageResponse: object) => {
+      return Object.assign(packageResponse, {
+        fetchingFiles,
+        fetchingManifest,
+      })
+    })
+
+  socket.sendMessage({
+    msgId,
+    opts: options,
+    wantedDependency,
+  }, (err) => err && console.error(err))
+
+  return response
+}
---
+++ test/index.ts
@@ -0,0 +1,64 @@
+import test = require('tape')
+import createServer from '@pnpm/server'
+import createPackageRequester from '@pnpm/package-requester'
+import createResolver from '@pnpm/npm-resolver'
+import createFetcher from '@pnpm/tarball-fetcher'
+import net = require('net')
+import JsonSocket = require('json-socket')
+import createClient from '@pnpm/server/lib/client'
+import { PackageFilesResponse } from '../../__package_previews__/server/@pnpm/server/node_modules/.registry.npmjs.org/@pnpm/package-requester/0.3.0/node_modules/@pnpm/package-requester/lib/packageRequester';
+
+test('server', async t => {
+  const registry = 'https://registry.npmjs.org/'
+  const rawNpmConfig = { registry }
+  const store = '.store'
+  const resolve = createResolver({
+    rawNpmConfig,
+    store,
+    metaCache: new Map<string, object>(),
+  })
+  const fetchers = createFetcher({
+    alwaysAuth: true,
+    registry,
+    strictSsl: true,
+    rawNpmConfig,
+  })
+  const requestPackage = createPackageRequester(resolve, fetchers, {
+    networkConcurrency: 1,
+    storePath: store,
+    storeIndex: {},
+  })
+
+  const port = 5813
+  const hostname = '127.0.0.1';
+  const server = createServer(requestPackage, {
+    port,
+    hostname,
+  })
+  const client = await createClient({port, hostname})
+  const response = await client(
+    {alias: 'is-positive', pref: '1.0.0'},
+    {
+      downloadPriority: 0,
+      loggedPkg: {rawSpec: 'sfdf'},
+      offline: false,
+      prefix: process.cwd(),
+      registry,
+      verifyStoreIntegrity: false,
+    }
+  )
+
+  t.equal(response.id, 'registry.npmjs.org/is-positive/1.0.0')
+
+  const manifest = await response.fetchingManifest
+  t.equal(manifest.name, 'is-positive')
+  t.equal(manifest.version, '1.0.0')
+
+  const files = await response['fetchingFiles'] as PackageFilesResponse
+  t.notOk(files.fromStore)
+  t.ok(files.index['package.json'])
+
+  server.close()
+  client['end']() // tslint:disable-line
+  t.end()
+})
