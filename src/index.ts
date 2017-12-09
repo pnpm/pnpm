8a7dee3048b7dd5bb585ca72574146d69e50e69f
Zoltan Kochan <z@kochan.io>
2017-12-10 01:42:21 +0200

    refactor: rename storeController.ts to storeIndex.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import getRegistryName = require('encode-registry') // TODO: remove this. BREAKING CHANGE
 import pkgIdToFilename from './fs/pkgIdToFilename'
-import {read, save, Store} from './fs/storeController'
+import {read, save, Store} from './fs/storeIndex'
 import pkgIsUntouched from './pkgIsUntouched'
 import resolveStore from './resolveStore'
 
