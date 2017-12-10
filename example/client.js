225f3f1b149ed1632677b0c3a6831dc5033d369d
Zoltan Kochan <z@kochan.io>
2017-12-11 01:53:09 +0200

    docs: update client example

--- example/client.js
+++ example/client.js
@@ -9,6 +9,7 @@ main()
   const port = 5813
   const hostname = '127.0.0.1'
   const registry = 'https://registry.npmjs.org/'
+  const prefix = process.cwd()
   const storeCtrl = await connectStoreController({port, hostname})
   const response = await storeCtrl.requestPackage(
     {alias: 'is-positive', pref: '1.0.0'},
@@ -16,7 +17,7 @@ main()
       downloadPriority: 0,
       loggedPkg: {rawSpec: 'sfdf'},
       offline: false,
-      prefix: process.cwd(),
+      prefix,
       registry,
       verifyStoreIntegrity: false,
     }
@@ -25,6 +26,9 @@ main()
   console.log(response)
   console.log(await response.fetchingManifest)
   console.log(await response['fetchingFiles'])
+
+  await storeCtrl.updateConnections(prefix, {addDependencies: [response.id], removeDependencies: []})
+  await storeCtrl.saveState()
 
   storeCtrl.close()
 }
