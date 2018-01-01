34955b95603f16e24eee30468a232f4da479ac18
Zoltan Kochan <z@kochan.io>
2018-01-01 21:19:25 +0200

    test: package requesting

--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,7 @@ import test = require('tape')
 import createPackageRequester from '@pnpm/package-requester'
 import createResolver from '@pnpm/npm-resolver'
 import createFetcher from '@pnpm/tarball-fetcher'
+import tempy = require('tempy')
 
 const registry = 'https://registry.npmjs.org/'
 
@@ -19,12 +20,44 @@ const fetch = createFetcher({
   rawNpmConfig,
 })
 
-test('createPackageRequester', t => {
+test('request package', async t => {
   const requestPackage = createPackageRequester(resolve, fetch, {
     networkConcurrency: 1,
     storePath: '.store',
     storeIndex: {},
   })
   t.equal(typeof requestPackage, 'function')
+
+  const pkgResponse = await requestPackage({alias: 'is-positive', pref: '1.0.0'}, {
+    downloadPriority: 0,
+    offline: false,
+    loggedPkg: {},
+    prefix: tempy.directory(),
+    registry,
+    verifyStoreIntegrity: true,
+    preferredVersions: {},
+  })
+
+  t.ok(pkgResponse, 'response received')
+  t.ok(pkgResponse.body, 'response has body')
+
+  t.equal(pkgResponse.body.id, 'registry.npmjs.org/is-positive/1.0.0', 'responded with correct package ID')
+  t.equal(pkgResponse.body.inStoreLocation, '.store/registry.npmjs.org/is-positive/1.0.0', 'package location in store returned')
+  t.equal(pkgResponse.body.isLocal, false, 'package is not local')
+  t.equal(typeof pkgResponse.body.latest, 'string', 'latest is returned')
+  t.equal(pkgResponse.body.manifest.name, 'is-positive', 'package manifest returned')
+  t.ok(!pkgResponse.body.normalizedPref, 'no normalizedPref returned')
+  t.deepEqual(pkgResponse.body.resolution, {
+    integrity: 'sha1-iACYVrZKLx632LsBeUGEJK4EUss=',
+    registry: 'https://registry.npmjs.org/',
+    tarball: 'https://registry.npmjs.org/is-positive/-/is-positive-1.0.0.tgz',
+  }, 'resolution returned')
+
+  const files = await pkgResponse.fetchingFiles
+  t.deepEqual(files, {
+    filenames: [ 'package.json', 'index.js', 'license', 'readme.md' ],
+    fromStore: false,
+  }, 'returned info about files after fetch completed')
+
   t.end()
 })
