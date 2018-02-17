96125b16190ca62bafac29c78f8ae9bd04a85a28
Zoltan Kochan <z@kochan.io>
2018-02-17 20:44:37 +0200

    refactor: remove resolveStore
    BREAKING CHAGE:
    
    resolveStore moved to separate package called @pnpm/store-path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import getRegistryName = require('encode-registry') // TODO: remove this. BREAKING CHANGE
 import {read, save} from './fs/storeIndex'
-import resolveStore from './resolveStore'
 import createStore, {StoreController} from './storeController'
 
 export default createStore
@@ -9,6 +8,5 @@ export {
   read,
   save,
   getRegistryName,
-  resolveStore,
   StoreController,
 }
