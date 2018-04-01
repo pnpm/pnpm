f1f5698383c3610199b692371ecc65421ebc4c40
Zoltan Kochan <z@kochan.io>
2018-04-01 15:05:32 +0300

    ci: npm set verify-store-integrity false

--- test/utils/execPnpm.ts
+++ test/utils/execPnpm.ts
@@ -42,6 +42,9 @@ function createEnv () {
     npm_config_registry: 'http://localhost:4873/',
     npm_config_store: '../store',
     npm_config_silent: 'true',
+    // Although this is the default value of verify-store-integrity (as of pnpm 1.38.0)
+    // on CI servers we set it to `false`. That is why we set it back to true for the tests
+    npm_config_verify_store_integrity: 'true',
   })
   return _
 }
