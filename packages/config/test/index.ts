e17fc8abfa127be7a95073825203ab692806ea3e
Zoltan Kochan <z@kochan.io>
2018-05-13 16:51:35 +0300

    fix(deps): update dependencies of @pnpm/config

--- packages/config/test/index.ts
+++ packages/config/test/index.ts
@@ -2,6 +2,12 @@ import getConfigs from '@pnpm/config'
 import test = require('tape')
 
 test('getConfigs()', async (t) => {
-  t.ok(await getConfigs())
+  t.ok(await getConfigs({
+    cliArgs: [],
+    packageManager: {
+      name: 'pnpm',
+      version: '1.0.0',
+    },
+  }))
   t.end()
 })
