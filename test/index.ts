97bad92dbfaee34ba903457fc1560daaf5803f8e
Zoltan Kochan <z@kochan.io>
2018-06-10 03:38:36 +0300

    test: import by copying

--- test/index.ts
+++ test/index.ts
@@ -47,3 +47,42 @@ test('store.importPackage()', async (t) => {
   t.equal(typeof require(importTo), 'function', `sucessfully imported to ${importTo}`)
   t.end()
 })
+
+test('store.importPackage() by copying', async (t) => {
+  const store = tempy.directory()
+  const registry = 'https://registry.npmjs.org/'
+  const rawNpmConfig = {registry}
+  const resolver = createResolver({
+    metaCache: new Map(),
+    rawNpmConfig,
+    store,
+  })
+  const fetcher = createFetcher({
+    rawNpmConfig,
+    registry,
+  })
+  const storeController = await createStore(resolver, fetcher, {
+    store,
+    packageImportMethod: 'copy',
+  })
+  const pkgId = 'registry.npmjs.org/is-positive/1.0.0'
+  const fetchResult = await storeController.fetchPackage({
+    force: false,
+    verifyStoreIntegrity: true,
+    pkgId,
+    prefix: tempy.directory(),
+    resolution: {
+      integrity: 'sha1-iACYVrZKLx632LsBeUGEJK4EUss=',
+      registry: 'https://registry.npmjs.org/',
+      tarball: 'https://registry.npmjs.org/is-positive/-/is-positive-1.0.0.tgz',
+    }
+  })
+  const importTo = tempy.directory()
+  const importFrom = path.join(fetchResult.inStoreLocation, 'node_modules', 'is-positive')
+  await storeController.importPackage(importFrom, importTo, {
+    filesResponse: await fetchResult.fetchingFiles,
+    force: false,
+  })
+  t.equal(typeof require(importTo), 'function', `sucessfully imported to ${importTo}`)
+  t.end()
+})
