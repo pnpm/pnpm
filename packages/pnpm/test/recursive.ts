f2b7198ed5c3ad51237a5adaaa79570fd1cd72c3
Zoltan Kochan <z@kochan.io>
2018-06-14 02:36:10 +0300

    test: recursive --scope ignore excluded packages

--- packages/pnpm/test/recursive.ts
+++ packages/pnpm/test/recursive.ts
@@ -804,3 +804,43 @@ test('recursive --scope', async (t: tape.Test) => {
   projects['project-2'].has('is-negative')
   projects['project-3'].hasNot('minimatch')
 })
+
+test('recursive --scope ignore excluded packages', async (t: tape.Test) => {
+  const projects = prepare(t, [
+    {
+      name: 'project-1',
+      version: '1.0.0',
+      dependencies: {
+        'is-positive': '1.0.0',
+        'project-2': '1.0.0',
+      },
+    },
+    {
+      name: 'project-2',
+      version: '1.0.0',
+      dependencies: {
+        'is-negative': '1.0.0',
+      },
+    },
+    {
+      name: 'project-3',
+      version: '1.0.0',
+      dependencies: {
+        minimatch: '*',
+      },
+    },
+  ])
+
+  await writeYamlFile('pnpm-workspace.yaml', {
+    packages: [
+      '**',
+      '!project-1'
+    ],
+  })
+
+  await execPnpm('recursive', 'link', '--scope', 'project-1')
+
+  projects['project-1'].hasNot('is-positive')
+  projects['project-2'].hasNot('is-negative')
+  projects['project-3'].hasNot('minimatch')
+})
