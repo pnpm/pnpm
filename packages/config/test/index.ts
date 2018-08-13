c194572a8a1b69ba76fca9564ab78178627d3f4a
Zoltan Kochan <z@kochan.io>
2018-08-13 23:41:09 +0300

    fix: fetch retry configs should have default values
    close #1230

--- packages/config/test/index.ts
+++ packages/config/test/index.ts
@@ -2,12 +2,17 @@ import getConfigs from '@pnpm/config'
 import test = require('tape')
 
 test('getConfigs()', async (t) => {
-  t.ok(await getConfigs({
+  const configs = await getConfigs({
     cliArgs: [],
     packageManager: {
       name: 'pnpm',
       version: '1.0.0',
     },
-  }))
+  })
+  t.ok(configs)
+  t.equal(configs.fetchRetries, 2)
+  t.equal(configs.fetchRetryFactor, 10)
+  t.equal(configs.fetchRetryMintimeout, 10000)
+  t.equal(configs.fetchRetryMaxtimeout, 60000)
   t.end()
 })
