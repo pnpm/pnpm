07e01e48d78094f4b1da2485bf7c8f3a01b72724
Zoltan Kochan <z@kochan.io>
2018-08-25 22:23:18 +0300

    fix(run): pass through the correct prefix to npm run

--- packages/pnpm/src/cmd/run.ts
+++ packages/pnpm/src/cmd/run.ts
@@ -3,7 +3,7 @@ import {sync as runScriptSync} from '../runScript'
 
 export default function (input: string[], opts: PnpmOptions) {
   const result = runScriptSync('npm', ['run'].concat(input), {
-    cwd: process.cwd(),
+    cwd: opts.prefix || process.cwd(),
     stdio: 'inherit',
     userAgent: undefined,
   })
