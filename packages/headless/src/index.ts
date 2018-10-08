3f840943091aef9de86a5a8205edb58c5fc7f834
Zoltan Kochan <z@kochan.io>
2018-10-09 01:30:01 +0300

    fix: a dependency should always be linked if it is non-optional
    ref #1305

--- packages/headless/src/index.ts
+++ packages/headless/src/index.ts
@@ -591,7 +591,7 @@ async function linkAllModules (
         await Promise.all(
           R.keys(childrenToLink)
             .map(async (alias) => {
-              // if (!pkg.installable) return
+              // if (!pkg.installable && pkg.optional) return
               await symlinkDependencyTo(alias, childrenToLink[alias], depNode.modules)
             }),
         )
