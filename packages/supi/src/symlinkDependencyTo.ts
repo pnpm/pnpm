bd8dd61b66f3eb6a736ff360316158e105e61403
Vaughan Rouesnel <vrouesnel@gmail.com>
2018-10-31 18:18:06 +0100

    fix: don't symlink dep when parent package is outside of the monorepo root

--- packages/supi/src/symlinkDependencyTo.ts
+++ packages/supi/src/symlinkDependencyTo.ts
@@ -4,6 +4,13 @@ import symlinkDir = require('symlink-dir')
 
 export default function symlinkDependencyTo (alias: string, peripheralLocation: string, dest: string) {
   const linkPath = path.join(dest, alias)
+  // Don't symlink deps from packages that are outside of the monorepo root.
+  //   These may be symlinks from another project, and we don't want to break the other project.
+  // TODO(vjpr): Is `process.cwd()` a reliable way to get the monorepo root?
+  if (!linkPath.startsWith(process.cwd())) {
+    // TODO(vjpr): Should print some kind of warning maybe?
+    return {reused: true} // TODO(vjpr): Not sure we should misuse reused like this.
+  }
   linkLogger.debug({ target: peripheralLocation, link: linkPath })
   return symlinkDir(peripheralLocation, linkPath)
 }
