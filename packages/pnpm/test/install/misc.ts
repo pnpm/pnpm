pnpm/pnpm

commit 066910b6dc3bb08578ca704cdd08f580d3dba1e2

fix(shared-workspace-shrinkwrap): recursive removal
* feat(supi): merge installPkgs() with install()

* fix(shared-workspace-shrinkwrap): recursive removal

ref #1525

* refactor: recursive operations with shared shrinkwrap

* refactor(supi): revert mixing install() with uninstall()

* refactor: create mutateModules()

* fix: uninstalling a direct peer from a shared shrinkwrap

* refactor: rename mutations

* refactor(resolve-dependencies): opts.updateDepth

BREAKING CHANGE:

opts.depth and opts.update params deprecated
opts.updateDepth added

* refactor: pass pinnedVersions to mutateModules()

* refactor: rename properties in DependenciesMutation

* refactor(supi): remove unlink() and unlinkPkgs()

PR #1528

packages/pnpm/src/cmd/install.ts
@@ -1,11 +1,12 @@
 import { getSaveType } from '@pnpm/utils'
 import {
   install,
-  installPkgs,
+  mutateModules,
   rebuild,
 } from 'supi'
 import createStoreController from '../createStoreController'
 import findWorkspacePackages, { arrayOfLocalPackagesToMap } from '../findWorkspacePackages'
+import getPinnedVersion from '../getPinnedVersion'
 import requireHooks from '../requireHooks'
 import { PnpmOptions } from '../types'
 import { recursive } from './recursive'
@@ -46,22 +47,22 @@ export default async function installCmd (
     // so ignoring scripts for now
     ignoreScripts: !!localPackages || opts.ignoreScripts,
     localPackages,
+    pinnedVersion: getPinnedVersion(opts),
     store: store.path,
     storeController: store.ctrl,
   }
   if (!input || !input.length) {
     await install(installOpts)
   } else {
-    await installPkgs(Object.assign(installOpts, {
-      importers: [
-        {
-          bin: installOpts.bin,
-          prefix: installOpts.prefix,
-          targetDependencies: input,
-          targetDependenciesField: getSaveType(installOpts),
-        },
-      ],
-    }))
+    await mutateModules([
+      {
+        bin: installOpts.bin,
+        dependencySelectors: input,
+        mutation: 'installSome',
+        prefix: installOpts.prefix,
+        targetDependenciesField: getSaveType(installOpts),
+      },
+    ], installOpts)
   }
 
   if (opts.linkWorkspacePackages && opts.workspacePrefix) {

packages/supi/test/install/misc.ts
@@ -429,7 +429,7 @@ test("don't refetch package to store if it has been modified and verify-store-in
 // tslint:disable-next-line:no-string-literal
 test['skip']('relink package to project if the dependency is not linked from store', async (t: tape.Test) => {
   const project = prepare(t)
-  await addDependenciesToPackage(['magic-hook@2.0.0'], await testDefaults({ save: true, saveExact: true }))
+  await addDependenciesToPackage(['magic-hook@2.0.0'], await testDefaults({ save: true, pinnedVersion: 'patch' }))
 
   const pkgJsonPath = path.resolve('node_modules', 'magic-hook', 'package.json')
 

packages/headless/src/index.ts
@@ -64,6 +64,7 @@ export interface HeadlessOptions {
     id: string,
     pkg: PackageJson,
     prefix: string,
+    pruneDirectDependencies?: boolean,
     shamefullyFlatten: boolean,
   }>,
   shrinkwrapDirectory: string,
@@ -83,7 +84,6 @@ export interface HeadlessOptions {
     version: string,
   },
   pruneStore: boolean,
-  pruneDirectDependencies?: boolean,
   wantedShrinkwrap?: Shrinkwrap,
   ownLifecycleHooksStdio?: 'inherit' | 'pipe',
   pendingBuilds: string[],
@@ -144,7 +144,6 @@ export default async (opts: HeadlessOptions) => {
       importers: opts.importers,
       newShrinkwrap: filterShrinkwrap(wantedShrinkwrap, filterOpts),
       oldShrinkwrap: currentShrinkwrap,
-      pruneDirectDependencies: opts.pruneDirectDependencies,
       pruneStore: opts.pruneStore,
       registries: opts.registries,
       shrinkwrapDirectory,

packages/modules-cleaner/src/prune.ts
@@ -26,14 +26,14 @@ export default async function prune (
     importers: Array<{
       bin: string,
       hoistedAliases: {[depPath: string]: string[]},
-      modulesDir: string,
       id: string,
-      shamefullyFlatten: boolean,
+      modulesDir: string,
       prefix: string,
+      pruneDirectDependencies?: boolean,
+      shamefullyFlatten: boolean,
     }>,
     newShrinkwrap: Shrinkwrap,
     oldShrinkwrap: Shrinkwrap,
-    pruneDirectDependencies?: boolean,
     pruneStore?: boolean,
     registries: Registries,
     removePackages?: string[],
@@ -48,7 +48,7 @@ export default async function prune (
     const newPkgs = R.toPairs(mergeDependencies(opts.newShrinkwrap.importers[importer.id]))
 
     const allCurrentPackages = new Set(
-      (opts.pruneDirectDependencies || opts.removePackages && opts.removePackages.length)
+      (importer.pruneDirectDependencies || opts.removePackages && opts.removePackages.length)
         ? (await readModulesDir(importer.modulesDir) || [])
         : [],
     )
@@ -56,7 +56,7 @@ export default async function prune (
       ...(opts.removePackages || []).filter((removePackage) => allCurrentPackages.has(removePackage)),
       ...R.difference(oldPkgs, newPkgs).map(([depName]) => depName),
     ])
-    if (opts.pruneDirectDependencies) {
+    if (importer.pruneDirectDependencies) {
       if (allCurrentPackages.size > 0) {
         const newPkgsSet = new Set(newPkgs.map(([depName]) => depName))
         for (const currentPackage of Array.from(allCurrentPackages)) {

packages/pnpm/src/cmd/prune.ts
@@ -1,12 +1,17 @@
-import { install, InstallOptions } from 'supi'
+import { InstallOptions, mutateModules } from 'supi'
 import createStoreController from '../createStoreController'
 import { PnpmOptions } from '../types'
 
 export default async (input: string[], opts: PnpmOptions) => {
   const store = await createStoreController(opts)
-  return install({
+  return mutateModules([
+    {
+      mutation: 'install',
+      prefix: process.cwd(),
+      pruneDirectDependencies: true,
+    },
+  ], {
     ...opts,
-    pruneDirectDependencies: true,
     pruneStore: true,
     store: store.path,
     storeController: store.ctrl,

packages/pnpm/src/cmd/recursive/index.ts
@@ -16,16 +16,16 @@ import {
   addDependenciesToPackage,
   install,
   InstallOptions,
-  installPkgs,
+  MutatedImporter,
+  mutateModules,
   rebuild,
   rebuildPkgs,
   uninstall,
-  unlink,
-  unlinkPkgs,
 } from 'supi'
 import createStoreController from '../../createStoreController'
 import findWorkspacePackages, { arrayOfLocalPackagesToMap } from '../../findWorkspacePackages'
 import getCommandFullName from '../../getCommandFullName'
+import getPinnedVersion from '../../getPinnedVersion'
 import parsePackageSelector, { PackageSelector } from '../../parsePackageSelectors'
 import requireHooks from '../../requireHooks'
 import { PnpmOptions } from '../../types'
@@ -199,6 +199,61 @@ export async function recursive (
   const memReadLocalConfigs = mem(readLocalConfigs)
 
   if (cmdFullName !== 'rebuild') {
+    let pkgPaths = chunks.length === 0
+      ? chunks[0]
+      : Object.keys(pkgGraphResult.graph).sort()
+    if (opts.shrinkwrapDirectory && ['install', 'uninstall', 'update'].indexOf(cmdFullName) !== -1) {
+      if (opts.ignoredPackages) {
+        pkgPaths = pkgPaths.filter((prefix) => !opts.ignoredPackages!.has(prefix))
+      }
+      const isFromWorkspace = isSubdir.bind(null, opts.shrinkwrapDirectory)
+      pkgPaths = await pFilter(pkgPaths, async (pkgPath: string) => isFromWorkspace(await fs.realpath(pkgPath)))
+      if (pkgPaths.length === 0) return true
+      const hooks = opts.ignorePnpmfile ? {} : requireHooks(opts.shrinkwrapDirectory, opts)
+      const mutation = cmdFullName === 'uninstall' ? 'uninstallSome' : (input.length === 0 ? 'install' : 'installSome')
+      const importers = await Promise.all<MutatedImporter>(pkgPaths.map(async (prefix) => {
+        const localConfigs = await memReadLocalConfigs(prefix)
+        const shamefullyFlatten = typeof localConfigs.shamefullyFlatten === 'boolean'
+          ? localConfigs.shamefullyFlatten
+          : opts.shamefullyFlatten
+        switch (mutation) {
+          case 'uninstallSome':
+            return {
+              dependencyNames: input,
+              mutation,
+              prefix,
+              shamefullyFlatten,
+              targetDependenciesField: getSaveType(installOpts),
+            } as MutatedImporter
+          case 'installSome':
+            return {
+              allowNew: cmdFullName === 'install',
+              dependencySelectors: input,
+              mutation,
+              pinnedVersion: getPinnedVersion({
+                saveExact: typeof localConfigs.saveExact === 'boolean' ? localConfigs.saveExact : opts.saveExact,
+                savePrefix: typeof localConfigs.savePrefix === 'string' ? localConfigs.savePrefix : opts.savePrefix,
+              }),
+              prefix,
+              shamefullyFlatten,
+              targetDependenciesField: getSaveType(installOpts),
+            } as MutatedImporter
+          case 'install':
+            return {
+              mutation,
+              prefix,
+              shamefullyFlatten,
+            } as MutatedImporter
+        }
+      }))
+      await mutateModules(importers, {
+        ...installOpts,
+        hooks,
+        storeController: store.ctrl,
+      })
+      return true
+    }
+
     let action!: any // tslint:disable-line:no-any
     switch (cmdFullName) {
       case 'unlink':
@@ -208,46 +263,8 @@ export async function recursive (
         action = uninstall.bind(null, input)
         break
       default:
-        action = (input.length === 0 ? install : (opts.shrinkwrapDirectory ? installPkgs : addDependenciesToPackage.bind(null, input)))
+        action = input.length === 0 ? install : addDependenciesToPackage.bind(null, input)
         break
-    }
-
-    let pkgPaths = chunks.length === 0
-      ? chunks[0]
-      : Object.keys(pkgGraphResult.graph).sort()
-    if (opts.shrinkwrapDirectory && ['install', 'update', 'link'].indexOf(cmdFullName) !== -1) {
-      if (opts.ignoredPackages) {
-        pkgPaths = pkgPaths.filter((prefix) => !opts.ignoredPackages!.has(prefix))
-      }
-      const isFromWorkspace = isSubdir.bind(null, opts.shrinkwrapDirectory)
-      pkgPaths = await pFilter(pkgPaths, async (pkgPath: string) => isFromWorkspace(await fs.realpath(pkgPath)))
-      if (pkgPaths.length === 0) return true
-      const hooks = opts.ignorePnpmfile ? {} : requireHooks(opts.shrinkwrapDirectory, opts)
-      await action({
-        ...installOpts,
-        hooks,
-        importers: await Promise.all(pkgPaths.map(async (prefix) => {
-          const localConfigs = await memReadLocalConfigs(prefix)
-          return {
-            allowNew: cmdFullName === 'install',
-            operation: 'add',
-            prefix,
-            saveExact: typeof localConfigs.saveExact === 'boolean'
-              ? localConfigs.saveExact
-              : opts.saveExact,
-            savePrefix: typeof localConfigs.savePrefix === 'string'
-              ? localConfigs.savePrefix
-              : opts.savePrefix,
-            shamefullyFlatten: typeof localConfigs.shamefullyFlatten === 'boolean'
-              ? localConfigs.shamefullyFlatten
-              : opts.shamefullyFlatten,
-            targetDependencies: input,
-            targetDependenciesField: getSaveType(installOpts),
-          }
-        })),
-        storeController: store.ctrl,
-      })
-      return true
     }
     const limitInstallation = pLimit(opts.workspaceConcurrency)
     await Promise.all(pkgPaths.map((prefix: string) =>
@@ -341,6 +358,31 @@ export async function recursive (
   throwOnFail(result)
 
   return true
+}
+
+function unlink (opts: any) { // tslint:disable-line:no-any
+  return mutateModules(
+    [
+      {
+        mutation: 'unlink',
+        prefix: opts.prefix,
+      },
+    ],
+    opts,
+  )
+}
+
+function unlinkPkgs (dependencyNames: string[], opts: any) { // tslint:disable-line:no-any
+  return mutateModules(
+    [
+      {
+        dependencyNames,
+        mutation: 'unlinkSome',
+        prefix: opts.prefix,
+      },
+    ],
+    opts,
+  )
 }
 
 function sortPackages (pkgGraph: {[nodeId: string]: PackageNode}): string[][] {

packages/pnpm/src/cmd/uninstall.ts
@@ -1,4 +1,5 @@
 import {
+  mutateModules,
   uninstall,
 } from 'supi'
 import createStoreController from '../createStoreController'
@@ -13,6 +14,15 @@ export default async function uninstallCmd (
     store: store.path,
     storeController: store.ctrl,
   })
-
+  if (opts.shrinkwrapDirectory !== opts.prefix) {
+    return mutateModules([
+      {
+        bin: opts.bin,
+        dependencyNames: input,
+        mutation: 'uninstallSome',
+        prefix: opts.prefix,
+      },
+    ], uninstallOpts)
+  }
   return uninstall(input, uninstallOpts)
 }

packages/pnpm/src/cmd/unlink.ts
@@ -1,7 +1,4 @@
-import {
-  unlink,
-  unlinkPkgs,
-} from 'supi'
+import { mutateModules } from 'supi'
 import createStoreController from '../createStoreController'
 import { PnpmOptions } from '../types'
 
@@ -13,7 +10,18 @@ export default async function (input: string[], opts: PnpmOptions) {
   })
 
   if (!input || !input.length) {
-    return unlink(unlinkOpts)
+    return mutateModules([
+      {
+        dependencyNames: input,
+        mutation: 'unlinkSome',
+        prefix: opts.prefix,
+      },
+    ], unlinkOpts)
   }
-  return unlinkPkgs(input, unlinkOpts)
+  return mutateModules([
+    {
+      mutation: 'unlink',
+      prefix: opts.prefix,
+    },
+  ], unlinkOpts)
 }

commit 73cc484b6e20f4f5bf4f4980085c8ad0ea5e74ae

fix: pass --save-exact/--save-prefix to supi
fixes a regression introduced by #1528
bug is present in pnpm v2.20.0 but it wasn't tagged as latest

packages/pnpm/src/cmd/install.ts
@@ -47,7 +47,6 @@ export default async function installCmd (
     // so ignoring scripts for now
     ignoreScripts: !!localPackages || opts.ignoreScripts,
     localPackages,
-    pinnedVersion: getPinnedVersion(opts),
     store: store.path,
     storeController: store.ctrl,
   }
@@ -59,6 +58,7 @@ export default async function installCmd (
         bin: installOpts.bin,
         dependencySelectors: input,
         mutation: 'installSome',
+        pinnedVersion: getPinnedVersion(opts),
         prefix: installOpts.prefix,
         targetDependenciesField: getSaveType(installOpts),
       },

packages/pnpm/test/install/misc.ts
@@ -1,4 +1,5 @@
 import prepare from '@pnpm/prepare'
+import { fromDir as readPackageJsonFromDir } from '@pnpm/read-package-json'
 import caw = require('caw')
 import isWindows = require('is-windows')
 import path = require('path')
@@ -15,6 +16,7 @@ import {
 
 const IS_WINDOWS = isWindows()
 const test = promisifyTape(tape)
+const testOnly = promisifyTape(tape.only)
 
 if (!caw() && !IS_WINDOWS) {
   process.env.VCR_MODE = 'cache'
@@ -106,4 +108,16 @@ test('install with external shrinkwrap directory', async (t: tape.Test) => {
   const shr = await readYamlFile<Shrinkwrap>(path.resolve('..', 'shrinkwrap.yaml'))
 
   t.deepEqual(Object.keys(shr.importers), ['project'], 'shrinkwrap created in correct location')
+})
+
+test('install --save-exact', async (t: tape.Test) => {
+  const project = prepare(t)
+
+  await execPnpm('install', 'is-positive@3.1.0', '--save-exact', '--save-dev')
+
+  await project.has('is-positive')
+
+  const pkg = await readPackageJsonFromDir(process.cwd())
+
+  t.deepEqual(pkg.devDependencies, { 'is-positive': '3.1.0' })
 })