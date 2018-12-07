2ce8f851b0e92413a53e0852058bc5fd2007c7a3
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2018-12-07 15:28:42 -0500

    chore(assert-store): create new private package @pnpm/assert-store
    Add new module to test pnpm stores
    Add a test directory
    
    PR #1536

---
+++ privatePackages/assert-store/src/index.ts
@@ -0,0 +1,25 @@
+import path = require('path')
+import exists = require('path-exists')
+import { Test } from 'tape'
+
+export default (t: Test, storePath: string, encodedRegistryName?: string) => {
+  const ern = encodedRegistryName || 'localhost+4873'
+  const store = {
+    async storeHas (pkgName: string, version?: string) {
+      const pathToCheck = await store.resolve(pkgName, version)
+      t.ok(await exists(pathToCheck), `${pkgName}@${version} is in store (at ${pathToCheck})`)
+    },
+    async storeHasNot (pkgName: string, version: string) {
+      const pathToCheck = await store.resolve(pkgName, version)
+      t.notOk(await exists(pathToCheck), `${pkgName}@${version} is not in store (at ${pathToCheck})`)
+    },
+    async resolve (pkgName: string, version?: string, relativePath?: string) {
+      const pkgFolder = version ? path.join(ern, pkgName, version) : pkgName
+      if (relativePath) {
+        return path.join(await storePath, pkgFolder, 'package', relativePath)
+      }
+      return path.join(await storePath, pkgFolder, 'package')
+    },
+  }
+  return store
+}
---
+++ privatePackages/assert-store/test/index.ts
@@ -0,0 +1,24 @@
+import assertStore from '@pnpm/assert-store'
+import path = require('path')
+import test = require('tape')
+
+test('assertStore() store assertions', async (t) => {
+  const storePath = path.join(__dirname, 'fixture/store/2/')
+  const encodedRegistryName = 'registry.npmjs.org'
+  const store = assertStore(t, storePath, encodedRegistryName)
+
+  await store.storeHas('is-positive', '3.1.0')
+  await store.storeHasNot('ansi-regex', '2.0.0')
+  await store.storeHasNot('is-positive', '2.0.0')
+
+  t.end()
+})
+
+test('assertStore() resolve', async (t) => {
+  const storePath = path.join(__dirname, 'fixture/store/2/')
+  const encodedRegistryName = 'registry.npmjs.org'
+  const store = assertStore(t, storePath, encodedRegistryName)
+
+  t.equal(typeof await store.resolve('is-positive', '3.1.0'), 'string')
+  t.end()
+})
