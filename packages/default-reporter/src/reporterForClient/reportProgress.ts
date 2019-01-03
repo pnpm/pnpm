2cc94eebf1dcd0929f056efe54618b2ec6e85197
Zoltan Kochan <z@kochan.io>
2019-01-03 21:08:01 +0200

    refactor: progress reporting

--- packages/default-reporter/src/reporterForClient/reportProgress.ts
+++ packages/default-reporter/src/reporterForClient/reportProgress.ts
@@ -4,13 +4,15 @@ import most = require('most')
 import { hlValue } from './outputConstants'
 import { zoomOut } from './utils/zooming'
 
-type GroupedProgressMessage = {
-  modulesDir: string,
-  progress$: most.Stream<{
-    fetched: number,
-    resolved: number,
-    reused: number,
-  }>,
+type ProgressStats = {
+  fetched: number,
+  resolved: number,
+  reused: number,
+}
+
+type ModulesInstallProgress = {
+  progress$: most.Stream<ProgressStats>,
+  requirer: string,
   stage$: most.Stream<StageLog>,
 }
 
@@ -24,140 +26,150 @@ export default (
     throttleProgress?: number,
   },
 ) => {
-  const groupedProgressPushStream = new PushStream<GroupedProgressMessage>()
+  const modulesInstallProgressPushStream = getModulesInstallProgressPushStream(log$.stage, log$.progress)
 
-  const progessPushStreamByModulesDir: {
-    [modulesDir: string]: PushStream<{
-      fetched: number,
-      resolved: number,
-      reused: number,
-    }>,
-  } = {}
+  const progressOutput = typeof opts.throttleProgress === 'number' && opts.throttleProgress > 0
+    ? throttledProgressOutput.bind(null, opts.throttleProgress)
+    : nonThrottledProgressOutput
 
-  const stagePushStreamByModulesDir: {
-    [modulesDir: string]: PushStream<StageLog>,
-  } = {}
-  const reportingPushStream = new PushStream()
-
-  most.from(groupedProgressPushStream.observable)
-    .forEach((groupedProgress: GroupedProgressMessage) => {
-      const importingDone$ = groupedProgress.stage$
+  return most.from(modulesInstallProgressPushStream.observable)
+    .map(({ stage$, progress$, requirer }: ModulesInstallProgress) => {
+      const importingDone$ = stage$
         .filter((log: StageLog) => log.stage === 'importing_done')
         .constant(true)
         .take(1)
         .startWith(false)
         .multicast()
 
-      let progress$
-      if (typeof opts.throttleProgress === 'number' && opts.throttleProgress > 0) {
-        // Reporting is done every `throttleProgress` milliseconds
-        // and once all packages are fetched.
-        const sampler = most.merge(
-          most.periodic(opts.throttleProgress).until(importingDone$),
-          importingDone$,
-        )
-        progress$ = most.sample(
-          createStatusMessage,
-          sampler,
-          groupedProgress.progress$,
-          importingDone$,
-        )
-        // Avoid logs after all resolved packages were downloaded.
-        // Fixing issue: https://github.com/pnpm/pnpm/issues/1028#issuecomment-364782901
-        .skipAfter((msg) => msg['done'] === true)
-      } else {
-        progress$ = most.combine(
-          createStatusMessage,
-          groupedProgress.progress$,
-          importingDone$,
-        )
-      }
-      if (groupedProgress.modulesDir === opts.cwd) {
-        reportingPushStream.next(progress$)
-      } else {
-        reportingPushStream.next(progress$.map((msg) => {
-          msg['msg'] = zoomOut(opts.cwd, groupedProgress.modulesDir, msg['msg'])
-          return msg
-        }))
-      }
-    })
+      const output$ = progressOutput(importingDone$, progress$)
 
-  log$.stage
+      if (requirer === opts.cwd) {
+        return output$
+      }
+      return output$.map((msg: any) => { // tslint:disable-line:no-any
+        msg['msg'] = zoomOut(opts.cwd, requirer, msg['msg'])
+        return msg
+      })
+    })
+}
+
+function throttledProgressOutput (
+  throttleProgress: number,
+  importingDone$: most.Stream<boolean>,
+  progress$: most.Stream<ProgressStats>,
+) {
+  // Reporting is done every `throttleProgress` milliseconds
+  // and once all packages are fetched.
+  const sampler = most.merge(
+    most.periodic(throttleProgress).until(importingDone$),
+    importingDone$,
+  )
+  return most.sample(
+    createStatusMessage,
+    sampler,
+    progress$,
+    importingDone$,
+  )
+  // Avoid logs after all resolved packages were downloaded.
+  // Fixing issue: https://github.com/pnpm/pnpm/issues/1028#issuecomment-364782901
+  .skipAfter((msg) => msg['done'] === true)
+}
+
+function nonThrottledProgressOutput (
+  importingDone$: most.Stream<boolean>,
+  progress$: most.Stream<ProgressStats>,
+) {
+  return most.combine(
+    createStatusMessage,
+    progress$,
+    importingDone$,
+  )
+}
+
+function getModulesInstallProgressPushStream (
+  stage$: most.Stream<StageLog>,
+  progress$: most.Stream<ProgressLog>,
+) {
+  const modulesInstallProgressPushStream = new PushStream<ModulesInstallProgress>()
+  const progessStatsPushStreamByRequirer = getProgessStatsPushStreamByRequirer(progress$)
+
+  const stagePushStreamByRequirer: {
+    [requirer: string]: PushStream<StageLog>,
+  } = {}
+  stage$
     .forEach((log: StageLog) => {
-      if (!stagePushStreamByModulesDir[log.prefix]) {
-        stagePushStreamByModulesDir[log.prefix] = new PushStream<StageLog>()
-        if (!progessPushStreamByModulesDir[log.prefix]) {
-          progessPushStreamByModulesDir[log.prefix] = new PushStream()
+      if (!stagePushStreamByRequirer[log.prefix]) {
+        stagePushStreamByRequirer[log.prefix] = new PushStream<StageLog>()
+        if (!progessStatsPushStreamByRequirer[log.prefix]) {
+          progessStatsPushStreamByRequirer[log.prefix] = new PushStream()
         }
-        groupedProgressPushStream.next({
-          modulesDir: log.prefix,
-          progress$: most.from(progessPushStreamByModulesDir[log.prefix].observable),
-          stage$: most.from(stagePushStreamByModulesDir[log.prefix].observable),
+        modulesInstallProgressPushStream.next({
+          progress$: most.from(progessStatsPushStreamByRequirer[log.prefix].observable),
+          requirer: log.prefix,
+          stage$: most.from(stagePushStreamByRequirer[log.prefix].observable),
         })
       }
-      stagePushStreamByModulesDir[log.prefix].next(log)
+      stagePushStreamByRequirer[log.prefix].next(log)
       if (log.stage === 'importing_done') {
-        progessPushStreamByModulesDir[log.prefix].complete()
-        stagePushStreamByModulesDir[log.prefix].complete()
+        progessStatsPushStreamByRequirer[log.prefix].complete()
+        stagePushStreamByRequirer[log.prefix].complete()
       }
     })
 
-  const prevProgressByModulesDir: {
-    [modulesDir: string]: {
-      fetched: number,
-      resolved: number,
-      reused: number,
-    },
+  return modulesInstallProgressPushStream
+}
+
+function getProgessStatsPushStreamByRequirer (progress$: most.Stream<ProgressLog>) {
+  const progessStatsPushStreamByRequirer: {
+    [requirer: string]: PushStream<ProgressStats>,
   } = {}
-  log$.progress
+
+  const previousProgressStatsByRequirer: { [requirer: string]: ProgressStats } = {}
+  progress$
     .forEach((log: ProgressLog) => {
-      if (!prevProgressByModulesDir[log.requester]) {
-        prevProgressByModulesDir[log.requester] = {
+      if (!previousProgressStatsByRequirer[log.requester]) {
+        previousProgressStatsByRequirer[log.requester] = {
           fetched: 0,
           resolved: 0,
           reused: 0,
         }
       } else {
-        prevProgressByModulesDir[log.requester] = {
-          ...prevProgressByModulesDir[log.requester]
+        previousProgressStatsByRequirer[log.requester] = {
+          ...previousProgressStatsByRequirer[log.requester]
         }
       }
       switch (log.status) {
         case 'resolved':
-          prevProgressByModulesDir[log.requester].resolved++
+          previousProgressStatsByRequirer[log.requester].resolved++
           break
         case 'fetched':
-          prevProgressByModulesDir[log.requester].fetched++
+          previousProgressStatsByRequirer[log.requester].fetched++
           break
         case 'found_in_store':
-          prevProgressByModulesDir[log.requester].reused++
+          previousProgressStatsByRequirer[log.requester].reused++
           break
       }
-      progessPushStreamByModulesDir[log.requester] = progessPushStreamByModulesDir[log.requester] || new PushStream<ProgressLog>()
-      progessPushStreamByModulesDir[log.requester].next(prevProgressByModulesDir[log.requester])
+      progessStatsPushStreamByRequirer[log.requester] = progessStatsPushStreamByRequirer[log.requester] || new PushStream<ProgressLog>()
+      progessStatsPushStreamByRequirer[log.requester].next(previousProgressStatsByRequirer[log.requester])
     })
 
-  return most.from(reportingPushStream.observable) as most.Stream<most.Stream<{ msg: string }>>
+  return progessStatsPushStreamByRequirer
+}
 
-  function createStatusMessage (
-    progress: {
-      fetched: number,
-      resolved: number,
-      reused: number,
-    },
-    importingDone: boolean,
-  ) {
-    const msg = `Resolving: total ${hlValue(progress.resolved.toString())}, reused ${hlValue(progress.reused.toString())}, downloaded ${hlValue(progress.fetched.toString())}`
-    if (importingDone) {
-      return {
-        done: true,
-        fixed: false,
-        msg: `${msg}, done`,
-      }
-    }
+function createStatusMessage (
+  progress: ProgressStats,
+  importingDone: boolean,
+) {
+  const msg = `Resolving: total ${hlValue(progress.resolved.toString())}, reused ${hlValue(progress.reused.toString())}, downloaded ${hlValue(progress.fetched.toString())}`
+  if (importingDone) {
     return {
-      fixed: true,
-      msg,
+      done: true,
+      fixed: false,
+      msg: `${msg}, done`,
     }
+  }
+  return {
+    fixed: true,
+    msg,
   }
 }
