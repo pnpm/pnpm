bebae93447e05e31505ddcffb894e380a11c93e7
Zoltan Kochan <z@kochan.io>
2019-03-05 16:15:01 +0200

    refactor: move @pnpm/resolver-base to monorepo

---
+++ packages/resolver-base/README.md
@@ -0,0 +1,43 @@
+# @pnpm/resolver-base
+
+> Types for pnpm-compatible resolvers
+
+<!--@shields('npm')-->
+[![npm version](https://img.shields.io/npm/v/@pnpm/resolver-base.svg)](https://www.npmjs.com/package/@pnpm/resolver-base)
+<!--/@-->
+
+## Installation
+
+```sh
+npm i -S @pnpm/resolver-base
+```
+
+## Usage
+
+Here's a template of a resolver using types from `@pnpm/resolver-base`:
+
+```ts
+import {
+  ResolveOptions,
+  ResolveResult,
+  WantedDependency,
+} from '@pnpm/resolver-base'
+
+export async function (
+  wantedDependency: WantedDependency,
+  opts: ResolveOptions,
+): Promise<ResolveResult> {
+  // ...
+  return {
+    id,
+    resolution,
+    package,
+    latest,
+    normalizedPref,
+  }
+}
+```
+
+## License
+
+[MIT](./LICENSE) © [Zoltan Kochan](https://www.kochan.io/)
---
+++ packages/resolver-base/src/index.ts
@@ -0,0 +1,70 @@
+import { PackageJson } from '@pnpm/types'
+
+/**
+ * tarball hosted remotely
+ */
+export interface TarballResolution {
+  type?: undefined,
+  tarball: string,
+  integrity?: string,
+  // needed in some cases to get the auth token
+  // sometimes the tarball URL is under a different path
+  // and the auth token is specified for the registry only
+  registry?: string,
+}
+
+/**
+ * directory on a file system
+ */
+export interface DirectoryResolution {
+  type: 'directory',
+  directory: string,
+}
+
+export type Resolution =
+  TarballResolution |
+  DirectoryResolution |
+  ({ type: string } & object)
+
+export interface ResolveResult {
+  id: string,
+  resolution: Resolution,
+  package?: PackageJson,
+  latest?: string,
+  normalizedPref?: string, // is null for npm-hosted dependencies
+  resolvedVia: 'npm-registry' | 'git-repository' | 'local-filesystem' | 'url' | string,
+}
+
+export interface LocalPackages {
+  [name: string]: {
+    [version: string]: {
+      directory: string,
+      package: PackageJson,
+    },
+  },
+}
+
+export interface ResolveOptions {
+  defaultTag?: string,
+  localPackages?: LocalPackages,
+  preferredVersions: {
+    [packageName: string]: {
+      selector: string,
+      type: 'version' | 'range' | 'tag',
+    },
+  },
+  prefix: string,
+  registry: string,
+  shrinkwrapDirectory?: string, // TODO: make it required in next major version
+}
+
+export type WantedDependency = {
+  alias?: string,
+  pref?: string,
+} & (
+  {alias: string, pref: string}
+  | {alias: string}
+  | {pref: string}
+)
+
+export type ResolveFunction = (wantedDependency: WantedDependency, opts: ResolveOptions) => Promise<ResolveResult>
