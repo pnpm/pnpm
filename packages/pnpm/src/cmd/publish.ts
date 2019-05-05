576752f1990da67b71cfb7785fb3233085f500b8
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-05-06 01:53:39 +0300

    feat: copy license files from workspace root when package has none
    close #1804

--- packages/pnpm/src/cmd/publish.ts
+++ packages/pnpm/src/cmd/publish.ts
@@ -1,4 +1,7 @@
 import readImporterManifest from '@pnpm/read-importer-manifest'
+import cpFile = require('cp-file')
+import fg = require('fast-glob')
+import fs = require('mz/fs')
 import path = require('path')
 import rimraf = require('rimraf-then')
 import writeJsonFile = require('write-json-file')
@@ -17,7 +20,7 @@ export default async function (
   const prefix = args.length && args[0] || process.cwd()
 
   let _status!: number
-  await fakeRegularManifest(prefix, async () => {
+  await fakeRegularManifest(prefix, opts.workspacePrefix || prefix, async () => {
     const { status } = await runNpm(['publish', ...opts.argv.original.slice(1)])
     _status = status
   })
@@ -32,7 +35,7 @@ export async function pack (
   command: string,
 ) {
   let _status!: number
-  await fakeRegularManifest(opts.prefix, async () => {
+  await fakeRegularManifest(opts.prefix, opts.workspacePrefix || opts.prefix, async () => {
     const { status } = await runNpm(['pack', ...opts.argv.original.slice(1)])
     _status = status
   })
@@ -41,7 +44,15 @@ export async function pack (
   }
 }
 
-async function fakeRegularManifest (prefix: string, fn: () => Promise<void>) {
+const LICENSE_GLOB = 'LICEN{S,C}E{,.*}'
+const findLicenses = fg.bind(fg, [LICENSE_GLOB]) as (opts: { cwd: string }) => Promise<string[]>
+
+async function fakeRegularManifest (prefix: string, workspacePrefix: string, fn: () => Promise<void>) {
+  // If a workspace package has no License of its own,
+  // license files from the root of the workspace are used
+  const copiedLicenses: string[] = prefix !== workspacePrefix && (await findLicenses({ cwd: prefix })).length === 0
+    ? await copyLicenses(workspacePrefix, prefix) : []
+
   const { fileName, manifest, writeImporterManifest } = await readImporterManifest(prefix)
   const exoticManifestFormat = fileName !== 'package.json'
   if (exoticManifestFormat) {
@@ -53,4 +64,24 @@ async function fakeRegularManifest (prefix: string, fn: () => Promise<void>) {
     await rimraf(path.join(prefix, 'package.json'))
     await writeImporterManifest(manifest, true)
   }
+  await Promise.all(
+    copiedLicenses.map((copiedLicense) => fs.unlink(copiedLicense))
+  )
+}
+
+async function copyLicenses (sourceDir: string, destDir: string) {
+  const licenses = await findLicenses({ cwd: sourceDir })
+  if (licenses.length === 0) return []
+
+  const copiedLicenses: string[] = []
+  await Promise.all(
+    licenses
+      .map((licenseRelPath) => path.join(sourceDir, licenseRelPath))
+      .map((licensePath) => {
+        const licenseCopyDest = path.join(destDir, path.basename(licensePath))
+        copiedLicenses.push(licenseCopyDest)
+        return cpFile(licensePath, licenseCopyDest)
+      })
+  )
+  return copiedLicenses
 }
