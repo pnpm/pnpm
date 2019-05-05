6b9442121bd5c0ddacf113f6a28ac546ba82879d
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-05-06 01:53:39 +0300

    feat: copy license files from workspace root when package has none
    close #1804

--- packages/pnpm/test/publish.ts
+++ packages/pnpm/test/publish.ts
@@ -1,7 +1,9 @@
-import prepare from '@pnpm/prepare'
+import prepare, { preparePackages } from '@pnpm/prepare'
+import fs = require('mz/fs')
 import exists = require('path-exists')
 import tape = require('tape')
 import promisifyTape from 'tape-promise'
+import writeYamlFile = require('write-yaml-file')
 import { execPnpm } from './utils'
 
 const test = promisifyTape(tape)
@@ -58,4 +60,80 @@ test('publish: package with package.json5 running publish from different folder'
 
   t.ok(await exists('project/package.json5'))
   t.notOk(await exists('project/package.json'))
+})
+
+test('pack packages with workspace LICENSE if no own LICENSE is present', async (t: tape.Test) => {
+  preparePackages(t, [
+    {
+      name: 'project-1',
+      version: '1.0.0',
+    },
+    {
+      name: 'project-2',
+      version: '1.0.0',
+    },
+    {
+      name: 'target',
+      version: '1.0.0',
+    },
+  ], { manifestFormat: 'YAML' })
+
+  await writeYamlFile('pnpm-workspace.yaml', { packages: ['**', '!store/**'] })
+  await fs.writeFile('LICENSE', 'workspace license', 'utf8')
+  await fs.writeFile('project-2/LICENSE', 'project-2 license', 'utf8')
+
+  process.chdir('project-1')
+  await execPnpm('pack')
+
+  process.chdir('../project-2')
+  await execPnpm('pack')
+
+  process.chdir('../target')
+
+  await execPnpm('add', '../project-1/project-1-1.0.0.tgz', '../project-2/project-2-1.0.0.tgz')
+
+  t.equal(await fs.readFile('node_modules/project-1/LICENSE', 'utf8'), 'workspace license')
+  t.equal(await fs.readFile('node_modules/project-2/LICENSE', 'utf8'), 'project-2 license')
+
+  process.chdir('..')
+  t.notOk(await exists('project-1/LICENSE'))
+  t.ok(await exists('project-2/LICENSE'))
+})
+
+test('publish packages with workspace LICENSE if no own LICENSE is present', async (t: tape.Test) => {
+  preparePackages(t, [
+    {
+      name: 'project-100',
+      version: '1.0.0',
+    },
+    {
+      name: 'project-200',
+      version: '1.0.0',
+    },
+    {
+      name: 'target',
+      version: '1.0.0',
+    },
+  ], { manifestFormat: 'YAML' })
+
+  await writeYamlFile('pnpm-workspace.yaml', { packages: ['**', '!store/**'] })
+  await fs.writeFile('LICENSE', 'workspace license', 'utf8')
+  await fs.writeFile('project-200/LICENSE', 'project-200 license', 'utf8')
+
+  process.chdir('project-100')
+  await execPnpm('publish')
+
+  process.chdir('../project-200')
+  await execPnpm('publish')
+
+  process.chdir('../target')
+
+  await execPnpm('add', 'project-100', 'project-200', '--no-link-workspace-packages')
+
+  t.equal(await fs.readFile('node_modules/project-100/LICENSE', 'utf8'), 'workspace license')
+  t.equal(await fs.readFile('node_modules/project-200/LICENSE', 'utf8'), 'project-200 license')
+
+  process.chdir('..')
+  t.notOk(await exists('project-100/LICENSE'))
+  t.ok(await exists('project-200/LICENSE'))
 })
