b13c16ae5131134de2727e777414c1a04f0a88bb
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-05-18 02:28:49 +0300

    fix(lockfile): relative tarballs should not start with slash
    close #1827

--- packages/lockfile-utils/test/pkgSnapshotToResolution.ts
+++ packages/lockfile-utils/test/pkgSnapshotToResolution.ts
@@ -12,5 +12,16 @@ test('pkgSnapshotToResolution()', (t) => {
     tarball: 'https://registry.npmjs.org/foo/-/foo-1.0.0.tgz',
   })
 
+  t.deepEqual(pkgSnapshotToResolution('/@mycompany/mypackage/2.0.0', {
+    resolution: {
+      integrity: 'AAAA',
+      tarball: '@mycompany/mypackage/-/@mycompany/mypackage-2.0.0.tgz',
+    },
+  }, { default: 'https://registry.npmjs.org/', '@mycompany': 'https://mycompany.jfrog.io/mycompany/api/npm/npm-local/' }), {
+    integrity: 'AAAA',
+    registry: 'https://mycompany.jfrog.io/mycompany/api/npm/npm-local/',
+    tarball: 'https://mycompany.jfrog.io/mycompany/api/npm/npm-local/@mycompany/mypackage/-/@mycompany/mypackage-2.0.0.tgz',
+  })
+
   t.end()
 })
