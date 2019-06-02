12af9600ff5389c04a61d75879234232aa985c00
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-06-02 17:54:10 +0300

    refactor: move "pnpm up -r" tests to separate file

--- packages/pnpm/test/recursive/misc.ts
+++ packages/pnpm/test/recursive/misc.ts
@@ -1,11 +1,9 @@
-import { Lockfile } from '@pnpm/lockfile-types'
 import { preparePackages } from '@pnpm/prepare'
 import isCI = require('is-ci')
 import isWindows = require('is-windows')
 import makeDir = require('make-dir')
 import fs = require('mz/fs')
 import path = require('path')
-import { addDistTag } from 'pnpm-registry-mock'
 import readYamlFile from 'read-yaml-file'
 import tape = require('tape')
 import promisifyTape from 'tape-promise'
@@ -165,34 +163,6 @@ test('recursive install with package that has link', async (t: tape.Test) => {
   t.ok(projects['project-1'].requireModule('is-positive'))
   t.ok(projects['project-1'].requireModule('project-2/package.json'))
   t.ok(projects['project-2'].requireModule('is-negative'))
-})
-
-test('recursive update', async (t: tape.Test) => {
-  const projects = preparePackages(t, [
-    {
-      name: 'project-1',
-      version: '1.0.0',
-
-      dependencies: {
-        'is-positive': '1.0.0',
-      },
-    },
-    {
-      name: 'project-2',
-      version: '1.0.0',
-
-      dependencies: {
-        'is-negative': '1.0.0',
-      },
-    },
-  ])
-
-  await execPnpm('recursive', 'install')
-
-  await execPnpm('recursive', 'update', 'is-positive@2.0.0')
-
-  t.equal(projects['project-1'].requireModule('is-positive/package.json').version, '2.0.0')
-  projects['project-2'].hasNot('is-positive')
 })
 
 test('recursive installation with package-specific .npmrc', async t => {
@@ -1124,89 +1094,4 @@ test('recursive install --no-bail', async (t: tape.Test) => {
   t.ok(failed, 'command failed')
 
   t.ok(projects['project-2'].requireModule('is-negative'))
-})
-
-// TODO: also cover the case of scoped package update
-test('recursive update --latest foo should only update workspace packages that have foo', async (t: tape.Test) => {
-  await addDistTag({ package: 'foo', version: '100.0.0', distTag: 'latest' })
-  await addDistTag({ package: 'bar', version: '100.0.0', distTag: 'latest' })
-  await addDistTag({ package: 'qar', version: '100.0.0', distTag: 'latest' })
-
-  const projects = preparePackages(t, [
-    {
-      name: 'project-1',
-      version: '1.0.0',
-
-      dependencies: {
-        'foo': '100.0.0',
-        'qar': '100.0.0',
-      },
-    },
-    {
-      name: 'project-2',
-      version: '1.0.0',
-
-      dependencies: {
-        'bar': '^100.0.0',
-      },
-    },
-  ])
-
-  await fs.writeFile('pnpm-workspace.yaml', '', 'utf8')
-
-  await execPnpm('recursive', 'install')
-
-  await addDistTag({ package: 'foo', version: '100.1.0', distTag: 'latest' })
-  await addDistTag({ package: 'bar', version: '100.1.0', distTag: 'latest' })
-
-  await execPnpm('recursive', 'update', '--latest', 'foo', 'qar@100.1.0')
-
-  const lockfile = await readYamlFile<Lockfile>('./pnpm-lock.yaml')
-
-  t.deepEqual(Object.keys(lockfile.packages || {}), ['/bar/100.0.0', '/foo/100.1.0', '/qar/100.1.0'])
-})
-
-test('recursive update --latest foo should only update packages that have foo', async (t: tape.Test) => {
-  await addDistTag({ package: 'foo', version: '100.0.0', distTag: 'latest' })
-  await addDistTag({ package: 'bar', version: '100.0.0', distTag: 'latest' })
-  await addDistTag({ package: 'qar', version: '100.0.0', distTag: 'latest' })
-
-  const projects = preparePackages(t, [
-    {
-      name: 'project-1',
-      version: '1.0.0',
-
-      dependencies: {
-        'foo': '100.0.0',
-        'qar': '100.0.0',
-      },
-    },
-    {
-      name: 'project-2',
-      version: '1.0.0',
-
-      dependencies: {
-        'bar': '^100.0.0',
-      },
-    },
-  ])
-
-  await execPnpm('recursive', 'install')
-
-  await addDistTag({ package: 'foo', version: '100.1.0', distTag: 'latest' })
-  await addDistTag({ package: 'bar', version: '100.1.0', distTag: 'latest' })
-
-  await execPnpm('recursive', 'update', '--latest', 'foo', 'qar@100.1.0')
-
-  {
-    const lockfile = await projects['project-1'].readLockfile()
-
-    t.deepEqual(Object.keys(lockfile.packages || {}), ['/foo/100.1.0', '/qar/100.1.0'])
-  }
-
-  {
-    const lockfile = await projects['project-2'].readLockfile()
-
-    t.deepEqual(Object.keys(lockfile.packages || {}), ['/bar/100.0.0'])
-  }
 })
