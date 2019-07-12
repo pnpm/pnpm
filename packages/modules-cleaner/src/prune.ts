pnpm/pnpm

commit 9360bc05676c9f6a42929c3ead71126d7d69ab88

fix: prunning by partial installation in a workspace
When running install on a subset of workspace packages, only
those dependencies may be prunned which were only used by the
selected subset of workspace packages.

BREAKING CHANGE: in @pnpm/modules-cleaner

PR #1908

packages/modules-cleaner/src/prune.ts
@@ -2,6 +2,7 @@ import {
   removalLogger,
   statsLogger,
 } from '@pnpm/core-loggers'
+import filterLockfile, { filterLockfileByImporters } from '@pnpm/filter-lockfile'
 import {
   Lockfile,
   LockfileImporter,
@@ -11,7 +12,11 @@ import { packageIdFromSnapshot } from '@pnpm/lockfile-utils'
 import logger from '@pnpm/logger'
 import readModulesDir from '@pnpm/read-modules-dir'
 import { StoreController } from '@pnpm/store-controller-types'
-import { DEPENDENCIES_FIELDS, Registries } from '@pnpm/types'
+import {
+  DEPENDENCIES_FIELDS,
+  DependenciesField,
+  Registries,
+} from '@pnpm/types'
 import * as dp from 'dependency-path'
 import vacuumCB = require('fs-vacuum')
 import path = require('path')
@@ -22,44 +27,46 @@ import removeDirectDependency from './removeDirectDependency'
 const vacuum = promisify(vacuumCB)
 
 export default async function prune (
+  importers: Array<{
+    bin: string,
+    hoistedAliases: {[depPath: string]: string[]},
+    id: string,
+    modulesDir: string,
+    prefix: string,
+    pruneDirectDependencies?: boolean,
+    removePackages?: string[],
+    shamefullyFlatten: boolean,
+  }>,
   opts: {
     dryRun?: boolean,
-    importers: Array<{
-      bin: string,
-      hoistedAliases: {[depPath: string]: string[]},
-      id: string,
-      modulesDir: string,
-      prefix: string,
-      pruneDirectDependencies?: boolean,
-      removePackages?: string[],
-      shamefullyFlatten: boolean,
-    }>,
-    newLockfile: Lockfile,
-    oldLockfile: Lockfile,
+    include: { [dependenciesField in DependenciesField]: boolean },
+    wantedLockfile: Lockfile,
+    currentLockfile: Lockfile,
     pruneStore?: boolean,
     registries: Registries,
+    skipped: Set<string>,
     virtualStoreDir: string,
     lockfileDirectory: string,
     storeController: StoreController,
   },
 ): Promise<Set<string>> {
-  await Promise.all(opts.importers.map(async (importer) => {
-    const oldLockfileImporter = opts.oldLockfile.importers[importer.id] || {} as LockfileImporter
-    const oldPkgs = R.toPairs(mergeDependencies(oldLockfileImporter))
-    const newPkgs = R.toPairs(mergeDependencies(opts.newLockfile.importers[importer.id]))
+  await Promise.all(importers.map(async ({ bin, id, modulesDir, prefix, pruneDirectDependencies, removePackages }) => {
+    const currentImporter = opts.currentLockfile.importers[id] || {} as LockfileImporter
+    const currentPkgs = R.toPairs(mergeDependencies(currentImporter))
+    const wantedPkgs = R.toPairs(mergeDependencies(opts.wantedLockfile.importers[id]))
 
     const allCurrentPackages = new Set(
-      (importer.pruneDirectDependencies || importer.removePackages && importer.removePackages.length)
-        ? (await readModulesDir(importer.modulesDir) || [])
+      (pruneDirectDependencies || removePackages && removePackages.length)
+        ? (await readModulesDir(modulesDir) || [])
         : [],
     )
     const depsToRemove = new Set([
-      ...(importer.removePackages || []).filter((removePackage) => allCurrentPackages.has(removePackage)),
-      ...R.difference(oldPkgs, newPkgs).map(([depName]) => depName),
+      ...(removePackages || []).filter((removePackage) => allCurrentPackages.has(removePackage)),
+      ...R.difference(currentPkgs, wantedPkgs).map(([depName]) => depName),
     ])
-    if (importer.pruneDirectDependencies) {
+    if (pruneDirectDependencies) {
       if (allCurrentPackages.size > 0) {
-        const newPkgsSet = new Set(newPkgs.map(([depName]) => depName))
+        const newPkgsSet = new Set(wantedPkgs.map(([depName]) => depName))
         for (const currentPackage of Array.from(allCurrentPackages)) {
           if (!newPkgsSet.has(currentPackage)) {
             depsToRemove.add(currentPackage)
@@ -68,13 +75,11 @@ export default async function prune (
       }
     }
 
-    const { bin, modulesDir, prefix } = importer
-
     return Promise.all(Array.from(depsToRemove).map((depName) => {
       return removeDirectDependency({
-        dependenciesField: oldLockfileImporter.devDependencies && oldLockfileImporter.devDependencies[depName] && 'devDependencies' ||
-          oldLockfileImporter.optionalDependencies && oldLockfileImporter.optionalDependencies[depName] && 'optionalDependencies' ||
-          oldLockfileImporter.dependencies && oldLockfileImporter.dependencies[depName] && 'dependencies' ||
+        dependenciesField: currentImporter.devDependencies && currentImporter.devDependencies[depName] && 'devDependencies' ||
+          currentImporter.optionalDependencies && currentImporter.optionalDependencies[depName] && 'optionalDependencies' ||
+          currentImporter.dependencies && currentImporter.dependencies[depName] && 'dependencies' ||
           undefined,
         name: depName,
       }, {
@@ -86,14 +91,25 @@ export default async function prune (
     }))
   }))
 
-  const oldPkgIdsByDepPaths = getPkgsDepPaths(opts.registries, opts.oldLockfile.packages || {})
-  const newPkgIdsByDepPaths = getPkgsDepPaths(opts.registries, opts.newLockfile.packages || {})
+  const selectedImporterIds = importers.map((importer) => importer.id).sort()
+  // In case installation is done on a subset of importers,
+  // we may only prune dependencies that are used only by that subset of importers.
+  // Otherwise, we would break the node_modules.
+  const currentPkgIdsByDepPaths = R.equals(selectedImporterIds, Object.keys(opts.currentLockfile.importers))
+    ? getPkgsDepPaths(opts.registries, opts.currentLockfile.packages || {})
+    : getPkgsDepPathsOwnedOnlyByImporters(selectedImporterIds, opts.registries, opts.currentLockfile, opts.include, opts.skipped)
+  const wantedPkgIdsByDepPaths = getPkgsDepPaths(opts.registries,
+    filterLockfile(opts.wantedLockfile, {
+      include: opts.include,
+      registries: opts.registries,
+      skipped: opts.skipped,
+    }).packages || {})
 
-  const oldDepPaths = Object.keys(oldPkgIdsByDepPaths)
-  const newDepPaths = Object.keys(newPkgIdsByDepPaths)
+  const oldDepPaths = Object.keys(currentPkgIdsByDepPaths)
+  const newDepPaths = Object.keys(wantedPkgIdsByDepPaths)
 
   const orphanDepPaths = R.difference(oldDepPaths, newDepPaths)
-  const orphanPkgIds = new Set(R.props<string, string>(orphanDepPaths, oldPkgIdsByDepPaths))
+  const orphanPkgIds = new Set(R.props<string, string>(orphanDepPaths, currentPkgIdsByDepPaths))
 
   statsLogger.debug({
     prefix: opts.lockfileDirectory,
@@ -102,8 +118,8 @@ export default async function prune (
 
   if (!opts.dryRun) {
     if (orphanDepPaths.length) {
-      if (opts.oldLockfile.packages) {
-        await Promise.all(opts.importers.filter((importer) => importer.shamefullyFlatten).map((importer) => {
+      if (opts.currentLockfile.packages) {
+        await Promise.all(importers.filter((importer) => importer.shamefullyFlatten).map((importer) => {
           const { bin, hoistedAliases, modulesDir, prefix } = importer
           return Promise.all(orphanDepPaths.map(async (orphanDepPath) => {
             if (hoistedAliases[orphanDepPath]) {
@@ -142,7 +158,7 @@ export default async function prune (
     }
 
     const addedDepPaths = R.difference(newDepPaths, oldDepPaths)
-    const addedPkgIds = new Set(R.props<string, string>(addedDepPaths, newPkgIdsByDepPaths))
+    const addedPkgIds = new Set(R.props<string, string>(addedDepPaths, wantedPkgIdsByDepPaths))
 
     await opts.storeController.updateConnections(path.dirname(opts.virtualStoreDir), {
       addDependencies: Array.from(addedPkgIds),
@@ -172,4 +188,31 @@ function getPkgsDepPaths (
     pkgIdsByDepPath[depPath] = packageIdFromSnapshot(relDepPath, packages[relDepPath], registries)
   }
   return pkgIdsByDepPath
+}
+
+function getPkgsDepPathsOwnedOnlyByImporters (
+  importerIds: string[],
+  registries: Registries,
+  lockfile: Lockfile,
+  include: { [dependenciesField in DependenciesField]: boolean },
+  skipped: Set<string>,
+) {
+  const selected = filterLockfileByImporters(lockfile,
+    importerIds,
+    {
+      failOnMissingDependencies: false,
+      include,
+      registries,
+      skipped,
+    })
+  const other = filterLockfileByImporters(lockfile,
+    R.difference(Object.keys(lockfile.importers), importerIds),
+    {
+      failOnMissingDependencies: false,
+      include,
+      registries,
+      skipped,
+    })
+  const packagesOfSelectedOnly = R.pickAll(R.difference(Object.keys(selected.packages!), Object.keys(other.packages!)), selected.packages!) as PackageSnapshots
+  return getPkgsDepPaths(registries, packagesOfSelectedOnly)
 }

packages/headless/src/index.ts
@@ -149,23 +149,22 @@ export default async (opts: HeadlessOptions) => {
   }
 
   const skipped = opts.skipped || new Set<string>()
-  const filterOpts = {
-    include: opts.include,
-    registries: opts.registries,
-    skipped,
-  }
   if (currentLockfile) {
-    await prune({
-      dryRun: false,
-      importers: opts.importers,
-      lockfileDirectory,
-      newLockfile: filterLockfile(wantedLockfile, filterOpts),
-      oldLockfile: currentLockfile,
-      pruneStore: opts.pruneStore,
-      registries: opts.registries,
-      storeController: opts.storeController,
-      virtualStoreDir,
-    })
+    await prune(
+      opts.importers,
+      {
+        currentLockfile,
+        dryRun: false,
+        include: opts.include,
+        lockfileDirectory,
+        pruneStore: opts.pruneStore,
+        registries: opts.registries,
+        skipped,
+        storeController: opts.storeController,
+        virtualStoreDir,
+        wantedLockfile,
+      },
+    )
   } else {
     statsLogger.debug({
       prefix: lockfileDirectory,
@@ -178,6 +177,11 @@ export default async (opts: HeadlessOptions) => {
     stage: 'importing_started',
   })
 
+  const filterOpts = {
+    include: opts.include,
+    registries: opts.registries,
+    skipped,
+  }
   const filteredLockfile = filterLockfileByImportersAndEngine(wantedLockfile, opts.importers.map(({ id }) => id), {
     ...filterOpts,
     currentEngine: opts.currentEngine,

packages/supi/src/install/link.ts
@@ -7,7 +7,7 @@ import {
   stageLogger,
   statsLogger,
 } from '@pnpm/core-loggers'
-import filterLockfile, {
+import {
   filterLockfileByImporters,
 } from '@pnpm/filter-lockfile'
 import { Lockfile } from '@pnpm/lockfile-file'
@@ -144,21 +144,17 @@ export default async function linkPackages (
   if (!opts.include.optionalDependencies) {
     depNodes = depNodes.filter(({ optional }) => !optional)
   }
-  const filterOpts = {
-    include: opts.include,
-    registries: opts.registries,
-    skipped: opts.skipped,
-  }
-  const removedDepPaths = await prune({
+  const removedDepPaths = await prune(importers, {
+    currentLockfile: opts.currentLockfile,
     dryRun: opts.dryRun,
-    importers,
+    include: opts.include,
     lockfileDirectory: opts.lockfileDirectory,
-    newLockfile: filterLockfile(newWantedLockfile, filterOpts),
-    oldLockfile: opts.currentLockfile,
     pruneStore: opts.pruneStore,
     registries: opts.registries,
+    skipped: opts.skipped,
     storeController: opts.storeController,
     virtualStoreDir: opts.virtualStoreDir,
+    wantedLockfile: newWantedLockfile,
   })
 
   stageLogger.debug({
@@ -167,6 +163,11 @@ export default async function linkPackages (
   })
 
   const importerIds = importers.map(({ id }) => id)
+  const filterOpts = {
+    include: opts.include,
+    registries: opts.registries,
+    skipped: opts.skipped,
+  }
   const newCurrentLockfile = filterLockfileByImporters(newWantedLockfile, importerIds, {
     ...filterOpts,
     failOnMissingDependencies: true,

packages/supi/src/link/index.ts
@@ -52,7 +52,7 @@ export default async function link (
   const ctx = await getContextForSingleImporter(opts.manifest, opts)
 
   const importerId = getLockfileImporterId(ctx.lockfileDirectory, opts.prefix)
-  const oldLockfile = R.clone(ctx.currentLockfile)
+  const currentLockfile = R.clone(ctx.currentLockfile)
   const linkedPkgs: Array<{path: string, manifest: DependencyManifest, alias: string}> = []
   const specsToUpsert = [] as Array<{name: string, pref: string, saveType: DependenciesField}>
   const saveType = getSaveType(opts)
@@ -99,8 +99,8 @@ export default async function link (
     warn,
   })
 
-  await prune({
-    importers: [
+  await prune(
+    [
       {
         bin: opts.bin,
         hoistedAliases: ctx.hoistedAliases,
@@ -110,13 +110,17 @@ export default async function link (
         shamefullyFlatten: opts.shamefullyFlatten,
       },
     ],
-    lockfileDirectory: opts.lockfileDirectory,
-    newLockfile: updatedCurrentLockfile,
-    oldLockfile,
-    registries: ctx.registries,
-    storeController: opts.storeController,
-    virtualStoreDir: ctx.virtualStoreDir,
-  })
+    {
+      currentLockfile,
+      include: ctx.include,
+      lockfileDirectory: opts.lockfileDirectory,
+      registries: ctx.registries,
+      skipped: ctx.skipped,
+      storeController: opts.storeController,
+      virtualStoreDir: ctx.virtualStoreDir,
+      wantedLockfile: updatedCurrentLockfile,
+    },
+  )
 
   // Linking should happen after removing orphans
   // Otherwise would've been removed

packages/supi/test/install/multipleImporters.ts
@@ -3,6 +3,7 @@ import { WANTED_LOCKFILE } from '@pnpm/constants'
 import { readCurrentLockfile } from '@pnpm/lockfile-file'
 import { prepareEmpty, preparePackages } from '@pnpm/prepare'
 import path = require('path')
+import exists = require('path-exists')
 import sinon = require('sinon')
 import {
   addDependenciesToPackage,
@@ -11,7 +12,8 @@ import {
 } from 'supi'
 import tape = require('tape')
 import promisifyTape from 'tape-promise'
-import { testDefaults } from '../utils'
+import writeYamlFile = require('write-yaml-file')
+import { addDistTag, testDefaults } from '../utils'
 
 const test = promisifyTape(tape)
 const testOnly = promisifyTape(tape.only)
@@ -302,4 +304,190 @@ test('current lockfile contains only installed dependencies when adding a new im
   const currentLockfile = await readCurrentLockfile(process.cwd(), { ignoreIncompatible: false })
 
   t.deepEqual(Object.keys(currentLockfile && currentLockfile.packages || {}), ['/is-negative/1.0.0'])
+})
+
+test('partial installation in a monorepo does not remove dependencies of other workspace packages', async (t: tape.Test) => {
+  await addDistTag('dep-of-pkg-with-1-dep', '100.1.0', 'latest')
+  prepareEmpty(t)
+
+  await mutateModules([
+    {
+      buildIndex: 0,
+      manifest: {
+        dependencies: {
+          'is-positive': '1.0.0',
+        },
+      },
+      mutation: 'install',
+      prefix: path.resolve('project-1'),
+    },
+    {
+      buildIndex: 0,
+      manifest: {
+        dependencies: {
+          'pkg-with-1-dep': '100.0.0',
+        },
+      },
+      mutation: 'install',
+      prefix: path.resolve('project-2'),
+    },
+  ], await testDefaults())
+
+  await writeYamlFile(path.resolve('pnpm-lock.yaml'), {
+    importers: {
+      'project-1': {
+        dependencies: {
+          'is-positive': '1.0.0'
+        },
+        specifiers: {
+          'is-positive': '1.0.0'
+        },
+      },
+      'project-2': {
+        dependencies: {
+          'pkg-with-1-dep': '100.0.0'
+        },
+        specifiers: {
+          'pkg-with-1-dep': '100.0.0'
+        },
+      },
+    },
+    lockfileVersion: 5.1,
+    packages: {
+      '/dep-of-pkg-with-1-dep/100.0.0': {
+        dev: false,
+        resolution: {
+          integrity: 'sha512-RWObNQIluSr56fVbOwD75Dt5CE2aiPReTMMUblYEMEqUI+iJw5ovTyO7LzUG/VJ4iVL2uUrbkQ6+rq4z4WOdDw=='
+        }
+      },
+      '/is-positive/1.0.0': {
+        dev: false,
+        engines: {
+          node: '>=0.10.0',
+        },
+        resolution: {
+          integrity: 'sha1-iACYVrZKLx632LsBeUGEJK4EUss='
+        },
+      },
+      '/pkg-with-1-dep/100.0.0': {
+        dependencies: {
+          'dep-of-pkg-with-1-dep': '100.0.0',
+        },
+        dev: false,
+        resolution: {
+          integrity: 'sha512-OStTw86MRiQHB1JTSy6wl+9GT46aK8w4ghZT3e8ZN899J+FUsfD1nFl5gANa4Qol1LTBRqXeKomgXIAo9R/RZA=='
+        },
+      },
+    },
+  })
+
+  await mutateModules([
+    {
+      buildIndex: 0,
+      manifest: {
+        dependencies: {
+          'is-positive': '2.0.0',
+        },
+      },
+      mutation: 'install',
+      prefix: path.resolve('project-1'),
+    },
+  ], await testDefaults())
+
+  t.ok(await exists(path.resolve('node_modules/.localhost+4873/is-positive/2.0.0/node_modules/is-positive')))
+  t.ok(await exists(path.resolve('node_modules/.localhost+4873/pkg-with-1-dep/100.0.0/node_modules/pkg-with-1-dep')))
+  t.ok(await exists(path.resolve('node_modules/.localhost+4873/dep-of-pkg-with-1-dep/100.1.0/node_modules/dep-of-pkg-with-1-dep')))
+})
+
+test('partial installation in a monorepo does not remove dependencies of other workspace packages when lockfile is frozen', async (t: tape.Test) => {
+  await addDistTag('dep-of-pkg-with-1-dep', '100.1.0', 'latest')
+  prepareEmpty(t)
+
+  await mutateModules([
+    {
+      buildIndex: 0,
+      manifest: {
+        dependencies: {
+          'is-positive': '1.0.0',
+        },
+      },
+      mutation: 'install',
+      prefix: path.resolve('project-1'),
+    },
+    {
+      buildIndex: 0,
+      manifest: {
+        dependencies: {
+          'pkg-with-1-dep': '100.0.0',
+        },
+      },
+      mutation: 'install',
+      prefix: path.resolve('project-2'),
+    },
+  ], await testDefaults())
+
+  await writeYamlFile(path.resolve('pnpm-lock.yaml'), {
+    importers: {
+      'project-1': {
+        dependencies: {
+          'is-positive': '1.0.0'
+        },
+        specifiers: {
+          'is-positive': '1.0.0'
+        },
+      },
+      'project-2': {
+        dependencies: {
+          'pkg-with-1-dep': '100.0.0'
+        },
+        specifiers: {
+          'pkg-with-1-dep': '100.0.0'
+        },
+      },
+    },
+    lockfileVersion: 5.1,
+    packages: {
+      '/dep-of-pkg-with-1-dep/100.0.0': {
+        dev: false,
+        resolution: {
+          integrity: 'sha512-RWObNQIluSr56fVbOwD75Dt5CE2aiPReTMMUblYEMEqUI+iJw5ovTyO7LzUG/VJ4iVL2uUrbkQ6+rq4z4WOdDw=='
+        }
+      },
+      '/is-positive/1.0.0': {
+        dev: false,
+        engines: {
+          node: '>=0.10.0',
+        },
+        resolution: {
+          integrity: 'sha1-iACYVrZKLx632LsBeUGEJK4EUss='
+        },
+      },
+      '/pkg-with-1-dep/100.0.0': {
+        dependencies: {
+          'dep-of-pkg-with-1-dep': '100.0.0',
+        },
+        dev: false,
+        resolution: {
+          integrity: 'sha512-OStTw86MRiQHB1JTSy6wl+9GT46aK8w4ghZT3e8ZN899J+FUsfD1nFl5gANa4Qol1LTBRqXeKomgXIAo9R/RZA=='
+        },
+      },
+    },
+  })
+
+  await mutateModules([
+    {
+      buildIndex: 0,
+      manifest: {
+        dependencies: {
+          'is-positive': '1.0.0',
+        },
+      },
+      mutation: 'install',
+      prefix: path.resolve('project-1'),
+    },
+  ], await testDefaults({ frozenLockfile: true }))
+
+  t.ok(await exists(path.resolve('node_modules/.localhost+4873/is-positive/1.0.0/node_modules/is-positive')))
+  t.ok(await exists(path.resolve('node_modules/.localhost+4873/pkg-with-1-dep/100.0.0/node_modules/pkg-with-1-dep')))
+  t.ok(await exists(path.resolve('node_modules/.localhost+4873/dep-of-pkg-with-1-dep/100.1.0/node_modules/dep-of-pkg-with-1-dep')))
 })

pnpm-lock.yaml
@@ -647,6 +647,7 @@ importers:
   packages/modules-cleaner:
     dependencies:
       '@pnpm/core-loggers': 'link:../core-loggers'
+      '@pnpm/filter-lockfile': 'link:../filter-lockfile'
       '@pnpm/lockfile-types': 'link:../lockfile-types'
       '@pnpm/lockfile-utils': 'link:../lockfile-utils'
       '@pnpm/package-bins': 3.1.1
@@ -665,6 +666,7 @@ importers:
       rimraf: 2.6.3
     specifiers:
       '@pnpm/core-loggers': 3.0.3
+      '@pnpm/filter-lockfile': 1.0.10
       '@pnpm/lockfile-types': 1.1.0
       '@pnpm/lockfile-utils': 1.0.9
       '@pnpm/logger': 2.1.1

commit 48e4a7af5ac7545cd77976245e50d92bb13031a8

fix: regression in 3.5.5
regression caused by PR #1908

packages/modules-cleaner/src/prune.ts
@@ -50,10 +50,15 @@ export default async function prune (
     storeController: StoreController,
   },
 ): Promise<Set<string>> {
+  const wantedLockfile = filterLockfile(opts.wantedLockfile, {
+    include: opts.include,
+    registries: opts.registries,
+    skipped: opts.skipped,
+  })
   await Promise.all(importers.map(async ({ bin, id, modulesDir, prefix, pruneDirectDependencies, removePackages }) => {
     const currentImporter = opts.currentLockfile.importers[id] || {} as LockfileImporter
     const currentPkgs = R.toPairs(mergeDependencies(currentImporter))
-    const wantedPkgs = R.toPairs(mergeDependencies(opts.wantedLockfile.importers[id]))
+    const wantedPkgs = R.toPairs(mergeDependencies(wantedLockfile.importers[id]))
 
     const allCurrentPackages = new Set(
       (pruneDirectDependencies || removePackages && removePackages.length)
@@ -98,12 +103,7 @@ export default async function prune (
   const currentPkgIdsByDepPaths = R.equals(selectedImporterIds, Object.keys(opts.currentLockfile.importers))
     ? getPkgsDepPaths(opts.registries, opts.currentLockfile.packages || {})
     : getPkgsDepPathsOwnedOnlyByImporters(selectedImporterIds, opts.registries, opts.currentLockfile, opts.include, opts.skipped)
-  const wantedPkgIdsByDepPaths = getPkgsDepPaths(opts.registries,
-    filterLockfile(opts.wantedLockfile, {
-      include: opts.include,
-      registries: opts.registries,
-      skipped: opts.skipped,
-    }).packages || {})
+  const wantedPkgIdsByDepPaths = getPkgsDepPaths(opts.registries, wantedLockfile.packages || {})
 
   const oldDepPaths = Object.keys(currentPkgIdsByDepPaths)
   const newDepPaths = Object.keys(wantedPkgIdsByDepPaths)