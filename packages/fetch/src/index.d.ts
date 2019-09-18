535a263d612ee42c2e2b83cf45c7a108cd9010c2
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-09-18 12:00:00 +0200

    feat(fetch): Add type information

---
+++ packages/fetch/src/index.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="node-fetch" />
+export {
+  FetchError,
+  Headers,
+  HeadersInit,
+  Request,
+  RequestInit,
+  RequestContext,
+  RequestMode,
+  RequestRedirect,
+  RequestCredentials,
+  RequestCache,
+  Response,
+  ResponseType,
+  ResponseInit,
+  default,
+} from 'node-fetch';
