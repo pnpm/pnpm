c2e487cfc0d06e05bdaa245e5453a866b7de74d8
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-09-19 22:53:59 +0300

    feat: move .pnpm-lock.yaml to .pnpm/pnpm-lock.yaml
    PR #2018
    
    BREAKING CHANGE:
    
    node_modules/.pnpm-lock.yaml => node_modules/.pnpm/pnpm-lock.yaml

--- privatePackages/assert-project/test/index.ts
+++ privatePackages/assert-project/test/index.ts
@@ -1,5 +1,5 @@
 ///<reference path="../../../typings/index.d.ts"/>
-import { WANTED_LOCKFILE } from '@pnpm/constants'
+import { CURRENT_LOCKFILE, WANTED_LOCKFILE } from '@pnpm/constants'
 import test = require('tape')
 import assertProject from '@pnpm/assert-project'
 import path = require('path')
@@ -23,7 +23,7 @@ test('assertProject() store functions', async (t) => {
   t.equal(typeof await project.resolve('is-positive', '3.1.0'), 'string')
   await project.storeHasNot('is-positive', '3.100.0')
   t.ok(await project.readLockfile(), `loads wanted ${WANTED_LOCKFILE}`)
-  t.ok(await project.readCurrentLockfile(), `loads current ${WANTED_LOCKFILE}`)
+  t.ok(await project.readCurrentLockfile(), `loads current ${CURRENT_LOCKFILE}`)
   t.ok(await project.readModulesManifest(), 'loads .modules.yaml')
 
   t.end()
