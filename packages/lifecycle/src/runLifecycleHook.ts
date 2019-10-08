d7dbadbad5c204c08c2cfad211c0bb5356dc9b67
Yao Ding <yaodingyd@gmail.com>
2019-10-08 14:35:24 -0400

    fix: show emptry string instead of undefined when package.json has no name
    close #2005
    PR #2059
    
    Co-Authored-By: Zoltan Kochan <zkochan@users.noreply.github.com>

--- packages/lifecycle/src/runLifecycleHook.ts
+++ packages/lifecycle/src/runLifecycleHook.ts
@@ -91,7 +91,9 @@ export default async function runLifecycleHook (
 
 function getId (manifest: ImporterManifest | DependencyManifest) {
   if (!manifest.name) {
-    return undefined
+    // Using an empty string instead of undefined or null
+    // because this gets printed to the console
+    return ''
   }
   if (!manifest.version) {
     return manifest.name
