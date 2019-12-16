69624301123438bd96804e912c69de8addb9be5e
Zoltan Kochan <z@kochan.io>
2019-12-16 12:57:10 +0200

    test(filter): use an array of fixtures

--- packages/filter-workspace-packages/test/parsePackageSelector.ts
+++ packages/filter-workspace-packages/test/parsePackageSelector.ts
@@ -1,49 +1,58 @@
-import { parsePackageSelector } from '@pnpm/filter-workspace-packages'
+import { PackageSelector, parsePackageSelector } from '@pnpm/filter-workspace-packages'
 import isWindows = require('is-windows')
 import path = require('path')
 import test = require('tape')
 
-test('parsePackageSelector()', (t) => {
-  t.deepEqual(
-    parsePackageSelector('foo', process.cwd()),
+const fixtures: Array<[string, PackageSelector]> = [
+  [
+    'foo',
     { excludeSelf: false, namePattern: 'foo' },
-  )
-  t.deepEqual(
-    parsePackageSelector('foo...', process.cwd()),
+  ],
+  [
+    'foo...',
     { excludeSelf: false, namePattern: 'foo', includeDependencies: true, includeDependents: false },
-  )
-  t.deepEqual(
-    parsePackageSelector('...foo', process.cwd()),
+  ],
+  [
+    '...foo',
     { excludeSelf: false, namePattern: 'foo', includeDependencies: false, includeDependents: true },
-  )
-  t.deepEqual(
-    parsePackageSelector('...foo...', process.cwd()),
+  ],
+  [
+    '...foo...',
     { excludeSelf: false, namePattern: 'foo', includeDependencies: true, includeDependents: true },
-  )
-  t.deepEqual(
-    parsePackageSelector('foo^...', process.cwd()),
+  ],
+  [
+    'foo^...',
     { excludeSelf: true, namePattern: 'foo', includeDependencies: true, includeDependents: false },
-  )
-  t.deepEqual(
-    parsePackageSelector('...^foo', process.cwd()),
+  ],
+  [
+    '...^foo',
     { excludeSelf: true, namePattern: 'foo', includeDependencies: false, includeDependents: true },
-  )
-  t.deepEqual(
-    parsePackageSelector('./foo', process.cwd()),
+  ],
+  [
+    './foo',
     { excludeSelf: false, parentDir: path.resolve('foo') },
-  )
-  t.deepEqual(
-    parsePackageSelector('../foo', process.cwd()),
+  ],
+  [
+    '../foo',
     { excludeSelf: false, parentDir: path.resolve('../foo') },
-  )
-  t.deepEqual(
-    parsePackageSelector('.', process.cwd()),
+  ],
+  [
+    '.',
     { excludeSelf: false, parentDir: process.cwd() },
-  )
-  t.deepEqual(
-    parsePackageSelector('..', process.cwd()),
+  ],
+  [
+    '..',
     { excludeSelf: false, parentDir: path.resolve('..') },
-  )
+  ],
+]
+
+test('parsePackageSelector()', (t) => {
+  for (let fixture of fixtures) {
+    t.deepEqual(
+      parsePackageSelector(fixture[0], process.cwd()),
+      fixture[1]
+    )
+  }
   if (isWindows()) {
     t.deepEqual(
       parsePackageSelector('.\\foo', process.cwd()),
