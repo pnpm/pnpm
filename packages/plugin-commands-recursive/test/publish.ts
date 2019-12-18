4c3658f1e8dff4a7c1650406a3d99049fc04a1f0
Zoltan Kochan <z@kochan.io>
2019-12-18 02:27:34 +0200

    test: -r publish --tag <tag>

--- packages/plugin-commands-recursive/test/publish.ts
+++ packages/plugin-commands-recursive/test/publish.ts
@@ -14,23 +14,25 @@ const CREDENTIALS = [
 ].join('\n')
 
 test('recursive publish', async (t) => {
+  const pkg1 = {
+    name: '@pnpmtest/test-recursive-publish-project-1',
+    version: '1.0.0',
+
+    dependencies: {
+      'is-positive': '1.0.0',
+    },
+  }
+  const pkg2 = {
+    name: '@pnpmtest/test-recursive-publish-project-2',
+    version: '1.0.0',
+
+    dependencies: {
+      'is-negative': '1.0.0',
+    },
+  }
   const projects = preparePackages(t, [
-    {
-      name: '@pnpmtest/test-recursive-publish-project-1',
-      version: '1.0.0',
-
-      dependencies: {
-        'is-positive': '1.0.0',
-      },
-    },
-    {
-      name: '@pnpmtest/test-recursive-publish-project-2',
-      version: '1.0.0',
-
-      dependencies: {
-        'is-negative': '1.0.0',
-      },
-    },
+    pkg1,
+    pkg2,
     // This will not be published because is-positive@1.0.0 is in the registry
     {
       name: 'is-positive',
@@ -50,12 +52,25 @@ test('recursive publish', async (t) => {
   })
 
   {
-    const { stdout } = await execa('npm', ['view', '@pnpmtest/test-recursive-publish-project-1', 'versions', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`, '--json'])
-    t.deepEqual(JSON.parse(stdout.toString()), ['1.0.0'])
+    const { stdout } = await execa('npm', ['view', pkg1.name, 'versions', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`, '--json'])
+    t.deepEqual(JSON.parse(stdout.toString()), [pkg1.version])
   }
   {
-    const { stdout } = await execa('npm', ['view', '@pnpmtest/test-recursive-publish-project-2', 'versions', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`, '--json'])
-    t.deepEqual(JSON.parse(stdout.toString()), ['1.0.0'])
+    const { stdout } = await execa('npm', ['view', pkg2.name, 'versions', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`, '--json'])
+    t.deepEqual(JSON.parse(stdout.toString()), [pkg2.version])
+  }
+
+  await projects[pkg1.name].writePackageJson({ ...pkg1, version: '2.0.0' })
+
+  await recursive.handler(['publish'], {
+    ...DEFAULT_OPTS,
+    dir: process.cwd(),
+    tag: 'next',
+  })
+
+  {
+    const { stdout } = await execa('npm', ['dist-tag', 'ls', pkg1.name, '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`])
+    t.ok(stdout.toString().includes('next: 2.0.0'), 'new version published with correct dist tag')
   }
 
   t.end()
