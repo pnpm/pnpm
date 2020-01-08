pnpm/pnpm

commit b506e8c97510c3fa5fdc15e68f69f73cdb4dc521

fix: resolving workspace deps with prerelease versions
close #2171
PR #2172

* chore: fix test

* chore: add test

packages/npm-resolver/src/index.ts
@@ -265,7 +265,10 @@ function pickMatchingLocalVersionOrNull (
     case 'version':
       return versions[spec.fetchSpec] ? spec.fetchSpec : null
     case 'range':
-      return semver.maxSatisfying(localVersions, spec.fetchSpec, true)
+      return semver.maxSatisfying(localVersions, spec.fetchSpec, {
+        includePrerelease: true,
+        loose: true,
+      })
     default:
       return null
   }

packages/npm-resolver/test/index.ts
@@ -1264,6 +1264,43 @@ test('workspace protocol: resolve from local directory even when it does not mat
   t.end()
 })
 
+test('workspace protocol: resolve from local package that has a pre-release version', async t => {
+  const storeDir = tempy.directory()
+  const resolve = createResolveFromNpm({
+    metaCache: new Map(),
+    rawConfig: { registry },
+    storeDir,
+  })
+  const resolveResult = await resolve({ alias: 'is-positive', pref: '*' }, {
+    importerDir: '/home/istvan/src',
+    localPackages: {
+      'is-positive': {
+        '3.0.0-alpha.1.2.3': {
+          dir: '/home/istvan/src/is-positive',
+          manifest: {
+            name: 'is-positive',
+            version: '3.0.0-alpha.1.2.3',
+          },
+        },
+      },
+    },
+    registry,
+  })
+
+  t.equal(resolveResult!.resolvedVia, 'local-filesystem')
+  t.equal(resolveResult!.id, 'link:is-positive')
+  t.notOk(resolveResult!.latest)
+  t.deepEqual(resolveResult!.resolution, {
+    directory: '/home/istvan/src/is-positive',
+    type: 'directory',
+  })
+  t.ok(resolveResult!.manifest)
+  t.equal(resolveResult!.manifest!.name, 'is-positive')
+  t.equal(resolveResult!.manifest!.version, '3.0.0-alpha.1.2.3')
+
+  t.end()
+})
+
 test('workspace protocol: resolution fails if there is no matching local package', async t => {
   const storeDir = tempy.directory()
   const resolve = createResolveFromNpm({

commit 0c0388a731b5bffe1a76bd75020dc9730a866242

fix: resolving packages in a monorepo
This fixes a regression introduced by #2172 (pnpm v4.3.3).

Local prerelease version of packages should be linked
only if the range is *

packages/npm-resolver/src/index.ts
@@ -263,8 +263,12 @@ function pickMatchingLocalVersionOrNull (
     case 'version':
       return versions[spec.fetchSpec] ? spec.fetchSpec : null
     case 'range':
+      if (spec.fetchSpec === '*') {
+        return semver.maxSatisfying(localVersions, '*', {
+          includePrerelease: true,
+        })
+      }
       return semver.maxSatisfying(localVersions, spec.fetchSpec, {
-        includePrerelease: true,
         loose: true,
       })
     default:

packages/supi/src/install/index.ts
@@ -524,8 +524,10 @@ async function linkedPackagesAreUpToDate (
         : workspacePackages?.[depName]?.[importerDeps[depName]]?.dir
       if (!dir) continue
       const linkedPkg = workspacePackagesByDirectory[dir] || await safeReadPkgFromDir(dir)
-      const availableVersion = pkgDeps[depName].startsWith('workspace:') ? pkgDeps[depName].substr(10) : pkgDeps[depName]
-      const localPackageSatisfiesRange = linkedPkg && semver.satisfies(linkedPkg.version, availableVersion)
+      const availableRange = pkgDeps[depName].startsWith('workspace:') ? pkgDeps[depName].substr(10) : pkgDeps[depName]
+      // This should pass the same options to semver as @pnpm/npm-resolver
+      const localPackageSatisfiesRange = availableRange === '*' ||
+        linkedPkg && semver.satisfies(linkedPkg.version, availableRange, { loose: true })
       if (isLinked !== localPackageSatisfiesRange) return false
     }
   }

packages/npm-resolver/test/index.ts
@@ -1412,6 +1412,43 @@ test('workspace protocol: resolve from local package that has a pre-release vers
   t.end()
 })
 
+test("workspace protocol: don't resolve from local package that has a pre-release version that don't satisfy the range", async t => {
+  nock(registry)
+    .get('/is-positive')
+    .reply(200, isPositiveMeta)
+
+  const storeDir = tempy.directory()
+  const resolve = createResolveFromNpm({
+    metaCache: new Map(),
+    rawConfig: { registry },
+    storeDir,
+  })
+  const resolveResult = await resolve({ alias: 'is-positive', pref: '2' }, {
+    projectDir: '/home/istvan/src',
+    registry,
+    workspacePackages: {
+      'is-positive': {
+        '3.0.0-alpha.1.2.3': {
+          dir: '/home/istvan/src/is-positive',
+          manifest: {
+            name: 'is-positive',
+            version: '3.0.0-alpha.1.2.3',
+          },
+        },
+      },
+    },
+  })
+
+  t.equal(resolveResult!.resolvedVia, 'npm-registry')
+  t.equal(resolveResult!.id, 'registry.npmjs.org/is-positive/2.0.0')
+  t.ok(resolveResult!.latest)
+  t.ok(resolveResult!.manifest)
+  t.equal(resolveResult!.manifest!.name, 'is-positive')
+  t.equal(resolveResult!.manifest!.version, '2.0.0')
+
+  t.end()
+})
+
 test('workspace protocol: resolution fails if there is no matching local package', async t => {
   const storeDir = tempy.directory()
   const resolve = createResolveFromNpm({