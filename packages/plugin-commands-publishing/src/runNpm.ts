376a74d2a24deec4b464c38a8b2babd6892c58b2
Yao Ding <yao.ding@compass.com>
2020-01-16 13:09:12 -0500

    fix lint

--- packages/plugin-commands-publishing/src/runNpm.ts
+++ packages/plugin-commands-publishing/src/runNpm.ts
@@ -1,18 +1,17 @@
-import getConfig, { types as allTypes } from '@pnpm/config'
 import { packageManager } from '@pnpm/cli-utils'
+import getConfig, { types as allTypes } from '@pnpm/config'
 import runNpm from '@pnpm/run-npm'
 import R = require('ramda')
 
-export default async function run(args: string[]) {
+export default async function run (args: string[]) {
   const { config } = await getConfig({
     cliOptions: {},
     packageManager,
     rcOptionsTypes: {
-        ...R.pick([
+      ...R.pick([
         'npm-path',
-      ], allTypes)
-    }
+      ], allTypes),
+    },
   })
   return runNpm(config.npmPath, args)
 }
-
