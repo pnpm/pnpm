595f7e930efd1ff04de10f9025298e3a06fffdcc
Zoltan Kochan <z@kochan.io>
2020-01-30 02:11:05 +0200

    fix: finding pnpm's main manifest location
    close #2273
    close #2302

--- packages/store-connection-manager/src/runServerInBackground.ts
+++ packages/store-connection-manager/src/runServerInBackground.ts
@@ -1,6 +1,6 @@
 import diable = require('diable')
 
-const pnpm = require.resolve('pnpm/bin/pnpm.js', { paths: [__dirname] })
+const pnpm = require.main!.filename
 
 export default (storePath: string) => {
   return diable.daemonize(pnpm, ['server', 'start', '--store-dir', storePath], { stdio: 'inherit' })
