pnpm/pnpm

commit 766919d768716deefed18ab7b705d8790147ea06

fix: finding pnpm's main manifest location
PR #2303
close #2273
close #2302

packages/cli-utils/src/pnpmPkgJson.ts
@@ -1,7 +1,10 @@
 import { DependencyManifest } from '@pnpm/types'
 import loadJsonFile = require('load-json-file')
+import path = require('path')
 
-const pkgJson = loadJsonFile.sync<DependencyManifest>(require.resolve('pnpm/package.json', { paths: [__dirname] }))
+const pkgJson = loadJsonFile.sync<DependencyManifest>(
+  path.join(path.dirname(require.main!.filename), '../package.json'),
+)
 const packageManager = {
   name: pkgJson.name,
   // Never a prerelease version

packages/store-connection-manager/src/runServerInBackground.ts
@@ -1,6 +1,6 @@
 import diable = require('diable')
 
-const pnpm = require.resolve('pnpm/bin/pnpm.js', { paths: [__dirname] })
+const pnpm = require.main!.filename
 
 export default (storePath: string) => {
   return diable.daemonize(pnpm, ['server', 'start', '--store-dir', storePath], { stdio: 'inherit' })

pnpm-lock.yaml
@@ -101,7 +101,6 @@ importers:
       '@pnpm/types': 'link:../types'
       '@types/nopt': 3.0.29
       '@types/ramda': 0.26.40
-      pnpm: 'link:../pnpm'
       rimraf: 3.0.0
     specifiers:
       '@pnpm/cli-utils': 'link:'
@@ -118,7 +117,6 @@ importers:
       chalk: 3.0.0
       load-json-file: 6.2.0
       nopt: 4.0.1
-      pnpm: 'link:../pnpm'
       ramda: 0.26.1
       rimraf: 3.0.0
       version-selector-type: 2.0.1
@@ -2474,7 +2472,6 @@ importers:
       '@pnpm/store-connection-manager': 'link:'
       '@types/lru-cache': 5.1.0
       '@types/mz': 2.7.0
-      pnpm: 'link:../pnpm'
       rimraf: 3.0.0
     specifiers:
       '@pnpm/cli-utils': 'workspace:0.3.2-0'
@@ -2495,7 +2492,6 @@ importers:
       lru-cache: 5.1.1
       make-dir: 3.0.0
       mz: 2.7.0
-      pnpm: 'link:../pnpm'
       rimraf: 3.0.0
   packages/store-controller-types:
     dependencies:

commit fb458d3e4849488ed67639a82f58704290888c99

fix: pnpx regression introduced by 4.9.0
Fix a regression of pnpx caused by #2303

close #2331
close #2324

packages/cli-utils/src/pnpmPkgJson.ts
@@ -2,9 +2,18 @@ import { DependencyManifest } from '@pnpm/types'
 import loadJsonFile = require('load-json-file')
 import path = require('path')
 
-const pkgJson = loadJsonFile.sync<DependencyManifest>(
-  path.join(path.dirname(require.main!.filename), '../package.json'),
-)
+let pkgJson
+try {
+  pkgJson = loadJsonFile.sync<DependencyManifest>(
+    path.join(path.dirname(require.main!.filename), '../package.json'),
+  )
+} catch (err) {
+  pkgJson = {
+    name: 'unknown',
+    version: '0.0.0',
+  }
+}
+
 const packageManager = {
   name: pkgJson.name,
   // Never a prerelease version

packages/pnpm/src/bin/pnpx.ts
@@ -2,7 +2,7 @@ import findWorkspaceDir from '@pnpm/find-workspace-dir'
 import npx = require('@zkochan/libnpx/index')
 import path = require('path')
 
-const PNPM_PATH = path.join(__dirname, 'pnpm.js')
+const PNPM_PATH = path.join(__dirname, '../../bin/pnpm.js')
 
 // tslint:disable-next-line: no-floating-promises
 ; (async () => {

packages/pnpm/test/cli.ts
@@ -8,6 +8,7 @@ import promisifyTape from 'tape-promise'
 import {
   execPnpm,
   execPnpmSync,
+  execPnpxSync,
 } from './utils'
 
 const test = promisifyTape(tape)
@@ -107,4 +108,13 @@ test('adding new dep does not fail if node_modules was created with --hoist-patt
   t.equal(execPnpmSync('add', 'is-negative').status, 0)
 
   await project.has('is-negative')
+})
+
+test('pnpx works', t => {
+  const result = execPnpxSync('hello-world-js-bin')
+
+  t.equal(result.status, 0)
+  t.ok(result.stdout.toString().includes('Hello world!'))
+
+  t.end()
 })