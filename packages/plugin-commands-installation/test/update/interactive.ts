7ba7cf2f5ce823d0360a0b42b37bf4253788ffe7
Zoltan Kochan <z@kochan.io>
2020-02-29 22:28:41 +0200

    wip

--- packages/plugin-commands-installation/test/update/interactive.ts
+++ packages/plugin-commands-installation/test/update/interactive.ts
@@ -144,3 +144,107 @@ test('interactively update', async (t) => {
 
   t.end()
 })
+
+test.only('interactively update of dev dependencies only', async (t) => {
+  const project = prepare(t, [
+    {
+      dependencies: {
+        'is-negative': '^1.0.0',
+      },
+    },
+  ])
+
+  await add.handler([
+    'is-negative@1.0.0',
+    'is-positive@2.0.0',
+    'micromatch@3.0.0',
+  ], {
+    ...DEFAULT_OPTIONS,
+    dir: process.cwd(),
+    linkWorkspacePackages: true,
+    save: false,
+  })
+
+  prompt.returns({
+    updateDependencies: ['is-negative'],
+  })
+
+  t.comment('update to compatible versions')
+  await update.handler([], {
+    ...DEFAULT_OPTIONS,
+    dir: process.cwd(),
+    interactive: true,
+    linkWorkspacePackages: true,
+  })
+
+  t.ok(prompt.calledWithMatch({
+    choices: [
+      {
+        message: chalk`is-negative 1.0.0 ❯ 1.0.{greenBright.bold 1}  `,
+        name: 'is-negative',
+      },
+      {
+        message: chalk`micromatch  3.0.0 ❯ 3.{yellowBright.bold 1.10} `,
+        name: 'micromatch',
+      },
+    ],
+    footer: '\nEnter to start updating. Ctrl-c to cancel.',
+    message: `Choose which packages to update ` +
+        `(Press ${chalk.cyan('<space>')} to select, ` +
+        `${chalk.cyan('<a>')} to toggle all, ` +
+        `${chalk.cyan('<i>')} to invert selection)`,
+    name: 'updateDependencies',
+    type: 'multiselect',
+  }))
+
+  {
+    const lockfile = await project.readLockfile()
+
+    t.ok(lockfile.packages['/micromatch/3.0.0'])
+    t.ok(lockfile.packages['/is-negative/1.0.1'])
+    t.ok(lockfile.packages['/is-positive/2.0.0'])
+  }
+
+  t.comment('update to latest versions')
+  await update.handler([], {
+    ...DEFAULT_OPTIONS,
+    dir: process.cwd(),
+    interactive: true,
+    latest: true,
+    linkWorkspacePackages: true,
+  })
+
+  t.ok(prompt.calledWithMatch({
+    choices: [
+      {
+        message: chalk`is-negative 1.0.1 ❯ {redBright.bold 2.1.0} `,
+        name: 'is-negative',
+      },
+      {
+        message: chalk`is-positive 2.0.0 ❯ {redBright.bold 3.1.0} `,
+        name: 'is-positive',
+      },
+      {
+        message: chalk`micromatch  3.0.0 ❯ {redBright.bold 4.0.2} `,
+        name: 'micromatch',
+      },
+    ],
+    footer: '\nEnter to start updating. Ctrl-c to cancel.',
+    message: `Choose which packages to update ` +
+        `(Press ${chalk.cyan('<space>')} to select, ` +
+        `${chalk.cyan('<a>')} to toggle all, ` +
+        `${chalk.cyan('<i>')} to invert selection)`,
+    name: 'updateDependencies',
+    type: 'multiselect',
+  }))
+
+  {
+    const lockfile = await project.readLockfile()
+
+    t.ok(lockfile.packages['/micromatch/3.0.0'])
+    t.ok(lockfile.packages['/is-negative/2.1.0'])
+    t.ok(lockfile.packages['/is-positive/2.0.0'])
+  }
+
+  t.end()
+})
