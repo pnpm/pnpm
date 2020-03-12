3ac627a7099ac8c56ff226177b10f950a65c54cf
Zoltan Kochan <zkochan@users.noreply.github.com>
2020-03-12 11:30:44 +0200

    chore(typescript): use project references
    PR #2413
    close #2410

--- utils/updater/src/index.ts
+++ utils/updater/src/index.ts
@@ -1,17 +1,46 @@
 import findWorkspacePackages from '@pnpm/find-workspace-packages'
+import { readWantedLockfile } from '@pnpm/lockfile-file'
 import { ProjectManifest } from '@pnpm/types'
 import isSubdir = require('is-subdir')
+import loadJsonFile = require('load-json-file')
 import path = require('path')
 import exists = require('path-exists')
+import writeJsonFile = require('write-json-file')
 
 const repoRoot = path.join(__dirname, '../../..')
 
 ; (async () => {
   const pkgs = await findWorkspacePackages(repoRoot, { engineStrict: false })
   const pkgsDir = path.join(repoRoot, 'packages')
+  const lockfile = await readWantedLockfile(repoRoot, { ignoreIncompatible: false })
   for (const { dir, manifest, writeProjectManifest } of pkgs) {
-    if (!isSubdir(pkgsDir, dir)) continue
-    await writeProjectManifest(await updateManifest(dir, manifest))
+    if (isSubdir(pkgsDir, dir)) {
+      await writeProjectManifest(await updateManifest(dir, manifest))
+    }
+    if (manifest.name === '@pnpm/fetch' || manifest.name === '@pnpm/tsconfig') continue
+    const relative = path.relative(repoRoot, dir)
+    const importer = lockfile.importers[relative]
+    if (!importer) continue
+    const tsconfigLoc = path.join(dir, 'tsconfig.json')
+    if (!await exists(tsconfigLoc)) continue
+    const deps = {
+      ...importer.dependencies,
+      ...importer.devDependencies,
+    }
+    const references = [] as Array<{ path: string }>
+    for (const spec of Object.values(deps)) {
+      if (!spec.startsWith('link:') || spec.length === 5 || spec === 'link:../fetch') continue
+      references.push({ path: spec.substr(5) })
+    }
+    const tsConfig = await loadJsonFile<Object>(tsconfigLoc)
+    await writeJsonFile(tsconfigLoc, {
+      ...tsConfig,
+      compilerOptions: {
+        ...tsConfig['compilerOptions'],
+        rootDir: 'src',
+      },
+      references,
+    }, { indent: 2 })
   }
 })()
 
@@ -50,7 +79,7 @@ async function updateManifest (dir: string, manifest: ProjectManifest) {
         scripts.test = 'pnpm run _test'
         scripts._test = `pnpm run tsc && cross-env PNPM_REGISTRY_MOCK_PORT=${port} pnpm run test:e2e`
       } else {
-        scripts.test = 'pnpm run tsc -- --sourceMap && pnpm run _test'
+        scripts.test = 'pnpm run tsc && pnpm run _test'
         scripts._test = `cross-env PNPM_REGISTRY_MOCK_PORT=${port} pnpm run test:e2e`
       }
       break
@@ -59,15 +88,20 @@ async function updateManifest (dir: string, manifest: ProjectManifest) {
         scripts = {
           ...manifest.scripts,
           _test: `cd ../.. && c8 --reporter lcov --reports-dir ${path.join(relative, 'coverage')} ts-node ${path.join(relative, 'test')} --type-check`,
-          test: 'pnpm run tsc -- --sourceMap && pnpm run _test',
+          test: 'pnpm run tsc && pnpm run _test',
         }
       } else {
         scripts = {
           ...manifest.scripts,
-          test: 'pnpm run tsc -- --sourceMap',
+          test: 'pnpm run tsc',
         }
       }
       break
+  }
+  if (manifest.name === '@pnpm/fetch') {
+    scripts.tsc = 'rimraf lib && tsc && cpy src/**/*.d.ts lib'
+  } else {
+    scripts.tsc = 'rimraf lib tsconfig.tsbuildinfo && tsc --build'
   }
   let homepage: string
   let repository: string | { type: 'git', url: string }
@@ -81,6 +115,11 @@ async function updateManifest (dir: string, manifest: ProjectManifest) {
     homepage = `https://github.com/pnpm/pnpm/blob/master/${relative}#readme`
     repository = `https://github.com/pnpm/pnpm/blob/master/${relative}`
   }
+  const files = ['lib', '!*.map']
+  if (manifest.bin) {
+    files.push('bin')
+  }
+  files.sort()
   return {
     ...manifest,
     author: 'Zoltan Kochan <z@kochan.io> (https://www.kochan.io/)',
@@ -90,6 +129,7 @@ async function updateManifest (dir: string, manifest: ProjectManifest) {
     engines: {
       node: '>=10',
     },
+    files,
     homepage,
     license: 'MIT',
     repository,
