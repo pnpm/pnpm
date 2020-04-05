f3311884b2168923ab1dab99f88704e4c7b826a2
Zoltan Kochan <z@kochan.io>
2020-04-06 01:54:17 +0300

    fix: --silent should not print known errors

--- packages/pnpm/src/reporter/silentReporter.ts
+++ packages/pnpm/src/reporter/silentReporter.ts
@@ -8,6 +8,9 @@ export default (
   streamParser.on('data', (obj: LogBase) => {
     if (obj.level !== 'error') return
 
+    // Known errors are not printed
+    if (obj['err'].code?.startsWith('ERR_PNPM_')) return
+
     console.log(obj['err']?.message ?? obj['message']) // tslint:disable-line
     if (obj['err']?.stack) { // tslint:disable-line
       console.log(`\n${obj['err'].stack}`) // tslint:disable-line
