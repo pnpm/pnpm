bc08c94e7cb25fb058b2aaf99de87a4a785a2c49
Zoltan Kochan <z@kochan.io>
2020-04-23 20:46:46 +0300

    test: fix coverage calc on Windows

--- pnpmfile.js
+++ pnpmfile.js
@@ -4,6 +4,9 @@ module.exports = {
       if (pkg.dependencies['@nodelib/fs.walk'] === '^1.1.0') {
         pkg.dependencies['@nodelib/fs.walk'] = '1.1.1'
       }
+      if (pkg.dependencies['istanbul-reports']) {
+        pkg.dependencies['istanbul-reports'] = 'npm:@zkochan/istanbul-reports'
+      }
       if (pkg.name === 'verdaccio') {
         pkg.dependencies['http-errors'] = '^1.7.3'
       }
