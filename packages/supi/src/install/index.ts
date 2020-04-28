c164c1bb6787bd9945d7b8cb10a29a0da3490a4a
Zoltan Kochan <z@kochan.io>
2020-04-29 02:13:05 +0300

    refactor: project up-to-date checking

--- packages/supi/src/install/index.ts
+++ packages/supi/src/install/index.ts
@@ -17,13 +17,11 @@ import {
 } from '@pnpm/lifecycle'
 import linkBins from '@pnpm/link-bins'
 import {
-  Lockfile,
   ProjectSnapshot,
   writeCurrentLockfile,
   writeLockfiles,
   writeWantedLockfile,
 } from '@pnpm/lockfile-file'
-import { satisfiesPackageManifest } from '@pnpm/lockfile-utils'
 import logger, { streamParser } from '@pnpm/logger'
 import { getAllDependenciesFromManifest } from '@pnpm/manifest-utils'
 import { write as writeModulesYaml } from '@pnpm/modules-yaml'
@@ -48,18 +46,17 @@ import rimraf = require('@zkochan/rimraf')
 import * as dp from 'dependency-path'
 import isInnerLink = require('is-inner-link')
 import isSubdir = require('is-subdir')
-import pEvery from 'p-every'
 import pFilter = require('p-filter')
 import pLimit from 'p-limit'
 import path = require('path')
 import R = require('ramda')
-import semver = require('semver')
 import getSpecFromPackageManifest from '../getSpecFromPackageManifest'
 import lock from '../lock'
 import parseWantedDependencies from '../parseWantedDependencies'
 import safeIsInnerLink from '../safeIsInnerLink'
 import removeDeps from '../uninstall/removeDeps'
 import { updateProjectManifest } from '../utils/getPref'
+import allProjectsAreUpToDate from './allProjectsAreUpToDate'
 import extendOptions, {
   InstallOptions,
   StrictInstallOptions,
@@ -517,86 +514,6 @@ function forgetResolutionsOfPrevWantedDeps (importer: ProjectSnapshot, wantedDep
       delete importer.optionalDependencies[alias]
     }
   }
-}
-
-async function linkedPackagesAreUpToDate (
-  manifestsByDir: Record<string, DependencyManifest>,
-  workspacePackages: WorkspacePackages,
-  manifest: ProjectManifest,
-  projectSnapshot: ProjectSnapshot,
-  projectDir: string,
-) {
-  for (const depField of DEPENDENCIES_FIELDS) {
-    const importerDeps = projectSnapshot[depField]
-    const pkgDeps = manifest[depField]
-    if (!importerDeps || !pkgDeps) continue
-    const depNames = Object.keys(importerDeps)
-    for (const depName of depNames) {
-      if (!pkgDeps[depName]) continue
-      const isLinked = importerDeps[depName].startsWith('link:')
-      if (isLinked && (pkgDeps[depName].startsWith('link:') || pkgDeps[depName].startsWith('file:'))) continue
-      const dir = isLinked
-        ? path.join(projectDir, importerDeps[depName].substr(5))
-        : workspacePackages?.[depName]?.[importerDeps[depName]]?.dir
-      if (!dir) continue
-      const linkedPkg = manifestsByDir[dir] || await safeReadPkgFromDir(dir)
-      const availableRange = getVersionRange(pkgDeps[depName])
-      // This should pass the same options to semver as @pnpm/npm-resolver
-      const localPackageSatisfiesRange = availableRange === '*' ||
-        linkedPkg && semver.satisfies(linkedPkg.version, availableRange, { loose: true })
-      if (isLinked !== localPackageSatisfiesRange) return false
-    }
-  }
-  return true
-}
-
-function getVersionRange (spec: string) {
-  if (spec.startsWith('workspace:')) return spec.substr(10)
-  if (spec.startsWith('npm:')) {
-    spec = spec.substr(4)
-    const index = spec.indexOf('@', 1)
-    if (index === -1) return '*'
-    return spec.substr(index + 1) || '*'
-  }
-  return spec
-}
-
-function getWorkspacePackagesByDirectory (workspacePackages: WorkspacePackages) {
-  const workspacePackagesByDirectory = {}
-  Object.keys(workspacePackages || {}).forEach((pkgName) => {
-    Object.keys(workspacePackages[pkgName] || {}).forEach((pkgVersion) => {
-      workspacePackagesByDirectory[workspacePackages[pkgName][pkgVersion].dir] = workspacePackages[pkgName][pkgVersion].manifest
-    })
-  })
-  return workspacePackagesByDirectory
-}
-
-async function allProjectsAreUpToDate (
-  projects: Array<ProjectOptions & { id: string }>,
-  opts: {
-    wantedLockfile: Lockfile,
-    workspacePackages: WorkspacePackages,
-  },
-) {
-  const manifestsByDir = opts.workspacePackages ? getWorkspacePackagesByDirectory(opts.workspacePackages) : {}
-  const _satisfiesPackageManifest = satisfiesPackageManifest.bind(null, opts.wantedLockfile)
-  const _linkedPackagesAreUpToDate = linkedPackagesAreUpToDate.bind(null, manifestsByDir, opts.workspacePackages)
-  return pEvery(projects, async (project) => {
-    const importer = opts.wantedLockfile.importers[project.id]
-    return importer && !hasLocalTarballDepsInRoot(importer) &&
-      _satisfiesPackageManifest(project.manifest, project.id) &&
-      _linkedPackagesAreUpToDate(project.manifest, importer, project.rootDir)
-  })
-}
-
-function hasLocalTarballDepsInRoot (importer: ProjectSnapshot) {
-  return R.any(refIsLocalTarball, Object.values(importer.dependencies ?? {}))
-    || R.any(refIsLocalTarball, Object.values(importer.devDependencies ?? {}))
-    || R.any(refIsLocalTarball, Object.values(importer.optionalDependencies ?? {}))
-}
-
-function refIsLocalTarball (ref: string) {
-  return ref.startsWith('file:') && (ref.endsWith('.tgz') || ref.endsWith('.tar.gz') || ref.endsWith('.tar'))
 }
 
 export async function addDependenciesToPackage (
