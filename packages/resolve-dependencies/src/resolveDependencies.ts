pnpm/pnpm

commit b5f66c0f2bd225f44e46cbd0892bb973d9500d08

perf: less dirs in the virtual store directory
Reduce the number of directories in the virtual store directory.
Don't create a subdirectory for the package version. Append the
package version to the package name directory.

close #2521
PR #2531

pnpm-lock.yaml
@@ -604,7 +604,7 @@ importers:
       '@pnpm/modules-cleaner': 'link:../modules-cleaner'
       '@pnpm/modules-yaml': 'link:../modules-yaml'
       '@pnpm/package-requester': 'link:../package-requester'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/read-package-json': 'link:../read-package-json'
       '@pnpm/read-project-manifest': 'link:../read-project-manifest'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
@@ -664,7 +664,7 @@ importers:
       '@pnpm/modules-yaml': 'workspace:6.0.2'
       '@pnpm/package-requester': 'workspace:12.0.0-alpha.2'
       '@pnpm/package-store': 'workspace:*'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/read-package-json': 'workspace:3.1.0'
       '@pnpm/read-project-manifest': 'workspace:1.0.5'
       '@pnpm/read-projects-context': 'workspace:*'
@@ -703,7 +703,7 @@ importers:
       '@pnpm/lockfile-types': 'link:../lockfile-types'
       '@pnpm/lockfile-utils': 'link:../lockfile-utils'
       '@pnpm/lockfile-walker': 'link:../lockfile-walker'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/symlink-dependency': 'link:../symlink-dependency'
       '@pnpm/types': 'link:../types'
       dependency-path: 'link:../dependency-path'
@@ -718,7 +718,7 @@ importers:
       '@pnpm/lockfile-utils': 'workspace:2.0.11'
       '@pnpm/lockfile-walker': 'workspace:2.0.2'
       '@pnpm/logger': 3.2.2
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/symlink-dependency': 'workspace:3.0.4'
       '@pnpm/types': 'workspace:5.0.0'
       '@types/ramda': ^0.27.4
@@ -991,6 +991,7 @@ importers:
       '@pnpm/lockfile-types': 'link:../lockfile-types'
       '@pnpm/lockfile-utils': 'link:../lockfile-utils'
       '@pnpm/package-bins': 'link:../package-bins'
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/read-modules-dir': 'link:../read-modules-dir'
       '@pnpm/read-package-json': 'link:../read-package-json'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
@@ -1013,6 +1014,7 @@ importers:
       '@pnpm/logger': 3.2.2
       '@pnpm/modules-cleaner': 'link:'
       '@pnpm/package-bins': 'workspace:4.0.3'
+      '@pnpm/pkgid-to-filename': ^3.0.0-0
       '@pnpm/read-modules-dir': 'workspace:2.0.1'
       '@pnpm/read-package-json': 'workspace:3.1.0'
       '@pnpm/store-controller-types': 'workspace:8.0.0-alpha.2'
@@ -1219,7 +1221,7 @@ importers:
       '@pnpm/cafs': 'link:../cafs'
       '@pnpm/core-loggers': 'link:../core-loggers'
       '@pnpm/fetcher-base': 'link:../fetcher-base'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/read-package-json': 'link:../read-package-json'
       '@pnpm/resolver-base': 'link:../resolver-base'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
@@ -1261,7 +1263,7 @@ importers:
       '@pnpm/logger': 3.2.2
       '@pnpm/npm-resolver': 'workspace:*'
       '@pnpm/package-requester': 'link:'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/read-package-json': 'workspace:3.1.0'
       '@pnpm/resolver-base': 'workspace:7.0.0'
       '@pnpm/store-controller-types': 'workspace:8.0.0-alpha.2'
@@ -1297,7 +1299,7 @@ importers:
       '@pnpm/fetcher-base': 'link:../fetcher-base'
       '@pnpm/fs-locker': 3.0.1
       '@pnpm/package-requester': 'link:../package-requester'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/resolver-base': 'link:../resolver-base'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
       '@pnpm/types': 'link:../types'
@@ -1333,7 +1335,7 @@ importers:
       '@pnpm/npm-resolver': 'workspace:*'
       '@pnpm/package-requester': 'workspace:12.0.0-alpha.2'
       '@pnpm/package-store': 'link:'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/resolver-base': 'workspace:7.0.0'
       '@pnpm/store-controller-types': 'workspace:8.0.0-alpha.2'
       '@pnpm/tarball-fetcher': 'workspace:*'
@@ -1807,7 +1809,7 @@ importers:
       '@pnpm/lockfile-walker': 'link:../lockfile-walker'
       '@pnpm/modules-yaml': 'link:../modules-yaml'
       '@pnpm/normalize-registries': 'link:../normalize-registries'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/sort-packages': 'link:../sort-packages'
       '@pnpm/store-connection-manager': 'link:../store-connection-manager'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
@@ -1855,7 +1857,7 @@ importers:
       '@pnpm/logger': ^3.2.2
       '@pnpm/modules-yaml': 'workspace:6.0.2'
       '@pnpm/normalize-registries': 'workspace:1.0.0'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/plugin-commands-rebuild': 'link:'
       '@pnpm/prepare': 'workspace:0.0.1'
       '@pnpm/sort-packages': 'workspace:1.0.9'
@@ -2004,6 +2006,7 @@ importers:
       '@pnpm/normalize-registries': 'link:../normalize-registries'
       '@pnpm/parse-wanted-dependency': 'link:../parse-wanted-dependency'
       '@pnpm/pick-registry-for-package': 'link:../pick-registry-for-package'
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/store-connection-manager': 'link:../store-connection-manager'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
       '@pnpm/store-path': 4.0.0-0
@@ -2047,6 +2050,7 @@ importers:
       '@pnpm/normalize-registries': 'workspace:1.0.0'
       '@pnpm/parse-wanted-dependency': 'workspace:1.0.0'
       '@pnpm/pick-registry-for-package': 'workspace:1.0.0'
+      '@pnpm/pkgid-to-filename': ^3.0.0-0
       '@pnpm/plugin-commands-store': 'link:'
       '@pnpm/prepare': 'workspace:0.0.1'
       '@pnpm/store-connection-manager': 'workspace:0.3.0-alpha.2'
@@ -2566,7 +2570,7 @@ importers:
       '@pnpm/normalize-registries': 'link:../normalize-registries'
       '@pnpm/package-requester': 'link:../package-requester'
       '@pnpm/parse-wanted-dependency': 'link:../parse-wanted-dependency'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/prune-lockfile': 'link:../prune-lockfile'
       '@pnpm/read-modules-dir': 'link:../read-modules-dir'
       '@pnpm/read-package-json': 'link:../read-package-json'
@@ -2671,7 +2675,7 @@ importers:
       '@pnpm/package-requester': 'workspace:12.0.0-alpha.2'
       '@pnpm/package-store': 'workspace:*'
       '@pnpm/parse-wanted-dependency': 'workspace:1.0.0'
-      '@pnpm/pkgid-to-filename': 2.0.0
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/prepare': 'workspace:0.0.1'
       '@pnpm/prune-lockfile': 'workspace:2.0.7'
       '@pnpm/read-modules-dir': 'workspace:2.0.1'
@@ -3430,14 +3434,14 @@ packages:
       node: '>=10'
     resolution:
       integrity: sha512-/hQK/HSxnqPavF/Wc1YanCtcrYnpLVEqv3fcHJ5SQmKbcaoIVHeUnBsI2Ep6JZiTTYxCfkzRw9c6mr9sn3Uk3w==
-  /@pnpm/pkgid-to-filename/2.0.0:
+  /@pnpm/pkgid-to-filename/3.0.0-0:
     dependencies:
       normalize-path: 3.0.0
     dev: false
     engines:
-      node: '>=6'
+      node: '>=10.13'
     resolution:
-      integrity: sha512-5cf9ik7ntSpPYLXFPs8eoCNgXuDVqa58/9P0Hrb6KmsVpchKi72Xh5TRd6dctqH4mJtfK0eVRaSt4+VDyGxDkQ==
+      integrity: sha512-DojhkQRsAUqo39xnHqVUwMh3zIMRbMoSzv2AwT/+XOlpVPQbFCq7pOmQYs+sf78cDt06gTKJMaaO/gkMY6GsxQ==
   /@pnpm/registry-mock/2.0.1:
     dependencies:
       anonymous-npm-registry-client: 0.1.2

packages/constants/src/index.ts
@@ -2,6 +2,6 @@ export const WANTED_LOCKFILE = 'pnpm-lock.yaml'
 export const LOCKFILE_VERSION = 5.1
 
 export const ENGINE_NAME = `${process.platform}-${process.arch}-node-${process.version.split('.')[0]}`
-export const LAYOUT_VERSION = 3
+export const LAYOUT_VERSION = 4
 
 export const WORKSPACE_MANIFEST_FILENAME = 'pnpm-workspace.yaml'

packages/headless/test/index.ts
@@ -43,7 +43,7 @@ test('installing a simple project', async (t) => {
   t.ok(project.requireModule('colors'), 'optional dep installed')
 
   // test that independent leaves is false by default
-  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors`) // colors is not symlinked from the store
+  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors@1.2.0`) // colors is not symlinked from the store
 
   await project.isExecutable('.bin/rimraf')
 
@@ -274,10 +274,10 @@ test('installing with independent-leaves and hoistPattern=*', async (t) => {
   await project.has('.pnpm/node_modules/path-is-absolute')
 
   // wrappy is linked directly from the store
-  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/wrappy/1.0.2`)
+  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/wrappy@1.0.2`)
   await project.storeHas('wrappy', '1.0.2')
 
-  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/rimraf/2.5.1`)
+  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/rimraf@2.5.1`)
 
   await project.isExecutable('.bin/rimraf')
 
@@ -556,8 +556,8 @@ test('independent-leaves: installing a simple project', async (t) => {
   t.ok(project.requireModule('rimraf'), 'prod dep installed')
   t.ok(project.requireModule('is-negative'), 'dev dep installed')
   t.ok(project.requireModule('colors'), 'optional dep installed')
-  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/rimraf`) // rimraf is not symlinked from the store
-  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors`) // colors is symlinked from the store
+  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/rimraf@2.7.1`) // rimraf is not symlinked from the store
+  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors@1.2.0`) // colors is symlinked from the store
 
   await project.isExecutable('.bin/rimraf')
 
@@ -605,7 +605,7 @@ test('installing with hoistPattern=*', async (t) => {
   t.ok(project.requireModule('colors'), 'optional dep installed')
 
   // test that independent leaves is false by default
-  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors`) // colors is not symlinked from the store
+  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors@1.2.0`) // colors is not symlinked from the store
 
   await project.isExecutable('.bin/rimraf')
   await project.isExecutable('.pnpm/node_modules/.bin/hello-world-js-bin')
@@ -665,7 +665,7 @@ test('installing with hoistPattern=* and shamefullyHoist=true', async (t) => {
   t.ok(project.requireModule('colors'), 'optional dep installed')
 
   // test that independent leaves is false by default
-  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors`) // colors is not symlinked from the store
+  await project.has(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/colors@1.2.0`) // colors is not symlinked from the store
 
   await project.isExecutable('.bin/rimraf')
   await project.isExecutable('.bin/hello-world-js-bin')
@@ -725,7 +725,7 @@ test.skip('using side effects cache', async (t) => {
   }, {}, {}, { packageImportMethod: 'copy' })
   await headless(opts)
 
-  const cacheIntegrityPath = path.join(opts.storeDir, `localhost+${REGISTRY_MOCK_PORT}/diskusage/1.1.3/side_effects/${ENGINE_DIR}/integrity.json`)
+  const cacheIntegrityPath = path.join(opts.storeDir, `localhost+${REGISTRY_MOCK_PORT}/diskusage@1.1.3/side_effects/${ENGINE_DIR}/integrity.json`)
   const cacheIntegrity = await loadJsonFile(cacheIntegrityPath)
   t.ok(cacheIntegrity['build/Makefile'])
   delete cacheIntegrity['build/Makefile']
@@ -771,7 +771,7 @@ test.skip('using side effects cache and hoistPattern=*', async (t) => {
   const project = assertProject(t, lockfileDir)
   await project.has('.pnpm/node_modules/es6-promise') // verifying that a flat node_modules was created
 
-  const cacheBuildDir = path.join(opts.storeDir, `localhost+${REGISTRY_MOCK_PORT}/diskusage/1.1.3/side_effects/${ENGINE_DIR}/package/build`)
+  const cacheBuildDir = path.join(opts.storeDir, `localhost+${REGISTRY_MOCK_PORT}/diskusage@1.1.3/side_effects/${ENGINE_DIR}/package/build`)
   fse.writeFileSync(path.join(cacheBuildDir, 'new-file.txt'), 'some new content')
 
   await rimraf(path.join(lockfileDir, 'node_modules'))
@@ -853,7 +853,7 @@ test('independent-leaves: installing in a workspace', async (t) => {
   const projectBar = assertProject(t, path.join(workspaceFixture, 'bar'))
 
   await projectBar.has('foo')
-  t.ok(await exists(path.join(workspaceFixture, `node_modules/.pnpm/localhost+${REGISTRY_MOCK_PORT}/express/4.16.4/node_modules/array-flatten`)), 'independent package linked')
+  t.ok(await exists(path.join(workspaceFixture, `node_modules/.pnpm/localhost+${REGISTRY_MOCK_PORT}/express@4.16.4/node_modules/array-flatten`)), 'independent package linked')
 
   t.end()
 })

packages/modules-cleaner/src/prune.ts
@@ -10,6 +10,7 @@ import {
 } from '@pnpm/lockfile-types'
 import { packageIdFromSnapshot } from '@pnpm/lockfile-utils'
 import logger from '@pnpm/logger'
+import pkgIdToFilename from '@pnpm/pkgid-to-filename'
 import readModulesDir from '@pnpm/read-modules-dir'
 import { StoreController } from '@pnpm/store-controller-types'
 import {
@@ -140,7 +141,7 @@ export default async function prune (
       }
 
       await Promise.all(orphanDepPaths.map(async (orphanDepPath) => {
-        const pathToRemove = path.join(opts.virtualStoreDir, orphanDepPath, 'node_modules')
+        const pathToRemove = path.join(opts.virtualStoreDir, pkgIdToFilename(orphanDepPath, opts.lockfileDir), 'node_modules')
         removalLogger.debug(pathToRemove)
         try {
           await vacuum(pathToRemove, {

packages/package-requester/src/packageRequester.ts
@@ -496,7 +496,7 @@ function fetchToStore (
         await fs.writeFile(path.join(target, TARBALL_INTEGRITY_FILENAME), opts.resolution['integrity'], 'utf8') // tslint:disable-line:no-string-literal
       }
 
-      ctx.storeIndex[targetRelative] = ctx.storeIndex[targetRelative] || []
+      ctx.storeIndex[opts.pkgId] = ctx.storeIndex[opts.pkgId] || []
       files.resolve({
         filesIndex: integrity,
         fromStore: false,

packages/package-requester/test/index.ts
@@ -4,6 +4,7 @@ import localResolver from '@pnpm/local-resolver'
 import { streamParser } from '@pnpm/logger'
 import createResolver from '@pnpm/npm-resolver'
 import createPackageRequester, { PackageFilesResponse, PackageResponse } from '@pnpm/package-requester'
+import pkgIdToFilename from '@pnpm/pkgid-to-filename'
 import { ResolveFunction } from '@pnpm/resolver-base'
 import createFetcher from '@pnpm/tarball-fetcher'
 import { DependencyManifest } from '@pnpm/types'
@@ -62,7 +63,7 @@ test('request package', async t => {
 
   t.equal(pkgResponse.body.id, 'registry.npmjs.org/is-positive/1.0.0', 'responded with correct package ID')
   t.equal(pkgResponse.body.resolvedVia, 'npm-registry', 'responded with correct resolvedVia')
-  t.equal(pkgResponse.body.inStoreLocation, path.join(storeDir, 'registry.npmjs.org', 'is-positive', '1.0.0'), 'package location in store returned')
+  t.equal(pkgResponse.body.inStoreLocation, path.join(storeDir, 'registry.npmjs.org', 'is-positive@1.0.0'), 'package location in store returned')
   t.equal(pkgResponse.body.isLocal, false, 'package is not local')
   t.equal(typeof pkgResponse.body.latest, 'string', 'latest is returned')
   t.equal(pkgResponse.body.manifest.name, 'is-positive', 'package manifest returned')
@@ -108,7 +109,7 @@ test('request package but skip fetching', async t => {
   t.ok(pkgResponse.body, 'response has body')
 
   t.equal(pkgResponse.body.id, 'registry.npmjs.org/is-positive/1.0.0', 'responded with correct package ID')
-  t.equal(pkgResponse.body.inStoreLocation, path.join('.store', 'registry.npmjs.org', 'is-positive', '1.0.0'), 'package location in store returned')
+  t.equal(pkgResponse.body.inStoreLocation, path.join('.store', 'registry.npmjs.org', 'is-positive@1.0.0'), 'package location in store returned')
   t.equal(pkgResponse.body.isLocal, false, 'package is not local')
   t.equal(typeof pkgResponse.body.latest, 'string', 'latest is returned')
   t.equal(pkgResponse.body.manifest.name, 'is-positive', 'package manifest returned')
@@ -163,7 +164,7 @@ test('request package but skip fetching, when resolution is already available', 
   t.ok(pkgResponse.body, 'response has body')
 
   t.equal(pkgResponse.body.id, 'registry.npmjs.org/is-positive/1.0.0', 'responded with correct package ID')
-  t.equal(pkgResponse.body.inStoreLocation, path.join('.store', 'registry.npmjs.org', 'is-positive', '1.0.0'), 'package location in store returned')
+  t.equal(pkgResponse.body.inStoreLocation, path.join('.store', 'registry.npmjs.org', 'is-positive@1.0.0'), 'package location in store returned')
   t.equal(pkgResponse.body.isLocal, false, 'package is not local')
   t.equal(typeof pkgResponse.body.latest, 'string', 'latest is returned')
   t.equal(pkgResponse.body.manifest.name, 'is-positive', 'package manifest returned')
@@ -717,7 +718,7 @@ test('refetch package to store if it has been modified', async (t) => {
 
   t.ok(reporter.calledWithMatch({
     level: 'warn',
-    message: `Refetching ${path.join(storeDir, pkgId)} to store. It was either modified or had no integrity checksums`,
+    message: `Refetching ${path.join(storeDir, pkgIdToFilename(pkgId, process.cwd()))} to store. It was either modified or had no integrity checksums`,
     name: 'pnpm:package-requester',
     prefix: lockfileDir,
   }), 'refetch logged')

packages/plugin-commands-rebuild/test/index.ts
@@ -1,26 +1,26 @@
 ///<reference path="../../../typings/index.d.ts" />
 import { WANTED_LOCKFILE } from '@pnpm/constants'
 import { rebuild } from '@pnpm/plugin-commands-rebuild'
-import prepare, { prepareEmpty, preparePackages } from '@pnpm/prepare'
+import prepare, { prepareEmpty } from '@pnpm/prepare'
 import { REGISTRY_MOCK_PORT } from '@pnpm/registry-mock'
 import { copyFixture } from '@pnpm/test-fixtures'
-import { PackageManifest } from '@pnpm/types'
 import execa = require('execa')
 import path = require('path')
 import exists = require('path-exists')
 import sinon = require('sinon')
 import test = require('tape')
-import { promisify } from 'util'
 import './recursive'
 import { DEFAULT_OPTS } from './utils'
 
 const REGISTRY = `http://localhost:${REGISTRY_MOCK_PORT}/`
+const pnpmBin = path.join(__dirname, '../../pnpm/bin/pnpm.js')
 
 test('rebuilds dependencies', async (t) => {
   const project = prepareEmpty(t)
   const storeDir = path.resolve('store')
 
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     '--save-dev',
     'pre-and-postinstall-scripts-example',
@@ -75,7 +75,8 @@ test('rebuild does not fail when a linked package is present', async (t) => {
   const storeDir = path.resolve('store')
   await copyFixture('local-pkg', path.resolve('..', 'local-pkg'))
 
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     'link:../local-pkg',
     'is-positive',
@@ -101,7 +102,8 @@ test('rebuild does not fail when a linked package is present', async (t) => {
 test('rebuilds specific dependencies', async (t) => {
   const project = prepareEmpty(t)
   const storeDir = path.resolve('store')
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     '--save-dev',
     'pre-and-postinstall-scripts-example',
@@ -134,7 +136,8 @@ test('rebuilds specific dependencies', async (t) => {
 test('rebuild with pending option', async (t) => {
   const project = prepareEmpty(t)
   const storeDir = path.resolve('store')
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     'pre-and-postinstall-scripts-example',
     '--registry',
@@ -143,7 +146,8 @@ test('rebuild with pending option', async (t) => {
     storeDir,
     '--ignore-scripts',
   ])
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     'zkochan/install-scripts-example',
     '--registry',
@@ -198,7 +202,8 @@ test('rebuild dependencies in correct order', async (t) => {
   const project = prepareEmpty(t)
   const storeDir = path.resolve('store')
 
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     'with-postinstall-a',
     '--registry',
@@ -212,7 +217,7 @@ test('rebuild dependencies in correct order', async (t) => {
   t.ok(modules)
   t.doesNotEqual(modules!.pendingBuilds.length, 0)
 
-  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b/1.0.0/node_modules/with-postinstall-b/output.json`)
+  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b@1.0.0/node_modules/with-postinstall-b/output.json`)
   await project.hasNot('with-postinstall-a/output.json')
 
   await rebuild.handler({
@@ -226,7 +231,7 @@ test('rebuild dependencies in correct order', async (t) => {
   t.ok(modules)
   t.equal(modules!.pendingBuilds.length, 0)
 
-  t.ok(+project.requireModule(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b/1.0.0/node_modules/with-postinstall-b/output.json`)[0] < +project.requireModule('with-postinstall-a/output.json')[0])
+  t.ok(+project.requireModule(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b@1.0.0/node_modules/with-postinstall-b/output.json`)[0] < +project.requireModule('with-postinstall-a/output.json')[0])
   t.end()
 })
 
@@ -234,7 +239,8 @@ test('rebuild dependencies in correct order when node_modules uses independent-l
   const project = prepareEmpty(t)
   const storeDir = path.resolve('store')
 
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     'with-postinstall-a',
     '--registry',
@@ -250,7 +256,7 @@ test('rebuild dependencies in correct order when node_modules uses independent-l
   t.ok(modules)
   t.doesNotEqual(modules!.pendingBuilds.length, 0)
 
-  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b/1.0.0/node_modules/with-postinstall-b/output.json`)
+  await project.hasNot(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b@1.0.0/node_modules/with-postinstall-b/output.json`)
   await project.hasNot('with-postinstall-a/output.json')
 
   await rebuild.handler({
@@ -265,7 +271,7 @@ test('rebuild dependencies in correct order when node_modules uses independent-l
   t.ok(modules)
   t.equal(modules!.pendingBuilds.length, 0)
 
-  t.ok(+project.requireModule(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b/1.0.0/node_modules/with-postinstall-b/output.json`)[0] < +project.requireModule('with-postinstall-a/output.json')[0])
+  t.ok(+project.requireModule(`.pnpm/localhost+${REGISTRY_MOCK_PORT}/with-postinstall-b@1.0.0/node_modules/with-postinstall-b/output.json`)[0] < +project.requireModule('with-postinstall-a/output.json')[0])
   t.end()
 })
 
@@ -273,7 +279,8 @@ test('rebuild links bins', async (t) => {
   const project = prepareEmpty(t)
   const storeDir = path.resolve('store')
 
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'add',
     'has-generated-bins-as-dep',
     'generated-bins',
@@ -316,7 +323,8 @@ test(`rebuild should not fail on incomplete ${WANTED_LOCKFILE}`, async (t) => {
   })
   const storeDir = path.resolve('store')
 
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'install',
     '--registry',
     REGISTRY,

packages/plugin-commands-rebuild/test/recursive.ts
@@ -8,6 +8,8 @@ import test = require('tape')
 import writeYamlFile = require('write-yaml-file')
 import { DEFAULT_OPTS, REGISTRY } from './utils'
 
+const pnpmBin = path.join(__dirname, '../../pnpm/bin/pnpm.js')
+
 test('pnpm recursive rebuild', async (t) => {
   const projects = preparePackages(t, [
     {
@@ -29,7 +31,8 @@ test('pnpm recursive rebuild', async (t) => {
   ])
 
   const { allProjects, selectedProjectsGraph } = await readProjects(process.cwd(), [])
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'install',
     '-r',
     '--registry',
@@ -109,7 +112,8 @@ test.skip('rebuild multiple packages in correct order', async (t) => {
   await writeYamlFile('pnpm-workspace.yaml', { packages: ['project-1'] })
 
   const { allProjects, selectedProjectsGraph } = await readProjects(process.cwd(), [])
-  await execa('pnpm', [
+  await execa('node', [
+    pnpmBin,
     'install',
     '-r',
     '--registry',

commit 0730bb93814f0e70e561b02c41a4b2071ebef9a2

fix: check the existense of a dep at the right dir
fixes a performance regression caused by #2531

packages/resolve-dependencies/src/resolveDependencies.ts
@@ -17,6 +17,7 @@ import {
 import logger from '@pnpm/logger'
 import packageIsInstallable from '@pnpm/package-is-installable'
 import pickRegistryForPackage from '@pnpm/pick-registry-for-package'
+import pkgIdToFilename from '@pnpm/pkgid-to-filename'
 import {
   DirectoryResolution,
   PreferredVersions,
@@ -503,7 +504,7 @@ async function resolveDependency (
     // we can safely assume that it doesn't exist in `node_modules`
     currentLockfileContainsTheDep &&
     options.relDepPath && options.dependencyLockfile &&
-    await exists(path.join(ctx.virtualStoreDir, `${options.depPath}/node_modules/${nameVerFromPkgSnapshot(options.relDepPath, options.dependencyLockfile).name}/package.json`)) &&
+    await exists(path.join(ctx.virtualStoreDir, `${pkgIdToFilename(options.depPath, ctx.prefix)}/node_modules/${nameVerFromPkgSnapshot(options.relDepPath, options.dependencyLockfile).name}/package.json`)) &&
     (options.currentDepth > 0 || wantedDependency.alias && await exists(path.join(ctx.modulesDir, wantedDependency.alias))))
 
   if (!proceed && depIsLinked) {

pnpm-lock.yaml
@@ -2385,6 +2385,7 @@ importers:
       '@pnpm/npm-resolver': 'link:../npm-resolver'
       '@pnpm/package-is-installable': 'link:../package-is-installable'
       '@pnpm/pick-registry-for-package': 'link:../pick-registry-for-package'
+      '@pnpm/pkgid-to-filename': 3.0.0-0
       '@pnpm/resolver-base': 'link:../resolver-base'
       '@pnpm/store-controller-types': 'link:../store-controller-types'
       '@pnpm/types': 'link:../types'
@@ -2407,6 +2408,7 @@ importers:
       '@pnpm/npm-resolver': 'workspace:8.0.0-alpha.1'
       '@pnpm/package-is-installable': 'workspace:4.0.7'
       '@pnpm/pick-registry-for-package': 'workspace:1.0.0'
+      '@pnpm/pkgid-to-filename': ^3.0.0-0
       '@pnpm/resolve-dependencies': 'link:'
       '@pnpm/resolver-base': 'workspace:7.0.0'
       '@pnpm/store-controller-types': 'workspace:8.0.0-alpha.2'