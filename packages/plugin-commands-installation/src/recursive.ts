ecfd7f524054f4b8bcd3e23081f49a9ada9caf3b
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2020-05-05 12:46:31 -0700

    Allow workspaces to reference packages that are not under the directory containing pnpm-workspace.yaml

--- packages/plugin-commands-installation/src/recursive.ts
+++ packages/plugin-commands-installation/src/recursive.ts
@@ -18,10 +18,7 @@ import {
   ProjectManifest,
 } from '@pnpm/types'
 import camelcaseKeys = require('camelcase-keys')
-import isSubdir = require('is-subdir')
 import mem = require('mem')
-import fs = require('mz/fs')
-import pFilter = require('p-filter')
 import pLimit from 'p-limit'
 import path = require('path')
 import R = require('ramda')
@@ -171,8 +168,6 @@ export default async function recursive (
   // For a workspace with shared lockfile
   if (opts.lockfileDir && ['add', 'install', 'remove', 'update'].includes(cmdFullName)) {
     let importers = await getImporters()
-    const isFromWorkspace = isSubdir.bind(null, opts.workspaceDir)
-    importers = await pFilter(importers, async ({ rootDir }: { rootDir: string }) => isFromWorkspace(await fs.realpath(rootDir)))
     if (importers.length === 0) return true
     const hooks = opts.ignorePnpmfile ? {} : requireHooks(opts.lockfileDir, opts)
     const mutation = cmdFullName === 'remove' ? 'uninstallSome' : (params.length === 0 && !updateToLatest ? 'install' : 'installSome')
