86d21759da65326d24f77815a1af25be40e6013e
Zoltan Kochan <z@kochan.io>
2020-06-01 20:50:03 +0300

    fix: print a meaningful error on Node.js v13.6

---
+++ .changeset/perfect-singers-unite.md
@@ -0,0 +1,5 @@
+---
+"pnpm": patch
+---
+
+Print a meaningful error when pnpm is executed with Node.js v13.0-v13.6
--- packages/pnpm/bin/pnpm.js
+++ packages/pnpm/bin/pnpm.js
@@ -5,6 +5,10 @@ if (major < 10 || major == 10 && minor < 13) {
   console.log(`ERROR: This version of pnpm requires at least Node.js v10.13
 The current version of Node.js is ${process.version}`)
   process.exit(1)
+} else if (major == 13 && minor < 7) {
+  console.log(`ERROR: This version of pnpm requires at least Node.js v13.7
+The current version of Node.js is ${process.version}`)
+  process.exit(1)
 }
 
 require('../lib/bin/pnpm')
