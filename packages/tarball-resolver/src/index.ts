b3d01e1992b545908c97692161bc170341a253a2
Zoltan Kochan <z@kochan.io>
2020-07-07 02:26:27 +0300

    fix: install git-hosted dep through repo URL
    close #2627

--- packages/tarball-resolver/src/index.ts
+++ packages/tarball-resolver/src/index.ts
@@ -1,9 +1,20 @@
 import { ResolveResult } from '@pnpm/resolver-base'
 
+const GIT_HOSTERS = new Set([
+  'github.com',
+  'gitlab.com',
+  'bitbucket.org',
+])
+
 export default async function resolveTarball (
   wantedDependency: {pref: string}
 ): Promise<ResolveResult | null> {
   if (!wantedDependency.pref.startsWith('http:') && !wantedDependency.pref.startsWith('https:')) {
+    return null
+  }
+
+  const parts = wantedDependency.pref.split('/')
+  if (parts.length === 5 && GIT_HOSTERS.has(parts[2])) {
     return null
   }
 
