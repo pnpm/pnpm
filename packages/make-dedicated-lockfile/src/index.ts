664bc08a99ca2a30bbc3117d481a47beda64f029
Zoltan Kochan <z@kochan.io>
2020-08-04 01:57:04 +0300

    refactor

--- packages/make-dedicated-lockfile/src/index.ts
+++ packages/make-dedicated-lockfile/src/index.ts
@@ -2,14 +2,15 @@ import pnpmExec from '@pnpm/exec'
 import exportableManifest from '@pnpm/exportable-manifest'
 import {
   getLockfileImporterId,
+  ProjectSnapshot,
   readWantedLockfile,
   writeWantedLockfile,
 } from '@pnpm/lockfile-file'
 import { pruneSharedLockfile } from '@pnpm/prune-lockfile'
 import readProjectManifest from '@pnpm/read-project-manifest'
 import { DEPENDENCIES_FIELDS } from '@pnpm/types'
-import fs = require('mz/fs')
 import path = require('path')
+import R = require('ramda')
 import renameOverwrite = require('rename-overwrite')
 
 export default async function (lockfileDir: string, projectDir: string) {
@@ -17,37 +18,29 @@ export default async function (lockfileDir: string, projectDir: string) {
   if (!lockfile) {
     throw new Error('no lockfile found')
   }
-  const oldImporters = lockfile.importers
+  const allImporters = lockfile.importers
   lockfile.importers = {}
   const baseImporterId = getLockfileImporterId(lockfileDir, projectDir)
-  for (const importerId of Object.keys(oldImporters)) {
+  for (const [importerId, importer] of Object.entries(allImporters)) {
     if (importerId.startsWith(`${baseImporterId}/`)) {
-      lockfile.importers[importerId.substr(baseImporterId.length + 1)] = oldImporters[importerId]
+      const newImporterId = importerId.substr(baseImporterId.length + 1)
+      lockfile.importers[newImporterId] = projectSnapshotWithoutLinkedDeps(importer)
       continue
     }
     if (importerId === baseImporterId) {
-      lockfile.importers['.'] = oldImporters[importerId]
+      lockfile.importers['.'] = projectSnapshotWithoutLinkedDeps(importer)
     }
   }
   const dedicatedLockfile = pruneSharedLockfile(lockfile)
 
-  for (const importerId of Object.keys(dedicatedLockfile.importers)) {
-    for (const depField of DEPENDENCIES_FIELDS) {
-      if (!dedicatedLockfile.importers[importerId][depField]) continue
-      for (const depName of Object.keys(dedicatedLockfile.importers[importerId][depField]!)) {
-        if (dedicatedLockfile.importers[importerId][depField]![depName].startsWith('link:')) {
-          delete dedicatedLockfile.importers[importerId][depField]![depName]
-        }
-      }
-    }
-  }
-
   await writeWantedLockfile(projectDir, dedicatedLockfile)
   const modulesDir = path.join(projectDir, 'node_modules')
-  const tempModulesDir = path.join(projectDir, 'test/.tmp_node_modules')
+  const tmp = path.join(projectDir, 'tmp_node_modules')
+  const tempModulesDir = path.join(projectDir, 'node_modules/.tmp')
   let modulesRenamed = false
   try {
-    await renameOverwrite(modulesDir, tempModulesDir)
+    await renameOverwrite(modulesDir, tmp)
+    await renameOverwrite(tmp, tempModulesDir)
     modulesRenamed = true
   } catch (err) {
     if (err['code'] !== 'ENOENT') throw err
@@ -71,8 +64,23 @@ export default async function (lockfileDir: string, projectDir: string) {
     throw err
   } finally {
     if (modulesRenamed) {
-      await renameOverwrite(tempModulesDir, modulesDir)
+      await renameOverwrite(tempModulesDir, tmp)
+      await renameOverwrite(tmp, modulesDir)
     }
     await writeProjectManifest(manifest)
   }
+}
+
+function projectSnapshotWithoutLinkedDeps (projectSnapshot: ProjectSnapshot) {
+  const newProjectSnapshot: ProjectSnapshot = {
+    specifiers: projectSnapshot.specifiers,
+  }
+  for (const depField of DEPENDENCIES_FIELDS) {
+    if (!projectSnapshot[depField]) continue
+    newProjectSnapshot[depField] = R.fromPairs(
+      Object.entries(projectSnapshot[depField]!)
+        .filter(([depName, depVersion]) => !depVersion.startsWith('link:'))
+    )
+  }
+  return newProjectSnapshot
 }
