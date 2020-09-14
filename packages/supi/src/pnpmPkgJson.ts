fdea0ddc15381d2a55f3b561cc3ee31e188a1b78
Zoltan Kochan <z@kochan.io>
2020-09-15 02:10:16 +0300

    refactor: remove redundant files from supi

--- packages/supi/src/pnpmPkgJson.ts
+++
@@ -1,5 +0,0 @@
-import { PackageManifest } from '@pnpm/types'
-import { sync as loadJsonFileSync } from 'load-json-file'
-import path = require('path')
-
-export default loadJsonFileSync<PackageManifest>(path.resolve(__dirname, '../package.json'))
