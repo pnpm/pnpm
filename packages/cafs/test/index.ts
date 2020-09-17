0fb7b8d699995d1a397ef05cd4cab637d8933b26
Juan Picado <juanpicado19@gmail.com>
2020-09-17 07:25:44 +0200

    refactor: migrate carfs to jest

--- packages/cafs/test/index.ts
+++ packages/cafs/test/index.ts
@@ -1,15 +1,17 @@
 import createCafs from '../src'
 import fs = require('fs')
 import path = require('path')
-import test = require('tape')
 import tempy = require('tempy')
 
-test('unpack', async (t) => {
-  const dest = tempy.directory()
-  t.comment(dest)
-  const cafs = createCafs(dest)
-  await cafs.addFilesFromTarball(
-    fs.createReadStream(path.join(__dirname, '../__fixtures__/node-gyp-6.1.0.tgz'))
-  )
-  t.end()
+describe('unpack', () => {
+  test('unpack', async () => {
+    const dest = tempy.directory()
+    // t.comment(dest)
+    console.log(test)
+    const cafs = createCafs(dest)
+    await cafs.addFilesFromTarball(
+      fs.createReadStream(path.join(__dirname, '../__fixtures__/node-gyp-6.1.0.tgz'))
+    )
+    //  t.end()
+  })
 })
