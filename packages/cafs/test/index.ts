923e11b73b7a2118a1950058eb394211baf55b16
Juan Picado <juanpicado19@gmail.com>
2020-09-18 08:07:49 +0200

    chore: add expectation

--- packages/cafs/test/index.ts
+++ packages/cafs/test/index.ts
@@ -6,12 +6,10 @@ import tempy = require('tempy')
 describe('unpack', () => {
   test('unpack', async () => {
     const dest = tempy.directory()
-    // t.comment(dest)
-    console.log(test)
     const cafs = createCafs(dest)
-    await cafs.addFilesFromTarball(
+    const listOfFiles = await cafs.addFilesFromTarball(
       fs.createReadStream(path.join(__dirname, '../__fixtures__/node-gyp-6.1.0.tgz'))
     )
-    //  t.end()
+    expect(Object.keys(listOfFiles)).toHaveLength(121)
   })
 })
