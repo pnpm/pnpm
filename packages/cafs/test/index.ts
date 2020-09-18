9289a5039d5b2af9a23ac69aff0509f0b5b694a7
Juan Picado <juanpicado19@gmail.com>
2020-09-18 10:59:08 +0200

    test(cafs): migrate to Jest
    PR #2867

--- packages/cafs/test/index.ts
+++ packages/cafs/test/index.ts
@@ -1,15 +1,15 @@
 import createCafs from '../src'
 import fs = require('fs')
 import path = require('path')
-import test = require('tape')
 import tempy = require('tempy')
 
-test('unpack', async (t) => {
-  const dest = tempy.directory()
-  t.comment(dest)
-  const cafs = createCafs(dest)
-  await cafs.addFilesFromTarball(
-    fs.createReadStream(path.join(__dirname, '../__fixtures__/node-gyp-6.1.0.tgz'))
-  )
-  t.end()
+describe('cafs', () => {
+  test('unpack', async () => {
+    const dest = tempy.directory()
+    const cafs = createCafs(dest)
+    const filesIndex = await cafs.addFilesFromTarball(
+      fs.createReadStream(path.join(__dirname, '../__fixtures__/node-gyp-6.1.0.tgz'))
+    )
+    expect(Object.keys(filesIndex)).toHaveLength(121)
+  })
 })
