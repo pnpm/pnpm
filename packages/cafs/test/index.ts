0cfb4aeace4acee76f6a31a53ec6014c29329711
Zoltan Kochan <z@kochan.io>
2020-09-18 08:57:02 +0300

    refactor

--- packages/cafs/test/index.ts
+++ packages/cafs/test/index.ts
@@ -3,13 +3,13 @@ import fs = require('fs')
 import path = require('path')
 import tempy = require('tempy')
 
-describe('unpack', () => {
+describe('cafs', () => {
   test('unpack', async () => {
     const dest = tempy.directory()
     const cafs = createCafs(dest)
-    const listOfFiles = await cafs.addFilesFromTarball(
+    const filesIndex = await cafs.addFilesFromTarball(
       fs.createReadStream(path.join(__dirname, '../__fixtures__/node-gyp-6.1.0.tgz'))
     )
-    expect(Object.keys(listOfFiles)).toHaveLength(121)
+    expect(Object.keys(filesIndex)).toHaveLength(121)
   })
 })
