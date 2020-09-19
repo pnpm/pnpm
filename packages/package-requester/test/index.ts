d47cc3aa8ce6a13acfc6d149e91d8b6afa7eb9e5
Zoltan Kochan <z@kochan.io>
2020-09-19 16:53:09 +0300

    test: new field is added to the package index file

--- packages/package-requester/test/index.ts
+++ packages/package-requester/test/index.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../../typings/index.d.ts" />
 import { promisify } from 'util'
-import { getFilePathInCafs } from '@pnpm/cafs'
+import { getFilePathInCafs, PackageFilesIndex } from '@pnpm/cafs'
 import createClient from '@pnpm/client'
 import { streamParser } from '@pnpm/logger'
 import createPackageRequester, { PackageFilesResponse, PackageResponse } from '@pnpm/package-requester'
@@ -8,6 +8,7 @@ import pkgIdToFilename from '@pnpm/pkgid-to-filename'
 import { DependencyManifest } from '@pnpm/types'
 import delay from 'delay'
 import path = require('path')
+import loadJsonFile = require('load-json-file')
 import fs = require('mz/fs')
 import ncpCB = require('ncp')
 import nock = require('nock')
@@ -357,6 +358,10 @@ test('fetchPackageToStore()', async (t) => {
     ['package.json', 'index.js', 'license', 'readme.md'].sort(),
     'returned info about files after fetch completed')
   t.notOk(files.fromStore)
+
+  const indexFile = await loadJsonFile<PackageFilesIndex>(fetchResult.filesIndexFile)
+  t.ok(indexFile, 'index file is written')
+  t.ok(typeof indexFile.files['package.json'].checkedAt, 'number')
 
   t.ok(fetchResult.finishing())
 
