3e13f5632b98922f0110e8296cc92198635fc3a5
Zoltan Kochan <z@kochan.io>
2020-09-19 09:09:29 +0300

    birthtimeMs should be nullable

--- packages/store-controller-types/src/index.ts
+++ packages/store-controller-types/src/index.ts
@@ -64,7 +64,7 @@ export type ImportPackageFunction = (
 ) => Promise<{ isBuilt: boolean, importMethod: undefined | string }>
 
 export interface PackageFileInfo {
-  birthtimeMs: number
+  birthtimeMs?: number // Nullable for backward compatibility
   integrity: string
   mode: number
   size: number
