4e8184458db7ba5c3babdb4fc8954ef04e7595b7
Juan Picado <juanpicado19@gmail.com>
2020-09-19 11:18:48 +0200

    chore: remove describe

--- packages/client/test/index.ts
+++ packages/client/test/index.ts
@@ -1,20 +1,18 @@
 /// <reference path="../../../typings/index.d.ts"/>
 import createClient, { createResolver } from '@pnpm/client'
 
-describe('client', () => {
-  test('createClient()', () => {
-    const client = createClient({
-      authConfig: { registry: 'https://registry.npmjs.org/' },
-      storeDir: '',
-    })
-    expect(typeof client === 'object').toBeTruthy()
+test('createClient()', () => {
+  const client = createClient({
+    authConfig: { registry: 'https://registry.npmjs.org/' },
+    storeDir: '',
   })
+  expect(typeof client === 'object').toBeTruthy()
+})
 
-  test('createResolver()', () => {
-    const resolver = createResolver({
-      authConfig: { registry: 'https://registry.npmjs.org/' },
-      storeDir: '',
-    })
-    expect(typeof resolver === 'function').toBeTruthy()
+test('createResolver()', () => {
+  const resolver = createResolver({
+    authConfig: { registry: 'https://registry.npmjs.org/' },
+    storeDir: '',
   })
+  expect(typeof resolver === 'function').toBeTruthy()
 })
