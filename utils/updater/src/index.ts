80b8f5ef24e2074a450a0945b661d5d4136ec092
Zoltan Kochan <z@kochan.io>
2020-09-23 01:09:33 +0300

    feat: support Node.js from v10.14

--- utils/updater/src/index.ts
+++ utils/updater/src/index.ts
@@ -142,7 +142,7 @@ async function updateManifest (dir: string, manifest: ProjectManifest) {
       url: 'https://github.com/pnpm/pnpm/issues',
     },
     engines: {
-      node: '>=10.13',
+      node: '>=10.14',
     },
     files,
     funding: 'https://opencollective.com/pnpm',
