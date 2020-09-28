eff54d0d685276a0ef0aec1ea1d267bd27515e49
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-09-28 10:45:00 +0200

    feat(lockfile‑to‑pnp): correct `lockfileToPackageRegistry` return type

---
+++ .changeset/lovely-bobcats-invent.md
@@ -0,0 +1,5 @@
+---
+'@pnpm/lockfile-to-pnp': patch
+---
+
+Use correct return type for `lockfileToPackageRegistry`.
--- packages/lockfile-to-pnp/src/index.ts
+++ packages/lockfile-to-pnp/src/index.ts
@@ -7,7 +7,7 @@ import pkgIdToFilename from '@pnpm/pkgid-to-filename'
 import readImporterManifest from '@pnpm/read-project-manifest'
 import { Registries } from '@pnpm/types'
 import { refToRelative } from 'dependency-path'
-import pnp = require('@yarnpkg/pnp')
+import { generateInlinedScript, PackageRegistry } from '@yarnpkg/pnp'
 import fs = require('mz/fs')
 import normalizePath = require('normalize-path')
 import path = require('path')
@@ -16,7 +16,7 @@ import R = require('ramda')
 export async function lockfileToPnp (lockfileDirectory: string) {
   const lockfile = await readWantedLockfile(lockfileDirectory, { ignoreIncompatible: true })
   if (!lockfile) throw new Error('Cannot generate a .pnp.js without a lockfile')
-  const importerNames = {} as { [importerId: string]: string }
+  const importerNames: { [importerId: string]: string } = {}
   await Promise.all(
     Object.keys(lockfile.importers)
       .map(async (importerId) => {
@@ -36,7 +36,7 @@ export async function lockfileToPnp (lockfileDirectory: string) {
     virtualStoreDir: virtualStoreDir ?? path.join(lockfileDirectory, 'node_modules/.pnpm'),
   })
 
-  const loaderFile = pnp.generateInlinedScript({
+  const loaderFile = generateInlinedScript({
     blacklistedLocations: undefined,
     dependencyTreeRoots: [],
     ignorePattern: undefined,
@@ -54,7 +54,7 @@ export function lockfileToPackageRegistry (
     virtualStoreDir: string
     registries: Registries
   }
-) {
+): PackageRegistry {
   const packageRegistry = new Map()
   for (const importerId of Object.keys(lockfile.importers)) {
     const importer = lockfile.importers[importerId]
