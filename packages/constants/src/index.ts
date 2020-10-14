a8cdce336100225899543acb1045f6cd635b73ac
Zoltan Kochan <z@kochan.io>
2020-10-14 15:09:15 +0300

    fix: recursive peer dependency
    ref #2919

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,5 +1,5 @@
 export const WANTED_LOCKFILE = 'pnpm-lock.yaml'
-export const LOCKFILE_VERSION = 5.1
+export const LOCKFILE_VERSION = 5.2
 
 export const ENGINE_NAME = `${process.platform}-${process.arch}-node-${process.version.split('.')[0]}`
 export const LAYOUT_VERSION = 4
