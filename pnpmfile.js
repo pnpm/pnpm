68aedc3e735b7e3944e2800ce181495893bf2d7a
Zoltan Kochan <z@kochan.io>
2020-10-20 13:43:37 +0300

    chore: use resolutions

--- pnpmfile.js
+++ pnpmfile.js
@@ -1,17 +1,7 @@
 module.exports = {
   hooks: {
     readPackage (pkg) {
-      if (pkg.dependencies['@nodelib/fs.walk'] === '^1.1.0') {
-        pkg.dependencies['@nodelib/fs.walk'] = '1.1.1'
-      }
-      if (pkg.dependencies['istanbul-reports']) {
-        pkg.dependencies['istanbul-reports'] = 'npm:@zkochan/istanbul-reports'
-      }
       switch (pkg.name) {
-      case 'verdaccio': {
-        pkg.dependencies['http-errors'] = '^1.7.3'
-        break
-      }
       case '@babel/parser':
         pkg.peerDependencies['@babel/types'] = '*'
         break
