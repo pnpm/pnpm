c700c4865ca86c3424170bf5d5d596ded50c9784
Zoltan Kochan <z@kochan.io>
2020-10-25 23:39:54 +0200

    refactor: add comment to @pnpm/cafs

--- packages/cafs/src/addFilesFromTarball.ts
+++ packages/cafs/src/addFilesFromTarball.ts
@@ -15,6 +15,11 @@ export default async function (
   const filesIndex = {}
   await new Promise((resolve, reject) => {
     extract.on('entry', (header, fileStream, next) => {
+      // There are some edge cases, where the same files are extracted multiple times.
+      // So there will be an entry for "lib/index.js" and another one for "lib//index.js",
+      // which are the same file.
+      // Hence, we are normalizing the file name, replacing // with / and checking for duplicates.
+      // Example of such package: @pnpm/colorize-semver-diff@1.0.1
       const filename = header.name.substr(header.name.indexOf('/') + 1).replace(/\/\//g, '/')
       if (header.type !== 'file' || ignore(filename) || filesIndex[filename]) {
         fileStream.resume()
