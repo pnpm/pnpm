6a3851a8cb4c8d224988b5eabf393b14acb5679d
Zoltan Kochan <z@kochan.io>
2020-12-07 12:27:28 +0200

    test(cli): migrate to Jest

--- packages/pnpm/test/uninstall.ts
+++ packages/pnpm/test/uninstall.ts
@@ -1,17 +1,13 @@
 import { execPnpm } from './utils'
 import { fromDir as readPkgFromDir } from '@pnpm/read-package-json'
 import prepare from '@pnpm/prepare'
-import promisifyTape from 'tape-promise'
 import fs = require('mz/fs')
 import path = require('path')
 import exists = require('path-exists')
 import PATH = require('path-name')
-import tape = require('tape')
 
-const test = promisifyTape(tape)
-
-test('uninstall package and remove from appropriate property', async function (t: tape.Test) {
-  const project = prepare(t)
+test('uninstall package and remove from appropriate property', async () => {
+  const project = prepare()
   await execPnpm(['install', '--save-optional', 'is-positive@3.1.0'])
 
   // testing the CLI directly as there was an issue where `npm.config` started to set save = true by default
@@ -27,11 +23,11 @@ test('uninstall package and remove from appropriate property', async function (t
   await project.hasNot('is-positive')
 
   const pkgJson = await readPkgFromDir(process.cwd())
-  t.equal(pkgJson.optionalDependencies, undefined, 'is-negative has been removed from optionalDependencies')
+  expect(pkgJson.optionalDependencies).toBeUndefined()
 })
 
-test('uninstall global package with its bin files', async (t: tape.Test) => {
-  prepare(t)
+test('uninstall global package with its bin files', async () => {
+  prepare()
   process.chdir('..')
 
   const global = path.resolve('global')
@@ -47,10 +43,10 @@ test('uninstall global package with its bin files', async (t: tape.Test) => {
   await execPnpm(['add', '-g', 'sh-hello-world@1.0.1'], { env })
 
   let stat = await exists(path.resolve(globalBin, 'sh-hello-world'))
-  t.ok(stat, 'sh-hello-world is in .bin')
+  expect(stat).toBeTruthy() // sh-hello-world is in .bin
 
   await execPnpm(['uninstall', '-g', 'sh-hello-world'], { env })
 
   stat = await exists(path.resolve(globalBin, 'sh-hello-world'))
-  t.notOk(stat, 'sh-hello-world is removed from .bin')
+  expect(stat).toBeFalsy() // sh-hello-world is removed from .bin
 })
