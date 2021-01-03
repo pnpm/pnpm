401f82c750904ae920c1958a6e575a891946df70
Zoltan Kochan <z@kochan.io>
2021-01-03 16:23:23 +0200

    chore: link packages to themselves using a pnpmfile hook

--- pnpmfile.js
+++ pnpmfile.js
@@ -6,6 +6,9 @@ module.exports = {
         pkg.peerDependencies['@babel/types'] = '*'
         break
       }
+      if (typeof pkg.repository === 'string' && pkg.repository.startsWith('https://github.com/pnpm/pnpm/')) {
+        pkg.devDependencies[pkg.name] = 'link:'
+      }
       return pkg
     }
   }
