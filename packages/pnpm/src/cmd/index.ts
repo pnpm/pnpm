b00a53af320b7b3ef212a3d2e5d2cb06ea04f986
Zoltan Kochan <z@kochan.io>
2021-01-10 18:43:31 +0200

    fix: the ll command should work

--- packages/pnpm/src/cmd/index.ts
+++ packages/pnpm/src/cmd/index.ts
@@ -3,7 +3,7 @@ import { types as allTypes } from '@pnpm/config'
 import { audit } from '@pnpm/plugin-commands-audit'
 import { importCommand } from '@pnpm/plugin-commands-import'
 import { add, install, link, prune, remove, unlink, update } from '@pnpm/plugin-commands-installation'
-import { list, why } from '@pnpm/plugin-commands-listing'
+import { list, ll, why } from '@pnpm/plugin-commands-listing'
 import { outdated } from '@pnpm/plugin-commands-outdated'
 import { pack, publish } from '@pnpm/plugin-commands-publishing'
 import { rebuild } from '@pnpm/plugin-commands-rebuild'
@@ -64,6 +64,7 @@ const commands: Array<{
   installTest,
   link,
   list,
+  ll,
   outdated,
   pack,
   prune,
