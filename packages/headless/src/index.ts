pnpm/pnpm

commit d064b77364ca089f06c0bdbd072278f148af24ab

fix(headless): don't create broken symlinks
close #2916
PR #3032

packages/headless/src/index.ts
@@ -223,6 +223,11 @@ export default async (opts: HeadlessOptions) => {
       pnpmVersion: opts.currentEngine.pnpmVersion,
     } as LockfileToDepGraphOptions
   )
+  if (filteredLockfile.packages) {
+    for (const skippedDepPath of Array.from(skipped)) {
+      delete filteredLockfile.packages[skippedDepPath]
+    }
+  }
   if (opts.enablePnp) {
     const importerNames = R.fromPairs(
       opts.projects.map(({ manifest, id }) => [id, manifest.name ?? id])

packages/supi/test/install/hoist.ts
@@ -499,3 +499,27 @@ test('should recreate node_modules with hoisting', async () => {
     expect(Object.keys(modulesManifest?.hoistedDependencies ?? {}).length > 0).toBeTruthy()
   }
 })
+
+test('hoisting should not create a broken symlink to a skipped optional dependency', async () => {
+  const project = prepareEmpty()
+  console.log(process.cwd())
+
+  await install({
+    optionalDependencies: {
+      'not-compatible-with-any-os': '*',
+    },
+  }, await testDefaults({ publicHoistPattern: '*' }))
+
+  await project.hasNot('dep-of-optional-pkg')
+
+  // Verifying the same with headless installation
+  await rimraf('node_modules')
+
+  await install({
+    optionalDependencies: {
+      'not-compatible-with-any-os': '*',
+    },
+  }, await testDefaults({ publicHoistPattern: '*' }))
+
+  await project.hasNot('dep-of-optional-pkg')
+})

commit c025e4bc6cc8547c4d51e1cacbef8ff90190091e

perf: fix a perf regression shipped in v5.13.7
Fixed a performance regression that was caused by #3032
and shipped in pnpm v5.13.7

The performance of repeat `pnpm install` execution
was in some cases significantly slower.

packages/headless/src/index.ts
@@ -223,11 +223,6 @@ export default async (opts: HeadlessOptions) => {
       pnpmVersion: opts.currentEngine.pnpmVersion,
     } as LockfileToDepGraphOptions
   )
-  if (filteredLockfile.packages) {
-    for (const skippedDepPath of Array.from(skipped)) {
-      delete filteredLockfile.packages[skippedDepPath]
-    }
-  }
   if (opts.enablePnp) {
     const importerNames = R.fromPairs(
       opts.projects.map(({ manifest, id }) => [id, manifest.name ?? id])
@@ -276,8 +271,19 @@ export default async (opts: HeadlessOptions) => {
 
     let newHoistedDependencies!: HoistedDependencies
     if (opts.hoistPattern != null || opts.publicHoistPattern != null) {
+      const hoistLockfile = {
+        ...filteredLockfile,
+        packages: {
+          ...filteredLockfile.packages,
+        },
+      }
+      if (hoistLockfile.packages) {
+        for (const skippedDepPath of Array.from(skipped)) {
+          delete hoistLockfile.packages[skippedDepPath]
+        }
+      }
       newHoistedDependencies = await hoist({
-        lockfile: filteredLockfile,
+        lockfile: hoistLockfile,
         lockfileDir,
         privateHoistedModulesDir: hoistedModulesDir,
         privateHoistPattern: opts.hoistPattern ?? [],

packages/supi/test/install/hoist.ts
@@ -502,7 +502,6 @@ test('should recreate node_modules with hoisting', async () => {
 
 test('hoisting should not create a broken symlink to a skipped optional dependency', async () => {
   const project = prepareEmpty()
-  console.log(process.cwd())
 
   await install({
     optionalDependencies: {
@@ -522,4 +521,9 @@ test('hoisting should not create a broken symlink to a skipped optional dependen
   }, await testDefaults({ publicHoistPattern: '*' }))
 
   await project.hasNot('dep-of-optional-pkg')
+
+  const rootModules = assertProject(process.cwd())
+  const currentLockfile = await rootModules.readCurrentLockfile()
+  const wantedLockfile = await rootModules.readLockfile()
+  expect(currentLockfile).toStrictEqual(wantedLockfile)
 })