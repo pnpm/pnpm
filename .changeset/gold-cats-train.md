92450c9edaf6e9b5bcb15d232491580bea4ed490
Zoltan Kochan <z@kochan.io>
2021-01-24 00:50:49 +0200

    feat: add link to compatibility page on unsupported Node error
    close pnpm/self-installer#15

---
+++ .changeset/gold-cats-train.md
@@ -0,0 +1,5 @@
+---
+"pnpm": patch
+---
+
+When pnpm crashes because the Node.js version is unsupported, the error message will now contain a link to the compatibility page of the pnpm documentation website.
--- packages/pnpm/bin/pnpm.js
+++ packages/pnpm/bin/pnpm.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 const [major, minor] = process.version.substr(1).split('.')
+const COMPATIBILITY_PAGE = `Visit https://bit.ly/pnpmcomp to see the list of past pnpm versions with respective Node.js version support.`
 
 if (major < 10 || major == 10 && minor < 13) {
   console.log(`ERROR: This version of pnpm requires at least Node.js v10.13
-The current version of Node.js is ${process.version}`)
+The current version of Node.js is ${process.version}
+${COMPATIBILITY_PAGE}`)
   process.exit(1)
 } else if (major == 13 && minor < 7) {
   console.log(`ERROR: This version of pnpm requires at least Node.js v13.7
-The current version of Node.js is ${process.version}`)
+The current version of Node.js is ${process.version}
+${COMPATIBILITY_PAGE}`)
   process.exit(1)
 }
 
