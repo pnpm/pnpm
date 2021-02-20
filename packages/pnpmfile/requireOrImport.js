750c760229033da1764050ec14d2277b54002825
CxRes <CxRes@users.noreply.github.com>
2021-02-21 01:22:47 +0530

    [fix] import of ESM pnpmFile
    + We ESM export `requireOrImportPnpmfile` instead of CJS module.export.
    + Import gets a URL as argument.
    + Works with both default and named export.
    + ESM File Loader is only invoked when error is ERR_REQUIRE_ESM.

--- packages/pnpmfile/requireOrImport.js
+++ packages/pnpmfile/requireOrImport.js
@@ -1,7 +1,25 @@
-module.exports = async function requireOrImportPnpmfile (pnpmfilePath) {
+import libUrl from 'url';
+
+async function esmFileLoader(filePath) => {
   try {
-    return require(pnpmfilePath)
-  } catch (err) {
-    return (await import(pnpmfilePath)).default
+    const result = await import(libUrl.pathToFileURL(filePath));
+    return result.default ? result.default : result;
   }
+  catch (err) {
+    console.error(`Failed to load ESM configuration file ${filePath}`);
+    throw err;
+  }
+};
+
+async function requireOrImportPnpmfile (pnpmfilePath) {
+  try {
+    return require(pnpmfilePath);
+  } catch (err) {
+    if (err.code === 'ERR_REQUIRE_ESM') {
+      return esmFileLoader(pnpmfilePath);
+    }
+    throw err;
+   }
 }
+
+export default requireOrImportPnpmfile;
