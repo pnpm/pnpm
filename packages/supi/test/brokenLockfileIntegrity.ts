ad7fbca25c13b602baa0c13c4a6ce50c83bc6351
Zoltan Kochan <z@kochan.io>
2021-03-08 23:50:57 +0200

    fix: the lockfile should be autofixed if it has invalid checksums
    close #3137

--- packages/supi/test/brokenLockfileIntegrity.ts
+++ packages/supi/test/brokenLockfileIntegrity.ts
@@ -1,6 +1,7 @@
 import { WANTED_LOCKFILE } from '@pnpm/constants'
 import { prepareEmpty } from '@pnpm/prepare'
 import rimraf from '@zkochan/rimraf'
+import path from 'path'
 import {
   addDependenciesToPackage,
   mutateModules,
@@ -58,4 +59,77 @@ test('installation breaks if the lockfile contains the wrong checksum', async ()
       rootDir: process.cwd(),
     },
   ], await testDefaults({ preferFrozenLockfile: false }))
+})
+
+test('installation breaks if the lockfile contains the wrong checksum and the store is clean', async () => {
+  await addDistTag('dep-of-pkg-with-1-dep', '100.0.0', 'latest')
+  const project = prepareEmpty()
+
+  const manifest = await addDependenciesToPackage({},
+    [
+      'pkg-with-1-dep@100.0.0',
+    ],
+    await testDefaults({ lockfileOnly: true })
+  )
+
+  const corruptedLockfile = await project.readLockfile()
+  const correctIntegrity = corruptedLockfile.packages['/pkg-with-1-dep/100.0.0'].resolution['integrity']
+  // breaking the lockfile
+  corruptedLockfile.packages['/pkg-with-1-dep/100.0.0'].resolution['integrity'] = 'sha512-pl8WtlGAnoIQ7gPxT187/YwhKRnsFBR4h0YY+v0FPQjT5WPuZbI9dPRaKWgKBFOqWHylJ8EyPy34V5u9YArfng=='
+  await writeYamlFile(WANTED_LOCKFILE, corruptedLockfile, { lineWidth: 1000 })
+
+  const storeDir = path.resolve('new-store')
+  const opts = await testDefaults({
+    storeDir,
+  }, {
+    retry: { retries: 0 },
+  })
+  await expect(mutateModules([
+    {
+      buildIndex: 0,
+      manifest,
+      mutation: 'install',
+      rootDir: process.cwd(),
+    },
+  ], { ...opts, frozenLockfile: true })).rejects.toThrowError(/Got unexpected checksum/)
+
+  await mutateModules([
+    {
+      buildIndex: 0,
+      manifest,
+      mutation: 'install',
+      rootDir: process.cwd(),
+    },
+  ], opts)
+
+  {
+    const lockfile = await project.readLockfile()
+    expect(lockfile.packages['/pkg-with-1-dep/100.0.0'].resolution['integrity']).toBe(correctIntegrity)
+  }
+
+  // Breaking the lockfile again
+  await writeYamlFile(WANTED_LOCKFILE, corruptedLockfile, { lineWidth: 1000 })
+
+  await rimraf('node_modules')
+
+  const reporter = jest.fn()
+  await mutateModules([
+    {
+      buildIndex: 0,
+      manifest,
+      mutation: 'install',
+      rootDir: process.cwd(),
+    },
+  ], await testDefaults({ preferFrozenLockfile: false, reporter, storeDir }, { retry: { retries: 0 } }))
+
+  expect(reporter).toBeCalledWith(expect.objectContaining({
+    level: 'warn',
+    name: 'pnpm',
+    prefix: process.cwd(),
+    message: 'The lockfile is broken! A full installation will be performed in an attempt to fix it.',
+  }))
+  {
+    const lockfile = await project.readLockfile()
+    expect(lockfile.packages['/pkg-with-1-dep/100.0.0'].resolution['integrity']).toBe(correctIntegrity)
+  }
 })
