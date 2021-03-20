907847b24b21fba69a163d40467f0860e4ba5771
Zoltan Kochan <z@kochan.io>
2021-03-20 17:09:10 +0200

    fix: list with details

--- packages/list/src/readPkg.ts
+++ packages/list/src/readPkg.ts
@@ -1,6 +1,13 @@
 import readPackageJson from '@pnpm/read-package-json'
+import normalizePackageData from 'normalize-package-data'
 import pLimit from 'p-limit'
 
 const limitPkgReads = pLimit(4)
 
-export default async (pkgPath: string) => limitPkgReads(async () => readPackageJson(pkgPath))
+export default async (pkgPath: string) => limitPkgReads(
+  async () => {
+    const pkgJson = await readPackageJson(pkgPath)
+    normalizePackageData(pkgJson)
+    return pkgJson
+  }
+)
