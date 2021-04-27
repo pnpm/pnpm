52184571b252e9b21fd07674f023a0749326c1f0
Zoltan Kochan <z@kochan.io>
2021-04-27 23:37:10 +0300

    feat: use preferred pm

--- packages/prepare-package/src/index.ts
+++ packages/prepare-package/src/index.ts
@@ -2,11 +2,13 @@ import path from 'path'
 import { fromDir as readPackageJsonFromDir } from '@pnpm/read-package-json'
 import rimraf from '@zkochan/rimraf'
 import execa from 'execa'
+import preferredPM from 'preferred-pm'
 
 export default async function preparePackage (pkgDir: string) {
   const manifest = await readPackageJsonFromDir(pkgDir)
   if (manifest.scripts?.prepare != null && manifest.scripts.prepare !== '') {
-    await execa('pnpm', ['install'], { cwd: pkgDir })
+    const pm = (await preferredPM(pkgDir))?.name ?? 'npm'
+    await execa(pm, ['install'], { cwd: pkgDir })
     await rimraf(path.join(pkgDir, 'node_modules'))
   }
 }
