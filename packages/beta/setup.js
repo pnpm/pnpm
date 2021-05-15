bfc99279bb0a761418a6dedd896ad31708b1341b
Zoltan Kochan <z@kochan.io>
2021-05-15 12:29:17 +0300

    fix: install

--- packages/beta/setup.js
+++ packages/beta/setup.js
@@ -7,24 +7,27 @@ const arch = platform == 'win' && process.arch == 'ia32' ? 'x86' : process.arch
 const pkgName = `@pnpm/${platform}-${arch}`
 const pkgJson = require.resolve(`${pkgName}/package.json`)
 const subpkg = JSON.parse(fs.readFileSync(pkgJson, 'utf8'))
-const executable = subpkg.bin.pnpm
-const bin = path.resolve(path.dirname(pkgJson), executable)
 
-try {
-  fs.mkdirSync(path.resolve(process.cwd(), 'bin'))
-} catch (e) {
-  if (e.code != 'EEXIST') {
-    throw e
+if (subpkg.bin != null) {
+  const executable = subpkg.bin.pnpm
+  const bin = path.resolve(path.dirname(pkgJson), executable)
+
+  try {
+    fs.mkdirSync(path.resolve(process.cwd(), 'bin'))
+  } catch (e) {
+    if (e.code != 'EEXIST') {
+      throw e
+    }
   }
-}
 
-linkSync(bin, path.resolve(process.cwd(), executable))
+  linkSync(bin, path.resolve(process.cwd(), executable))
 
-if (platform == 'win') {
-  const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')))
-  fs.writeFileSync(path.resolve(process.cwd(), 'bin/pnpm'), 'This file intentionally left blank')
-  pkg.bin.pnpm = 'bin/pnpm.exe'
-  fs.writeFileSync(path.resolve(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2))
+  if (platform == 'win') {
+    const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')))
+    fs.writeFileSync(path.resolve(process.cwd(), 'bin/pnpm'), 'This file intentionally left blank')
+    pkg.bin.pnpm = 'bin/pnpm.exe'
+    fs.writeFileSync(path.resolve(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2))
+  }
 }
 
 function linkSync(src, dest) {
