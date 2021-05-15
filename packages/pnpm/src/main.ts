1c659cf4a332169b730f287d27c2be4c54ab1970
Zoltan Kochan <z@kochan.io>
2021-05-15 12:16:09 +0300

    fix: execution of subprocesses

--- packages/pnpm/src/main.ts
+++ packages/pnpm/src/main.ts
@@ -36,6 +36,9 @@ const DEPRECATED_OPTIONS = new Set([
   'resolution-strategy',
 ])
 
+// A workaround for the https://github.com/vercel/pkg/issues/897 issue.
+delete process.env.PKG_EXECPATH
+
 export default async function run (inputArgv: string[]) {
   let parsedCliArgs!: ParsedCliArgs
   try {
