6e8cedb79c50ec566c734db58ebb3e5ebbb56ab4
Daniel <3473356+D4N14L@users.noreply.github.com>
2021-05-19 17:15:33 -0700

    feat(find-workspace-dir): add env var support for setting the workspace dir (#3464)

---
+++ .changeset/six-files-drive.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/find-workspace-dir": patch
+---
+
+Add support for NPM_CONFIG_WORKSPACE_DIR override environment variable when finding the workspace directory
--- packages/find-workspace-dir/src/index.ts
+++ packages/find-workspace-dir/src/index.ts
@@ -2,10 +2,14 @@ import path from 'path'
 import PnpmError from '@pnpm/error'
 import findUp from 'find-up'
 
+const WORKSPACE_DIR_ENV_VAR = 'NPM_CONFIG_WORKSPACE_DIR'
 const WORKSPACE_MANIFEST_FILENAME = 'pnpm-workspace.yaml'
 
 export default async function findWorkspaceDir (cwd: string) {
-  const workspaceManifestLocation = await findUp([WORKSPACE_MANIFEST_FILENAME, 'pnpm-workspace.yml'], { cwd })
+  const workspaceManifestDirEnvVar = process.env[WORKSPACE_DIR_ENV_VAR] ?? process.env[WORKSPACE_DIR_ENV_VAR.toLowerCase()]
+  const workspaceManifestLocation = workspaceManifestDirEnvVar
+    ? path.join(workspaceManifestDirEnvVar, 'pnpm-workspace.yaml')
+    : await findUp([WORKSPACE_MANIFEST_FILENAME, 'pnpm-workspace.yml'], { cwd })
   if (workspaceManifestLocation?.endsWith('.yml')) {
     throw new PnpmError('BAD_WORKSPACE_MANIFEST_NAME', `The workspace manifest file should be named "pnpm-workspace.yaml". File found: ${workspaceManifestLocation}`)
   }
