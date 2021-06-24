795148ce7dc4b251a8f6461f87c5d043e6850f17
Zoltan Kochan <z@kochan.io>
2021-06-24 14:09:07 +0300

    chore: use packageExtensions

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,14 +1,6 @@
 module.exports = {
   hooks: {
     readPackage (pkg) {
-      switch (pkg.name) {
-      case '@babel/parser':
-        pkg.peerDependencies['@babel/types'] = '*'
-        break
-      case 'jest-circus':
-        pkg.dependencies['slash'] = '3'
-        break
-      }
       if (typeof pkg.repository === 'string' && pkg.repository.startsWith('https://github.com/pnpm/pnpm/')) {
         pkg.devDependencies[pkg.name] = 'link:'
       }
