9069a3a95e1a81c50cd71a7a913f2a41f1f9218c
Zoltan Kochan <z@kochan.io>
2021-07-18 14:42:07 +0300

    fix: test

--- privatePackages/test-fixtures/src/index.ts
+++ privatePackages/test-fixtures/src/index.ts
@@ -6,12 +6,13 @@ import ncpCB from 'ncp'
 const ncp = promisify(ncpCB)
 
 export async function copyFixture (fixtureName: string, dest: string, searchFromDir?: string) {
-  const fixturePath = pathToLocalPkg(fixtureName, searchFromDir ?? __dirname)
+  const fixturePath = pathToLocalPkg(fixtureName, searchFromDir)
   if (!fixturePath) throw new Error(`${fixtureName} not found`)
   return ncp(fixturePath, dest)
 }
 
-export function pathToLocalPkg (pkgName: string, dir: string) {
+export function pathToLocalPkg (pkgName: string, _dir?: string) {
+  let dir = _dir ?? __dirname
   const { root } = path.parse(dir)
   while (true) {
     const checkDir = path.join(dir, 'fixtures', pkgName)
