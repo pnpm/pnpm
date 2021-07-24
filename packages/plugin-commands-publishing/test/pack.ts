612c52953c224f9b69cc22e4f281b5fe75113c0d
Zoltan Kochan <zkochan@users.noreply.github.com>
2021-07-25 00:09:41 +0300

    fix(pack): executables should be packed with exec mode (#3617)

--- packages/plugin-commands-publishing/test/pack.ts
+++ packages/plugin-commands-publishing/test/pack.ts
@@ -1,6 +1,9 @@
+import fs from 'fs'
+import path from 'path'
 import { pack } from '@pnpm/plugin-commands-publishing'
-import prepare from '@pnpm/prepare'
+import prepare, { tempDir } from '@pnpm/prepare'
 import exists from 'path-exists'
+import tar from 'tar'
 import { DEFAULT_OPTS } from './utils'
 
 test('pack: package with package.json', async () => {
@@ -94,4 +97,29 @@ test('pack: runs prepack, prepare, and postpack', async () => {
   expect(await exists('prepack')).toBeTruthy()
   expect(await exists('prepare')).toBeTruthy()
   expect(await exists('postpack')).toBeTruthy()
+})
+
+const modeIsExecutable = (mode: number) => (mode & 0o111) === 0o111
+
+;(process.platform === 'win32' ? test.skip : test)('the mode of executable is changed', async () => {
+  tempDir()
+
+  await pack.handler({
+    ...DEFAULT_OPTS,
+    argv: { original: [] },
+    dir: path.join(__dirname, '../fixtures/has-bin'),
+    extraBinPaths: [],
+    packDestination: process.cwd(),
+  })
+
+  await tar.x({ file: 'has-bin-0.0.0.tgz' })
+
+  {
+    const stat = fs.statSync(path.resolve('package/exec'))
+    expect(modeIsExecutable(stat.mode)).toBeTruthy()
+  }
+  {
+    const stat = fs.statSync(path.resolve('package/index.js'))
+    expect(modeIsExecutable(stat.mode)).toBeFalsy()
+  }
 })
