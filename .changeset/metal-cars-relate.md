00e9bf346f231e7a2f4995ed12d8aa7ed7c6c138
Zoltan Kochan <z@kochan.io>
2021-08-11 23:49:42 +0300

    test: lifecycle

---
+++ .changeset/metal-cars-relate.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/build-modules": patch
+---
+
+Link own binaries of package before running its lifecycle scripts.
--- packages/build-modules/src/index.ts
+++ packages/build-modules/src/index.ts
@@ -198,10 +198,10 @@ export async function linkBinsOfDependencies (
 
   const binPath = path.join(depNode.dir, 'node_modules/.bin')
 
-  const pkgs = await Promise.all(
-    Object.keys(childrenToLink)
-      .filter((alias) => {
-        const dep = depGraph[childrenToLink[alias]]
+  const pkgNodes = [
+    ...Object.entries(childrenToLink)
+      .map(([alias, childKey]) => ({ alias, dep: depGraph[childrenToLink[childKey]] }))
+      .filter(({ alias, dep }) => {
         if (!dep) {
           // TODO: Try to reproduce this issue with a test in supi
           logger.debug({ message: `Failed to link bins of "${alias}" to "${binPath}". This is probably not an issue.` })
@@ -209,18 +209,15 @@ export async function linkBinsOfDependencies (
         }
         return dep.hasBin && dep.installable !== false
       })
-      .map(async (alias) => {
-        const dep = depGraph[childrenToLink[alias]]
-        return {
-          location: dep.dir,
-          manifest: await dep.fetchingBundledManifest?.() ?? (await readPackageFromDir(dep.dir) as DependencyManifest),
-        }
-      })
+      .map(({ dep }) => dep),
+    depNode,
+  ]
+  const pkgs = await Promise.all(pkgNodes
+    .map(async (dep) => ({
+      location: dep.dir,
+      manifest: await dep.fetchingBundledManifest?.() ?? (await readPackageFromDir(dep.dir) as DependencyManifest),
+    }))
   )
-  pkgs.push({
-    location: depNode.dir,
-    manifest: await depNode.fetchingBundledManifest?.() ?? (await readPackageFromDir(depNode.dir) as DependencyManifest),
-  })
 
   await linkBinsOfPackages(pkgs, binPath, { warn: opts.warn })
 
