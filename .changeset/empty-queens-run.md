03af1c6c0f4cc8c425c243f4f89ed8a894d4df68
Zoltan Kochan <z@kochan.io>
2021-09-03 02:51:49 +0300

    fix: overrides that contain parent with range
    close #3732

---
+++ .changeset/empty-queens-run.md
@@ -0,0 +1,5 @@
+---
+"supi": patch
+---
+
+Fix overrides that specify the parent package with a range.
--- packages/supi/src/install/createVersionsOverrider.ts
+++ packages/supi/src/install/createVersionsOverrider.ts
@@ -4,6 +4,8 @@ import parseWantedDependency from '@pnpm/parse-wanted-dependency'
 import normalizePath from 'normalize-path'
 import semver from 'semver'
 
+const DELIMITER_REGEX = /[^ |@]>/
+
 export default function (
   overrides: Record<string, string>,
   rootDir: string
@@ -16,8 +18,9 @@ export default function (
       if (newPref.startsWith('link:')) {
         linkTarget = path.join(rootDir, newPref.substring(5))
       }
-      if (selector.includes('>') && (!selector.includes('@') || selector.indexOf('>') < selector.lastIndexOf('@'))) {
-        const delimiterIndex = selector.indexOf('>')
+      let delimiterIndex = selector.search(DELIMITER_REGEX)
+      if (delimiterIndex !== -1) {
+        delimiterIndex++
         const parentSelector = selector.substring(0, delimiterIndex)
         const childSelector = selector.substring(delimiterIndex + 1)
         versionOverrides.push({
