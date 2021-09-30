4a4d42d8f7308d140f33e3fdc2737321fadfba33
Michael Schmuki <boardend@users.noreply.github.com>
2021-09-30 23:31:55 +0200

    fix: allow dependencies without a package.json (#3782)
    close #1866
    
    Co-authored-by: Zoltan Kochan <z@kochan.io>

---
+++ .changeset/fair-boats-tell.md
@@ -0,0 +1,6 @@
+---
+"@pnpm/lifecycle": patch
+"@pnpm/prepare-package": patch
+---
+
+Packages that have no `package.json` files should be skipped.
--- packages/prepare-package/src/index.ts
+++ packages/prepare-package/src/index.ts
@@ -1,12 +1,12 @@
 import path from 'path'
-import { fromDir as readPackageJsonFromDir } from '@pnpm/read-package-json'
+import { safeReadPackageFromDir } from '@pnpm/read-package-json'
 import rimraf from '@zkochan/rimraf'
 import execa from 'execa'
 import preferredPM from 'preferred-pm'
 
 export default async function preparePackage (pkgDir: string) {
-  const manifest = await readPackageJsonFromDir(pkgDir)
-  if (manifest.scripts?.prepare != null && manifest.scripts.prepare !== '') {
+  const manifest = await safeReadPackageFromDir(pkgDir)
+  if (manifest?.scripts?.prepare != null && manifest.scripts.prepare !== '') {
     const pm = (await preferredPM(pkgDir))?.name ?? 'npm'
     await execa(pm, ['install'], { cwd: pkgDir })
     await rimraf(path.join(pkgDir, 'node_modules'))
