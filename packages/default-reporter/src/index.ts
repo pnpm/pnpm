82b49a35bb76ee9832a67cc1c67b9b23386f483c
Cheng Liu <liucheng.tech@outlook.com>
2021-10-09 12:44:50 +0800

    feat(pnpm): add filterLog hook

--- packages/default-reporter/src/index.ts
+++ packages/default-reporter/src/index.ts
@@ -2,7 +2,7 @@ import { Config } from '@pnpm/config'
 import * as logs from '@pnpm/core-loggers'
 import { LogLevel } from '@pnpm/logger'
 import * as Rx from 'rxjs'
-import { map, mergeAll } from 'rxjs/operators'
+import { filter, map, mergeAll } from 'rxjs/operators'
 import createDiffer from 'ansi-diff'
 import { EOL } from './constants'
 import mergeOutputs from './mergeOutputs'
@@ -177,6 +177,7 @@ export function toOutput$ (
       }
     })
   }, 0)
+  let filterLog: ((log: logs.Log) => boolean) | undefined
   const log$ = {
     context: Rx.from(contextPushStream),
     deprecation: Rx.from(deprecationPushStream),
@@ -185,7 +186,12 @@ export function toOutput$ (
     installCheck: Rx.from(installCheckPushStream),
     lifecycle: Rx.from(lifecyclePushStream),
     link: Rx.from(linkPushStream),
-    other: Rx.from(otherPushStream),
+    other: Rx.from(otherPushStream).pipe(
+      filter((log) => {
+        filterLog = filterLog ?? opts.context.config?.hooks?.filterLog
+        return (filterLog == null || filterLog(log))
+      })
+    ),
     packageImportMethod: Rx.from(packageImportMethodPushStream),
     packageManifest: Rx.from(packageManifestPushStream),
     progress: Rx.from(progressPushStream),
