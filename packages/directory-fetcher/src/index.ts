8134beaadaaad09a75bf9be3364b6a6f1a493071
Zoltan Kochan <z@kochan.io>
2021-10-30 17:01:07 +0300

    fix(directory-fetcher): index only those files that would get published

--- packages/directory-fetcher/src/index.ts
+++ packages/directory-fetcher/src/index.ts
@@ -1,8 +1,9 @@
-import { promises as fs } from 'fs'
 import path from 'path'
 import { Cafs, DeferredManifestPromise } from '@pnpm/fetcher-base'
 import { DirectoryResolution } from '@pnpm/resolver-base'
+import fromPairs from 'ramda/src/fromPairs'
 import loadJsonFile from 'load-json-file'
+import packlist from 'npm-packlist'
 
 export interface DirectoryFetcherOptions {
   manifest?: DeferredManifestPromise
@@ -22,8 +23,8 @@ export async function fetchFromDir (
   dir: string,
   opts: DirectoryFetcherOptions
 ) {
-  const filesIndex: Record<string, string> = {}
-  await mapDirectory(dir, dir, filesIndex)
+  const files = await packlist({ path: dir })
+  const filesIndex: Record<string, string> = fromPairs(files.map((file) => [file, path.join(dir, file)]))
   if (opts.manifest) {
     opts.manifest.resolve(await loadJsonFile(path.join(dir, 'package.json')))
   }
@@ -32,24 +33,4 @@ export async function fetchFromDir (
     filesIndex,
     packageImportMethod: 'hardlink',
   }
-}
-
-async function mapDirectory (
-  rootDir: string,
-  currDir: string,
-  index: Record<string, string>
-) {
-  const files = await fs.readdir(currDir)
-  await Promise.all(files.filter((file) => file !== 'node_modules').map(async (file) => {
-    const fullPath = path.join(currDir, file)
-    const stat = await fs.stat(fullPath)
-    if (stat.isDirectory()) {
-      await mapDirectory(rootDir, fullPath, index)
-      return
-    }
-    if (stat.isFile()) {
-      const relativePath = path.relative(rootDir, fullPath)
-      index[relativePath] = fullPath
-    }
-  }))
 }
