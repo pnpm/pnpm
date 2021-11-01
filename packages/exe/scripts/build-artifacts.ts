88614549587bbbe9776b64c7a2ba9cf8944f24cf
Zoltan Kochan <zkochan@users.noreply.github.com>
2021-11-02 00:43:31 +0200

    fix: build the correct artifacts (#3939)
    close #3937

--- packages/exe/scripts/build-artifacts.ts
+++ packages/exe/scripts/build-artifacts.ts
@@ -1,11 +1,23 @@
 import * as execa from 'execa'
+import fs from 'fs'
 import path from 'path'
 
+const artifactsDir = path.join(__dirname, '../../artifacts')
+
 function build (target: string) {
+  let artifactFile = path.join(artifactsDir, target, 'pnpm')
+  if (target.startsWith('win-')) {
+    artifactFile += '.exe'
+  }
+  try {
+    fs.unlinkSync(artifactFile)
+  } catch (err) {}
   execa.sync('pkg', ['../pnpm/dist/pnpm.cjs', `--config=../pnpm/package-${target}.json`], {
     cwd: path.join(__dirname, '..'),
     stdio: 'inherit',
   })
+  // Verifying that the artifact was created.
+  fs.statSync(artifactFile);
 }
 
 build('win-x64')
@@ -18,3 +30,4 @@ if (process.platform === 'linux' || isM1Mac) {
   build('macos-arm64')
   build('linux-arm64')
 }
+
