5f4be9ba1bf80c398facca98e3b6088537326f9d
Zoltan Kochan <z@kochan.io>
2021-11-03 23:17:11 +0200

    fix: url path may start with numbers

--- packages/git-resolver/test/parsePref.test.ts
+++ packages/git-resolver/test/parsePref.test.ts
@@ -4,10 +4,11 @@ test.each([
   ['ssh://username:password@example.com:repo.git', 'ssh://username:password@example.com/repo.git'],
   ['ssh://username:password@example.com:repo/@foo.git', 'ssh://username:password@example.com/repo/@foo.git'],
   ['ssh://username:password@example.com:22:repo/@foo.git', 'ssh://username:password@example.com:22/repo/@foo.git'],
+  ['ssh://username:password@example.com:22repo/@foo.git', 'ssh://username:password@example.com/22repo/@foo.git'],
   ['git+ssh://username:password@example.com:repo.git', 'ssh://username:password@example.com/repo.git'],
   ['git+ssh://username:password@example.com:repo/@foo.git', 'ssh://username:password@example.com/repo/@foo.git'],
   ['git+ssh://username:password@example.com:22:repo/@foo.git', 'ssh://username:password@example.com:22/repo/@foo.git'],
-])('the right colon is escaped', async (input, output) => {
+])('the right colon is escaped in %s', async (input, output) => {
   const parsed = await parsePref(input)
   expect(parsed?.fetchSpec).toBe(output)
 })
