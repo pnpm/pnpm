48fffe57a0f346b128a77b402d5e1337818e84c1
ylemkimon <y@ylem.kim>
2021-11-07 14:37:07 +0900

    feat: support async filterLog hook

--- packages/pnpmfile/src/requireHooks.ts
+++ packages/pnpmfile/src/requireHooks.ts
@@ -14,7 +14,7 @@ interface Hooks {
   // eslint-disable-next-line
   readPackage?: (pkg: any, context: HookContext) => any
   afterAllResolved?: (lockfile: Lockfile, context: HookContext) => Lockfile | Promise<Lockfile>
-  filterLog?: (log: Log) => boolean
+  filterLog?: (log: Log) => boolean | Promise<boolean>
 }
 
 // eslint-disable-next-line
