080742313f68c87cbb1d6505608e2ebbf27e4e68
Zoltan Kochan <z@kochan.io>
2021-11-12 14:24:20 +0200

    test(publishing): fix

--- packages/plugin-commands-publishing/test/recursivePublish.ts
+++ packages/plugin-commands-publishing/test/recursivePublish.ts
@@ -99,11 +99,11 @@ test('recursive publish', async () => {
 
   {
     const { stdout } = await execa('npm', ['view', pkg1.name, 'versions', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`, '--json'])
-    expect(JSON.parse(stdout.toString())).toStrictEqual([pkg1.version])
+    expect(JSON.parse(stdout.toString())).toStrictEqual(pkg1.version)
   }
   {
     const { stdout } = await execa('npm', ['view', pkg2.name, 'versions', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`, '--json'])
-    expect(JSON.parse(stdout.toString())).toStrictEqual([pkg2.version])
+    expect(JSON.parse(stdout.toString())).toStrictEqual(pkg2.version)
   }
 
   await projects[pkg1.name].writePackageJson({ ...pkg1, version: '2.0.0' })
