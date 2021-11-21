5a11c8bac4d4d1e0f01a7b76c9cb7fb2b2af3902
Lev Chelyadinov <leva181777@gmail.com>
2021-11-21 20:13:08 +0300

    feat(dlx): handle version specifiers in command names (#4024)
    close #4023

---
+++ .changeset/rich-horses-know.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/plugin-commands-script-runners": patch
+---
+
+`pnpm dlx` will now support version specifiers for packages.
--- packages/plugin-commands-script-runners/test/dlx.ts
+++ packages/plugin-commands-script-runners/test/dlx.ts
@@ -4,10 +4,25 @@ import { prepareEmpty } from '@pnpm/prepare'
 
 jest.mock('execa')
 
+beforeEach((execa as jest.Mock).mockClear)
+
 test('dlx should work with scoped packages', async () => {
   prepareEmpty()
 
   await dlx.handler({}, ['@foo/bar'])
 
+  expect(execa).toBeCalledWith('bar', [], expect.anything())
+})
+
+test('dlx should work with versioned packages', async () => {
+  prepareEmpty()
+
+  await dlx.handler({}, ['@foo/bar@next'])
+
+  expect(execa).toBeCalledWith(
+    'pnpm',
+    expect.arrayContaining(['add', '@foo/bar@next']),
+    expect.anything()
+  )
   expect(execa).toBeCalledWith('bar', [], expect.anything())
 })
