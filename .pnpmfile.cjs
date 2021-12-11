94377de6eee28c58efbc13188d249accec1943ca
Zoltan Kochan <z@kochan.io>
2021-12-11 02:45:52 +0200

    chore: fix peer dependency issues

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -4,6 +4,12 @@ module.exports = {
       if (typeof pkg.repository === 'string' && pkg.repository.startsWith('https://github.com/pnpm/pnpm/')) {
         pkg.devDependencies[pkg.name] = 'link:'
       }
+      if (pkg.peerDependencies['eslint']) {
+        pkg.peerDependencies['eslint'] = '*'
+      }
+      if (pkg.peerDependencies['@typescript-eslint/eslint-plugin'] === '^4.0.1') {
+        pkg.peerDependencies['@typescript-eslint/eslint-plugin'] = '^5.6.0'
+      }
       return pkg
     }
   }
