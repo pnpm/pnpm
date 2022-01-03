4a0880b54079900f20c6ab9e5614fec623cf3b7f
Zoltan Kochan <z@kochan.io>
2022-01-03 13:45:53 +0200

    test: nodeLinker=hoisted

--- packages/core/test/hoistedNodeLinker/install.ts
+++ packages/core/test/hoistedNodeLinker/install.ts
@@ -1,7 +1,8 @@
 import fs from 'fs'
 import path from 'path'
-import { install } from '@pnpm/core'
+import { addDependenciesToPackage, install } from '@pnpm/core'
 import { prepareEmpty } from '@pnpm/prepare'
+import { sync as loadJsonFile } from 'load-json-file'
 import { testDefaults } from '../utils'
 
 test('installing with hoisted node-linker', async () => {
@@ -21,4 +22,25 @@ test('installing with hoisted node-linker', async () => {
   expect(fs.realpathSync('node_modules/has-flag')).toEqual(path.resolve('node_modules/has-flag'))
   expect(fs.realpathSync('node_modules/ms')).toEqual(path.resolve('node_modules/ms'))
   expect(fs.existsSync('node_modules/send/node_modules/ms')).toBeTruthy()
+})
+
+test('overwriting (is-positive@3.0.0 with is-positive@latest)', async () => {
+  const project = prepareEmpty()
+  const manifest = await addDependenciesToPackage(
+    {},
+    ['is-positive@3.0.0'],
+    await testDefaults({ nodeLinker: 'hoisted', save: true })
+  )
+
+  await project.storeHas('is-positive', '3.0.0')
+
+  const updatedManifest = await addDependenciesToPackage(
+    manifest,
+    ['is-positive@latest'],
+    await testDefaults({ nodeLinker: 'hoisted', save: true })
+  )
+
+  await project.storeHas('is-positive', '3.1.0')
+  expect(updatedManifest.dependencies?.['is-positive']).toBe('3.1.0')
+  expect(loadJsonFile<{ version: string }>('node_modules/is-positive/package.json').version).toBe('3.1.0')
 })
