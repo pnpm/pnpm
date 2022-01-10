29ba6bb697de649c756d782bfde3502beacb8b57
Zoltan Kochan <z@kochan.io>
2022-01-10 17:43:37 +0200

    test: peers patcher

--- packages/core/src/install/createPeerDependencyPatcher.ts
+++ packages/core/src/install/createPeerDependencyPatcher.ts
@@ -7,14 +7,17 @@ export default function (
   const ignoreMissing = new Set(peerDependencyRules.ignoreMissing ?? [])
   return ((pkg) => {
     if (isEmpty(pkg.peerDependencies)) return pkg
-    for (const peerName of Object.keys(pkg.peerDependencies ?? {})) {
+    for (const [peerName, peerVersion] of Object.entries(pkg.peerDependencies ?? {})) {
       if (ignoreMissing.has(peerName) && !pkg.peerDependenciesMeta?.[peerName]?.optional) {
         pkg.peerDependenciesMeta = pkg.peerDependenciesMeta ?? {}
         pkg.peerDependenciesMeta[peerName] = {
           optional: true,
         }
       }
-      if (peerDependencyRules.allowedVersions?.[peerName]) {
+      if (
+        peerDependencyRules.allowedVersions?.[peerName] &&
+        peerVersion !== '*'
+      ) {
         pkg.peerDependencies![peerName] += ` || ${peerDependencyRules.allowedVersions[peerName]}`
       }
     }
