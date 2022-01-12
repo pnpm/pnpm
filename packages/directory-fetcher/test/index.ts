c06e8da709b691cbf5fb2e3d647a2fae691ae4ea
Zoltan Kochan <z@kochan.io>
2022-01-12 15:34:02 +0200

    fix(directory-fetcher): don't fail if the linked package has no `package.json`
    This change was need to support injection in Bit workspace:
    https://github.com/teambit/bit/pull/5251

--- packages/directory-fetcher/test/index.ts
+++ packages/directory-fetcher/test/index.ts
@@ -1,28 +1,57 @@
 /// <reference path="../../../typings/index.d.ts"/>
 import path from 'path'
 import createFetcher from '@pnpm/directory-fetcher'
+import fixtures from '@pnpm/test-fixtures'
+
+const f = fixtures(__dirname)
 
 test('fetch', async () => {
+  process.chdir(f.find('simple-pkg'))
   const fetcher = createFetcher()
 
   // eslint-disable-next-line
   const fetchResult = await fetcher.directory({} as any, {
-    directory: '..',
+    directory: '.',
     type: 'directory',
   }, {
-    lockfileDir: __dirname,
+    lockfileDir: process.cwd(),
   })
 
   expect(fetchResult.local).toBe(true)
   expect(fetchResult.packageImportMethod).toBe('hardlink')
-  expect(fetchResult.filesIndex['package.json']).toBe(path.join(__dirname, '../package.json'))
+  expect(fetchResult.filesIndex['package.json']).toBe(path.resolve('package.json'))
 
   // Only those files are included which would get published
   expect(Object.keys(fetchResult.filesIndex).sort()).toStrictEqual([
-    'README.md',
-    'lib/index.d.ts',
-    'lib/index.js',
-    'lib/index.js.map',
+    'index.js',
     'package.json',
+  ])
+})
+
+test('fetch a directory that has no package.json', async () => {
+  process.chdir(f.find('no-manifest'))
+  const fetcher = createFetcher()
+  const manifest = {
+    resolve: jest.fn(),
+    reject: jest.fn(),
+  }
+
+  // eslint-disable-next-line
+  const fetchResult = await fetcher.directory({} as any, {
+    directory: '.',
+    type: 'directory',
+  }, {
+    lockfileDir: process.cwd(),
+    manifest,
+  })
+
+  expect(manifest.resolve).toBeCalledWith({})
+  expect(fetchResult.local).toBe(true)
+  expect(fetchResult.packageImportMethod).toBe('hardlink')
+  expect(fetchResult.filesIndex['index.js']).toBe(path.resolve('index.js'))
+
+  // Only those files are included which would get published
+  expect(Object.keys(fetchResult.filesIndex).sort()).toStrictEqual([
+    'index.js',
   ])
 })
