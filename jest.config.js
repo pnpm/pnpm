b39cabde82e420ae0d065dd5e2e8c0b7e11381bd
Valentin Semirulnik <v7rulnik@gmail.com>
2022-01-19 01:13:13 +0300

    ci: setup cache for TS and Jest (#4049)

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
-const path = require('path')
+const path = require("path");
+
+const pathAsArr = process.env.PNPM_SCRIPT_SRC_DIR.split(path.sep);
+const packageName = pathAsArr[pathAsArr.length - 1];
 
 module.exports = {
-  preset: 'ts-jest',
+  preset: "ts-jest",
   testMatch: ["**/test/**/*.[jt]s?(x)", "**/src/**/*.test.ts"],
-  testEnvironment: 'node',
+  testEnvironment: "node",
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  testPathIgnorePatterns: [
-    '/fixtures/',
-    '<rootDir>/test/utils/.+',
-  ],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  testPathIgnorePatterns: ["/fixtures/", "<rootDir>/test/utils/.+"],
   testTimeout: 4 * 60 * 1000, // 4 minutes
-  setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.js')],
+  setupFilesAfterEnv: [path.join(__dirname, "jest.setup.js")],
+  cacheDirectory: path.join(__dirname, ".jest-cache", packageName),
 };
