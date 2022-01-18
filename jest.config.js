96a68d544eb3549820b6f2b18e260a25724eb9af
Valentin Semirulnik <v7rulnik@gmail.com>
2022-01-18 20:47:41 +0300

    store cache for jest in project folder

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+const pathAsArr = process.env.PNPM_SCRIPT_SRC_DIR.split(path.sep);
+const packageName = pathAsArr[pathAsArr.length - 1];
+
 module.exports = {
   preset: "ts-jest",
   testMatch: ["**/test/**/*.[jt]s?(x)", "**/src/**/*.test.ts"],
@@ -9,4 +12,5 @@ module.exports = {
   testPathIgnorePatterns: ["/fixtures/", "<rootDir>/test/utils/.+"],
   testTimeout: 4 * 60 * 1000, // 4 minutes
   setupFilesAfterEnv: [path.join(__dirname, "jest.setup.js")],
+  cacheDirectory: path.join(__dirname, ".jest-cache", packageName),
 };
