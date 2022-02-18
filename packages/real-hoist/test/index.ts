fa4ece873fd6f9a9c126273a685545fd01faa9f3
aslemammad <zorofight94@gmail.com>
2022-02-18 18:14:43 +0330

    feat: add vitest

--- packages/real-hoist/test/index.ts
+++ packages/real-hoist/test/index.ts
@@ -1,8 +1,11 @@
-import path from 'path'
-import hoist from '@pnpm/real-hoist'
-import { readWantedLockfile } from '@pnpm/lockfile-file'
+import { test, expect } from "vitest";
+import path from "path";
+import hoist from "@pnpm/real-hoist";
+import { readWantedLockfile } from "@pnpm/lockfile-file";
 
-test('hoist', async () => {
-  const lockfile = await readWantedLockfile(path.join(__dirname, '../../..'), { ignoreIncompatible: true })
-  expect(hoist(lockfile!)).toBeTruthy()
-})
+test("hoist", async () => {
+  const lockfile = await readWantedLockfile(path.join(__dirname, "../../.."), {
+    ignoreIncompatible: true,
+  });
+  expect(hoist(lockfile!)).toBeTruthy();
+});
