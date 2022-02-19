d64d2bfa1cef776529abf84837570a4c351a3092
aslemammad <zorofight94@gmail.com>
2022-02-19 19:32:01 +0330

    chore: relint

--- packages/real-hoist/test/index.ts
+++ packages/real-hoist/test/index.ts
@@ -1,11 +1,11 @@
-import { test, expect } from "vitest";
-import path from "path";
-import hoist from "@pnpm/real-hoist";
-import { readWantedLockfile } from "@pnpm/lockfile-file";
+import { test, expect } from 'vitest'
+import path from 'path'
+import hoist from '@pnpm/real-hoist'
+import { readWantedLockfile } from '@pnpm/lockfile-file'
 
-test("hoist", async () => {
-  const lockfile = await readWantedLockfile(path.join(__dirname, "../../.."), {
+test('hoist', async () => {
+  const lockfile = await readWantedLockfile(path.join(__dirname, '../../..'), {
     ignoreIncompatible: true,
-  });
-  expect(hoist(lockfile!)).toBeTruthy();
-});
+  })
+  expect(hoist(lockfile!)).toBeTruthy()
+})
