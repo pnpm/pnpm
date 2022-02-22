56249f8c3c7280296671bf82674093c09e6f8ee1
Zoltan Kochan <z@kochan.io>
2022-02-23 01:54:55 +0200

    fix: onlyBuiltDependencies should work (#4377)

--- packages/core/src/install/extendInstallOptions.ts
+++ packages/core/src/install/extendInstallOptions.ts
@@ -48,7 +48,7 @@ export interface StrictInstallOptions {
   rawConfig: object
   verifyStoreIntegrity: boolean
   engineStrict: boolean
-  neverBuiltDependencies: string[]
+  neverBuiltDependencies?: string[]
   onlyBuiltDependencies?: string[]
   nodeExecPath?: string
   nodeLinker?: 'isolated' | 'hoisted' | 'pnp'
@@ -131,7 +131,6 @@ const defaults = async (opts: InstallOptions) => {
     },
     lockfileDir: opts.lockfileDir ?? opts.dir ?? process.cwd(),
     lockfileOnly: false,
-    neverBuiltDependencies: [] as string[],
     nodeVersion: process.version,
     nodeLinker: 'isolated',
     overrides: {},
