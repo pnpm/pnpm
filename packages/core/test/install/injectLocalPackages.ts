a9de4818bad87b6636289269bdebbd37b237bfbb
Zoltan Kochan <z@kochan.io>
2022-03-06 01:41:12 +0200

    docs: add changesets

--- packages/core/test/install/injectLocalPackages.ts
+++ packages/core/test/install/injectLocalPackages.ts
@@ -768,6 +768,106 @@ test('inject local packages and relink them after build', async () => {
   expect(await pathExists(path.resolve('project-2/node_modules/project-1/main.js'))).toBeTruthy()
 })
 
+test('inject local packages and relink them after build (file protocol is used)', async () => {
+  const project1Manifest = {
+    name: 'project-1',
+    version: '1.0.0',
+    dependencies: {
+      'is-negative': '1.0.0',
+    },
+    devDependencies: {
+      'dep-of-pkg-with-1-dep': '100.0.0',
+    },
+    peerDependencies: {
+      'is-positive': '1.0.0',
+    },
+    scripts: {
+      prepublishOnly: 'touch main.js',
+    },
+  }
+  const project2Manifest = {
+    name: 'project-2',
+    version: '1.0.0',
+    dependencies: {
+      'is-positive': '1.0.0',
+      'project-1': 'file:../project-1',
+    },
+  }
+  const projects = preparePackages([
+    {
+      location: 'project-1',
+      package: project1Manifest,
+    },
+    {
+      location: 'project-2',
+      package: project2Manifest,
+    },
+  ])
+
+  const importers: MutatedProject[] = [
+    {
+      buildIndex: 0,
+      manifest: project1Manifest,
+      mutation: 'install',
+      rootDir: path.resolve('project-1'),
+    },
+    {
+      buildIndex: 0,
+      manifest: project2Manifest,
+      mutation: 'install',
+      rootDir: path.resolve('project-2'),
+    },
+  ]
+  await mutateModules(importers, await testDefaults())
+
+  await projects['project-1'].has('is-negative')
+  await projects['project-1'].has('dep-of-pkg-with-1-dep')
+  await projects['project-1'].hasNot('is-positive')
+
+  await projects['project-2'].has('is-positive')
+  await projects['project-2'].has('project-1')
+
+  expect(await pathExists(path.resolve('project-2/node_modules/project-1/main.js'))).toBeTruthy()
+
+  const rootModules = assertProject(process.cwd())
+  const lockfile = await rootModules.readLockfile()
+  expect(lockfile.packages['file:project-1_is-positive@1.0.0']).toEqual({
+    resolution: {
+      directory: 'project-1',
+      type: 'directory',
+    },
+    id: 'file:project-1',
+    name: 'project-1',
+    version: '1.0.0',
+    peerDependencies: {
+      'is-positive': '1.0.0',
+    },
+    dependencies: {
+      'is-negative': '1.0.0',
+      'is-positive': '1.0.0',
+    },
+    dev: false,
+  })
+
+  await rimraf('node_modules')
+  await rimraf('project-1/main.js')
+  await rimraf('project-1/node_modules')
+  await rimraf('project-2/node_modules')
+
+  await mutateModules(importers, await testDefaults({
+    frozenLockfile: true,
+  }))
+
+  await projects['project-1'].has('is-negative')
+  await projects['project-1'].has('dep-of-pkg-with-1-dep')
+  await projects['project-1'].hasNot('is-positive')
+
+  await projects['project-2'].has('is-positive')
+  await projects['project-2'].has('project-1')
+
+  expect(await pathExists(path.resolve('project-2/node_modules/project-1/main.js'))).toBeTruthy()
+})
+
 test('inject local packages when node-linker is hoisted', async () => {
   const project1Manifest = {
     name: 'project-1',
