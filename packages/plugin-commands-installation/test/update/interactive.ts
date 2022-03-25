426f751aa7f27228320f2f640b7a647c367d5e76
Zoltan Kochan <z@kochan.io>
2022-03-25 22:17:56 +0200

    test: fix

--- packages/plugin-commands-installation/test/update/interactive.ts
+++ packages/plugin-commands-installation/test/update/interactive.ts
@@ -134,7 +134,7 @@ test('interactively update', async () => {
       name: 'is-positive',
     },
     {
-      message: chalk`micromatch  3.0.0 ❯ {redBright.bold 4.0.4} `,
+      message: chalk`micromatch  3.0.0 ❯ {redBright.bold 4.0.5} `,
       name: 'micromatch',
     },
   ])
