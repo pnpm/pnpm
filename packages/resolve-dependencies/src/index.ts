f7c04733a190904d8c4704b64b87e7c00597bd28
Zoltan Kochan <z@kochan.io>
2022-04-09 15:59:52 +0300

    fix: resolve peers from linked in dependencies (#4541)

--- packages/resolve-dependencies/src/index.ts
+++ packages/resolve-dependencies/src/index.ts
@@ -127,7 +127,7 @@ export default async function (
       )
     }
 
-    const topParents = project.manifest
+    const topParents: Array<{ name: string, version: string, linkedDir?: string }> = project.manifest
       ? await getTopParents(
         difference(
           Object.keys(getAllDependenciesFromManifest(project.manifest)),
@@ -138,6 +138,13 @@ export default async function (
         project.modulesDir
       )
       : []
+    resolvedImporter.linkedDependencies.forEach((linkedDependency) => {
+      topParents.push({
+        name: linkedDependency.alias,
+        version: linkedDependency.version,
+        linkedDir: `link:${path.relative(opts.lockfileDir, linkedDependency.resolution.directory)}`,
+      })
+    })
 
     const manifest = updatedOriginalManifest ?? project.originalManifest ?? project.manifest
     importers[index].manifest = manifest
