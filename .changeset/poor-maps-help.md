72b79f55a0693e678dd0ee24f45adb454caf5336
Zoltan Kochan <z@kochan.io>
2022-04-12 01:05:19 +0300

    fix: auto-install-peers

---
+++ .changeset/poor-maps-help.md
@@ -0,0 +1,6 @@
+---
+"@pnpm/config": patch
+"pnpm": patch
+---
+
+Setting the `auto-install-peers` to `true` should work.
--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -32,6 +32,7 @@ async function which (cmd: string) {
 }
 
 export const types = Object.assign({
+  'auto-install-peers': Boolean,
   bail: Boolean,
   'cache-dir': String,
   'child-concurrency': Number,
@@ -164,6 +165,7 @@ export default async (
   }
   const rcOptionsTypes = { ...types, ...opts.rcOptionsTypes }
   const npmConfig = loadNpmConf(cliOptions, rcOptionsTypes, {
+    'auto-install-peers': false,
     bail: true,
     color: 'auto',
     'enable-modules-dir': true,
