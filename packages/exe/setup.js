8f08066c89f4dc2a819ba25b905d58b0ed78e811
Gao Sheng <gaosheng08@meituan.com>
2022-04-14 19:44:53 +0800

    fix(exe): use the strict equality operator, and make `JSON.parse` execute correctly

--- packages/exe/setup.js
+++ packages/exe/setup.js
@@ -1,25 +1,25 @@
 const path = require('path')
 const fs = require('fs')
 
-const platform = process.platform == 'win32'
+const platform = process.platform === 'win32'
   ? 'win'
-  : process.platform == 'darwin'
+  : process.platform === 'darwin'
   ? 'macos'
   : process.platform
-const arch = platform == 'win' && process.arch == 'ia32' ? 'x86' : process.arch
+const arch = platform === 'win' && process.arch === 'ia32' ? 'x86' : process.arch
 
 const pkgName = `@pnpm/${platform}-${arch}`
 const pkgJson = require.resolve(`${pkgName}/package.json`)
 const subpkg = JSON.parse(fs.readFileSync(pkgJson, 'utf8'))
 
-if (subpkg.bin != null) {
+if (subpkg.bin !== null) {
   const executable = subpkg.bin.pnpm
   const bin = path.resolve(path.dirname(pkgJson), executable)
 
   linkSync(bin, path.resolve(process.cwd(), executable))
 
-  if (platform == 'win') {
-    const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')))
+  if (platform === 'win') {
+    const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8'))
     fs.writeFileSync(path.resolve(process.cwd(), 'pnpm'), 'This file intentionally left blank')
     pkg.bin.pnpm = 'pnpm.exe'
     fs.writeFileSync(path.resolve(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2))
@@ -30,7 +30,7 @@ function linkSync(src, dest) {
   try {
     fs.unlinkSync(dest)
   } catch (e) {
-    if (e.code != 'ENOENT') {
+    if (e.code !== 'ENOENT') {
       throw e
     }
   }
