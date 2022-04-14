30e7e65238175ef996e5e113583447a2a99a5d22
那里好脏不可以 <gaosheng08@meituan.com>
2022-04-14 20:48:11 +0800

    fix(exe): use the strict equality operator, and make `JSON.parse` execute correctly (#4571)

--- packages/exe/setup.js
+++ packages/exe/setup.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const fs = require('fs')
 
-const platform = process.platform == 'win32'
+const platform = process.platform === 'win32'
   ? 'win'
-  : process.platform == 'darwin'
+  : process.platform === 'darwin'
   ? 'macos'
   : process.platform
-const arch = platform == 'win' && process.arch == 'ia32' ? 'x86' : process.arch
+const arch = platform === 'win' && process.arch === 'ia32' ? 'x86' : process.arch
 
 const pkgName = `@pnpm/${platform}-${arch}`
 const pkgJson = require.resolve(`${pkgName}/package.json`)
@@ -18,8 +18,8 @@ if (subpkg.bin != null) {
 
   linkSync(bin, path.resolve(process.cwd(), executable))
 
-  if (platform == 'win') {
-    const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')))
+  if (platform === 'win') {
+    const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8'))
     fs.writeFileSync(path.resolve(process.cwd(), 'pnpm'), 'This file intentionally left blank')
     pkg.bin.pnpm = 'pnpm.exe'
     fs.writeFileSync(path.resolve(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2))
@@ -30,7 +30,7 @@ function linkSync(src, dest) {
   try {
     fs.unlinkSync(dest)
   } catch (e) {
-    if (e.code != 'ENOENT') {
+    if (e.code !== 'ENOENT') {
       throw e
     }
   }
