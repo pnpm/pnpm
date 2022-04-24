22ab407700779721be0ff685cf9dada2a1200b99
Zoltan Kochan <z@kochan.io>
2022-04-24 21:50:23 +0300

    fix(update-checker): suggest to update @pnpm/exe, when @pnpm/exe is used

--- packages/default-reporter/src/reporterForClient/reportUpdateCheck.ts
+++ packages/default-reporter/src/reporterForClient/reportUpdateCheck.ts
@@ -6,6 +6,7 @@ import { filter, map, take } from 'rxjs/operators'
 import semver from 'semver'
 
 export default (log$: Rx.Observable<UpdateCheckLog>) => {
+  const pkgName = process['pkg'] != null ? '@pnpm/exe' : 'pnpm'
   return log$.pipe(
     take(1),
     filter((log) => semver.gt(log.latestVersion, log.currentVersion)),
@@ -13,7 +14,7 @@ export default (log$: Rx.Observable<UpdateCheckLog>) => {
       msg: boxen(`\
 Update available! ${chalk.red(log.currentVersion)} → ${chalk.green(log.latestVersion)}.
 ${chalk.magenta('Changelog:')} https://github.com/pnpm/pnpm/releases/tag/v${log.latestVersion}
-Run ${chalk.magenta('pnpm add -g pnpm')} to update.
+Run ${chalk.magenta(`pnpm add -g ${pkgName}`)} to update.
 
 Follow ${chalk.magenta('@pnpmjs')} for updates: https://twitter.com/pnpmjs`,
       {
