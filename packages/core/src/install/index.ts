82554b9b7fd7f4f6204600b94ceed15bd7378350
Zoltan Kochan <z@kochan.io>
2022-05-05 17:54:44 +0300

    fix: use Yarn's compatibility database
    ref https://github.com/yarnpkg/berry/pull/4436

--- packages/core/src/install/index.ts
+++ packages/core/src/install/index.ts
@@ -53,6 +53,7 @@ import {
   ProjectManifest,
   ReadPackageHook,
 } from '@pnpm/types'
+import { packageExtensions as compatPackageExtensions } from '@yarnpkg/plugin-compat/lib/extensions'
 import rimraf from '@zkochan/rimraf'
 import isInnerLink from 'is-inner-link'
 import pFilter from 'p-filter'
@@ -515,6 +516,7 @@ export function createReadPackageHook (
   if (!isEmpty(overrides ?? {})) {
     hooks.push(createVersionsOverrider(overrides!, lockfileDir))
   }
+  hooks.push(createPackageExtender(fromPairs(compatPackageExtensions)))
   if (!isEmpty(packageExtensions ?? {})) {
     hooks.push(createPackageExtender(packageExtensions!))
   }
