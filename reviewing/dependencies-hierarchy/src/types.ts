60f5778934275c6c9ad6b4b59ddd5591b8e236ba
Brandon Cheng <gluxon@users.noreply.github.com>
2022-05-27 23:33:10 -0400

    Split dependencies-hierarchy into separate files

---
+++ reviewing/dependencies-hierarchy/src/types.ts
@@ -0,0 +1,17 @@
+export type SearchFunction = (pkg: { name: string, version: string }) => boolean
+
+export interface PackageNode {
+  alias: string
+  circular?: true
+  dependencies?: PackageNode[]
+  dev?: boolean
+  isPeer: boolean
+  isSkipped: boolean
+  isMissing: boolean
+  name: string
+  optional?: true
+  path: string
+  resolved?: string
+  searched?: true
+  version: string
+}
