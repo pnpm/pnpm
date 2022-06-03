f132ebf73904fdc263fb5b9f878299368679e283
Cheng <chengcyber@outlook.com>
2022-06-03 18:45:46 +0800

    Update packages/lockfile-file/src/gitBranchLockfile.ts
    Co-authored-by: David Michon <dmichon@microsoft.com>

--- packages/lockfile-file/src/gitBranchLockfile.ts
+++ packages/lockfile-file/src/gitBranchLockfile.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 export async function getGitBranchLockfileNames (lockfileDir: string) {
   const files = await fs.readdir(lockfileDir)
-  const gitBranchLockfileNames: string[] = files.filter(file => file.match(/pnpm-lock.(?:.*).yaml/))
+  const gitBranchLockfileNames: string[] = files.filter(file => file.match(/^pnpm-lock.(?:.*).yaml$/))
   return gitBranchLockfileNames
 }
 
