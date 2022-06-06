3554d6c6f29d1dcb593bb239d841efa5018ca62a
Zoltan Kochan <z@kochan.io>
2022-06-06 23:12:24 +0300

    test: setup

---
+++ packages/plugin-commands-setup/test/setup.test.ts
@@ -0,0 +1,70 @@
+import PnpmError from '@pnpm/error'
+import { setup } from '@pnpm/plugin-commands-setup'
+import { addDirToEnvPath, PathExtenderReport } from '@pnpm/os.env.path-extender'
+
+jest.mock('@pnpm/os.env.path-extender', () => ({
+  addDirToEnvPath: jest.fn(),
+}))
+
+jest.mock('fs')
+
+test('setup makes no changes', async () => {
+  addDirToEnvPath['mockReturnValue'](Promise.resolve<PathExtenderReport>({
+    oldSettings: 'PNPM_HOME=dir',
+    newSettings: 'PNPM_HOME=dir',
+  }))
+  const output = await setup.handler({ pnpmHomeDir: '' })
+  expect(output).toBe('No changes to the environment were made. Everything is already up-to-date.')
+})
+
+test('setup makes changes on POSIX', async () => {
+  addDirToEnvPath['mockReturnValue'](Promise.resolve<PathExtenderReport>({
+    configFile: {
+      changeType: 'created',
+      path: '~/.bashrc',
+    },
+    oldSettings: 'export PNPM_HOME=dir1',
+    newSettings: 'export PNPM_HOME=dir2',
+  }))
+  const output = await setup.handler({ pnpmHomeDir: '' })
+  expect(output).toBe(`Created ~/.bashrc
+
+Next configuration changes were made:
+export PNPM_HOME=dir2
+
+Setup complete. Open a new terminal to start using pnpm.`)
+})
+
+test('setup makes changes on Windows', async () => {
+  addDirToEnvPath['mockReturnValue'](Promise.resolve<PathExtenderReport>({
+    oldSettings: 'export PNPM_HOME=dir1',
+    newSettings: 'export PNPM_HOME=dir2',
+  }))
+  const output = await setup.handler({ pnpmHomeDir: '' })
+  expect(output).toBe(`Next configuration changes were made:
+export PNPM_HOME=dir2
+
+Setup complete. Open a new terminal to start using pnpm.`)
+})
+
+test('hint is added to ERR_PNPM_BAD_ENV_FOUND error object', async () => {
+  addDirToEnvPath['mockReturnValue'](Promise.reject(new PnpmError('BAD_ENV_FOUND', '')))
+  let err!: PnpmError
+  try {
+    await setup.handler({ pnpmHomeDir: '' })
+  } catch (_err: any) { // eslint-disable-line
+    err = _err
+  }
+  expect(err?.hint).toBe('If you want to override the existing env variable, use the --force option')
+})
+
+test('hint is added to ERR_PNPM_BAD_SHELL_SECTION error object', async () => {
+  addDirToEnvPath['mockReturnValue'](Promise.reject(new PnpmError('BAD_SHELL_SECTION', '')))
+  let err!: PnpmError
+  try {
+    await setup.handler({ pnpmHomeDir: '' })
+  } catch (_err: any) { // eslint-disable-line
+    err = _err
+  }
+  expect(err?.hint).toBe('If you want to override the existing configuration section, use the --force option')
+})
