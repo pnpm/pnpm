6434a82910e69caf29da5034bc9660416b6f735a
Maksym Dogadailo <m.dogadailo@gmail.com>
2022-06-26 13:36:24 +0200

    fix: resolve real path for case insensitive systems (#4935)
    close #4904

---
+++ .changeset/quiet-icons-wait.md
@@ -0,0 +1,6 @@
+---
+"@pnpm/find-workspace-dir": patch
+"pnpm": patch
+---
+
+Resolve native workspace path for case-insensitive file systems [#4904](https://github.com/pnpm/pnpm/issues/4904).
--- packages/find-workspace-dir/test/index.ts
+++ packages/find-workspace-dir/test/index.ts
@@ -1,12 +1,32 @@
 /// <reference path="../../../typings/index.d.ts"/>
 import path from 'path'
+import fs from 'fs'
 import findWorkspaceDir from '@pnpm/find-workspace-dir'
 
 const NPM_CONFIG_WORKSPACE_DIR_ENV_VAR = 'NPM_CONFIG_WORKSPACE_DIR'
 const FAKE_PATH = 'FAKE_PATH'
 
+function isFileSystemCaseSensitive () {
+  try {
+    fs.realpathSync.native(process.cwd().toUpperCase())
+    return false
+  } catch (_) {
+    return true
+  }
+}
+
+// We don't need to validate case-sensitive systems
+// because it is not possible to reach process.cwd() with wrong case there.
+const testOnCaseInSensitiveSystems = isFileSystemCaseSensitive() ? test.skip : test
+
 test('finds actual workspace dir', async () => {
   const workspaceDir = await findWorkspaceDir(process.cwd())
+
+  expect(workspaceDir).toBe(path.resolve(__dirname, '..', '..', '..'))
+})
+
+testOnCaseInSensitiveSystems('finds workspace dir with wrong case from cwd', async () => {
+  const workspaceDir = await findWorkspaceDir(process.cwd().toUpperCase())
 
   expect(workspaceDir).toBe(path.resolve(__dirname, '..', '..', '..'))
 })
