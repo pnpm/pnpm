3cc151af5accaab2e33da81df6798e5170f2c1a4
Zoltan Kochan <z@kochan.io>
2022-06-28 02:34:37 +0300

    fix: audit fix should not add an override for a vulnerability that has no fix

--- packages/plugin-commands-audit/src/fix.ts
+++ packages/plugin-commands-audit/src/fix.ts
@@ -22,7 +22,7 @@ export default async function fix (dir: string, auditReport: AuditReport) {
 function createOverrides (advisories: AuditAdvisory[]) {
   return fromPairs(
     advisories
-      .filter(({ vulnerable_versions }) => vulnerable_versions !== '>=0.0.0') // eslint-disable-line
+      .filter(({ vulnerable_versions, patched_versions }) => vulnerable_versions !== '>=0.0.0' && patched_versions !== '<0.0.0') // eslint-disable-line
       .map((advisory) => [
         `${advisory.module_name}@${advisory.vulnerable_versions}`,
         advisory.patched_versions,
