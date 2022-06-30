d50d5fb76225742b2b986bfa178afe5fe6611c1f
Zoltan Kochan <z@kochan.io>
2022-06-30 17:07:36 +0300

    test(env): fix

--- packages/plugin-commands-env/test/normalizeArch.test.ts
+++
@@ -1,9 +0,0 @@
-import normalizeArch from '../lib/normalizeArch'
-
-test.each([
-  ['win32', 'ia32', 'x86'],
-  ['linux', 'arm', 'armv7l'], // Raspberry Pi 4
-  ['linux', 'x64', 'x64'],
-])('normalizedArch(%s, %s)', (platform, arch, normalizedArch) => {
-  expect(normalizeArch(platform, arch)).toBe(normalizedArch)
-})
