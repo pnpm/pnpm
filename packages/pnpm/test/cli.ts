572b265ba0f26f1d359923fbf3a70ac41ba3d3c2
William Huang <william2958@gmail.com>
2022-07-11 19:41:15 -0400

    feat(file-reporter): remove file reporter (#5012)
    close #4949

--- packages/pnpm/test/cli.ts
+++ packages/pnpm/test/cli.ts
@@ -1,6 +1,5 @@
 import { createReadStream, promises as fs } from 'fs'
 import path from 'path'
-import pathExists from 'path-exists'
 import prepare, { prepareEmpty } from '@pnpm/prepare'
 import rimraf from '@zkochan/rimraf'
 import execa from 'execa'
@@ -145,7 +144,6 @@ test('exit code from plugin is used to end the process', () => {
 
   expect(result.status).toBe(1)
   expect(result.stdout.toString()).toMatch(/is-positive/)
-  expect(pathExists.sync('node_modules/.pnpm-debug.log')).toBeFalsy()
 })
 
 const PNPM_CLI = path.join(__dirname, '../dist/pnpm.cjs')
