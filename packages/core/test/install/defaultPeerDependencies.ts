0e8af7390d6c4cb55256a7d9c8f1eb463a3fee74
Zoltan Kochan <z@kochan.io>
2022-08-15 02:07:26 +0300

    fix: packages with default peer dependencies

--- packages/core/test/install/defaultPeerDependencies.ts
+++ packages/core/test/install/defaultPeerDependencies.ts
@@ -1,7 +1,9 @@
+import path from 'path'
 import { prepareEmpty } from '@pnpm/prepare'
-import {
-  addDependenciesToPackage,
-} from '@pnpm/core'
+import { addDistTag } from '@pnpm/registry-mock'
+import { addDependenciesToPackage } from '@pnpm/core'
+import deepRequireCwd from 'deep-require-cwd'
+import exists from 'path-exists'
 import { testDefaults } from '../utils'
 
 test('package with default peer dependency, when auto install peers is on', async () => {
@@ -32,4 +34,20 @@ test('install the default peer dependency when it cannot be resolved from parent
     '/dep-of-pkg-with-1-dep/100.0.0',
     '/has-default-peer/1.0.0_zoa4n3e4gklatv43gdjet6532m',
   ])
+})
+
+test('package that resolves its own peer dependency', async () => {
+  await addDistTag({ package: 'peer-c', version: '1.0.0', distTag: 'latest' })
+  const project = prepareEmpty()
+  await addDependenciesToPackage({}, ['pkg-with-resolved-peer', 'peer-c@2.0.0'], await testDefaults())
+
+  expect(deepRequireCwd(['pkg-with-resolved-peer', 'peer-c', './package.json']).version).toBe('2.0.0')
+
+  expect(await exists(path.resolve('node_modules/.pnpm/pkg-with-resolved-peer@1.0.0_peer-c@2.0.0/node_modules/pkg-with-resolved-peer'))).toBeTruthy()
+
+  const lockfile = await project.readLockfile()
+
+  expect(lockfile.packages['/pkg-with-resolved-peer/1.0.0_peer-c@2.0.0']?.peerDependencies).toStrictEqual({ 'peer-c': '*' })
+  expect(lockfile.packages['/pkg-with-resolved-peer/1.0.0_peer-c@2.0.0'].dependencies).toHaveProperty(['peer-c'])
+  expect(lockfile.packages['/pkg-with-resolved-peer/1.0.0_peer-c@2.0.0'].optionalDependencies).toHaveProperty(['peer-b'])
 })
