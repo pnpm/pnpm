fe3d4fcfc7e82100834e1f215a95f5f35e603b4f
Zoltan Kochan <z@kochan.io>
2022-08-15 01:15:55 +0300

    fix: packages with default peer dependencies

---
+++ packages/core/test/install/defaultPeerDependencies.ts
@@ -0,0 +1,26 @@
+import { prepareEmpty } from '@pnpm/prepare'
+import {
+  addDependenciesToPackage,
+} from '@pnpm/core'
+import { testDefaults } from '../utils'
+
+/*
+ * peer ^1.0.0
+ *  prod dep 1.0.0
+ * tests to add
+ * 1 auto install peers = true. In this case, the package should just work as if there was no prod dep.
+ *   installs the latest matching ^1.0.0 (1.1.0)
+ *
+ * 2 auto install peers = false. There is dep v2 in the root. default peer is not installed
+ *
+ * 3 auto install peers = false. The prod dep version is installed
+ *
+*/
+
+test('package with default peer dependency, when auto install peers is on', async () => {
+  const project = prepareEmpty()
+  await addDependenciesToPackage({}, ['has-default-peer'], await testDefaults({ autoInstallPeers: true }))
+
+  const lockfile = await project.readLockfile()
+  expect(lockfile.packages).toHaveProperty(['/dep-of-pkg-with-1-dep/100.1.0'])
+})
