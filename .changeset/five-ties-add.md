596af9a7856aa71fb10b5e147ac7dfb069322ab7
Ambar Mutha <muthaa@vmware.com>
2022-08-20 16:45:04 +0530

    fix(node.fetcher): node < 16 download fail on apple silicon
    Add a check for macos apple silicon and required node version < 16.
    In such case, download x64 binary.
    Because arm build does not exist.
    Previous behaviour:
    Try to download non existing arm build and fail with 404.
    
    NO breaking change
    fixes #4489

---
+++ .changeset/five-ties-add.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/node.fetcher": patch
+---
+
+For node version < 16, install x64 build on darwin arm as arm build is not available.
--- packages/node.fetcher/src/getNodeTarball.ts
+++ packages/node.fetcher/src/getNodeTarball.ts
@@ -8,8 +8,10 @@ export function getNodeTarball (
 ) {
   const platform = processPlatform === 'win32' ? 'win' : processPlatform
   const arch = normalizeArch(processPlatform, processArch)
+  const nodeMajorVersion = +nodeVersion.split('.')[0]
+  const nodeBinaryArch = (platform === 'darwin' && arch === 'arm64' && (nodeMajorVersion < 16)) ? 'x64' : arch
   const extension = platform === 'win' ? 'zip' : 'tar.gz'
-  const pkgName = `node-v${nodeVersion}-${platform}-${arch}`
+  const pkgName = `node-v${nodeVersion}-${platform}-${nodeBinaryArch}`
   return {
     pkgName,
     tarball: `${nodeMirror}v${nodeVersion}/${pkgName}.${extension}`,
