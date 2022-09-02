7e047cf759e62e5159058ccd65c544ac10a55feb
Zoltan Kochan <z@kochan.io>
2022-09-02 19:02:06 +0300

    test: resolution mode

--- packages/core/test/install/timeBasedResolutionMode.ts
+++ packages/core/test/install/timeBasedResolutionMode.ts
@@ -1,5 +1,6 @@
 import { prepareEmpty } from '@pnpm/prepare'
-import { addDependenciesToPackage } from '@pnpm/core'
+import { addDistTag } from '@pnpm/registry-mock'
+import { addDependenciesToPackage, install } from '@pnpm/core'
 import { testDefaults } from '../utils'
 
 test('time-based resolution mode', async () => {
@@ -14,4 +15,30 @@ test('time-based resolution mode', async () => {
     '/@pnpm.e2e/romeo-dep/1.0.0',
     '/@pnpm.e2e/romeo/1.0.0',
   ])
+})
+
+test('the lowest version of a direct dependency is installed when resolution mode is time-based', async () => {
+  await addDistTag({ package: '@pnpm.e2e/foo', version: '100.1.0', distTag: 'latest' })
+  const project = prepareEmpty()
+
+  let manifest = await install({
+    dependencies: {
+      '@pnpm.e2e/foo': '^100.0.0',
+    },
+  }, await testDefaults({ resolutionMode: 'time-based' }))
+
+  {
+    const lockfile = await project.readLockfile()
+    expect(lockfile.packages['/@pnpm.e2e/foo/100.0.0']).toBeTruthy()
+  }
+
+  manifest = await install(manifest, await testDefaults({ resolutionMode: 'time-based', update: true }))
+
+  {
+    const lockfile = await project.readLockfile()
+    expect(lockfile.packages['/@pnpm.e2e/foo/100.1.0']).toBeTruthy()
+  }
+  expect(manifest.dependencies).toStrictEqual({
+    '@pnpm.e2e/foo': '^100.1.0',
+  })
 })
