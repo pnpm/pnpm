f676b5fff3a4482d33e6f5d2b2c5fbdc5d8d6eca
lvqq <nicksonlvqq@gmail.com>
2022-09-19 09:50:51 +0800

    fix: error in pnpm --dir <path> link --global

--- packages/plugin-commands-installation/src/link.ts
+++ packages/plugin-commands-installation/src/link.ts
@@ -7,7 +7,7 @@ import {
   tryReadProjectManifest,
 } from '@pnpm/cli-utils'
 import { UNIVERSAL_OPTIONS } from '@pnpm/common-cli-options-help'
-import { Config, types as allTypes } from '@pnpm/config'
+import { CliOptions, Config, types as allTypes } from '@pnpm/config'
 import PnpmError from '@pnpm/error'
 import findWorkspaceDir from '@pnpm/find-workspace-dir'
 import findWorkspacePackages, { arrayOfWorkspacePackagesToMap } from '@pnpm/find-workspace-packages'
@@ -116,7 +116,7 @@ export async function handler (
     const { manifest, writeProjectManifest } = await tryReadProjectManifest(opts.dir, opts)
     const newManifest = await addDependenciesToPackage(
       manifest ?? {},
-      [`link:${cwd}`],
+      [`link:${(opts.cliOptions as CliOptions)?.dir ?? cwd}`],
       linkOpts
     )
     await writeProjectManifest(newManifest)
