41235013d3d54adc998240be67fb7a2ca16f979f
Shinyaigeek <Philispaxil@gmail.com>
2022-09-29 00:17:48 +0900

    refactor: rename field pnpm.update -> pnpm.updateConfig

--- packages/plugin-commands-installation/test/update/update.ts
+++ packages/plugin-commands-installation/test/update/update.ts
@@ -225,7 +225,7 @@ test('update should work normal when set empty string version', async () => {
   expect(lockfile.devDependencies['@pnpm.e2e/peer-c']).toEqual('2.0.0')
 })
 
-test('ignore packages in package.json > pnpm.update.ignoreDependencies fields in update command', async () => {
+test('ignore packages in package.json > Config.ignoreDependencies fields in update command', async () => {
   await addDistTag({ package: '@pnpm.e2e/foo', version: '2.0.0', distTag: 'latest' })
   await addDistTag({ package: '@pnpm.e2e/bar', version: '100.0.0', distTag: 'latest' })
 
@@ -235,7 +235,7 @@ test('ignore packages in package.json > pnpm.update.ignoreDependencies fields in
       '@pnpm.e2e/bar': '100.0.0',
     },
     pnpm: {
-      update: {
+      updateConfig: {
         ignoreDependencies: [
           '@pnpm.e2e/foo',
         ],
@@ -279,7 +279,7 @@ test('not ignore packages if these are specified in parameter even if these are 
       '@pnpm.e2e/bar': '100.0.0',
     },
     pnpm: {
-      update: {
+      updateConfig: {
         ignoreDependencies: [
           '@pnpm.e2e/foo',
         ],
