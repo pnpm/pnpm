d30f3ac680d36012148ac8b2d2f448e37f098db6
Zoltan Kochan <z@kochan.io>
2022-10-02 05:10:39 +0300

    test: set maxWorkers to 1

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,8 @@ module.exports = {
   testTimeout: 4 * 60 * 1000, // 4 minutes
   setupFilesAfterEnv: [path.join(__dirname, "jest.setup.js")],
   cacheDirectory: path.join(__dirname, ".jest-cache", packageName),
+  // Many tests change the dist tags of packages.
+  // Unfortunately, this means that if two such tests will run at the same time,
+  // they may break each other.
+  maxWorkers: 1,
 };
--- packages/plugin-commands-installation/jest.config.js
+++ packages/plugin-commands-installation/jest.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  ...require('../../jest.config.js'),
-  // This is a temporary workaround.
-  // Currently, multiple tests use the @pnpm.e2e/foo package and they change it's dist-tags.
-  // These tests are in separate files, so sometimes they will simultaneously set the dist tag and fail because they expect different versions to be tagged.
-  maxWorkers: 1,
-}
+module.exports = require('../../jest.config.js')
