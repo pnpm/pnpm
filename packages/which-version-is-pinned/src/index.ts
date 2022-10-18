2d5d38a990d981124fef929fac6729121be608d6
luci <haru.lucinyan@gmail.com>
2022-10-18 23:22:37 +0800

    feat(which-version-is-pinned): write actual version to package.json when spec is *

--- packages/which-version-is-pinned/src/index.ts
+++ packages/which-version-is-pinned/src/index.ts
@@ -3,7 +3,7 @@ import { parseRange } from 'semver-utils'
 export function whichVersionIsPinned (spec: string) {
   const isWorkspaceProtocol = spec.startsWith('workspace:')
   if (isWorkspaceProtocol) spec = spec.slice('workspace:'.length)
-  if (spec === '*') return isWorkspaceProtocol ? 'patch' : 'none'
+  if (spec === '*') return isWorkspaceProtocol ? 'patch' : 'major'
   const parsedRange = parseRange(spec)
   if (parsedRange.length !== 1) return undefined
   const versionObject = parsedRange[0]
