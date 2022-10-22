35b6c72fa641216e8da0009ecd107632ce6a9ffa
lvqq <nicksonlvqq@gmail.com>
2022-10-22 10:17:54 +0800

    fix: update

--- packages/plugin-commands-doctor/src/doctor.ts
+++ packages/plugin-commands-doctor/src/doctor.ts
@@ -6,9 +6,7 @@ import { Config } from '@pnpm/config'
 export const rcOptionsTypes = cliOptionsTypes
 
 export function cliOptionsTypes () {
-  return {
-    config: Boolean,
-  }
+  return {}
 }
 
 export const shorthands = {}
@@ -28,12 +26,12 @@ export async function handler (
 ) {
   const { failedToLoadBuiltInConfig } = opts
   if (
-  // If error, means loading npm builtin config failed
+    // If error, means loading npm builtin config failed
     failedToLoadBuiltInConfig &&
-      // Npm installed via brew may have prefix error, related: https://github.com/pnpm/pnpm/issues/5404
-      process.platform === 'darwin' &&
-      process.env.HOMEBREW_PREFIX &&
-      process.execPath.startsWith(process.env.HOMEBREW_PREFIX)
+    // Npm installed via brew may have prefix error, related: https://github.com/pnpm/pnpm/issues/5404
+    process.platform === 'darwin' &&
+    process.env.HOMEBREW_PREFIX &&
+    process.execPath.startsWith(process.env.HOMEBREW_PREFIX)
   ) {
     logger.warn({
       message: 'Load npm builtin configs failed. If the prefix builtin config does not work, you can use "pnpm config ls" to show builtin configs. And then use "pnpm config --global set <key> <value>" to migrate configs from builtin to global.',
