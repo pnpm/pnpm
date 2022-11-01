44a2e30044bd4b7aff3402b37178955b6b1cad87
Weyert de Boer <weyert@innerfuse.biz>
2022-11-01 20:48:17 +0000

    Revert "feat: create `licenses`-command for PNPM"
    This reverts commit 131f8d5f384ce7518d46029905e35ab23f9135da.

--- packages/plugin-commands-licenses/src/licenses.ts
+++ packages/plugin-commands-licenses/src/licenses.ts
@@ -2,55 +2,55 @@ import {
   docsUrl,
   readDepNameCompletions,
   readProjectManifestOnly,
-} from "@pnpm/cli-utils";
-import { CompletionFunc } from "@pnpm/command";
-import { WANTED_LOCKFILE } from "@pnpm/constants";
-import { readWantedLockfile } from "@pnpm/lockfile-file";
+} from '@pnpm/cli-utils'
+import { CompletionFunc } from '@pnpm/command'
+import { WANTED_LOCKFILE } from '@pnpm/constants'
+import { readWantedLockfile } from '@pnpm/lockfile-file'
 import {
   FILTERING,
   OPTIONS,
   UNIVERSAL_OPTIONS,
-} from "@pnpm/common-cli-options-help";
-import { Config, types as allTypes } from "@pnpm/config";
-import { PnpmError } from "@pnpm/error";
-import { licences } from "@pnpm/licenses";
-import pick from "ramda/src/pick";
-import renderHelp from "render-help";
-import { renderLicences } from "./outputRenderer";
+} from '@pnpm/common-cli-options-help'
+import { Config, types as allTypes } from '@pnpm/config'
+import { PnpmError } from '@pnpm/error'
+import { licences } from '@pnpm/licenses'
+import pick from 'ramda/src/pick'
+import renderHelp from 'render-help'
+import { renderLicences } from './outputRenderer'
 
-export function rcOptionsTypes() {
+export function rcOptionsTypes () {
   return {
     ...pick(
       [
-        "depth",
-        "dev",
-        "global-dir",
-        "global",
-        "json",
-        "long",
-        "optional",
-        "production",
+        'depth',
+        'dev',
+        'global-dir',
+        'global',
+        'json',
+        'long',
+        'optional',
+        'production',
       ],
       allTypes
     ),
     compatible: Boolean,
     table: Boolean,
-  };
+  }
 }
 
 export const cliOptionsTypes = () => ({
   ...rcOptionsTypes(),
   recursive: Boolean,
-});
+})
 
 export const shorthands = {
-  D: "--dev",
-  P: "--production",
-};
+  D: '--dev',
+  P: '--production',
+}
 
-export const commandNames = ["licenses"];
+export const commandNames = ['licenses']
 
-export function help() {
+export function help () {
   return renderHelp({
     description: `Check for licenses packages. The check can be limited to a subset of the installed packages by providing arguments (patterns are supported).
 
@@ -60,37 +60,37 @@ pnpm licenses --long
 pnpm licenses gulp-* @babel/core`,
     descriptionLists: [
       {
-        title: "Options",
+        title: 'Options',
 
         list: [
           {
             description:
-              "By default, details about the outdated packages (such as a link to the repo) are not displayed. \
-To display the details, pass this option.",
-            name: "--long",
+              'By default, details about the outdated packages (such as a link to the repo) are not displayed. \
+To display the details, pass this option.',
+            name: '--long',
           },
           {
-            description: "Show information in JSON format",
-            name: "--json",
+            description: 'Show information in JSON format',
+            name: '--json',
           },
           {
             description:
-              "Prints the outdated packages in a list. Good for small consoles",
-            name: "--no-table",
+              'Prints the outdated packages in a list. Good for small consoles',
+            name: '--no-table',
           },
           {
             description: 'Check only "dependencies" and "optionalDependencies"',
-            name: "--prod",
-            shortAlias: "-P",
+            name: '--prod',
+            shortAlias: '-P',
           },
           {
             description: 'Check only "devDependencies"',
-            name: "--dev",
-            shortAlias: "-D",
+            name: '--dev',
+            shortAlias: '-D',
           },
           {
             description: 'Don\'t check "optionalDependencies"',
-            name: "--no-optional",
+            name: '--no-optional',
           },
           OPTIONS.globalDir,
           ...UNIVERSAL_OPTIONS,
@@ -98,90 +98,85 @@ To display the details, pass this option.",
       },
       FILTERING,
     ],
-    url: docsUrl("licenses"),
-    usages: ["pnpm licenses [<pkg> ...]"],
-  });
+    url: docsUrl('licenses'),
+    usages: ['pnpm licenses [<pkg> ...]'],
+  })
 }
 
 export const completion: CompletionFunc = async (cliOpts) => {
-  return readDepNameCompletions(cliOpts.dir as string);
-};
+  return readDepNameCompletions(cliOpts.dir as string)
+}
 
 export type LicensesCommandOptions = {
-  compatible?: boolean;
-  long?: boolean;
-  recursive?: boolean;
-  json?: boolean;
+  compatible?: boolean
+  long?: boolean
+  recursive?: boolean
+  json?: boolean
 } & Pick<
-  Config,
-  | "allProjects"
-  | "ca"
-  | "cacheDir"
-  | "cert"
-  | "dev"
-  | "dir"
-  | "engineStrict"
-  | "fetchRetries"
-  | "fetchRetryFactor"
-  | "fetchRetryMaxtimeout"
-  | "fetchRetryMintimeout"
-  | "fetchTimeout"
-  | "global"
-  | "httpProxy"
-  | "httpsProxy"
-  | "key"
-  | "localAddress"
-  | "lockfileDir"
-  | "networkConcurrency"
-  | "noProxy"
-  | "offline"
-  | "optional"
-  | "production"
-  | "rawConfig"
-  | "registries"
-  | "selectedProjectsGraph"
-  | "strictSsl"
-  | "tag"
-  | "userAgent"
-  | "virtualStoreDir"
-  | "modulesDir"
+Config,
+| 'allProjects'
+| 'ca'
+| 'cacheDir'
+| 'cert'
+| 'dev'
+| 'dir'
+| 'engineStrict'
+| 'fetchRetries'
+| 'fetchRetryFactor'
+| 'fetchRetryMaxtimeout'
+| 'fetchRetryMintimeout'
+| 'fetchTimeout'
+| 'global'
+| 'httpProxy'
+| 'httpsProxy'
+| 'key'
+| 'localAddress'
+| 'lockfileDir'
+| 'networkConcurrency'
+| 'noProxy'
+| 'offline'
+| 'optional'
+| 'production'
+| 'rawConfig'
+| 'registries'
+| 'selectedProjectsGraph'
+| 'strictSsl'
+| 'tag'
+| 'userAgent'
+| 'virtualStoreDir'
+| 'modulesDir'
 > &
-  Partial<Pick<Config, "userConfig">>;
+Partial<Pick<Config, 'userConfig'>>
 
-export async function handler(
+export async function handler (
   opts: LicensesCommandOptions,
   params: string[] = []
 ) {
-  const lockfile = await readWantedLockfile(opts.lockfileDir ?? opts.dir, {
-    ignoreIncompatible: true,
-  });
+  const lockfile = await readWantedLockfile(opts.lockfileDir ?? opts.dir, { ignoreIncompatible: true })
   if (lockfile == null) {
-    throw new PnpmError(
-      "LICENSES_NO_LOCKFILE",
-      `No ${WANTED_LOCKFILE} found: Cannot check a project without a lockfile`
-    );
+    throw new PnpmError('LICENSES_NO_LOCKFILE', `No ${WANTED_LOCKFILE} found: Cannot check a project without a lockfile`)
   }
 
   const include = {
     dependencies: opts.production !== false,
     devDependencies: opts.dev !== false,
     optionalDependencies: opts.optional !== false,
-  };
+  }
 
-  const manifest = await readProjectManifestOnly(opts.dir, opts);
+  const manifest = await readProjectManifestOnly(opts.dir, opts)
 
   const licensePackages = await licences({
     include,
     lockfileDir: opts.dir,
     prefix: opts.dir,
-    virtualStoreDir: opts.virtualStoreDir ?? ".",
+    virtualStoreDir: opts.virtualStoreDir ?? '.',
     modulesDir: opts.modulesDir,
     registries: opts.registries,
     wantedLockfile: lockfile,
     manifest,
-  });
+  })
 
-  if (licensePackages.length === 0) return { output: "", exitCode: 0 };
+  if (licensePackages.length === 0) return { output: '', exitCode: 0 }
 
-  return renderLicences(licensePackages, opts);
+  return renderLicences(licensePackages, opts)
 }
--- packages/plugin-commands-licenses/src/outputRenderer.ts
+++ packages/plugin-commands-licenses/src/outputRenderer.ts
@@ -1,80 +1,80 @@
-import { TABLE_OPTIONS } from "@pnpm/cli-utils";
-import { LicensePackage } from "@pnpm/licenses";
-import chalk from "chalk";
-import stripAnsi from "strip-ansi";
-import { table } from "@zkochan/table";
-import { groupBy, sortWith } from "ramda";
+import { TABLE_OPTIONS } from '@pnpm/cli-utils'
+import { LicensePackage } from '@pnpm/licenses'
+import chalk from 'chalk'
+import stripAnsi from 'strip-ansi'
+import { table } from '@zkochan/table'
+import { groupBy, sortWith } from 'ramda'
 
 /**
  *
  * @param licensePackages
  * @returns
  */
-function sortLicensesPackages(licensePackages: readonly LicensePackage[]) {
+function sortLicensesPackages (licensePackages: readonly LicensePackage[]) {
   return sortWith(
     [
       (o1: LicensePackage, o2: LicensePackage) =>
         o1.license.localeCompare(o2.license),
     ],
     licensePackages
-  );
+  )
 }
 
-export function getCellWidth(
+export function getCellWidth (
   data: string[][],
   columnNumber: number,
   maxWidth: number
 ) {
   const maxCellWidth = data.reduce((cellWidth, row) => {
-    const cellLines = stripAnsi(row[columnNumber])?.split("\n") ?? [];
+    const cellLines = stripAnsi(row[columnNumber])?.split('\n') ?? []
     const currentCellWidth = cellLines.reduce((lineWidth, line) => {
-      return Math.max(lineWidth, line.length);
-    }, 0);
-    return Math.max(cellWidth, currentCellWidth);
-  }, 0);
-  return Math.min(maxWidth, maxCellWidth);
+      return Math.max(lineWidth, line.length)
+    }, 0)
+    return Math.max(cellWidth, currentCellWidth)
+  }, 0)
+  return Math.min(maxWidth, maxCellWidth)
 }
 
-export function renderPackageName({ belongsTo, packageName }: LicensePackage) {
+export function renderPackageName ({ belongsTo, packageName }: LicensePackage) {
   switch (belongsTo) {
-    case "devDependencies":
-      return `${packageName} ${chalk.dim("(dev)")}`;
-    case "optionalDependencies":
-      return `${packageName} ${chalk.dim("(optional)")}`;
-    default:
-      return packageName as string;
+  case 'devDependencies':
+    return `${packageName} ${chalk.dim('(dev)')}`
+  case 'optionalDependencies':
+    return `${packageName} ${chalk.dim('(optional)')}`
+  default:
+    return packageName as string
   }
 }
 
-export function renderPackageLicense({ license }: LicensePackage) {
-  const output = license ?? "Unknown";
-  return output as string;
+export function renderPackageLicense ({ license }: LicensePackage) {
+  const output = license ?? 'Unknown'
+  return output as string
 }
 
-export function renderDetails(licensePackage: LicensePackage) {
-  const { packageManifest, author } = licensePackage;
-  if (packageManifest == null) return "";
-  const outputs = [];
+export function renderDetails (licensePackage: LicensePackage) {
+  const { packageManifest, author } = licensePackage
+  if (packageManifest == null) return ''
+  const outputs = []
   if (author) {
-    outputs.push(author);
+    outputs.push(author)
   }
   if (packageManifest.homepage) {
-    outputs.push(chalk.underline(packageManifest.homepage));
+    outputs.push(chalk.underline(packageManifest.homepage))
   }
-  return outputs.join("\n");
+  return outputs.join('\n')
 }
-export function renderLicences(
+export function renderLicences (
   licensesMap: LicensePackage[],
-  opts: { long?: boolean; json?: boolean }
+  opts: { long?: boolean, json?: boolean }
 ) {
   if (opts.json) {
-    return { output: renderLicensesJson(licensesMap), exitCode: 0 };
+    return { output: renderLicensesJson(licensesMap), exitCode: 0 }
   }
 
-  return { output: renderLicensesTable(licensesMap, opts), exitCode: 0 };
+  return { output: renderLicensesTable(licensesMap, opts), exitCode: 0 }
 }
 
-function renderLicensesJson(licensePackages: readonly LicensePackage[]) {
+function renderLicensesJson (licensePackages: readonly LicensePackage[]) {
   const data = [
     ...licensePackages.map((licensePkg) => {
       return {
@@ -85,49 +85,50 @@ function renderLicensesJson(licensePackages: readonly LicensePackage[]) {
         licenseContents: licensePkg.licenseContents,
         vendorName: licensePkg.author,
         vendorUrl: licensePkg.packageManifest?.homepage,
-      } as LicensePackageJson;
+      } as LicensePackageJson
     }),
-  ].flat();
+  ].flat()
 
   // Group the package by license
-  const groupByLicense = groupBy((item: LicensePackageJson) => item.license);
-  const groupedByLicense = groupByLicense(data);
+  const groupByLicense = groupBy((item: LicensePackageJson) => item.license)
+  const groupedByLicense = groupByLicense(data)
 
-  return JSON.stringify(groupedByLicense, null, 2);
+  return JSON.stringify(groupedByLicense, null, 2)
 }
 
 export interface LicensePackageJson {
-  name: string;
-  license: string;
-  vendorName: string;
-  vendorUrl: string;
-  path: string;
+  name: string
+  license: string
+  vendorName: string
+  vendorUrl: string
+  path: string
 }
 
-function renderLicensesTable(
+function renderLicensesTable (
   licensePackages: readonly LicensePackage[],
   opts: { long?: boolean }
 ) {
-  const columnNames = ["Package", "License"];
+  const columnNames = ['Package', 'License']
 
-  const columnFns = [renderPackageName, renderPackageLicense];
+  const columnFns = [renderPackageName, renderPackageLicense]
 
   if (opts.long) {
-    columnNames.push("Details");
-    columnFns.push(renderDetails);
+    columnNames.push('Details')
+    columnFns.push(renderDetails)
   }
 
   // Avoid the overhead of allocating a new array caused by calling `array.map()`
   for (let i = 0; i < columnNames.length; i++)
-    columnNames[i] = chalk.blueBright(columnNames[i]);
+    columnNames[i] = chalk.blueBright(columnNames[i])
 
   return table(
     [
       columnNames,
       ...sortLicensesPackages(licensePackages).map((licensePkg) => {
-        return columnFns.map((fn) => fn(licensePkg));
-      }),
+        return columnFns.map((fn) => fn(licensePkg))
+      }
+      ),
     ],
     TABLE_OPTIONS
-  );
+  )
 }
