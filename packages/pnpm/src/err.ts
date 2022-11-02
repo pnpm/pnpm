14974d8029b645b618bbd6cf86b4ae04f4368769
Zoltan Kochan <z@kochan.io>
2022-11-02 10:36:42 +0200

    fix: cannot set property name of which has only a getter (#5574)
    close #5572

--- packages/pnpm/src/err.ts
+++ packages/pnpm/src/err.ts
@@ -11,7 +11,11 @@ export function errorHandler (error: Error) {
     return
   }
   if (error.name != null && error.name !== 'pnpm' && !error.name.startsWith('pnpm:')) {
-    error.name = 'pnpm'
+    try {
+      error.name = 'pnpm'
+    } catch {
+      // Sometimes the name property is read-only
+    }
   }
 
   // bole passes only the name, message and stack of an error
