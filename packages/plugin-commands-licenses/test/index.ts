71d8d3971d81c142564a93412044bee6abfd7ac6
Weyert de Boer <weyert@innerfuse.biz>
2022-11-04 00:17:21 +0000

    style: fix styling issues in the code

--- packages/plugin-commands-licenses/test/index.ts
+++ packages/plugin-commands-licenses/test/index.ts
@@ -1,16 +1,16 @@
 /// <reference path="../../../typings/index.d.ts" />
-import path from "path";
-import { licenses } from "@pnpm/plugin-commands-licenses";
-import { install } from "@pnpm/plugin-commands-installation";
-import { REGISTRY_MOCK_PORT } from "@pnpm/registry-mock";
-import stripAnsi from "strip-ansi";
-import { DEFAULT_OPTS } from "./utils";
-import tempy from "tempy";
+import path from 'path'
+import { licenses } from '@pnpm/plugin-commands-licenses'
+import { install } from '@pnpm/plugin-commands-installation'
+import { REGISTRY_MOCK_PORT } from '@pnpm/registry-mock'
+import stripAnsi from 'strip-ansi'
+import { DEFAULT_OPTS } from './utils'
+import tempy from 'tempy'
 
-const REGISTRY_URL = `http://localhost:${REGISTRY_MOCK_PORT}`;
+const REGISTRY_URL = `http://localhost:${REGISTRY_MOCK_PORT}`
 
 const LICENSES_OPTIONS = {
-  cacheDir: "cache",
+  cacheDir: 'cache',
   fetchRetries: 1,
   fetchRetryFactor: 1,
   fetchRetryMaxtimeout: 60,
@@ -21,116 +21,116 @@ const LICENSES_OPTIONS = {
   rawConfig: { registry: REGISTRY_URL },
   registries: { default: REGISTRY_URL },
   strictSsl: false,
-  tag: "latest",
-  userAgent: "",
+  tag: 'latest',
+  userAgent: '',
   userConfig: {},
-};
+}
 
-test("pnpm licenses", async () => {
-  const workspaceDir = path.resolve("./test/fixtures/complex-licenses");
+test('pnpm licenses', async () => {
+  const workspaceDir = path.resolve('./test/fixtures/complex-licenses')
 
-  const tmp = tempy.directory();
-  const storeDir = path.join(tmp, "store");
+  const tmp = tempy.directory()
+  const storeDir = path.join(tmp, 'store')
   await install.handler({
     ...DEFAULT_OPTS,
     dir: workspaceDir,
-    pnpmHomeDir: "",
+    pnpmHomeDir: '',
     storeDir,
-  });
+  })
 
   // Attempt to run the licenses command now
   const { output, exitCode } = await licenses.handler({
     ...LICENSES_OPTIONS,
     dir: workspaceDir,
-    pnpmHomeDir: "",
+    pnpmHomeDir: '',
     long: false,
     // we need to prefix it with v3 otherwise licenses tool can't find anything
     // in the content-addressable directory
-    storeDir: path.resolve(storeDir, "v3"),
-  });
+    storeDir: path.resolve(storeDir, 'v3'),
+  })
 
-  expect(exitCode).toBe(0);
-  expect(stripAnsi(output)).toMatchSnapshot("show-packages");
-});
+  expect(exitCode).toBe(0)
+  expect(stripAnsi(output)).toMatchSnapshot('show-packages')
+})
 
-test("pnpm licenses: show details", async () => {
-  const workspaceDir = path.resolve("./test/fixtures/simple-licenses");
+test('pnpm licenses: show details', async () => {
+  const workspaceDir = path.resolve('./test/fixtures/simple-licenses')
 
-  const tmp = tempy.directory();
-  const storeDir = path.join(tmp, "store");
+  const tmp = tempy.directory()
+  const storeDir = path.join(tmp, 'store')
   await install.handler({
     ...DEFAULT_OPTS,
     dir: workspaceDir,
-    pnpmHomeDir: "",
+    pnpmHomeDir: '',
     storeDir,
-  });
+  })
 
   // Attempt to run the licenses command now
   const { output, exitCode } = await licenses.handler({
     ...LICENSES_OPTIONS,
     dir: workspaceDir,
-    pnpmHomeDir: "",
+    pnpmHomeDir: '',
     long: true,
     // we need to prefix it with v3 otherwise licenses tool can't find anything
     // in the content-addressable directory
-    storeDir: path.resolve(storeDir, "v3"),
-  });
+    storeDir: path.resolve(storeDir, 'v3'),
+  })
 
-  expect(exitCode).toBe(0);
-  expect(stripAnsi(output)).toMatchSnapshot("show-packages-details");
-});
+  expect(exitCode).toBe(0)
+  expect(stripAnsi(output)).toMatchSnapshot('show-packages-details')
+})
 
-test("pnpm licenses: output as json", async () => {
-  const workspaceDir = path.resolve("./test/fixtures/simple-licenses");
+test('pnpm licenses: output as json', async () => {
+  const workspaceDir = path.resolve('./test/fixtures/simple-licenses')
 
-  const tmp = tempy.directory();
-  const storeDir = path.join(tmp, "store");
+  const tmp = tempy.directory()
+  const storeDir = path.join(tmp, 'store')
   await install.handler({
     ...DEFAULT_OPTS,
     dir: workspaceDir,
-    pnpmHomeDir: "",
+    pnpmHomeDir: '',
     storeDir,
-  });
+  })
 
   // Attempt to run the licenses command now
   const { output, exitCode } = await licenses.handler({
     ...LICENSES_OPTIONS,
     dir: workspaceDir,
-    pnpmHomeDir: "",
+    pnpmHomeDir: '',
     long: false,
     json: true,
     // we need to prefix it with v3 otherwise licenses tool can't find anything
     // in the content-addressable directory
-    storeDir: path.resolve(storeDir, "v3"),
-  });
+    storeDir: path.resolve(storeDir, 'v3'),
+  })
 
-  expect(exitCode).toBe(0);
-  expect(output).not.toHaveLength(0);
-  expect(output).not.toBe("No packages found");
-  const parsedOutput = JSON.parse(output);
-  expect(Object.keys(parsedOutput)).toMatchSnapshot("found-license-types");
-  const packagesWithMIT = parsedOutput["MIT"];
-  expect(packagesWithMIT.length).toBeGreaterThan(0);
+  expect(exitCode).toBe(0)
+  expect(output).not.toHaveLength(0)
+  expect(output).not.toBe('No packages found')
+  const parsedOutput = JSON.parse(output)
+  expect(Object.keys(parsedOutput)).toMatchSnapshot('found-license-types')
+  const packagesWithMIT = parsedOutput['MIT']
+  expect(packagesWithMIT.length).toBeGreaterThan(0)
   expect(Object.keys(packagesWithMIT[0])).toEqual([
-    "name",
-    "version",
-    "path",
-    "license",
-    "vendorName",
-    "vendorUrl",
-  ]);
-  expect(packagesWithMIT[0].name).toBe("is-positive");
-});
+    'name',
+    'version',
+    'path',
+    'license',
+    'vendorName',
+    'vendorUrl',
+  ])
+  expect(packagesWithMIT[0].name).toBe('is-positive')
+})
 
-test("pnpm licenses: fails when lockfile is missing", async () => {
+test('pnpm licenses: fails when lockfile is missing', async () => {
   await expect(
     licenses.handler({
       ...LICENSES_OPTIONS,
-      dir: path.resolve("./test/fixtures/invalid"),
-      pnpmHomeDir: "",
+      dir: path.resolve('./test/fixtures/invalid'),
+      pnpmHomeDir: '',
       long: true,
     })
   ).rejects.toThrowErrorMatchingInlineSnapshot(
     '"No pnpm-lock.yaml found: Cannot check a project without a lockfile"'
-  );
-});
+  )
+})
