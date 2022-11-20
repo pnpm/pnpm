e8dd6566e85453854447baebd4aacf41e6c05e77
Zoltan Kochan <z@kochan.io>
2022-11-20 03:17:20 +0200

    chore: fix pd.js

--- pnpm/dev/pd.js
+++ pnpm/dev/pd.js
@@ -2,42 +2,49 @@
 const fs = require('fs')
 const esbuild = require('esbuild')
 const pathLib = require('path')
-const packagesDir = pathLib.resolve(__dirname, '../..')
-
-const localPackages = fs.readdirSync(packagesDir)
+const { findWorkspacePackagesNoCheck } = require('@pnpm/find-workspace-packages')
+const { findWorkspaceDir } = require('@pnpm/find-workspace-dir')
 
 const pnpmPackageJson = JSON.parse(fs.readFileSync(pathLib.join(__dirname, 'package.json'), 'utf8'))
 
-// This plugin rewrites imports to reference the `src` dir instead of `lib` so
-// esbuild can compile the original TypeScript
-const spnpmImportsPlugin = {
-  name: 'spnpmImports',
-  setup: (build) => {
-    // This is an exception to the rule that all local packages start with `@pnpm`
-    build.onResolve({ filter: /^dependency-path$/ }, ({path, resolveDir}) => ({
-      path: pathLib.resolve(packagesDir, 'dependency-path', 'src', 'index.ts')
-    }))
-
-    // E.g. @pnpm/config -> /<some_dir>/pnpm/packages/config/src/index.ts
-    build.onResolve({ filter: /@pnpm\// }, ({path, resolveDir}) => {
-      const pathParts = path.split('/')
-      const packageName = pathParts[1]
-
-      // Bail if the package isn't present locally
-      if (!localPackages.includes(packageName)) {
-        return
-      }
-
-      const newPath = pathLib.resolve(packagesDir, packageName, 'src', 'index.ts')
-
-      return {
-        path: newPath
-      }
-    })
-  }
-}
-
 ;(async () => {
+  const workspaceDir = await findWorkspaceDir(process.cwd())
+  const pkgs = await findWorkspacePackagesNoCheck(workspaceDir)
+  const localPackages = pkgs.map(pkg => pkg.manifest.name)
+  const dirByPackageName = pkgs.reduce((acc, pkg) => {
+    acc[pkg.manifest.name] = pkg.dir
+    return acc
+  })
+
+  // This plugin rewrites imports to reference the `src` dir instead of `lib` so
+  // esbuild can compile the original TypeScript
+  const spnpmImportsPlugin = {
+    name: 'spnpmImports',
+    setup: (build) => {
+      // This is an exception to the rule that all local packages start with `@pnpm`
+      build.onResolve({ filter: /^dependency-path$/ }, ({path, resolveDir}) => ({
+        path: pathLib.resolve(dirByPackageName['dependency-path'], 'src', 'index.ts')
+      }))
+
+      // E.g. @pnpm/config -> /<some_dir>/pnpm/packages/config/src/index.ts
+      build.onResolve({ filter: /@pnpm\// }, ({path, resolveDir}) => {
+        const pathParts = path.split('/')
+        const packageName = pathParts[1]
+
+        // Bail if the package isn't present locally
+        if (!localPackages.includes(packageName)) {
+          return
+        }
+
+        const newPath = pathLib.resolve(dirByPackageName[packageName], packageName, 'src', 'index.ts')
+
+        return {
+          path: newPath
+        }
+      })
+    }
+  }
+
   await esbuild.build({
     bundle: true,
     platform: 'node',
