feb8e93bb498122feb33ca70939b15b4309e6289
Zoltan Kochan <z@kochan.io>
2022-11-25 03:55:17 +0200

    refactor: dedupe deps

--- pkg-manager/core/test/install/dedupeDirectDeps.ts
+++ pkg-manager/core/test/install/dedupeDirectDeps.ts
@@ -14,6 +14,10 @@ test('dedupe direct dependencies', async () => {
       location: 'project-2',
       package: { name: 'project-2' },
     },
+    {
+      location: 'project-3',
+      package: { name: 'project-3' },
+    },
   ])
 
   const importers: MutatedProject[] = [
@@ -24,6 +28,10 @@ test('dedupe direct dependencies', async () => {
     {
       mutation: 'install',
       rootDir: path.resolve('project-2'),
+    },
+    {
+      mutation: 'install',
+      rootDir: path.resolve('project-3'),
     },
   ]
   const allProjects = [
@@ -52,9 +60,22 @@ test('dedupe direct dependencies', async () => {
       },
       rootDir: path.resolve('project-2'),
     },
+    {
+      buildIndex: 0,
+      manifest: {
+        name: 'project-3',
+        version: '1.0.0',
+
+        dependencies: {
+          'is-negative': '1.0.0',
+        },
+      },
+      rootDir: path.resolve('project-3'),
+    },
   ]
   await mutateModules(importers, await testDefaults({ allProjects, dedupeDirectDeps: true }))
   await projects['project-2'].has('is-negative')
+  await projects['project-3'].has('is-negative')
 
   allProjects[0].manifest.dependencies['is-negative'] = '1.0.0'
   allProjects[1].manifest.dependencies['is-positive'] = '1.0.0'
@@ -69,4 +90,6 @@ test('dedupe direct dependencies', async () => {
     'is-positive',
   ])
   expect(fs.readdirSync('project-2/node_modules').sort()).toEqual(['is-odd'])
+  await projects['project-3'].hasNot('is-negative')
+  expect(fs.existsSync('project-3/node_modules')).toBeFalsy()
 })
--- pkg-manager/direct-dep-linker/src/linkDirectDeps.ts
+++ pkg-manager/direct-dep-linker/src/linkDirectDeps.ts
@@ -4,6 +4,7 @@ import { rootLogger } from '@pnpm/core-loggers'
 import { symlinkDependency, symlinkDirectRootDependency } from '@pnpm/symlink-dependency'
 import omit from 'ramda/src/omit'
 import { readModulesDir } from '@pnpm/read-modules-dir'
+import rimraf from '@zkochan/rimraf'
 import resolveLinkTarget from 'resolve-link-target'
 
 export interface LinkedDirectDep {
@@ -43,11 +44,18 @@ async function linkDirectDepsAndDedupe (
   const pkgsLinkedToRoot = await readLinkedDeps(rootProject.modulesDir)
   await Promise.all(
     Object.values(projects).map(async (project) => {
-      await deletePkgsPresentInRoot(project.modulesDir, pkgsLinkedToRoot)
-      return linkDirectDepsOfProject({
-        ...project,
-        dependencies: omitDepsFromRoot(project.dependencies, pkgsLinkedToRoot),
-      })
+      const deletedAll = await deletePkgsPresentInRoot(project.modulesDir, pkgsLinkedToRoot)
+      const dependencies = omitDepsFromRoot(project.dependencies, pkgsLinkedToRoot)
+      if (dependencies.length > 0) {
+        await linkDirectDepsOfProject({
+          ...project,
+          dependencies,
+        })
+        return
+      }
+      if (deletedAll) {
+        await rimraf(project.modulesDir)
+      }
     })
   )
 }
@@ -70,14 +78,15 @@ async function readLinkedDeps (modulesDir: string): Promise<string[]> {
 async function deletePkgsPresentInRoot (
   modulesDir: string,
   pkgsLinkedToRoot: string[]
-) {
+): Promise<boolean> {
   const pkgsLinkedToCurrentProject = await readLinkedDepsWithRealLocations(modulesDir)
-  await Promise.all(
+  const removedCount = (await Promise.all(
     pkgsLinkedToCurrentProject
       .filter(({ linkedFrom }) => pkgsLinkedToRoot
         .some((pkgLinkedToRoot) => path.relative(linkedFrom, pkgLinkedToRoot) === ''))
       .map(({ linkedTo }) => fs.promises.unlink(linkedTo))
-  )
+  )).length
+  return removedCount === pkgsLinkedToCurrentProject.length
 }
 
 async function readLinkedDepsWithRealLocations (modulesDir: string) {
