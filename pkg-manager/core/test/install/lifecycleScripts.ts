pnpm/pnpm

commit 3360c9f4b06757cf352a3ff5a9278500aa3eaced

perf: do not build the same dependency multiple times when node-linker is hoisted (#5814)

pkg-manager/core/test/install/lifecycleScripts.ts
@@ -1,11 +1,14 @@
 import * as path from 'path'
 import { promises as fs } from 'fs'
+import { assertProject } from '@pnpm/assert-project'
 import { LifecycleLog } from '@pnpm/core-loggers'
-import { prepareEmpty } from '@pnpm/prepare'
+import { prepareEmpty, preparePackages } from '@pnpm/prepare'
 import {
   addDependenciesToPackage,
   install,
   mutateModulesInSingleProject,
+  MutatedProject,
+  mutateModules,
 } from '@pnpm/core'
 import rimraf from '@zkochan/rimraf'
 import isWindows from 'is-windows'
@@ -607,4 +610,109 @@ test('ignore-dep-scripts', async () => {
 
     expect(await exists('node_modules/@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')).toBeFalsy()
   }
+})
+
+test('run pre/postinstall scripts in a workspace that uses node-linker=hoisted', async () => {
+  const projects = preparePackages([
+    {
+      location: 'project-1',
+      package: { name: 'project-1' },
+    },
+    {
+      location: 'project-2',
+      package: { name: 'project-2' },
+    },
+    {
+      location: 'project-3',
+      package: { name: 'project-3' },
+    },
+    {
+      location: 'project-4',
+      package: { name: 'project-4' },
+    },
+  ])
+  const importers: MutatedProject[] = [
+    {
+      mutation: 'install',
+      rootDir: path.resolve('project-1'),
+    },
+    {
+      mutation: 'install',
+      rootDir: path.resolve('project-2'),
+    },
+    {
+      mutation: 'install',
+      rootDir: path.resolve('project-3'),
+    },
+    {
+      mutation: 'install',
+      rootDir: path.resolve('project-4'),
+    },
+  ]
+  const allProjects = [
+    {
+      buildIndex: 0,
+      manifest: {
+        name: 'project-1',
+        version: '1.0.0',
+
+        dependencies: {
+          '@pnpm.e2e/pre-and-postinstall-scripts-example': '1',
+        },
+      },
+      rootDir: path.resolve('project-1'),
+    },
+    {
+      buildIndex: 0,
+      manifest: {
+        name: 'project-2',
+        version: '1.0.0',
+
+        dependencies: {
+          '@pnpm.e2e/pre-and-postinstall-scripts-example': '1',
+        },
+      },
+      rootDir: path.resolve('project-2'),
+    },
+    {
+      buildIndex: 0,
+      manifest: {
+        name: 'project-3',
+        version: '1.0.0',
+
+        dependencies: {
+          '@pnpm.e2e/pre-and-postinstall-scripts-example': '2',
+        },
+      },
+      rootDir: path.resolve('project-3'),
+    },
+    {
+      buildIndex: 0,
+      manifest: {
+        name: 'project-4',
+        version: '1.0.0',
+
+        dependencies: {
+          '@pnpm.e2e/pre-and-postinstall-scripts-example': '2',
+        },
+      },
+      rootDir: path.resolve('project-4'),
+    },
+  ]
+  await mutateModules(importers, await testDefaults({
+    allProjects,
+    fastUnpack: false,
+    nodeLinker: 'hoisted',
+  }))
+  const rootProject = assertProject(process.cwd())
+  await rootProject.has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')
+  await rootProject.has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall.js')
+  await projects['project-1'].hasNot('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')
+  await projects['project-1'].hasNot('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall.js')
+  await projects['project-2'].hasNot('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')
+  await projects['project-2'].hasNot('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall.js')
+  await projects['project-3'].has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')
+  await projects['project-3'].has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall.js')
+  await projects['project-4'].has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')
+  await projects['project-4'].has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall.js')
 })

exec/build-modules/src/index.ts
@@ -5,9 +5,11 @@ import { PnpmError } from '@pnpm/error'
 import { runPostinstallHooks } from '@pnpm/lifecycle'
 import { linkBins, linkBinsOfPackages } from '@pnpm/link-bins'
 import { logger } from '@pnpm/logger'
+import { hardLinkDir } from '@pnpm/fs.hard-link-dir'
 import { readPackageJsonFromDir, safeReadPackageJsonFromDir } from '@pnpm/read-package-json'
 import { StoreController } from '@pnpm/store-controller-types'
 import { DependencyManifest } from '@pnpm/types'
+import pDefer, { DeferredPromise } from 'p-defer'
 import { applyPatch } from 'patch-package/dist/applyPatches'
 import pickBy from 'ramda/src/pickBy'
 import runGroups from 'run-groups'
@@ -38,12 +40,16 @@ export async function buildModules (
     sideEffectsCacheWrite: boolean
     storeController: StoreController
     rootModulesDir: string
+    hoistedLocations?: Record<string, string[]>
   }
 ) {
   const warn = (message: string) => logger.warn({ message, prefix: opts.lockfileDir })
   // postinstall hooks
 
   const buildDepOpts = { ...opts, warn }
+  if (opts.hoistedLocations) {
+    buildDepOpts['builtHoistedDeps'] = {}
+  }
   const chunks = buildSequence(depGraph, rootDepPaths)
   const groups = chunks.map((chunk) => {
     chunk = chunk.filter((depPath) => {
@@ -81,10 +87,19 @@ async function buildDependency (
     sideEffectsCacheWrite: boolean
     storeController: StoreController
     unsafePerm: boolean
+    hoistedLocations?: Record<string, string[]>
+    builtHoistedDeps?: Record<string, DeferredPromise<void>>
     warn: (message: string) => void
   }
 ) {
   const depNode = depGraph[depPath]
+  if (opts.builtHoistedDeps) {
+    if (opts.builtHoistedDeps[depNode.depPath]) {
+      await opts.builtHoistedDeps[depNode.depPath].promise
+      return
+    }
+    opts.builtHoistedDeps[depNode.depPath] = pDefer()
+  }
   try {
     await linkBinsOfDependencies(depNode, depGraph, opts)
     const isPatched = depNode.patchFile?.path != null
@@ -147,6 +162,18 @@ async function buildDependency (
       return
     }
     throw err
+  } finally {
+    const hoistedLocationsOfDep = opts.hoistedLocations?.[depNode.depPath]
+    if (hoistedLocationsOfDep) {
+      // There is no need to build the same package in every location.
+      // We just copy the built package to every location where it is present.
+      const currentHoistedLocation = path.relative(opts.lockfileDir, depNode.dir)
+      const nonBuiltHoistedDeps = hoistedLocationsOfDep?.filter((hoistedLocation) => hoistedLocation !== currentHoistedLocation)
+      await hardLinkDir(depNode.dir, nonBuiltHoistedDeps)
+    }
+    if (opts.builtHoistedDeps) {
+      opts.builtHoistedDeps[depNode.depPath].resolve()
+    }
   }
 }
 

pkg-manager/headless/src/index.ts
@@ -467,6 +467,7 @@ export async function headlessInstall (opts: HeadlessOptions) {
       extraEnv,
       depsStateCache,
       ignoreScripts: opts.ignoreScripts || opts.ignoreDepScripts,
+      hoistedLocations,
       lockfileDir,
       optional: opts.include.optionalDependencies,
       preferSymlinkedExecutables: opts.preferSymlinkedExecutables,

pnpm-lock.yaml
@@ -857,6 +857,9 @@ importers:
       '@pnpm/error':
         specifier: workspace:*
         version: link:../../packages/error
+      '@pnpm/fs.hard-link-dir':
+        specifier: workspace:*
+        version: link:../../fs/hard-link-dir
       '@pnpm/graph-sequencer':
         specifier: 1.0.0
         version: 1.0.0
@@ -878,6 +881,9 @@ importers:
       '@pnpm/types':
         specifier: workspace:*
         version: link:../../packages/types
+      p-defer:
+        specifier: ^3.0.0
+        version: 3.0.0
       patch-package:
         specifier: ^6.5.0
         version: 6.5.0
@@ -6767,7 +6773,7 @@ packages:
     engines: {node: ^14.15.0 || ^16.10.0 || >=18.0.0}
     dependencies:
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       chalk: 4.1.2
       jest-message-util: 29.3.1
       jest-util: 29.3.1
@@ -6788,14 +6794,14 @@ packages:
       '@jest/test-result': 29.3.1
       '@jest/transform': 29.3.1_@babel+types@7.20.5
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       ansi-escapes: 4.3.2
       chalk: 4.1.2
       ci-info: 3.6.1
       exit: 0.1.2
       graceful-fs: 4.2.10
       jest-changed-files: 29.2.0
-      jest-config: 29.3.1_x4zmdo4imr2n2hhykfyzszwk6y
+      jest-config: 29.3.1_uvwivoxixlgocwjbikx7evbpf4
       jest-haste-map: 29.3.1
       jest-message-util: 29.3.1
       jest-regex-util: 29.2.0
@@ -6883,7 +6889,7 @@ packages:
       '@jest/transform': 29.3.1_@babel+types@7.20.5
       '@jest/types': 29.3.1
       '@jridgewell/trace-mapping': 0.3.17
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       chalk: 4.1.2
       collect-v8-coverage: 1.0.1
       exit: 0.1.2
@@ -6973,7 +6979,7 @@ packages:
       '@jest/schemas': 29.0.0
       '@types/istanbul-lib-coverage': 2.0.4
       '@types/istanbul-reports': 3.0.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       '@types/yargs': 17.0.13
       chalk: 4.1.2
     dev: true
@@ -6985,7 +6991,7 @@ packages:
       '@jest/schemas': 29.0.0
       '@types/istanbul-lib-coverage': 2.0.4
       '@types/istanbul-reports': 3.0.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       '@types/yargs': 17.0.13
       chalk: 4.1.2
     dev: true
@@ -8469,7 +8475,7 @@ packages:
     dependencies:
       '@types/http-cache-semantics': 4.0.1
       '@types/keyv': 3.1.4
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       '@types/responselike': 1.0.0
 
   /@types/concat-stream/2.0.0:
@@ -8497,7 +8503,7 @@ packages:
     resolution: {integrity: sha512-l6NQsDDyQUVeoTynNpC9uRvCUint/gSUXQA2euwmTuWGvPY5LSDUu6tkCtJB2SvGQlJQzLaKqcGZP4//7EDveA==}
     dependencies:
       '@types/minimatch': 5.1.2
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
     dev: true
 
   /@types/graceful-fs/4.1.5:
@@ -8567,7 +8573,7 @@ packages:
   /@types/keyv/3.1.4:
     resolution: {integrity: sha512-BQ5aZNSCpj7D6K2ksrRCTmKRLEpnPvWDiLPfoGyhZ++8YtiK9d/3DBKPJgry359X/P1PfruyYwvnvwFjuEiEIg==}
     dependencies:
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
 
   /@types/lodash/4.14.181:
     resolution: {integrity: sha512-n3tyKthHJbkiWhDZs3DkhkCzt2MexYHXlX0td5iMplyfwketaOeKboEVBqzceH7juqvEg3q5oUoBFxSLu7zFag==}
@@ -8611,7 +8617,6 @@ packages:
 
   /@types/node/18.11.17:
     resolution: {integrity: sha512-HJSUJmni4BeDHhfzn6nF0sVmd1SMezP7/4F0Lq+aXzmp2xm9O7WXrUtHW/CHlYVtZUbByEvWidHqRtcJXGF2Ng==}
-    dev: true
 
   /@types/node/18.11.9:
     resolution: {integrity: sha512-CRpX21/kGdzjOpFsZSkcrXMGIBWMGNIHXXBVFSH+ggkftxg+XYP20TESbh+zFvFj3EQOl5byk0HTRn1IL6hbqg==}
@@ -8648,7 +8653,7 @@ packages:
   /@types/responselike/1.0.0:
     resolution: {integrity: sha512-85Y2BjiufFzaMIlvJDvTTB8Fxl2xfLo4HgmHzVBz08w4wDePCTjYw66PdrolO0kzli3yam/YCgRufyo1DdQVTA==}
     dependencies:
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
 
   /@types/retry/0.12.2:
     resolution: {integrity: sha512-XISRgDJ2Tc5q4TRqvgJtzsRkFYNJzZrhTdtMoGVBttwzzQJkPnS3WWTFc7kuDRoPtPakl+T+OfdEUjYJj7Jbow==}
@@ -9068,7 +9073,7 @@ packages:
     resolution: {integrity: sha512-xGj0G1xvOIRrQkB2aT/oBWscDqq20NAG4qSbM9gT7px58nyVAZvhBOSMyEEkVyTeHtJtPIMdADKbXawdZqnrVQ==}
     engines: {node: '>=14.15.0'}
     dependencies:
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       '@yarnpkg/fslib': 3.0.0-rc.25
 
   /@yarnpkg/shell/3.2.0-rc.8_typanion@3.12.1:
@@ -12841,7 +12846,7 @@ packages:
       - supports-color
     dev: true
 
-  /jest-config/29.3.1_x4zmdo4imr2n2hhykfyzszwk6y:
+  /jest-config/29.3.1_uvwivoxixlgocwjbikx7evbpf4:
     resolution: {integrity: sha512-y0tFHdj2WnTEhxmGUK1T7fgLen7YK4RtfvpLFBXfQkh2eMJAQq24Vx9472lvn5wg0MAO6B+iPfJfzdR9hJYalg==}
     engines: {node: ^14.15.0 || ^16.10.0 || >=18.0.0}
     peerDependencies:
@@ -12856,7 +12861,7 @@ packages:
       '@babel/core': 7.20.5
       '@jest/test-sequencer': 29.3.1
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       babel-jest: 29.3.1_5zl3o24ezxc3cc3y3khgvpammi
       chalk: 4.1.2
       ci-info: 3.6.1
@@ -12933,7 +12938,7 @@ packages:
     dependencies:
       '@jest/types': 29.3.1
       '@types/graceful-fs': 4.1.5
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       anymatch: 3.1.2
       fb-watchman: 2.0.2
       graceful-fs: 4.2.10
@@ -13039,7 +13044,7 @@ packages:
       '@jest/test-result': 29.3.1
       '@jest/transform': 29.3.1_@babel+types@7.20.5
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       chalk: 4.1.2
       emittery: 0.13.1
       graceful-fs: 4.2.10
@@ -13071,7 +13076,7 @@ packages:
       '@jest/test-result': 29.3.1
       '@jest/transform': 29.3.1_@babel+types@7.20.5
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       chalk: 4.1.2
       cjs-module-lexer: 1.2.2
       collect-v8-coverage: 1.0.1
@@ -13128,7 +13133,7 @@ packages:
     engines: {node: ^14.15.0 || ^16.10.0 || >=18.0.0}
     dependencies:
       '@jest/types': 29.1.2
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       chalk: 4.1.2
       ci-info: 3.6.1
       graceful-fs: 4.2.10
@@ -13140,7 +13145,7 @@ packages:
     engines: {node: ^14.15.0 || ^16.10.0 || >=18.0.0}
     dependencies:
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       chalk: 4.1.2
       ci-info: 3.6.1
       graceful-fs: 4.2.10
@@ -13165,7 +13170,7 @@ packages:
     dependencies:
       '@jest/test-result': 29.3.1
       '@jest/types': 29.3.1
-      '@types/node': 18.11.9
+      '@types/node': 18.11.17
       ansi-escapes: 4.3.2
       chalk: 4.1.2
       emittery: 0.13.1

commit c5f4d339be58f4ea5d52fe99c905170908afbe48

fix: side effects upload when node-linker is set to hoisted
This fixes a bug introduced by #5814

pkg-manager/headless/src/lockfileToHoistedDepGraph.ts
@@ -182,18 +182,23 @@ async function fetchDeps (
     const resolution = pkgSnapshotToResolution(depPath, pkgSnapshot, opts.registries)
     let fetchResponse!: ReturnType<FetchPackageToStoreFunction>
     const skipFetch = opts.currentHoistedLocations?.[depPath]?.includes(depLocation)
+    const pkgResolution = {
+      id: packageId,
+      resolution,
+    }
     if (skipFetch) {
-      fetchResponse = {} as any // eslint-disable-line @typescript-eslint/no-explicit-any
+      const { filesIndexFile } = opts.storeController.getFilesIndexFilePath({
+        ignoreScripts: opts.ignoreScripts,
+        pkg: pkgResolution,
+      })
+      fetchResponse = { filesIndexFile } as any // eslint-disable-line @typescript-eslint/no-explicit-any
     } else {
       try {
         fetchResponse = opts.storeController.fetchPackage({
           force: false,
           lockfileDir: opts.lockfileDir,
           ignoreScripts: opts.ignoreScripts,
-          pkg: {
-            id: packageId,
-            resolution,
-          },
+          pkg: pkgResolution,
           expectedPkg: {
             name: pkgName,
             version: pkgVersion,

pkg-manager/package-requester/src/packageRequester.ts
@@ -33,6 +33,7 @@ import {
   BundledManifestFunction,
   FetchPackageToStoreFunction,
   FetchPackageToStoreOptions,
+  GetFilesIndexFilePath,
   PackageResponse,
   PkgNameVersion,
   RequestPackageFunction,
@@ -96,6 +97,7 @@ export function createPackageRequester (
   }
 ): RequestPackageFunction & {
     fetchPackageToStore: FetchPackageToStoreFunction
+    getFilesIndexFilePath: GetFilesIndexFilePath
     requestPackage: RequestPackageFunction
   } {
   opts = opts || {}
@@ -135,7 +137,14 @@ export function createPackageRequester (
     storeDir: opts.storeDir,
   })
 
-  return Object.assign(requestPackage, { fetchPackageToStore, requestPackage })
+  return Object.assign(requestPackage, {
+    fetchPackageToStore,
+    getFilesIndexFilePath: getFilesIndexFilePath.bind(null, {
+      getFilePathInCafs,
+      storeDir: opts.storeDir,
+    }),
+    requestPackage,
+  })
 }
 
 async function resolveAndFetch (
@@ -296,6 +305,21 @@ interface FetchLock {
   finishing: Promise<void>
 }
 
+function getFilesIndexFilePath (
+  ctx: {
+    getFilePathInCafs: (integrity: string, fileType: FileType) => string
+    storeDir: string
+  },
+  opts: Pick<FetchPackageToStoreOptions, 'pkg' | 'ignoreScripts'>
+) {
+  const targetRelative = depPathToFilename(opts.pkg.id)
+  const target = path.join(ctx.storeDir, targetRelative)
+  const filesIndexFile = opts.pkg.resolution['integrity']
+    ? ctx.getFilePathInCafs(opts.pkg.resolution['integrity'], 'index')
+    : path.join(target, opts.ignoreScripts ? 'integrity-not-built.json' : 'integrity.json')
+  return { filesIndexFile, target }
+}
+
 function fetchToStore (
   ctx: {
     checkFilesIntegrity: (
@@ -323,18 +347,14 @@ function fetchToStore (
   if (!opts.pkg.name) {
     opts.fetchRawManifest = true
   }
-  const targetRelative = depPathToFilename(opts.pkg.id)
-  const target = path.join(ctx.storeDir, targetRelative)
 
   if (!ctx.fetchingLocker.has(opts.pkg.id)) {
     const bundledManifest = pDefer<BundledManifest>()
     const files = pDefer<PackageFilesResponse>()
     const finishing = pDefer<undefined>()
-    const filesIndexFile = opts.pkg.resolution['integrity']
-      ? ctx.getFilePathInCafs(opts.pkg.resolution['integrity'], 'index')
-      : path.join(target, opts.ignoreScripts ? 'integrity-not-built.json' : 'integrity.json')
+    const { filesIndexFile, target } = getFilesIndexFilePath(ctx, opts)
 
-    doFetchToStore(filesIndexFile, bundledManifest, files, finishing) // eslint-disable-line
+    doFetchToStore(filesIndexFile, bundledManifest, files, finishing, target) // eslint-disable-line
 
     if (opts.fetchRawManifest) {
       ctx.fetchingLocker.set(opts.pkg.id, {
@@ -426,7 +446,8 @@ function fetchToStore (
     filesIndexFile: string,
     bundledManifest: pDefer.DeferredPromise<BundledManifest>,
     files: pDefer.DeferredPromise<PackageFilesResponse>,
-    finishing: pDefer.DeferredPromise<void>
+    finishing: pDefer.DeferredPromise<void>,
+    target: string
   ) {
     try {
       const isLocalTarballDep = opts.pkg.id.startsWith('file:')

store/package-store/src/storeController/index.ts
@@ -50,6 +50,7 @@ export async function createPackageStore (
   return {
     close: async () => {}, // eslint-disable-line:no-empty
     fetchPackage: packageRequester.fetchPackageToStore,
+    getFilesIndexFilePath: packageRequester.getFilesIndexFilePath,
     importPackage: cafs.importPackage,
     prune: prune.bind(null, { storeDir, cacheDir: initOpts.cacheDir }),
     requestPackage: packageRequester.requestPackage,

store/server/src/connectStoreController.ts
@@ -30,6 +30,7 @@ export async function connectStoreController (
     resolve({
       close: async () => { },
       fetchPackage: fetchPackage.bind(null, remotePrefix, limitedFetch),
+      getFilesIndexFilePath: () => ({ filesIndexFile: '', target: '' }), // NOT IMPLEMENTED
       importPackage: async (to: string, opts: {
         filesResponse: PackageFilesResponse
         force: boolean

store/store-controller-types/src/index.ts
@@ -45,6 +45,7 @@ export type UploadPkgToStore = (builtPkgLocation: string, opts: UploadPkgToStore
 export interface StoreController {
   requestPackage: RequestPackageFunction
   fetchPackage: FetchPackageToStoreFunction
+  getFilesIndexFilePath: GetFilesIndexFilePath
   importPackage: ImportPackageFunction
   close: () => Promise<void>
   prune: () => Promise<void>
@@ -58,6 +59,11 @@ export type FetchPackageToStoreFunction = (
   filesIndexFile: string
   files: () => Promise<PackageFilesResponse>
   finishing: () => Promise<void>
+}
+
+export type GetFilesIndexFilePath = (opts: Pick<FetchPackageToStoreOptions, 'pkg' | 'ignoreScripts'>) => {
+  filesIndexFile: string
+  target: string
 }
 
 export interface PkgNameVersion {

pkg-manager/core/test/install/lifecycleScripts.ts
@@ -717,3 +717,40 @@ test('run pre/postinstall scripts in a workspace that uses node-linker=hoisted',
   await projects['project-4'].has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')
   await projects['project-4'].has('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall.js')
 })
+
+test('run pre/postinstall scripts in a project that uses node-linker=hoisted. Should not fail on repeat install', async () => {
+  const project = prepareEmpty()
+  const manifest = await addDependenciesToPackage({},
+    ['@pnpm.e2e/pre-and-postinstall-scripts-example@1.0.0'],
+    await testDefaults({ fastUnpack: false, targetDependenciesField: 'devDependencies', nodeLinker: 'hoisted', sideEffectsCacheRead: true, sideEffectsCacheWrite: true })
+  )
+
+  {
+    expect(await exists('node_modules/@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-prepare.js')).toBeFalsy()
+    expect(await exists('node_modules/@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall.js')).toBeTruthy()
+
+    const generatedByPreinstall = project.requireModule('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-preinstall')
+    expect(typeof generatedByPreinstall).toBe('function')
+
+    const generatedByPostinstall = project.requireModule('@pnpm.e2e/pre-and-postinstall-scripts-example/generated-by-postinstall')
+    expect(typeof generatedByPostinstall).toBe('function')
+  }
+
+  const reporter = jest.fn()
+  await addDependenciesToPackage(manifest,
+    ['example@npm:@pnpm.e2e/pre-and-postinstall-scripts-example@2.0.0'],
+    await testDefaults({
+      fastUnpack: false,
+      targetDependenciesField: 'devDependencies',
+      nodeLinker: 'hoisted',
+      reporter,
+      sideEffectsCacheRead: true,
+      sideEffectsCacheWrite: true,
+    })
+  )
+
+  expect(reporter).not.toBeCalledWith(expect.objectContaining({
+    level: 'warn',
+    message: `An error occurred while uploading ${path.resolve('node_modules/@pnpm.e2e/pre-and-postinstall-scripts-example')}`,
+  }))
+})