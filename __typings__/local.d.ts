c710143f47514c253ac7e10cd5d27a91ab9fb7a2
josefaidt <josef.aidt@gmail.com>
2023-02-16 14:59:33 -0800

    fix: patch-package details on applyPatch, add patch

--- __typings__/local.d.ts
+++ __typings__/local.d.ts
@@ -1,157 +1,163 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '@pnpm/tabtab' {
-  const anything: any
-  export = anything
+declare module "@pnpm/tabtab" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'hyperdrive-schemas' {
-  const anything: any
-  export = anything
+declare module "hyperdrive-schemas" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'fuse-native' {
-  const anything: any
-  export = anything
+declare module "fuse-native" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@zkochan/libnpx/index' {
-  const anything: any
-  export = anything
+declare module "@zkochan/libnpx/index" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@pnpm/npm-conf' {
-  const anything: any
-  export = anything
+declare module "@pnpm/npm-conf" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@pnpm/npm-conf/lib/types' {
-  const anything: any
-  export = anything
+declare module "@pnpm/npm-conf/lib/types" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@pnpm/npm-lifecycle' {
-  const anything: any
-  export = anything
+declare module "@pnpm/npm-lifecycle" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@pnpm/npm-package-arg' {
-  const anything: any
-  export = anything
+declare module "@pnpm/npm-package-arg" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@zkochan/which' {
-  const anything: any
-  export = anything
+declare module "@zkochan/which" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'anonymous-npm-registry-client' {
-  const anything: any
-  export = anything
+declare module "anonymous-npm-registry-client" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'ansi-diff' {
-  const anything: any
-  export = anything
+declare module "ansi-diff" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'better-path-resolve' {
-  const anything: any
-  export = anything
+declare module "better-path-resolve" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@zkochan/diable' {
-  const anything: any
-  export = anything
+declare module "@zkochan/diable" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'dint' {
-  const anything: any
-  export = anything
+declare module "dint" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'exists-link' {
-  const anything: any
-  export = anything
+declare module "exists-link" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'graceful-git' {
-  const anything: any
-  export = anything
+declare module "graceful-git" {
+  const anything: any;
+  export = anything;
 }
 
-declare module '@pnpm/graph-sequencer' {
+declare module "@pnpm/graph-sequencer" {
   namespace graphSequencer {
-    type Graph<T> = Map<T, T[]>
-    type Groups<T> = T[][]
+    type Graph<T> = Map<T, T[]>;
+    type Groups<T> = T[][];
 
     interface Options<T> {
-      graph: Graph<T>
-      groups: Groups<T>
+      graph: Graph<T>;
+      groups: Groups<T>;
     }
 
     interface Result<T> {
-      safe: boolean
-      chunks: Groups<T>
-      cycles: Groups<T>
+      safe: boolean;
+      chunks: Groups<T>;
+      cycles: Groups<T>;
     }
 
-    type GraphSequencer = <T>(opts: Options<T>) => Result<T>
+    type GraphSequencer = <T>(opts: Options<T>) => Result<T>;
   }
 
-  const graphSequencer: graphSequencer.GraphSequencer
-  export = graphSequencer
+  const graphSequencer: graphSequencer.GraphSequencer;
+  export = graphSequencer;
 }
 
-declare module 'is-inner-link' {
-  const anything: any
-  export = anything
+declare module "is-inner-link" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'is-port-reachable' {
-  const anything: any
-  export = anything
+declare module "is-port-reachable" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'process-exists' {
-  const anything: any
-  export = anything
+declare module "process-exists" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'read-package-json' {
-  const anything: any
-  export = anything
+declare module "read-package-json" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'stacktracey' {
-  const anything: any
-  export = anything
+declare module "stacktracey" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'yaml-tag' {
-  const anything: any
-  export = anything
+declare module "yaml-tag" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'decompress-maybe' {
-  const anything: any
-  export = anything
+declare module "decompress-maybe" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'nerf-dart' {
-  const anything: any
-  export = anything
+declare module "nerf-dart" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'patch-package/dist/applyPatches' {
-  export function applyPatch (opts: any): boolean
+declare module "patch-package/pnpm" {
+  export function applyPatch(opts: any): void;
+  export function getPackageDetailsFromPatchFilename(
+    patchFilename: string
+  ): Record<string, unknown>;
 }
 
-declare module 'string.prototype.replaceall' {
-  const anything: any
-  export = anything
+declare module "string.prototype.replaceall" {
+  const anything: any;
+  export = anything;
 }
 
-declare module 'ramda/src/map' {
-  function map <K extends string | number | symbol, V, U> (fn: (x: V) => U, obj: Record<K, V>): Record<K, U>
-  export = map
+declare module "ramda/src/map" {
+  function map<K extends string | number | symbol, V, U>(
+    fn: (x: V) => U,
+    obj: Record<K, V>
+  ): Record<K, U>;
+  export = map;
 }
--- patching/apply-patch/src/index.ts
+++ patching/apply-patch/src/index.ts
@@ -1,22 +1,35 @@
-import { PnpmError } from '@pnpm/error'
-import { applyPatch } from 'patch-package/dist/applyPatches'
+import path from "path";
+import { PnpmError } from "@pnpm/error";
+import {
+  applyPatch,
+  getPackageDetailsFromPatchFilename,
+} from "patch-package/pnpm";
 
 export interface ApplyPatchToDirOpts {
-  patchedDir: string
-  patchFilePath: string
+  patchedDir: string;
+  patchFilePath: string;
 }
 
-export function applyPatchToDir (opts: ApplyPatchToDirOpts) {
+export function applyPatchToDir(opts: ApplyPatchToDirOpts) {
   // Ideally, we would just run "patch" or "git apply".
   // However, "patch" is not available on Windows and "git apply" is hard to execute on a subdirectory of an existing repository
-  const cwd = process.cwd()
-  process.chdir(opts.patchedDir)
-  const success = applyPatch({
-    patchDir: opts.patchedDir,
-    patchFilePath: opts.patchFilePath,
-  })
-  process.chdir(cwd)
-  if (!success) {
-    throw new PnpmError('PATCH_FAILED', `Could not apply patch ${opts.patchFilePath} to ${opts.patchedDir}`)
+  const cwd = process.cwd();
+  process.chdir(opts.patchedDir);
+
+  try {
+    applyPatch({
+      packageDetails: getPackageDetailsFromPatchFilename(
+        path.basename(opts.patchFilePath)
+      ),
+      patchDir: opts.patchedDir,
+      patchFilePath: opts.patchFilePath,
+    });
+  } catch (err) {
+    throw new PnpmError(
+      "PATCH_FAILED",
+      `Could not apply patch ${opts.patchFilePath} to ${opts.patchedDir}`
+    );
+  } finally {
+    process.chdir(cwd);
   }
 }
