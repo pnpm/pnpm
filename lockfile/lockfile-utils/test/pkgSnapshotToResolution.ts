27042fb5ad85bd14c9642e39426af6736d3a028c
Zoltan Kochan <z@kochan.io>
2023-02-19 04:18:06 +0200

    test: fix

--- lockfile/lockfile-utils/test/pkgSnapshotToResolution.ts
+++ lockfile/lockfile-utils/test/pkgSnapshotToResolution.ts
@@ -32,16 +32,4 @@ test('pkgSnapshotToResolution()', () => {
     registry: 'https://mycompany.jfrog.io/mycompany/api/npm/npm-local',
     tarball: 'https://mycompany.jfrog.io/mycompany/api/npm/npm-local/@mycompany/mypackage/-/@mycompany/mypackage-2.0.0.tgz',
   })
-
-  expect(pkgSnapshotToResolution('/foo/1.0.0', {
-    resolution: {
-      integrity: 'AAAA',
-      registry: 'https://npm.pkg.github.com/',
-      tarball: 'https://npm.pkg.github.com/download/@foo/bar/1.0.0/aaa',
-    },
-  }, { default: 'https://registry.npmjs.org/' })).toEqual({
-    integrity: 'AAAA',
-    registry: 'https://npm.pkg.github.com/',
-    tarball: 'https://npm.pkg.github.com/download/@foo/bar/1.0.0/aaa',
-  })
 })
