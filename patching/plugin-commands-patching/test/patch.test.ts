6f56cc16e34b3c6f7a6ed4f1bd82f32bfae1974a
await-ovo <13152410380@163.com>
2023-02-23 04:56:35 +0800

    fix: patch-commit should auto apply patches in workspaces (#6120)
    close #6048

--- patching/plugin-commands-patching/test/patch.test.ts
+++ patching/plugin-commands-patching/test/patch.test.ts
@@ -1,7 +1,10 @@
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
-import { prepare } from '@pnpm/prepare'
+import { prepare, preparePackages } from '@pnpm/prepare'
+import { install } from '@pnpm/plugin-commands-installation'
+import { readProjects } from '@pnpm/filter-workspace-packages'
+import writeYamlFile from 'write-yaml-file'
 import tempy from 'tempy'
 import { patch, patchCommit } from '@pnpm/plugin-commands-patching'
 import { readProjectManifest } from '@pnpm/read-project-manifest'
@@ -260,6 +263,106 @@ describe('patching should work when there is a no EOL in the patched file', () =
     const patchContent = fs.readFileSync('patches/safe-execa@0.1.2.patch', 'utf8')
     expect(patchContent).toContain('No newline at end of file')
     expect(fs.readFileSync('node_modules/safe-execa/lib/index.js', 'utf8')).toContain('//# sourceMappingURL=index.js.map// patch without newline')
+  })
+})
+
+describe('patch and commit in workspaces', () => {
+  let defaultPatchOption: patch.PatchCommandOptions
+  let cacheDir: string
+  let storeDir: string
+
+  beforeEach(() => {
+    preparePackages([
+      {
+        location: '.',
+        package: {
+          name: 'patch-commit-workspaces',
+        },
+      },
+      {
+        name: 'project-1',
+        version: '1.0.0',
+        dependencies: {
+          'is-positive': '1.0.0',
+        },
+      },
+      {
+        name: 'project-2',
+        version: '1.0.0',
+        dependencies: {
+          'is-positive': '1.0.0',
+          'project-1': '1',
+        },
+      },
+    ])
+
+    cacheDir = path.resolve('cache')
+    storeDir = path.resolve('store')
+
+    defaultPatchOption = {
+      cacheDir,
+      dir: process.cwd(),
+      pnpmHomeDir: '',
+      rawConfig: {
+        registry: `http://localhost:${REGISTRY_MOCK_PORT}/`,
+      },
+      registries: { default: `http://localhost:${REGISTRY_MOCK_PORT}/` },
+      storeDir,
+      userConfig: {},
+    }
+  })
+
+  test('patch commit should work in workspaces', async () => {
+    await writeYamlFile('pnpm-workspace.yaml', { packages: ['project-1', 'project-2'] })
+    const { allProjects, allProjectsGraph, selectedProjectsGraph } = await readProjects(process.cwd(), [])
+    await install.handler({
+      ...DEFAULT_OPTS,
+      cacheDir,
+      storeDir,
+      allProjects,
+      allProjectsGraph,
+      dir: process.cwd(),
+      selectedProjectsGraph,
+      workspaceDir: process.cwd(),
+      saveLockfile: true,
+    })
+
+    const output = await patch.handler(defaultPatchOption, ['is-positive@1.0.0'])
+    const patchDir = getPatchDirFromPatchOutput(output)
+    const tempDir = os.tmpdir()
+
+    expect(patchDir).toContain(tempDir)
+    expect(fs.existsSync(patchDir)).toBe(true)
+
+    expect(fs.readFileSync(path.join(patchDir, 'license'), 'utf8')).toContain('The MIT License (MIT)')
+
+    fs.appendFileSync(path.join(patchDir, 'index.js'), '// test patching', 'utf8')
+    fs.unlinkSync(path.join(patchDir, 'license'))
+
+    await patchCommit.handler({
+      ...DEFAULT_OPTS,
+      allProjects,
+      allProjectsGraph,
+      selectedProjectsGraph,
+      dir: process.cwd(),
+      cacheDir,
+      storeDir,
+      lockfileDir: process.cwd(),
+      workspaceDir: process.cwd(),
+      saveLockfile: true,
+    }, [patchDir])
+
+    const { manifest } = await readProjectManifest(process.cwd())
+    expect(manifest.pnpm?.patchedDependencies).toStrictEqual({
+      'is-positive@1.0.0': 'patches/is-positive@1.0.0.patch',
+    })
+    const patchContent = fs.readFileSync('patches/is-positive@1.0.0.patch', 'utf8')
+    expect(patchContent).toContain('diff --git')
+    expect(patchContent).toContain('// test patching')
+    expect(fs.readFileSync('project-1/node_modules/is-positive/index.js', 'utf8')).toContain('// test patching')
+    expect(fs.existsSync('project-1/node_modules/is-positive/license')).toBe(false)
+    expect(fs.readFileSync('project-2/node_modules/is-positive/index.js', 'utf8')).toContain('// test patching')
+    expect(fs.existsSync('project-2/node_modules/is-positive/license')).toBe(false)
   })
 })
 
