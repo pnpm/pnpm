9cb051278c10ecb0d16be25ba382bffe5dd84343
await-ovo <13152410380@163.com>
2023-04-03 15:25:45 +0800

    fix(default-reporter): should print error summary as expected

--- pnpm/test/errorHandler.test.ts
+++ pnpm/test/errorHandler.test.ts
@@ -47,3 +47,36 @@ test('should clean up child processes when process exited', async () => {
   expect(await isPortInUse(9990)).toBe(false)
   expect(await isPortInUse(9999)).toBe(false)
 })
+
+test('should print error summary when some packages fail with --no-bail', async () => {
+  preparePackages([
+    {
+      location: 'project-1',
+      package: {
+        scripts: {
+          build: 'echo "build project-1"',
+        },
+      },
+    },
+    {
+      name: 'project-2',
+      version: '1.0.0',
+      scripts: {
+        build: 'exit 1',
+      },
+    },
+    {
+      name: 'project-3',
+      version: '1.0.0',
+      scripts: {
+        build: 'echo "build project-3"',
+      },
+    },
+  ])
+  await writeYamlFile('pnpm-workspace.yaml', { packages: ['**', '!store/**'] })
+  const { stdout } = execPnpmSync(['-r', '--no-bail', 'run', 'build'])
+  const output = stdout.toString()
+  expect(output).toContain('ERR_PNPM_RECURSIVE_FAIL')
+  expect(output).toContain('Summary: 1 fails, 2 passes')
+  expect(output).toContain('ERROR  project-2@1.0.0 build: `exit 1`')
+})
