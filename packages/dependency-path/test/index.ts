5274a1d475af01365ddfcff14644ff371f114704
Zoltan Kochan <z@kochan.io>
2023-04-04 00:18:43 +0300

    fix: installation of packages with () in scope name (#6349)
    close #6348

--- packages/dependency-path/test/index.ts
+++ packages/dependency-path/test/index.ts
@@ -98,6 +98,14 @@ test('parse()', () => {
     version: '1.0.0',
   })
 
+  expect(parse('/@(-.-)/foo/1.0.0(@types/babel__core@7.1.14)(foo@1.0.0)')).toStrictEqual({
+    host: undefined,
+    isAbsolute: false,
+    name: '@(-.-)/foo',
+    peersSuffix: '(@types/babel__core@7.1.14)(foo@1.0.0)',
+    version: '1.0.0',
+  })
+
   expect(() => parse('/foo/bar')).toThrow(/\/foo\/bar is an invalid relative dependency path/)
 
   expect(parse('file:project(foo@1.0.0)')).toStrictEqual({
