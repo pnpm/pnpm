886c4fdaf3072d46a867f56e718701a8179d3fa8
Brandon Cheng <gluxon@users.noreply.github.com>
2023-04-16 12:05:49 -0400

    chore: create @pnpm/dedupe.issues-renderer to show dedupe --check output

---
+++ dedupe/issues-renderer/src/index.ts
@@ -0,0 +1,56 @@
+import {
+  type ResolutionChange,
+  type DedupeCheckIssues,
+  type ResolutionChangesByAlias,
+  type SnapshotsChanges,
+} from '@pnpm/dedupe.types'
+import archy from 'archy'
+import chalk from 'chalk'
+
+export function renderDedupeCheckIssues (dedupeCheckIssues: DedupeCheckIssues) {
+  const importersReport = report(dedupeCheckIssues.importerIssuesByImporterId)
+  const packagesReport = report(dedupeCheckIssues.packageIssuesByDepPath)
+
+  const lines = []
+  if (importersReport !== '') {
+    lines.push(chalk.blueBright.underline('Importers'))
+    lines.push(importersReport)
+    lines.push('')
+  }
+  if (packagesReport !== '') {
+    lines.push(chalk.blueBright.underline('Packages'))
+    lines.push(packagesReport)
+    lines.push('')
+  }
+
+  return lines.join('\n')
+}
+
+/**
+ * Render snapshot changes. Expected to return an empty string for no changes.
+ */
+function report (snapshotChanges: SnapshotsChanges): string {
+  return [
+    ...Object.entries(snapshotChanges.updated).map(([alias, updates]) => archy(toArchy(alias, updates))),
+    ...snapshotChanges.added.map((id) => `${chalk.green('+')} ${id}`),
+    ...snapshotChanges.removed.map((id) => `${chalk.red('-')} ${id}`),
+  ].join('\n')
+}
+
+function toArchy (name: string, issue: ResolutionChangesByAlias): archy.Data {
+  return {
+    label: name,
+    nodes: Object.entries(issue).map(([alias, change]) => toArchyResolution(alias, change)),
+  }
+}
+
+function toArchyResolution (alias: string, change: ResolutionChange): archy.Data {
+  switch (change.type) {
+  case 'added':
+    return { label: `${chalk.green('+')} ${alias} ${chalk.gray(change.next)}` }
+  case 'removed':
+    return { label: `${chalk.red('-')} ${alias} ${chalk.gray(change.prev)}` }
+  case 'updated':
+    return { label: `${alias} ${chalk.red(change.prev)} ${chalk.gray('→')} ${chalk.green(change.next)}` }
+  }
+}
