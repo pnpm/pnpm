296f87a926b632d7ce8039d97f64378e29f20d5c
Lev Chelyadinov <leva181777@gmail.com>
2023-06-16 17:03:49 +0300

    fix: identify and fix the TS errors

--- config/package-is-installable/test/getSystemNodeVersion.test.ts
+++ config/package-is-installable/test/getSystemNodeVersion.test.ts
@@ -8,11 +8,13 @@ jest.mock('execa', () => ({
 }))
 
 test('getSystemNodeVersion() executed from an executable pnpm CLI', () => {
+  // @ts-expect-error
   process['pkg'] = {}
   expect(getSystemNodeVersionNonCached()).toBe(process.version)
 })
 
 test('getSystemNodeVersion() from a non-executable pnpm CLI', () => {
+  // @ts-expect-error
   delete process['pkg']
   expect(getSystemNodeVersionNonCached()).toBe('v10.0.0')
   expect(execa.sync).toHaveBeenCalledWith('node', ['--version'])
