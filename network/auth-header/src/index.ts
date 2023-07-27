b9b398d80c4fe6b8e6ec6966d54fb29b5729b223
Frederick Engelhardt <31741411+FrederickEngelhardt@users.noreply.github.com>
2023-07-27 07:18:29 -0700

    feat(always-auth): add https port parsing on registry matching for artifactory compatibility (#6864)
    Co-authored-by: Frederick Engelhardt <frederick.engelhardt@bestbuy.com>
    Co-authored-by: Zoltan Kochan <z@kochan.io>

--- network/auth-header/src/index.ts
+++ network/auth-header/src/index.ts
@@ -39,7 +39,7 @@ function getAuthHeaderByURI (authHeaders: Record<string, string>, maxParts: numb
 
 function removePort (originalUrl: string) {
   const urlObj = new URL(originalUrl)
-  if (urlObj.port === '') return originalUrl
+  if (urlObj.port === '') return urlObj.href
   urlObj.port = ''
   return urlObj.toString()
 }
--- network/auth-header/test/getAuthHeaderByURI.ts
+++ network/auth-header/test/getAuthHeaderByURI.ts
@@ -17,6 +17,17 @@ test('getAuthHeaderByURI()', () => {
   expect(getAuthHeaderByURI('https://reg.gg:8888/foo/-/foo-1.0.0.tgz')).toBe('Bearer 0000')
 })
 
+test('getAuthHeaderByURI() when default ports are specified', () => {
+  const getAuthHeaderByURI = createGetAuthHeaderByURI({
+    allSettings: {
+      '//reg.com/:_authToken': 'abc123',
+    },
+    userSettings: {},
+  })
+  expect(getAuthHeaderByURI('https://reg.com:443/')).toBe('Bearer abc123')
+  expect(getAuthHeaderByURI('http://reg.com:80/')).toBe('Bearer abc123')
+})
+
 test('returns undefined when the auth header is not found', () => {
   expect(createGetAuthHeaderByURI({ allSettings: {}, userSettings: {} })('http://reg.com')).toBe(undefined)
 })
