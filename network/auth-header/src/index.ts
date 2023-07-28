409890dc02dc22282d32a6b6c95cbf40781d3e46
Frederick Engelhardt <frederick.engelhardt@bestbuy.com>
2023-07-28 06:05:45 -0700

    chore(formatting): remove auto-formatting

--- network/auth-header/src/index.ts
+++ network/auth-header/src/index.ts
@@ -2,20 +2,18 @@ import nerfDart from 'nerf-dart'
 import { getAuthHeadersFromConfig } from './getAuthHeadersFromConfig'
 import { removePort } from './helpers/removePort'
 
-export function createGetAuthHeaderByURI (opts: {
-  allSettings: Record<string, string>
-  userSettings?: Record<string, string>
-}) {
+export function createGetAuthHeaderByURI (
+  opts: {
+    allSettings: Record<string, string>
+    userSettings?: Record<string, string>
+  }
+) {
   const authHeaders = getAuthHeadersFromConfig({
     allSettings: opts.allSettings,
     userSettings: opts.userSettings ?? {},
   })
   if (Object.keys(authHeaders).length === 0) return () => undefined
-  return getAuthHeaderByURI.bind(
-    null,
-    authHeaders,
-    getMaxParts(Object.keys(authHeaders))
-  )
+  return getAuthHeaderByURI.bind(null, authHeaders, getMaxParts(Object.keys(authHeaders)))
 }
 
 function getMaxParts (uris: string[]) {
@@ -25,11 +23,7 @@ function getMaxParts (uris: string[]) {
   }, 0)
 }
 
-function getAuthHeaderByURI (
-  authHeaders: Record<string, string>,
-  maxParts: number,
-  uri: string
-): string | undefined {
+function getAuthHeaderByURI (authHeaders: Record<string, string>, maxParts: number, uri: string): string | undefined {
   const nerfed = nerfDart(uri)
   const parts = nerfed.split('/')
   for (let i = Math.min(parts.length, maxParts) - 1; i >= 3; i--) {
