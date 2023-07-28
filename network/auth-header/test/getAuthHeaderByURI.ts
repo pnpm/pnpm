57cfc8e4d512a569dc924485f804add57bd95854
Zoltan Kochan <z@kochan.io>
2023-07-28 23:35:37 +0300

    style: reformat

--- network/auth-header/test/getAuthHeaderByURI.ts
+++ network/auth-header/test/getAuthHeaderByURI.ts
@@ -19,21 +19,23 @@ test('getAuthHeaderByURI()', () => {
   expect(getAuthHeaderByURI('https://reg.co/tarballs/foo/-/foo-1.0.0.tgz')).toBe('Bearer xxx')
   expect(getAuthHeaderByURI('https://reg.gg:8888/foo/-/foo-1.0.0.tgz')).toBe('Bearer 0000')
   expect(getAuthHeaderByURI('https://reg.gg:8888/foo/-/foo-1.0.0.tgz')).toBe('Bearer 0000')
+})
 
-  /**
-   * https port 443 checks
-   */
+test('getAuthHeaderByURI() https port 443 checks', () => {
+  const getAuthHeaderByURI = createGetAuthHeaderByURI(opts)
   expect(getAuthHeaderByURI('https://custom.domain.com:443/artifactory/api/npm/npm-virtual/')).toBe('Bearer xyz')
-
   expect(getAuthHeaderByURI('https://custom.domain.com:443/artifactory/api/npm/')).toBe(undefined)
-
   expect(getAuthHeaderByURI('https://custom.domain.com:443/artifactory/api/npm/-/@platform/device-utils-1.0.0.tgz')).toBe(undefined)
-
   expect(getAuthHeaderByURI('https://custom.domain.com:443/artifactory/api/npm/npm-virtual/@platform/device-utils/-/@platform/device-utils-1.0.0.tgz')).toBe('Bearer xyz')
 })
 
 test('getAuthHeaderByURI() when default ports are specified', () => {
-  const getAuthHeaderByURI = createGetAuthHeaderByURI({ allSettings: { '//reg.com/:_authToken': 'abc123' }, userSettings: {} })
+  const getAuthHeaderByURI = createGetAuthHeaderByURI({
+    allSettings: {
+      '//reg.com/:_authToken': 'abc123',
+    },
+    userSettings: {},
+  })
   expect(getAuthHeaderByURI('https://reg.com:443/')).toBe('Bearer abc123')
   expect(getAuthHeaderByURI('http://reg.com:80/')).toBe('Bearer abc123')
 })
