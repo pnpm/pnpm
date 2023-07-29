347ca9f30385042584ee220bfe422911d07c304f
Zoltan Kochan <z@kochan.io>
2023-07-29 22:53:16 +0300

    test: allow to run Jest directly for running tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
-const path = require("path");
+const path = require("path")
 
-const pathAsArr = process.env.PNPM_SCRIPT_SRC_DIR.split(path.sep);
-const packageName = pathAsArr[pathAsArr.length - 1];
-
-module.exports = {
+const config = {
   preset: "ts-jest",
   testMatch: ["**/test/**/*.[jt]s?(x)", "**/src/**/*.test.ts"],
   testEnvironment: "node",
@@ -12,9 +9,16 @@ module.exports = {
   testPathIgnorePatterns: ["/fixtures/", "/__fixtures__/", "<rootDir>/test/utils/.+"],
   testTimeout: 4 * 60 * 1000, // 4 minutes
   setupFilesAfterEnv: [path.join(__dirname, "jest.setup.js")],
-  cacheDirectory: path.join(__dirname, ".jest-cache", packageName),
   // Many tests change the dist tags of packages.
   // Unfortunately, this means that if two such tests will run at the same time,
   // they may break each other.
   maxWorkers: 1,
-};
+}
+
+if (process.env.PNPM_SCRIPT_SRC_DIR) {
+  const pathAsArr = process.env.PNPM_SCRIPT_SRC_DIR.split(path.sep)
+  const packageName = pathAsArr[pathAsArr.length - 1]
+  config.cacheDirectory = path.join(__dirname, ".jest-cache", packageName)
+}
+
+module.exports = config
