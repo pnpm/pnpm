9526adc6fac3574f71ea72e45d16f7230a35d6a6
Brandon Cheng <gluxon@users.noreply.github.com>
2023-08-31 17:13:03 -0400

    test: fix Jest code block misalignment in pnpm tests

--- pnpm/jest.config.js
+++ pnpm/jest.config.js
@@ -1,1 +1,18 @@
-module.exports = require('../jest-with-registry.config.js')
+const baseConfig = require('../jest-with-registry.config.js')
+
+module.exports = {
+  ...baseConfig,
+
+  // The preset option is set to "ts-jest" in the base config. The ts-jest docs
+  // recommend not using a preset if the transform block is configured.
+  //
+  // https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
+  preset: undefined,
+
+  transform: {
+    ...baseConfig.transform,
+
+    // Point towards tsconfig.test.json to fix code blocks.
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: "tsconfig.test.json" }],
+  },
+}
