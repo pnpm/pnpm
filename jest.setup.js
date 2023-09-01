42f93f96f7b13e754cc05d11cb88ebc966724516
Brandon Cheng <gluxon@users.noreply.github.com>
2023-09-01 15:21:05 -0400

    test: set logErrorsBeforeRetry to debug failing retries

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,11 @@
-jest.retryTimes(1);
+jest.retryTimes(1, {
+  // Some tests don't clean up their resources completely and cause the retried
+  // run fail. Set logErrorsBeforeRetry to make it more clear that the retried
+  // run was a retry and not the first attempt. Otherwise Jest hides the first
+  // attempt. This makes it easier to distinguish between a test that's truly
+  // broken and one that's not retry-able.
+  logErrorsBeforeRetry: true
+})
 
 afterAll(() => {
   // @ts-expect-error
