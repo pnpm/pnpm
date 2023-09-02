f5ed6c11b1c906dbd2ee02b45069a82d987c8a5e
Zoltan Kochan <z@kochan.io>
2023-09-03 00:48:59 +0300

    refactor: worker

--- fetching/tarball-fetcher/src/gitHostedTarballFetcher.ts
+++ fetching/tarball-fetcher/src/gitHostedTarballFetcher.ts
@@ -18,13 +18,13 @@ export interface CreateGitHostedTarballFetcher {
 
 export function createGitHostedTarballFetcher (fetchRemoteTarball: FetchFunction, fetcherOpts: CreateGitHostedTarballFetcher): FetchFunction {
   const fetch = async (cafs: Cafs, resolution: Resolution, opts: FetchOptions) => {
-    const { filesIndex } = await fetchRemoteTarball(cafs, resolution, opts)
+    const { filesIndex, manifest } = await fetchRemoteTarball(cafs, resolution, opts)
     try {
       const prepareResult = await prepareGitHostedPkg(filesIndex as Record<string, string>, cafs, opts.filesIndexFile, fetcherOpts)
       if (prepareResult.ignoredBuild) {
         globalWarn(`The git-hosted package fetched from "${resolution.tarball}" has to be built but the build scripts were ignored.`)
       }
-      return { filesIndex: prepareResult.filesIndex }
+      return { filesIndex: prepareResult.filesIndex, manifest }
     } catch (err: any) { // eslint-disable-line
       err.message = `Failed to prepare git-hosted package fetched from "${resolution.tarball}": ${err.message}`
       throw err
