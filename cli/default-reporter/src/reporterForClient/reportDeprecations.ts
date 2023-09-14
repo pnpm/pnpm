61b9ca1896f7e66bba5a7cae3d4e04240b39a253
Zoltan Kochan <z@kochan.io>
2023-09-15 01:56:14 +0300

    feat: print a summary about deprecated subdeps (#7090)
    close #6707

--- cli/default-reporter/src/reporterForClient/reportDeprecations.ts
+++ cli/default-reporter/src/reporterForClient/reportDeprecations.ts
@@ -1,27 +1,47 @@
-import { type DeprecationLog } from '@pnpm/core-loggers'
+import { type DeprecationLog, type StageLog } from '@pnpm/core-loggers'
 import * as Rx from 'rxjs'
-import { map } from 'rxjs/operators'
+import { map, filter, buffer, switchMap } from 'rxjs/operators'
 import chalk from 'chalk'
 import { formatWarn } from './utils/formatWarn'
 import { zoomOut } from './utils/zooming'
 
 export function reportDeprecations (
-  deprecation$: Rx.Observable<DeprecationLog>,
+  log$: {
+    deprecation: Rx.Observable<DeprecationLog>
+    stage: Rx.Observable<StageLog>
+  },
   opts: {
     cwd: string
     isRecursive: boolean
   }
 ) {
-  return deprecation$.pipe(
-    map((log) => {
-      if (!opts.isRecursive && log.prefix === opts.cwd) {
+  const [deprecatedDirectDeps$, deprecatedSubdeps$] = Rx.partition(log$.deprecation, (log) => log.depth === 0)
+  const resolutionDone$ = log$.stage.pipe(
+    filter((log) => log.stage === 'resolution_done')
+  )
+  return Rx.merge(
+    deprecatedDirectDeps$.pipe(
+      map((log) => {
+        if (!opts.isRecursive && log.prefix === opts.cwd) {
+          return Rx.of({
+            msg: formatWarn(`${chalk.red('deprecated')} ${log.pkgName}@${log.pkgVersion}: ${log.deprecated}`),
+          })
+        }
         return Rx.of({
-          msg: formatWarn(`${chalk.red('deprecated')} ${log.pkgName}@${log.pkgVersion}: ${log.deprecated}`),
+          msg: zoomOut(opts.cwd, log.prefix, formatWarn(`${chalk.red('deprecated')} ${log.pkgName}@${log.pkgVersion}`)),
         })
-      }
-      return Rx.of({
-        msg: zoomOut(opts.cwd, log.prefix, formatWarn(`${chalk.red('deprecated')} ${log.pkgName}@${log.pkgVersion}`)),
       })
-    })
+    ),
+    deprecatedSubdeps$.pipe(
+      buffer(resolutionDone$),
+      switchMap(deprecatedSubdeps => {
+        if (deprecatedSubdeps.length > 0) {
+          return Rx.of(Rx.of({
+            msg: formatWarn(`${chalk.red(`${deprecatedSubdeps.length} deprecated subdependencies found:`)} ${deprecatedSubdeps.map(log => `${log.pkgName}@${log.pkgVersion}`).sort().join(', ')}`),
+          }))
+        }
+        return Rx.EMPTY
+      })
+    )
   )
 }
