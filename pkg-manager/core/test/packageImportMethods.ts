f1d1a451b248788fae0457b3ccb8fadd21062380
Zoltan Kochan <z@kochan.io>
2023-09-21 04:06:39 +0300

    fix: updating dependencies node_modules when node-linker is hoisted
    close #7107

--- pkg-manager/core/test/packageImportMethods.ts
+++ pkg-manager/core/test/packageImportMethods.ts
@@ -1,5 +1,7 @@
+import fs from 'fs'
 import { prepareEmpty } from '@pnpm/prepare'
 import { addDependenciesToPackage } from '@pnpm/core'
+import { sync as loadJsonFile } from 'load-json-file'
 import { testDefaults } from './utils'
 
 test('packageImportMethod can be set to copy', async () => {
@@ -21,4 +23,28 @@ test('copy does not fail on package that self-requires itself', async () => {
 
   const lockfile = await project.readLockfile()
   expect(lockfile.packages['/@pnpm.e2e/requires-itself@1.0.0'].dependencies).toStrictEqual({ 'is-positive': '1.0.0' })
+})
+
+test('packages are updated in node_modules, when packageImportMethod is set to copy and modules manifest and current lockfile are incorrect', async () => {
+  prepareEmpty()
+  const opts = await testDefaults({ fastUnpack: false, force: false, nodeLinker: 'hoisted' }, {}, {}, { packageImportMethod: 'copy' })
+
+  await addDependenciesToPackage({}, ['is-negative@1.0.0'], opts)
+  const modulesManifestContent = fs.readFileSync('node_modules/.modules.yaml')
+  const currentLockfile = fs.readFileSync('node_modules/.pnpm/lock.yaml')
+  {
+    const pkg = loadJsonFile<any>('node_modules/is-negative/package.json') // eslint-disable-line
+    expect(pkg.version).toBe('1.0.0')
+  }
+  await addDependenciesToPackage({}, ['is-negative@2.0.0'], opts)
+  {
+    const pkg = loadJsonFile<any>('node_modules/is-negative/package.json') // eslint-disable-line
+    expect(pkg.version).toBe('2.0.0')
+  }
+  fs.writeFileSync('node_modules/.modules.yaml', modulesManifestContent, 'utf8')
+  fs.writeFileSync('node_modules/.pnpm/lock.yaml', currentLockfile, 'utf8')
+  await addDependenciesToPackage({}, ['is-negative@1.0.0'], opts)
+
+  const pkg = loadJsonFile<any>('node_modules/is-negative/package.json') // eslint-disable-line
+  expect(pkg.version).toBe('1.0.0')
 })
