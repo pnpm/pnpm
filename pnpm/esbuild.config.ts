a7a2234d74da37b3f11e3108ed5d50b168088fd7
Zoltan Kochan <z@kochan.io>
2023-10-04 00:33:24 +0300

    style: fix

--- pnpm/esbuild.config.ts
+++ pnpm/esbuild.config.ts
@@ -1,25 +1,26 @@
-import * as esbuild from "esbuild";
+import * as esbuild from 'esbuild'
 
 esbuild
   .build({
-    entryPoints: ["lib/pnpm.js"],
+    entryPoints: ['lib/pnpm.js'],
     bundle: true,
-    platform: "node",
-    outfile: "dist/pnpm.cjs",
-    external: ["node-gyp"],
+    platform: 'node',
+    outfile: 'dist/pnpm.cjs',
+    external: ['node-gyp'],
     define: {
-      "process.env.npm_package_name": JSON.stringify(
+      'process.env.npm_package_name': JSON.stringify(
         process.env.npm_package_name
       ),
-      "process.env.npm_package_version": JSON.stringify(
+      'process.env.npm_package_version': JSON.stringify(
         process.env.npm_package_version
       ),
     },
     loader: {
-      ".node": "copy",
+      '.node': 'copy',
     },
   })
   .catch((e: any) => {
-    console.error(e);
-    process.exit(1);
-  });
+    console.error(e)
+    process.exit(1)
+  })
+
--- pnpm/esbuild.worker.config.ts
+++ pnpm/esbuild.worker.config.ts
@@ -1,16 +1,17 @@
-import * as esbuild from "esbuild";
+import * as esbuild from 'esbuild';
 
 esbuild
   .build({
-    entryPoints: ["../worker/lib/worker.js"],
+    entryPoints: ['../worker/lib/worker.js'],
     bundle: true,
-    platform: "node",
-    outfile: "dist/worker.js",
+    platform: 'node',
+    outfile: 'dist/worker.js',
     loader: {
-      ".node": "copy",
+      '.node': "copy",
     },
   })
   .catch((e: any) => {
-    console.error(e);
-    process.exit(1);
-  });
+    console.error(e)
+    process.exit(1)
+  })
+
