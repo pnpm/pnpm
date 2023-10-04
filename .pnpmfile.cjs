a30c88736fb592e45a0829a8acf05c2f1b587aab
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-04 11:14:02 +0200

    fix: use `@refclone/refclone` in `.pnpmfile.cjs`

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   hooks: {
     readPackage: (manifest) => {
-      if (manifest.name === '@refclone/refclone') {
+      if (manifest.name === '@reflink/reflink') {
         for (const depName of Object.keys(manifest.optionalDependencies)) {
           // We don't need refclone on Linux as Node.js supports reflinks out of the box on Linux
           if (depName.includes('linux')) {
