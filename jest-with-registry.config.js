9d8c61541b9b5fbf5988991a1291eb502a4e46e7
tris203 <admin@snappeh.com>
2023-11-01 20:05:52 +0000

    test: change jest config worker count

--- jest-with-registry.config.js
+++ jest-with-registry.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 
 module.exports = {
   ...require('./jest.config'),
+  // Many tests change the dist tags of packages.
+  // Unfortunately, this means that if two such tests will run at the same time,
+  // they may break each other.
+  maxWorkers: 1,
   globalSetup: path.join(__dirname, 'jest.globalSetup.js'),
   globalTeardown: path.join(__dirname, 'jest.globalTeardown.js'),
 }
