291607c5a0ff8f467876057f6f8c3cb2994544bb
Tristan Knight <tris203@gmail.com>
2023-11-07 12:21:30 +0000

    feat: prune --force removes alien directories (#7272)
    add the ability to use --force to remove alien directory from the store

--- store/plugin-commands-store/src/storePrune.ts
+++ store/plugin-commands-store/src/storePrune.ts
@@ -6,13 +6,14 @@ export async function storePrune (
   opts: {
     reporter?: ReporterFunction
     storeController: StoreController
+    removeAlienFiles?: boolean
   }
 ) {
   const reporter = opts?.reporter
   if ((reporter != null) && typeof reporter === 'function') {
     streamParser.on('data', reporter)
   }
-  await opts.storeController.prune()
+  await opts.storeController.prune(opts.removeAlienFiles)
   await opts.storeController.close()
 
   if ((reporter != null) && typeof reporter === 'function') {
