fe43409dd1c1affa81cdaae68de155b8b58abfc2
Zoltan Kochan <z@kochan.io>
2023-11-12 13:09:53 +0200

    fix: downgrade npm-packlist to improve performance
    ref #6997
    ref #7250

--- fs/packlist/src/index.ts
+++ fs/packlist/src/index.ts
@@ -1,8 +1,11 @@
-import Arborist from '@npmcli/arborist'
 import npmPacklist from 'npm-packlist'
 
-export async function packlist (pkgDir: string) {
-  const arborist = new Arborist(({ path: pkgDir }))
-  const tree = await arborist.loadActual()
-  return npmPacklist(tree)
+export async function packlist (pkgDir: string): Promise<string[]> {
+  const files = await npmPacklist({ path: pkgDir })
+  // There's a bug in the npm-packlist version that we use,
+  // it sometimes returns duplicates.
+  // Related issue: https://github.com/pnpm/pnpm/issues/6997
+  // Unfortunately, we cannot upgrade the library
+  // newer versions of npm-packlist are very slow.
+  return Array.from(new Set(files.map((file) => file.replace(/^\.[/\\]/, ''))))
 }
