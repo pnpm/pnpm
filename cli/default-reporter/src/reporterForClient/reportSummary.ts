e05825d6be6af9e5e32fd935a1d0f5910c69e21c
Yanzi-dev <yanziweb@gmail.com>
2023-11-16 00:02:08 +0100

    feat: pnpm i a-module-already-in-dev-deps should show message that it wasn't moved to dependencies

--- cli/default-reporter/src/reporterForClient/reportSummary.ts
+++ cli/default-reporter/src/reporterForClient/reportSummary.ts
@@ -71,10 +71,11 @@ export function reportSummary (
             msg += EOL
             msg += _printDiffs(diffs)
             msg += EOL
-            if (depType === 'dev' && !opts.pnpmConfig?.saveDev) {
+            if (depType === 'dev' && opts.pnpmConfig?.saveDev === false) {
+              msg += EOL
               msg += 'was already in devDependencies but wasn\'t moved automatically to dependencies. Please move it manually if needed. '
+              msg += EOL
             }
-            msg += EOL
           } else if (opts.pnpmConfig?.[CONFIG_BY_DEP_TYPE[depType]] === false) {
             msg += EOL
             msg += `${chalk.cyanBright(`${propertyByDependencyType[depType] as string}:`)} skipped`
