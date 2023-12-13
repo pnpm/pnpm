d91aec5350dd9a930eca323ec2f749d4faac997d
btea <2356281422@qq.com>
2023-12-13 19:05:28 +0800

    chore(dep): update @types/node

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -26,7 +26,6 @@ export async function finishWorkers () {
 }
 
 function createTarballWorkerPool (): WorkerPool {
-  // @ts-expect-error - `availableParallelism` is not exist until update @types/node to v18.14.5
   const maxWorkers = Math.max(2, (os.availableParallelism?.() ?? os.cpus().length) - Math.abs(process.env.PNPM_WORKERS ? parseInt(process.env.PNPM_WORKERS) : 0)) - 1
   const workerPool = new WorkerPool({
     id: 'pnpm',
