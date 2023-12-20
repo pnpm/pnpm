c94af806cdd8577a3bed30f3a95643e6d3561e68
Hulk <33158585+byteHulk@users.noreply.github.com>
2023-12-20 10:20:49 +0800

    style: updating to eslint compliant code

--- pnpm/src/cmd/index.ts
+++ pnpm/src/cmd/index.ts
@@ -23,7 +23,7 @@ import {
 import { server } from '@pnpm/plugin-commands-server'
 import { setup } from '@pnpm/plugin-commands-setup'
 import { store } from '@pnpm/plugin-commands-store'
-import { catFile, catIndex } from '@pnpm/plugin-commands-store-search';
+import { catFile, catIndex } from '@pnpm/plugin-commands-store-search'
 import { init } from '@pnpm/plugin-commands-init'
 import pick from 'ramda/src/pick'
 import { type PnpmOptions } from '../types'
