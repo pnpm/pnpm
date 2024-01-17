42cc4b27e7fd097d600d3c445c61ee8da949ce37
Zoltan Kochan <z@kochan.io>
2024-01-17 01:59:45 +0100

    fix: building git-hosted dependencies (#7407)

--- fetching/tarball-fetcher/src/gitHostedTarballFetcher.ts
+++ fetching/tarball-fetcher/src/gitHostedTarballFetcher.ts
@@ -1,8 +1,11 @@
+import fs from 'node:fs/promises'
 import { type FetchFunction, type FetchOptions } from '@pnpm/fetcher-base'
 import type { Cafs } from '@pnpm/cafs-types'
 import { globalWarn } from '@pnpm/logger'
 import { preparePackage } from '@pnpm/prepare-package'
 import { addFilesFromDir } from '@pnpm/worker'
+import renameOverwrite from 'rename-overwrite'
+import { fastPathTemp as pathTemp } from 'path-temp'
 
 interface Resolution {
   integrity?: string
@@ -18,9 +21,15 @@ export interface CreateGitHostedTarballFetcher {
 
 export function createGitHostedTarballFetcher (fetchRemoteTarball: FetchFunction, fetcherOpts: CreateGitHostedTarballFetcher): FetchFunction {
   const fetch = async (cafs: Cafs, resolution: Resolution, opts: FetchOptions) => {
-    const { filesIndex, manifest } = await fetchRemoteTarball(cafs, resolution, opts)
+    // This solution is not perfect but inside the fetcher we don't currently know the location
+    // of the built and non-built index files.
+    const nonBuiltIndexFile = fetcherOpts.ignoreScripts ? opts.filesIndexFile : pathTemp(opts.filesIndexFile)
+    const { filesIndex, manifest } = await fetchRemoteTarball(cafs, resolution, {
+      ...opts,
+      filesIndexFile: nonBuiltIndexFile,
+    })
     try {
-      const prepareResult = await prepareGitHostedPkg(filesIndex as Record<string, string>, cafs, opts.filesIndexFile, fetcherOpts, opts)
+      const prepareResult = await prepareGitHostedPkg(filesIndex as Record<string, string>, cafs, nonBuiltIndexFile, opts.filesIndexFile, fetcherOpts, opts)
       if (prepareResult.ignoredBuild) {
         globalWarn(`The git-hosted package fetched from "${resolution.tarball}" has to be built but the build scripts were ignored.`)
       }
@@ -37,6 +46,7 @@ export function createGitHostedTarballFetcher (fetchRemoteTarball: FetchFunction
 async function prepareGitHostedPkg (
   filesIndex: Record<string, string>,
   cafs: Cafs,
+  filesIndexFileNonBuilt: string,
   filesIndexFile: string,
   opts: CreateGitHostedTarballFetcher,
   fetcherOpts: FetchOptions
@@ -50,6 +60,25 @@ async function prepareGitHostedPkg (
     force: true,
   })
   const shouldBeBuilt = await preparePackage(opts, tempLocation)
+  if (!shouldBeBuilt) {
+    if (filesIndexFileNonBuilt !== filesIndexFile) {
+      await renameOverwrite(filesIndexFileNonBuilt, filesIndexFile)
+    }
+    return {
+      filesIndex,
+      ignoredBuild: false,
+    }
+  }
+  if (opts.ignoreScripts) {
+    return {
+      filesIndex,
+      ignoredBuild: true,
+    }
+  }
+  try {
+    // The temporary index file may be deleted
+    await fs.unlink(filesIndexFileNonBuilt)
+  } catch {}
   // Important! We cannot remove the temp location at this stage.
   // Even though we have the index of the package,
   // the linking of files to the store is in progress.
@@ -60,6 +89,6 @@ async function prepareGitHostedPkg (
       filesIndexFile,
       pkg: fetcherOpts.pkg,
     }),
-    ignoredBuild: opts.ignoreScripts && shouldBeBuilt,
+    ignoredBuild: false,
   }
 }
