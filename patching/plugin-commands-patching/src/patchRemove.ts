3f6fb2c5a6766bbd461debbbaf3916f26e959e31
Zoltan Kochan <z@kochan.io>
2024-01-19 00:02:33 +0100

    test: fix

--- patching/plugin-commands-patching/src/patchRemove.ts
+++ patching/plugin-commands-patching/src/patchRemove.ts
@@ -57,11 +57,11 @@ export async function handler (opts: PatchRemoveCommandOptions, params: string[]
     throw new PnpmError('NO_PATCHES_TO_REMOVE', 'There are no patches that need to be removed')
   }
 
-  const patchesDir: string[] = []
+  const patchesDirs = new Set<string>()
   await Promise.all(patchesToRemove.map(async (patch) => {
     if (Object.prototype.hasOwnProperty.call(patchedDependencies, patch)) {
       const patchFile = path.join(lockfileDir, patchedDependencies[patch])
-      patchesDir.push(path.dirname(patchFile))
+      patchesDirs.add(path.dirname(patchFile))
       await fs.rm(patchFile, { force: true })
       delete rootProjectManifest.pnpm!.patchedDependencies![patch]
       if (!Object.keys(rootProjectManifest.pnpm!.patchedDependencies!).length) {
@@ -73,11 +73,13 @@ export async function handler (opts: PatchRemoveCommandOptions, params: string[]
     }
   }))
 
-  await Promise.all([...new Set(patchesDir)].map(async (dir) => {
-    const files = await fs.readdir(dir)
-    if (!files.length) {
-      await fs.rmdir(dir)
-    }
+  await Promise.all(Array.from(patchesDirs).map(async (dir) => {
+    try {
+      const files = await fs.readdir(dir)
+      if (!files.length) {
+        await fs.rmdir(dir)
+      }
+    } catch {}
   }))
 
   await writeProjectManifest(rootProjectManifest)
