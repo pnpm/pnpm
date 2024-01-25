6c89ad7ef9e4c576a8b89d0630b1167264dcc84d
khai96_ <hvksmr1996@gmail.com>
2024-01-25 21:35:14 +0700

    test: fix

--- pkg-manager/plugin-commands-installation/test/updateToLatestSpecsFromManifest.ts
+++ pkg-manager/plugin-commands-installation/test/updateToLatestSpecsFromManifest.ts
@@ -19,8 +19,8 @@ test('updateToLatestSpecsFromManifest()', () => {
     devDependencies: true,
   }))
   updateResult1.toHaveLength(2)
-  updateResult1.toContain('@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep@latest')
-  updateResult1.toContain('is-positive@latest')
+  updateResult1.toContain('@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep')
+  updateResult1.toContain('is-positive')
 
   const updateResult2 = expect(updateToLatestSpecsFromManifest(MANIFEST, {
     optionalDependencies: false,
@@ -28,15 +28,15 @@ test('updateToLatestSpecsFromManifest()', () => {
     devDependencies: false,
   }))
   updateResult2.toHaveLength(1)
-  updateResult2.toStrictEqual(['@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep@latest'])
+  updateResult2.toStrictEqual(['@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep'])
 })
 
 test('createLatestSpecs()', () => {
   expect(
     createLatestSpecs(['@pnpm.e2e/dep-of-pkg-with-1-dep', 'is-positive@2.0.0', 'foo'], MANIFEST)
-  ).toStrictEqual(['@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep@latest', 'is-positive@2.0.0', 'foo'])
+  ).toStrictEqual(['@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep', 'is-positive@2.0.0', 'foo'])
 
   expect(
     createLatestSpecs(['@pnpm.e2e/dep-of-pkg-with-1-dep', 'is-positive', 'foo', 'bar'], MANIFEST)
-  ).toStrictEqual(['@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep@latest', 'is-positive@latest', 'foo'])
+  ).toStrictEqual(['@pnpm.e2e/dep-of-pkg-with-1-dep@npm:@pnpm.e2e/pkg-with-1-dep', 'is-positive', 'foo'])
 })
