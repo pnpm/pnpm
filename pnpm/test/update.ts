dfd3ad0f45e1b8e712ec0ddaf8d8978053052ba6
Zoltan Kochan <z@kochan.io>
2024-01-27 16:24:28 +0100

    fix: test

--- pnpm/test/update.ts
+++ pnpm/test/update.ts
@@ -208,13 +208,13 @@ test('update --latest specific dependency', async function () {
   const lockfile = await project.readLockfile()
   expect(lockfile.dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version).toBe('100.0.0')
   expect(lockfile.dependencies['@pnpm.e2e/bar'].version).toBe('100.1.0')
-  expect(lockfile.dependencies['@pnpm.e2e/foo'].version).toBe('100.1.0')
+  expect(lockfile.dependencies['@pnpm.e2e/foo'].version).toBe('100.0.0')
   expect(lockfile.dependencies['alias'].version).toBe('/@pnpm.e2e/qar@100.1.0')
 
   const pkg = await readPackageJsonFromDir(process.cwd())
   expect(pkg.dependencies?.['@pnpm.e2e/dep-of-pkg-with-1-dep']).toBe('100.0.0')
   expect(pkg.dependencies?.['@pnpm.e2e/bar']).toBe('^100.1.0')
-  expect(pkg.dependencies?.['@pnpm.e2e/foo']).toBe('100.1.0')
+  expect(pkg.dependencies?.['@pnpm.e2e/foo']).toBe('100.0.0')
   expect(pkg.dependencies?.['alias']).toBe('npm:@pnpm.e2e/qar@^100.1.0')
   expect(pkg.dependencies?.['is-negative']).toBe('github:kevva/is-negative')
 })
@@ -400,12 +400,12 @@ test('recursive update --latest specific dependency on projects that do not shar
   const manifest1 = await readPackageJsonFromDir(path.resolve('project-1'))
   expect(manifest1.dependencies).toStrictEqual({
     alias: 'npm:@pnpm.e2e/qar@100.1.0',
-    '@pnpm.e2e/dep-of-pkg-with-1-dep': '101.0.0',
+    '@pnpm.e2e/dep-of-pkg-with-1-dep': '100.0.0',
     '@pnpm.e2e/foo': '^100.1.0',
   })
 
   const lockfile1 = await projects['project-1'].readLockfile()
-  expect(lockfile1.dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version).toBe('101.0.0')
+  expect(lockfile1.dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version).toBe('100.0.0')
   expect(lockfile1.dependencies['@pnpm.e2e/foo'].version).toBe('100.1.0')
   expect(lockfile1.dependencies['alias'].version).toBe('/@pnpm.e2e/qar@100.1.0')
 
@@ -575,7 +575,7 @@ test('recursive update --latest specific dependency on projects with a shared a 
   const manifest1 = await readPackageJsonFromDir(path.resolve('project-1'))
   expect(manifest1.dependencies).toStrictEqual({
     alias: 'npm:@pnpm.e2e/qar@100.1.0',
-    '@pnpm.e2e/dep-of-pkg-with-1-dep': '101.0.0',
+    '@pnpm.e2e/dep-of-pkg-with-1-dep': '100.0.0',
     '@pnpm.e2e/foo': '100.1.0',
   })
 
@@ -586,7 +586,7 @@ test('recursive update --latest specific dependency on projects with a shared a 
   })
 
   const lockfile = await readYamlFile<any>('pnpm-lock.yaml') // eslint-disable-line
-  expect(lockfile.importers['project-1'].dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version).toBe('101.0.0')
+  expect(lockfile.importers['project-1'].dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version).toBe('100.0.0')
   expect(lockfile.importers['project-1'].dependencies['@pnpm.e2e/foo'].version).toBe('100.1.0')
   expect(lockfile.importers['project-1'].dependencies['alias'].version).toBe('/@pnpm.e2e/qar@100.1.0')
   expect(lockfile.importers['project-2'].dependencies['@pnpm.e2e/bar'].version).toBe('100.0.0')
