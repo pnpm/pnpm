732430a1f32a4f2fa0f46c9c75b820ea96b17c1a
Zoltan Kochan <z@kochan.io>
2024-01-28 12:25:20 +0100

    fix: don't add bundleDependencies=false to the lockfile (#7580)
    close #7576

--- pkg-manager/resolve-dependencies/src/updateLockfile.ts
+++ pkg-manager/resolve-dependencies/src/updateLockfile.ts
@@ -150,9 +150,16 @@ function toLockfileDependency (
   if (pkg.additionalInfo.libc != null) {
     result['libc'] = pkg.additionalInfo.libc
   }
-  if (Array.isArray(pkg.additionalInfo.bundledDependencies) || Array.isArray(pkg.additionalInfo.bundleDependencies) ||
-    typeof pkg.additionalInfo.bundledDependencies === 'boolean' || typeof pkg.additionalInfo.bundleDependencies === 'boolean') {
-    result['bundledDependencies'] = pkg.additionalInfo.bundledDependencies ?? pkg.additionalInfo.bundleDependencies
+  if (
+    Array.isArray(pkg.additionalInfo.bundledDependencies) ||
+    pkg.additionalInfo.bundledDependencies === true
+  ) {
+    result['bundledDependencies'] = pkg.additionalInfo.bundledDependencies
+  } else if (
+    Array.isArray(pkg.additionalInfo.bundleDependencies) ||
+    pkg.additionalInfo.bundleDependencies === true
+  ) {
+    result['bundledDependencies'] = pkg.additionalInfo.bundleDependencies
   }
   if (pkg.additionalInfo.deprecated) {
     result['deprecated'] = pkg.additionalInfo.deprecated
