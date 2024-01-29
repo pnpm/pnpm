d636eed20b349c463d8fb05cfd062fa0192b19fe
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-29 11:33:44 -0800

    fix(object-hasher): switch to object-hash to fix hashing of large objects (#7591)

--- crypto/object-hasher/src/index.ts
+++ crypto/object-hasher/src/index.ts
@@ -1,4 +1,34 @@
-import { hasher } from 'node-object-hash'
+// We use object-hash even though node-object-hash is faster.
+// Unlike node-object-hash, object-hash is streaming the hash updates,
+// avoiding "Invalid string length" errors.
+import hash from 'object-hash'
 
-export const hashObjectWithoutSorting = hasher({ sort: false }).hash
-export const hashObject = hasher({}).hash
+const defaultOptions: hash.BaseOptions = {
+  respectType: false,
+  algorithm: 'sha1',
+}
+
+const withoutSortingOptions: hash.BaseOptions = {
+  ...defaultOptions,
+  unorderedArrays: false,
+  unorderedObjects: false,
+  unorderedSets: false,
+}
+
+const withSortingOptions: hash.BaseOptions = {
+  ...defaultOptions,
+  unorderedArrays: true,
+  unorderedObjects: true,
+  unorderedSets: true,
+}
+
+function hashUnknown (object: unknown, options: hash.BaseOptions) {
+  if (object === undefined) {
+    // '0'.repeat(40) to match the length of other returned sha1 hashes.
+    return '0000000000000000000000000000000000000000'
+  }
+  return hash(object, options)
+}
+
+export const hashObjectWithoutSorting = (object: unknown) => hashUnknown(object, withoutSortingOptions)
+export const hashObject = (object: unknown) => hashUnknown(object, withSortingOptions)
