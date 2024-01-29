d4af3dfcd231479ff1c87e8b1ac32b8edb691777
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-29 10:32:36 -0800

    fix(object-hasher): switch to object-hash to fix hashing of large objects

--- crypto/object-hasher/test/index.ts
+++ crypto/object-hasher/test/index.ts
@@ -3,8 +3,8 @@ import { hashObject, hashObjectWithoutSorting } from '@pnpm/crypto.object-hasher
 describe('hashObject', () => {
   const hash = hashObject
   it('creates a hash', () => {
-    expect(hash({ b: 1, a: 2 })).toEqual('c8c943f9321eb7f98834b58391eee848d458c7b35211fc4911cdb1bbd877b74a')
-    expect(hash(undefined)).toEqual('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+    expect(hash({ b: 1, a: 2 })).toEqual('e3d3f89836fac144779e57d0e831efd06336036b')
+    expect(hash(undefined)).toEqual('0000000000000000000000000000000000000000')
   })
   it('sorts', () => {
     expect(hash({ b: 1, a: 2 })).toEqual(hash({ a: 2, b: 1 }))
@@ -15,8 +15,8 @@ describe('hashObject', () => {
 describe('hashObjectWithoutSorting', () => {
   const hash = hashObjectWithoutSorting
   it('creates a hash', () => {
-    expect(hash({ b: 1, a: 2 })).toEqual('c0a68b14aa1886a799c2e7c1289b65ccb79a668881d5b6956f0b185a9ac112d7')
-    expect(hash(undefined)).toEqual('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+    expect(hash({ b: 1, a: 2 })).toEqual('dd34c1644a1d52da41808e5c1e6849829ef77999')
+    expect(hash(undefined)).toEqual('0000000000000000000000000000000000000000')
   })
   it('does not sort', () => {
     expect(hash({ b: 1, a: 2 })).not.toEqual(hash({ a: 2, b: 1 }))
