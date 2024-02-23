d90ff9f56dc0a99894825fad677155821e3cbef4
Zoltan Kochan <z@kochan.io>
2024-02-23 23:57:50 +0100

    fix: uploading symlinks to side-effects cache
    close #7691

---
+++ .changeset/rich-eyes-speak.md
@@ -0,0 +1,6 @@
+---
+"@pnpm/store.cafs": patch
+"pnpm": patch
+---
+
+Symlinks should be resolved and uploaded to the side-effects cache [#7691](https://github.com/pnpm/pnpm/issues/7691).
--- store/cafs/test/index.ts
+++ store/cafs/test/index.ts
@@ -46,6 +46,20 @@ describe('cafs', () => {
     const { filesIndex } = addFiles()
     expect(filesIndex[path.join('subdir', 'should-exist.txt')]).toBeDefined()
   })
+
+  it('symlinks are resolved and added as regular files', () => {
+    const storeDir = tempy.directory()
+    const srcDir = tempy.directory()
+    const filePath = path.join(srcDir, 'index.js')
+    const symlinkPath = path.join(srcDir, 'symlink.js')
+    fs.writeFileSync(filePath, '// comment', 'utf8')
+    fs.symlinkSync(filePath, symlinkPath)
+    const addFiles = () => createCafs(storeDir).addFilesFromDir(srcDir)
+
+    const { filesIndex } = addFiles()
+    expect(filesIndex['symlink.js']).toBeDefined()
+    expect(filesIndex['symlink.js']).toStrictEqual(filesIndex['index.js'])
+  })
 })
 
 describe('checkPkgFilesIntegrity()', () => {
