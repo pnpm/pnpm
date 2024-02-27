6a067465260bbe78d8d058a608df2ee03d6841e9
khai96_ <hvksmr1996@gmail.com>
2024-02-27 17:06:58 +0700

    refactor: correct a name

--- hooks/read-package-hook/src/createOptionalDependenciesRemover.ts
+++ hooks/read-package-hook/src/createOptionalDependenciesRemover.ts
@@ -1,10 +1,10 @@
 import { type PackageManifest, type ProjectManifest, type ReadPackageHook } from '@pnpm/types'
 
 export function createOptionalDependenciesRemover (toBeRemoved: string[]): ReadPackageHook {
-  return <Manifest extends PackageManifest | ProjectManifest> (manifest: Manifest) => removeOptionDependencies(manifest, toBeRemoved)
+  return <Manifest extends PackageManifest | ProjectManifest> (manifest: Manifest) => removeOptionalDependencies(manifest, toBeRemoved)
 }
 
-function removeOptionDependencies<Manifest extends PackageManifest | ProjectManifest> (manifest: Manifest, toBeRemoved: string[]): Manifest {
+function removeOptionalDependencies<Manifest extends PackageManifest | ProjectManifest> (manifest: Manifest, toBeRemoved: string[]): Manifest {
   for (const optionalDependency in manifest.optionalDependencies) {
     if (toBeRemoved.includes(optionalDependency)) {
       delete manifest.optionalDependencies[optionalDependency]
