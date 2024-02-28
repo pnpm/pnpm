0c43885ede5ddff9fce135a57efa2bc1e373ac48
khai96_ <hvksmr1996@gmail.com>
2024-02-28 17:58:20 +0700

    test: move a test

--- pkg-manager/core/test/install/ignoredOptionalDependencies.ts
+++ pkg-manager/core/test/install/ignoredOptionalDependencies.ts
@@ -1,5 +1,6 @@
+import type { ProjectManifest } from '@pnpm/types'
 import { prepareEmpty } from '@pnpm/prepare'
-import { addDependenciesToPackage } from '@pnpm/core'
+import { addDependenciesToPackage, install } from '@pnpm/core'
 import {
   testDefaults,
 } from '../utils'
@@ -45,4 +46,31 @@ test('names in ignoredOptionalDependencies are sorted alphabetically in the lock
 
   const lockfile = project.readLockfile()
   expect(lockfile.ignoredOptionalDependencies).toStrictEqual(['bar', 'baz', 'foo', 'qux'])
+})
+
+test('adding or changing manifest.pnpm.ignoredOptionalDependencies should change lockfile.ignoredOptionalDependencies and module structure', async () => {
+  const manifest: ProjectManifest = Object.freeze({
+    dependencies: {
+      '@pnpm.e2e/pkg-with-good-optional': '1.0.0',
+    },
+  })
+  const project = prepareEmpty()
+
+  await install(manifest, testDefaults())
+  {
+    const lockfile = project.readLockfile()
+    expect(lockfile).not.toHaveProperty(['ignoredOptionalDependencies'])
+    expect(lockfile.packages).toHaveProperty(['/@pnpm.e2e/pkg-with-good-optional@1.0.0'])
+    expect(lockfile.packages).toHaveProperty(['/is-positive@1.0.0'])
+  }
+
+  await install(manifest, testDefaults({
+    ignoredOptionalDependencies: ['is-positive'],
+  }))
+  {
+    const lockfile = project.readLockfile()
+    expect(lockfile.ignoredOptionalDependencies).toStrictEqual(['is-positive'])
+    expect(lockfile.packages).toHaveProperty(['/@pnpm.e2e/pkg-with-good-optional@1.0.0'])
+    expect(lockfile.packages).not.toHaveProperty(['/is-positive@1.0.0'])
+  }
 })
