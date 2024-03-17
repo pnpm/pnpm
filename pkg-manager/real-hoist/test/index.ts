ed537d79fdb20655b10e75ae0146731ea8f17968
Zoltan Kochan <z@kochan.io>
2024-03-17 14:34:45 +0100

    test: fix

--- pkg-manager/real-hoist/test/index.ts
+++ pkg-manager/real-hoist/test/index.ts
@@ -1,9 +1,11 @@
-import path from 'path'
 import { hoist } from '@pnpm/real-hoist'
 import { readWantedLockfile } from '@pnpm/lockfile-file'
+import { fixtures } from '@pnpm/test-fixtures'
+
+const f = fixtures(__dirname)
 
 test('hoist', async () => {
-  const lockfile = await readWantedLockfile(path.join(__dirname, '../../..'), { ignoreIncompatible: true })
+  const lockfile = await readWantedLockfile(f.find('fixture'), { ignoreIncompatible: true })
   expect(hoist(lockfile!)).toBeTruthy()
 })
 
