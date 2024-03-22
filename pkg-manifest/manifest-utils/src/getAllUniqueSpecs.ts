0d9249c434b0ae9cce981fdc96225d007879c5a4
Zoltan Kochan <z@kochan.io>
2024-03-22 19:45:54 +0100

    fix: resolve missing peer from versions already present in other parts of the dep graph (#7812)

---
+++ pkg-manifest/manifest-utils/src/getAllUniqueSpecs.ts
@@ -0,0 +1,20 @@
+import { type DependencyManifest } from '@pnpm/types'
+import { getAllDependenciesFromManifest } from './getAllDependenciesFromManifest'
+
+export function getAllUniqueSpecs (manifests: DependencyManifest[]) {
+  const allSpecs: Record<string, string> = {}
+  const ignored = new Set<string>()
+  for (const manifest of manifests) {
+    const specs = getAllDependenciesFromManifest(manifest)
+    for (const [name, spec] of Object.entries(specs)) {
+      if (ignored.has(name)) continue
+      if (allSpecs[name] != null && allSpecs[name] !== spec || spec.includes(':')) {
+        ignored.add(name)
+        delete allSpecs[name]
+        continue
+      }
+      allSpecs[name] = spec
+    }
+  }
+  return allSpecs
+}
