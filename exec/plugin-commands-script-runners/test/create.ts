545c793dbdc5bc1528f7a644e386b8113b746b43
khai96_ <hvksmr1996@gmail.com>
2024-03-29 23:58:15 +0700

    test: fix typescript errors

--- exec/plugin-commands-script-runners/test/create.ts
+++ exec/plugin-commands-script-runners/test/create.ts
@@ -10,6 +10,7 @@ it('throws an error if called without arguments', async () => {
   await expect(create.handler({
     ...DEFAULT_OPTS,
     dir: process.cwd(),
+    dlxCacheMaxAge: Infinity,
   }, [])).rejects.toThrow(PnpmError)
   expect(dlx.handler).not.toHaveBeenCalled()
 })
@@ -20,12 +21,14 @@ it(
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['some-app'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-some-app'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['create_no_dash'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-create_no_dash'])
   }
@@ -37,12 +40,14 @@ it(
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['create-some-app'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-some-app'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['create-'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-'])
   }
@@ -54,12 +59,14 @@ it(
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope/some-app'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create-some-app'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope/create_no_dash'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create-create_no_dash'])
   }
@@ -71,12 +78,14 @@ it(
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope/create-some-app'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create-some-app'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope/create-'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create-'])
   }
@@ -86,6 +95,7 @@ it('infers a package name from a plain scope', async () => {
   await create.handler({
     ...DEFAULT_OPTS,
     dir: process.cwd(),
+    dlxCacheMaxAge: Infinity,
   }, ['@scope'])
   expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create'])
 })
@@ -94,6 +104,7 @@ it('passes the remaining arguments to `dlx`', async () => {
   await create.handler({
     ...DEFAULT_OPTS,
     dir: process.cwd(),
+    dlxCacheMaxAge: Infinity,
   }, ['some-app', 'directory/', '--silent'])
   expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-some-app', 'directory/', '--silent'])
 })
@@ -104,35 +115,41 @@ it(
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['foo@2.0.0'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-foo@2.0.0'])
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['foo@latest'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['create-foo@latest'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope@2.0.0'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create@2.0.0'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope@next'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create@next'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope/foo@2.0.0'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create-foo@2.0.0'])
 
     await create.handler({
       ...DEFAULT_OPTS,
       dir: process.cwd(),
+      dlxCacheMaxAge: Infinity,
     }, ['@scope/create-a@2.0.0'])
     expect(dlx.handler).toHaveBeenCalledWith(expect.anything(), ['@scope/create-a@2.0.0'])
   }
