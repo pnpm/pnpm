ee48a538187f4e7107842b41c66be7329104e4a6
khai96_ <hvksmr1996@gmail.com>
2024-04-09 00:15:51 +0700

    refactor(init): return type annotations

--- packages/plugin-commands-init/src/utils.ts
+++ packages/plugin-commands-init/src/utils.ts
@@ -20,7 +20,7 @@ export function personToString (person: Person): string {
   return name + email + url
 }
 
-export function workWithInitModule (localConfig: Record<string, string>) {
+export function workWithInitModule (localConfig: Record<string, string>): Record<string, string> {
   const { initModule, ...restConfig } = localConfig
   if (initModule) {
     const filePath = path.resolve(localConfig.initModule)
@@ -34,7 +34,7 @@ export function workWithInitModule (localConfig: Record<string, string>) {
   return restConfig
 }
 
-export function workWithInitConfig (localConfig: Record<string, string>) {
+export function workWithInitConfig (localConfig: Record<string, string>): Record<string, string> {
   const packageJson: Record<string, string> = {}
   const authorInfo: Record<string, string> = {}
   for (const localConfigKey in localConfig) {
