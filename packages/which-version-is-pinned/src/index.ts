d19520537a8844fd4875c185fbe791996036a1a1
khai96_ <hvksmr1996@gmail.com>
2024-04-09 00:16:36 +0700

    refactor: return type annotations

--- packages/which-version-is-pinned/src/index.ts
+++ packages/which-version-is-pinned/src/index.ts
@@ -1,6 +1,12 @@
 import { parseRange } from 'semver-utils'
 
-export function whichVersionIsPinned (spec: string) {
+export type PinnedVersion =
+  | 'none'
+  | 'patch'
+  | 'minor'
+  | 'major'
+
+export function whichVersionIsPinned (spec: string): PinnedVersion | undefined {
   const isWorkspaceProtocol = spec.startsWith('workspace:')
   if (isWorkspaceProtocol) spec = spec.slice('workspace:'.length)
   if (spec === '*') return isWorkspaceProtocol ? 'patch' : 'none'
