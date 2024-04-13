f38fd2b48ed58bdf2c099b978fca92422ee24334
Zoltan Kochan <z@kochan.io>
2024-04-13 14:32:52 +0200

    test: dlx

--- pnpm/test/dlx.ts
+++ pnpm/test/dlx.ts
@@ -201,11 +201,12 @@ test('dlx should ignore .npmrc in the current directory', async () => {
   prepare({})
   fs.writeFileSync('.npmrc', 'hoist-pattern=', 'utf8')
 
+  const cacheDir = path.resolve('cache')
   await execPnpm([
     `--config.store-dir=${path.resolve('store')}`,
-    `--config.cache-dir=${path.resolve('cache')}`,
+    `--config.cache-dir=${cacheDir}`,
     'dlx', 'shx', 'echo', 'hi'])
 
-  const modulesManifest = await readModulesManifest(path.resolve('cache', 'dlx', createBase32Hash('shx'), 'pkg/node_modules'))
+  const modulesManifest = await readModulesManifest(path.join(cacheDir, 'dlx', createBase32Hash('shx'), 'pkg/node_modules'))
   expect(modulesManifest?.hoistPattern).toStrictEqual(['*'])
 })
