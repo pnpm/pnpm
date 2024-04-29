3f5182a1757fc71b8846758d9064868d0c7083be
Zoltan Kochan <z@kochan.io>
2024-04-29 15:28:15 +0200

    revert: "feat: read catalog configs from workspace manifest (feature flagged) (#8019)"
    This reverts commit 773434ea221fc8d58e0b9b63d9dd0c7b9c4f4f2f.

--- workspace/read-manifest/src/errors/InvalidWorkspaceManifestError.ts
+++
@@ -1,7 +0,0 @@
-import { PnpmError } from '@pnpm/error'
-
-export class InvalidWorkspaceManifestError extends PnpmError {
-  constructor (message: string) {
-    super('INVALID_WORKSPACE_CONFIGURATION', message)
-  }
-}
