d09c09c58d944c67176ceb5cf830aa1408d671e8
Zoltan Kochan <z@kochan.io>
2024-05-10 15:29:54 +0200

    fix: update @pnpm/registry-mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 const { finishWorkers } = require('@pnpm/worker')
 
-jest.retryTimes(0, {
+jest.retryTimes(1, {
   // Some tests don't clean up their resources completely and cause the retried
   // run fail. Set logErrorsBeforeRetry to make it more clear that the retried
   // run was a retry and not the first attempt. Otherwise Jest hides the first
