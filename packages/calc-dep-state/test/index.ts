479d4fd009d57cd4b62fb19a74e81c9b473dfa77
Zoltan Kochan <z@kochan.io>
2024-05-18 12:07:43 +0200

    test: fix

--- packages/calc-dep-state/test/index.ts
+++ packages/calc-dep-state/test/index.ts
@@ -1,16 +1,17 @@
 import { calcDepState } from '@pnpm/calc-dep-state'
 import { ENGINE_NAME } from '@pnpm/constants'
 import { hashObject } from '@pnpm/crypto.object-hasher'
+import { type PkgId } from '@pnpm/types'
 
 const depsGraph = {
   'registry/foo@1.0.0': {
-    depPath: 'foo@1.0.0',
+    packageId: 'foo@1.0.0' as PkgId,
     children: {
       bar: 'registry/bar@1.0.0',
     },
   },
   'registry/bar@1.0.0': {
-    depPath: 'bar@1.0.0',
+    packageId: 'bar@1.0.0' as PkgId,
     children: {
       foo: 'registry/foo@1.0.0',
     },
