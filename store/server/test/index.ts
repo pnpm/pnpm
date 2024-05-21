d80c3f5ddaa9c4410c7f400ca11498204bab5c65
Zoltan Kochan <z@kochan.io>
2024-05-21 23:57:44 +0200

    test: fix

--- store/server/test/index.ts
+++ store/server/test/index.ts
@@ -20,7 +20,7 @@ async function createStoreController (storeDir?: string) {
   }
   const authConfig = { registry }
   const cacheDir = path.join(tmp, 'cache')
-  const { resolve, fetchers } = createClient({
+  const { resolve, fetchers, clearResolutionCache } = createClient({
     authConfig,
     cacheDir,
     rawConfig: {},
@@ -31,6 +31,7 @@ async function createStoreController (storeDir?: string) {
     storeDir,
     verifyStoreIntegrity: true,
     virtualStoreDirMaxLength: 120,
+    clearResolutionCache,
   })
 }
 
