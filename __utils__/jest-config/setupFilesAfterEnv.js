17b342d88526c3f07b0fc896caada7c15f8e88d6
Brandon Cheng <gluxon@users.noreply.github.com>
2024-05-24 22:39:36 -0600

    test: turn off test retries since they can hide buggy tests

--- __utils__/jest-config/setupFilesAfterEnv.js
+++ __utils__/jest-config/setupFilesAfterEnv.js
@@ -1,14 +1,5 @@
 const { finishWorkers } = require('@pnpm/worker')
 
-jest.retryTimes(1, {
-  // Some tests don't clean up their resources completely and cause the retried
-  // run fail. Set logErrorsBeforeRetry to make it more clear that the retried
-  // run was a retry and not the first attempt. Otherwise Jest hides the first
-  // attempt. This makes it easier to distinguish between a test that's truly
-  // broken and one that's not retry-able.
-  logErrorsBeforeRetry: true
-})
-
 afterAll(async () => {
   await finishWorkers()
 })
