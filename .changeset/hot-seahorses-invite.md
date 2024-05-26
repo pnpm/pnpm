81d90c9dd259c11459a811069c0c03da88c0aefd
Zoltan Kochan <z@kochan.io>
2024-05-27 00:54:34 +0200

    fix: reduce memory usage by using numbers for Node IDs (#8135)

---
+++ .changeset/hot-seahorses-invite.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/resolve-dependencies": patch
+---
+
+Reduce memory usage during peer dependency resolution by using numbers for Node IDs.
--- pkg-manager/resolve-dependencies/src/nextNodeId.ts
+++ pkg-manager/resolve-dependencies/src/nextNodeId.ts
@@ -2,8 +2,8 @@ let nodeIdCounter = 0
 
 type Brand<K, T> = K & { __brand: T }
 
-export type NodeId = Brand<string, 'nodeId'>
+export type NodeId = Brand<string | number, 'nodeId'>
 
 export function nextNodeId (): NodeId {
-  return (++nodeIdCounter).toString() as NodeId
+  return ++nodeIdCounter as NodeId
 }
