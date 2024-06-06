de49e04c63599e3f8552cc236f3c89a48e8591e0
zcf0508 <zcf0508@live.com>
2024-06-06 22:54:06 +0800

    feat: disallow external diff drivers

--- patching/plugin-commands-patching/src/patchCommit.ts
+++ patching/plugin-commands-patching/src/patchCommit.ts
@@ -111,7 +111,7 @@ async function diffFolders (folderA: string, folderB: string): Promise<string> {
   let stderr!: string
 
   try {
-    const result = await execa('git', ['-c', 'core.safecrlf=false', 'diff', '--src-prefix=a/', '--dst-prefix=b/', '--ignore-cr-at-eol', '--irreversible-delete', '--full-index', '--no-index', '--text', folderAN, folderBN], {
+    const result = await execa('git', ['-c', 'core.safecrlf=false', 'diff', '--src-prefix=a/', '--dst-prefix=b/', '--ignore-cr-at-eol', '--irreversible-delete', '--full-index', '--no-index', '--text', '--no-ext-diff', folderAN, folderBN], {
       cwd: process.cwd(),
       env: {
         ...process.env,
