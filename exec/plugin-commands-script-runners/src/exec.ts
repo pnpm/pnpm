271386f0a0a9a0cfef088e6d463c8f8cc5d91e4f
Khải <hvksmr1996@gmail.com>
2024-06-06 16:19:14 +0700

    feat(exec): default `reporterHidePrefix` to `true` (#8176)
    close #8174

--- exec/plugin-commands-script-runners/src/exec.ts
+++ exec/plugin-commands-script-runners/src/exec.ts
@@ -186,7 +186,7 @@ export async function handler (
     './node_modules/.bin',
     ...opts.extraBinPaths,
   ]
-  const reporterShowPrefix = opts.recursive && !opts.reporterHidePrefix
+  const reporterShowPrefix = opts.recursive && opts.reporterHidePrefix === false
   for (const chunk of chunks) {
     // eslint-disable-next-line no-await-in-loop
     await Promise.all(chunk.map(async (prefix: string) =>
