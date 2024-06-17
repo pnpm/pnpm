8edee2ec7097236a450ee0d8b5b7d9449d6416bf
Brandon Cheng <gluxon@users.noreply.github.com>
2024-06-17 16:09:57 -0400

    feat: create new `@pnpm/catalogs.resolver` package (#8219)
    * feat: create new @pnpm/catalogs.resolver package
    
    * fix: work around CI setup not compiling orphan packages before testing
    
    This works around a problem with pnpm's CI setup not compiling
    packages that are not dependencies of the main pnpm package before
    running these tests.
    
    https://github.com/pnpm/pnpm/pull/8027#issuecomment-2081650304

---
+++ catalogs/resolver/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  resolveFromCatalog,
+  type CatalogResolution,
+  type CatalogResolutionFound,
+  type CatalogResolutionMisconfiguration,
+  type CatalogResolutionUnused as CatalogResolutionNotUsed,
+  type CatalogResolutionResult,
+  type CatalogResolver,
+  type WantedDependency,
+} from './resolveFromCatalog'
+export { type CatalogResultMatcher, matchCatalogResolveResult } from './matchCatalogResolveResult'
