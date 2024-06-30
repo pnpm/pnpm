1c4371fa718556df3ab1571223e75ff817df804d
Zoltan Kochan <z@kochan.io>
2024-07-01 01:27:04 +0200

    test: fix

--- fs/find-packages/test/index.ts
+++ fs/find-packages/test/index.ts
@@ -15,7 +15,7 @@ test('finds package', async () => {
   const pkgs = await findPackages(root)
 
   expect(pkgs).toHaveLength(1)
-  expect(pkgs[0].dir).toBeDefined()
+  expect(pkgs[0].rootDir).toBeDefined()
   expect(pkgs[0].manifest).toBeDefined()
 })
 
@@ -24,9 +24,9 @@ test('finds packages by patterns', async () => {
   const pkgs = await findPackages(root, { patterns: ['components/**'] })
 
   expect(pkgs).toHaveLength(2)
-  expect(pkgs[0].dir).toBeDefined()
+  expect(pkgs[0].rootDir).toBeDefined()
   expect(pkgs[0].manifest).toBeDefined()
-  expect(pkgs[1].dir).toBeDefined()
+  expect(pkgs[1].rootDir).toBeDefined()
   expect(pkgs[1].manifest).toBeDefined()
   expect([pkgs[0].manifest.name, pkgs[1].manifest.name].sort(compare)).toStrictEqual(['component-1', 'component-2'])
 })
@@ -52,9 +52,9 @@ test('ignore packages by patterns', async () => {
   const pkgs = await findPackages(root, { patterns: ['**', '!libs/**'] })
 
   expect(pkgs).toHaveLength(2)
-  expect(pkgs[0].dir).toBeDefined()
+  expect(pkgs[0].rootDir).toBeDefined()
   expect(pkgs[0].manifest).toBeDefined()
-  expect(pkgs[1].dir).toBeDefined()
+  expect(pkgs[1].rootDir).toBeDefined()
   expect(pkgs[1].manifest).toBeDefined()
   expect([pkgs[0].manifest.name, pkgs[1].manifest.name].sort(compare)).toStrictEqual(['component-1', 'component-2'])
 })
@@ -64,10 +64,10 @@ test('json and yaml manifests are also found', async () => {
   const pkgs = await findPackages(root)
 
   expect(pkgs).toHaveLength(3)
-  expect(pkgs[0].dir).toBeDefined()
+  expect(pkgs[0].rootDir).toBeDefined()
   expect(pkgs[0].manifest.name).toEqual('component-1')
-  expect(pkgs[1].dir).toBeDefined()
+  expect(pkgs[1].rootDir).toBeDefined()
   expect(pkgs[1].manifest.name).toEqual('component-2')
-  expect(pkgs[2].dir).toBeDefined()
+  expect(pkgs[2].rootDir).toBeDefined()
   expect(pkgs[2].manifest.name).toEqual('foo')
 })
