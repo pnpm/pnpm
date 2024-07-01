7e291c11fcd724e05513a51be069c42ca1b87486
Zoltan Kochan <z@kochan.io>
2024-07-01 14:21:19 +0200

    test: fix

--- workspace/find-packages/test/index.ts
+++ workspace/find-packages/test/index.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import {
   findWorkspacePackagesNoCheck,
-  arrayOfWorkspacePackagesToMap,
   findWorkspacePackages,
 } from '@pnpm/workspace.find-packages'
 import { readWorkspaceManifest } from '@pnpm/workspace.read-manifest'
@@ -13,21 +12,6 @@ beforeEach(() => {
 
 afterEach(() => {
   (logger.warn as jest.Mock).mockRestore()
-})
-
-// This is supported for compatibility with Yarn's implementation
-// see https://github.com/pnpm/pnpm/issues/2648
-test('arrayOfWorkspacePackagesToMap() treats private packages with no version as packages with 0.0.0 version', () => {
-  const privateProject = {
-    manifest: {
-      name: 'private-pkg',
-    },
-  }
-  expect(arrayOfWorkspacePackagesToMap([privateProject])).toStrictEqual({
-    'private-pkg': {
-      '0.0.0': privateProject,
-    },
-  })
 })
 
 test('findWorkspacePackagesNoCheck() skips engine checks', async () => {
