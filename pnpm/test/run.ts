b2ea3b5021ae130fe9a3b0a057bfefcb2981e8cd
khai96_ <hvksmr1996@gmail.com>
2024-07-07 05:39:57 +0700

    test: move it to where it works

--- pnpm/test/run.ts
+++ pnpm/test/run.ts
@@ -232,3 +232,47 @@ test('--reporter-hide-prefix should hide workspace prefix', async () => {
   expect(output).toContain('2')
   expect(output).not.toContain('script2: 2')
 })
+
+test('recursive run when some packages define node version, config does not define use-node-version', async () => {
+  preparePackages([
+    {
+      name: 'node-version-undefined',
+      scripts: {
+        'print-node-version': 'node -v',
+      },
+    },
+    {
+      name: 'node-version-18',
+      scripts: {
+        'print-node-version': 'node -v',
+      },
+      pnpm: {
+        useNodeVersion: '18.0.0',
+      },
+    },
+    {
+      name: 'node-version-20',
+      scripts: {
+        'print-node-version': 'node -v',
+      },
+      pnpm: {
+        useNodeVersion: '20.0.0',
+      },
+    },
+  ])
+
+  const output = execPnpmSync(['run', '-r', 'print-node-version'])
+  expect(
+    output
+      .stdout
+      .toString()
+      .trim()
+      .split('\n')
+      .filter(x => /v[0-9]+\.[0-9]+\.[0-9]+/.test(x))
+      .sort()
+  ).toStrictEqual([
+    'node-version-18 print-node-version: v18.0.0',
+    'node-version-20 print-node-version: v20.0.0',
+    `node-version-undefined print-node-version: ${process.version}`,
+  ])
+})
