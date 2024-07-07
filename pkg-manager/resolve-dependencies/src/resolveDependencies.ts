ec5ebea6e619fd86bd49f444db3b71de7d74e465
Brandon Cheng <gluxon@users.noreply.github.com>
2024-07-06 23:38:34 -0400

    fix: skip over cataloged deps on `pnpm update --latest`

--- pkg-manager/resolve-dependencies/src/resolveDependencies.ts
+++ pkg-manager/resolve-dependencies/src/resolveDependencies.ts
@@ -554,6 +554,12 @@ async function resolveDependenciesOfImporterDependency (
       ...importer.options,
       parentPkgAliases: importer.parentPkgAliases,
       pickLowestVersion: pickLowestVersion && !importer.updatePackageManifest,
+      // Cataloged dependencies cannot be upgraded yet since they require
+      // updating the pnpm-workspace.yaml file. This will be handled in a future
+      // version of pnpm.
+      updateToLatest: catalogLookup != null
+        ? false
+        : importer.options.updateToLatest,
     },
     extendedWantedDep
   )
