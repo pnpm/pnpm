ed8050f704177c5b315bfa9c2bb08fd3d92ba139
khai96_ <hvksmr1996@gmail.com>
2024-07-10 02:11:22 +0700

    refactor: make modifyBinPaths optional

--- pkg-manager/core/src/install/extendInstallOptions.ts
+++ pkg-manager/core/src/install/extendInstallOptions.ts
@@ -152,7 +152,7 @@ export interface StrictInstallOptions {
   hoistWorkspacePackages?: boolean
   virtualStoreDirMaxLength: number
   peersSuffixMaxLength: number
-  modifyBinPaths: (manifest: ProjectManifest, extraBinPaths: string[]) => string[] | Promise<string[]>
+  modifyBinPaths?: (manifest: ProjectManifest, extraBinPaths: string[]) => string[] | Promise<string[]>
 }
 
 export type InstallOptions =
