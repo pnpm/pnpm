726753f05d6512c93dd6f26787a92891ded59533
Khải <hvksmr1996@gmail.com>
2024-07-16 22:46:12 +0700

    fix(patch): error message for missing .modules.yaml (#8315)
    close #8257

--- patching/plugin-commands-patching/src/getPatchedDependency.ts
+++ patching/plugin-commands-patching/src/getPatchedDependency.ts
@@ -4,7 +4,6 @@ import { prompt } from 'enquirer'
 import { readCurrentLockfile, type TarballResolution } from '@pnpm/lockfile-file'
 import { nameVerFromPkgSnapshot } from '@pnpm/lockfile-utils'
 import { PnpmError } from '@pnpm/error'
-import { WANTED_LOCKFILE } from '@pnpm/constants'
 import { readModulesManifest } from '@pnpm/modules-yaml'
 import { isGitHostedPkgUrl } from '@pnpm/pick-fetcher'
 import realpathMissing from 'realpath-missing'
@@ -76,7 +75,10 @@ export async function getVersionsFromLockfile (dep: ParseWantedDependencyResult,
   if (!lockfile) {
     throw new PnpmError(
       'PATCH_NO_LOCKFILE',
-      `No ${WANTED_LOCKFILE} found: Cannot patch without a lockfile`
+      'The modules directory is not ready for patching',
+      {
+        hint: 'Run pnpm install first',
+      }
     )
   }
 
