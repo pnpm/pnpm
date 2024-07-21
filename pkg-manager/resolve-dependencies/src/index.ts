89b9141bd2eb4419d6bb308683030681981475c5
Zoltan Kochan <z@kochan.io>
2024-07-21 02:16:54 +0200

    test: fix

--- pkg-manager/resolve-dependencies/src/index.ts
+++ pkg-manager/resolve-dependencies/src/index.ts
@@ -309,10 +309,10 @@ export async function resolveDependencies (
   newLockfile.catalogs = getCatalogSnapshots(Object.values(resolvedImporters).flatMap(({ directDependencies }) => directDependencies))
   if (opts.catalogs) {
     for (const [catalogName, catalog] of Object.entries(opts.catalogs)) {
-      if (!newLockfile.catalogs[catalogName]) {
-        newLockfile.catalogs[catalogName] = {}
-      }
       if (catalog) {
+        if (!newLockfile.catalogs[catalogName]) {
+          newLockfile.catalogs[catalogName] = {}
+        }
         for (const [depName, specifier] of Object.entries(catalog)) {
           if (!newLockfile.catalogs[catalogName][depName]) {
             newLockfile.catalogs[catalogName][depName] = {
