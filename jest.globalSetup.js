08a92ef0e4a429686b580e2f9d1a9d68fee40e8f
Zoltan Kochan <z@kochan.io>
2024-07-25 03:17:39 +0200

    test: fix

--- jest.globalSetup.js
+++ jest.globalSetup.js
@@ -3,6 +3,10 @@ const { start, prepare } = require('@pnpm/registry-mock')
 module.exports = () => {
   prepare()
   const server = start({
+    // Verdaccio stopped working properly on Node.js 22.
+    // You can test the issue by running:
+    //   pnpm --filter=core run test test/install/auth.ts
+    useNodeVersion: '20.16.0',
     stdio: 'ignore',
   })
   let killed = false
