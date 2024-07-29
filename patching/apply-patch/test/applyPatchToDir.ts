79cde416f6ea71106a11f217d20c902ea17a9a8f
khai96_ <hvksmr1996@gmail.com>
2024-07-30 00:59:39 +0700

    test(apply-patch): new behaviors

--- patching/apply-patch/test/applyPatchToDir.ts
+++ patching/apply-patch/test/applyPatchToDir.ts
@@ -1,10 +1,53 @@
+import fs from 'fs'
+import path from 'path'
 import { applyPatchToDir } from '@pnpm/patching.apply-patch'
 import { fixtures } from '@pnpm/test-fixtures'
 import { tempDir } from '@pnpm/prepare'
+import { globalWarn } from '@pnpm/logger'
 
 const f = fixtures(__dirname)
 
+jest.mock('@pnpm/logger', () => {
+  const originalModule = jest.requireActual('@pnpm/logger')
+  return {
+    ...originalModule,
+    globalWarn: jest.fn(),
+  }
+})
+
+beforeEach(() => {
+  ;(globalWarn as jest.Mock).mockClear()
+})
+
+function prepareDirToPatch () {
+  const dir = tempDir()
+  f.copy('patch-target.txt', path.join(dir, 'patch-target.txt'))
+  return dir
+}
+
 describe('applyPatchToDir() without allowFailure', () => {
+  it('should succeed when patch is applicable', () => {
+    const patchFilePath = f.find('applicable.patch')
+    const successfullyPatched = f.find('successfully-patched.txt')
+    const patchedDir = prepareDirToPatch()
+    applyPatchToDir({
+      allowFailure: false,
+      patchFilePath,
+      patchedDir,
+    })
+    const patchTarget = path.join(patchedDir, 'patch-target.txt')
+    expect(fs.readFileSync(patchTarget, 'utf-8')).toBe(fs.readFileSync(successfullyPatched, 'utf-8'))
+  })
+  it('should fail when patch fails to apply', () => {
+    const patchFilePath = f.find('non-applicable.patch')
+    const patchedDir = prepareDirToPatch()
+    expect(() => applyPatchToDir({
+      allowFailure: false,
+      patchFilePath,
+      patchedDir,
+    })).toThrow(`Could not apply patch ${patchFilePath} to ${patchedDir}`)
+    expect(fs.readFileSync(path.join(patchedDir, 'patch-target.txt'), 'utf-8')).toBe(fs.readFileSync(f.find('patch-target.txt'), 'utf-8'))
+  })
   it('should fail on invalid patch', () => {
     const patchFilePath = f.find('invalid.patch')
     expect(() => {
@@ -27,6 +70,31 @@ describe('applyPatchToDir() without allowFailure', () => {
 })
 
 describe('applyPatchToDir() with allowFailure', () => {
+  it('should succeed when patch is applicable', () => {
+    const patchFilePath = f.find('applicable.patch')
+    const successfullyPatched = f.find('successfully-patched.txt')
+    const patchedDir = prepareDirToPatch()
+    applyPatchToDir({
+      allowFailure: true,
+      patchFilePath,
+      patchedDir,
+    })
+    const patchTarget = path.join(patchedDir, 'patch-target.txt')
+    expect(fs.readFileSync(patchTarget, 'utf-8')).toBe(fs.readFileSync(successfullyPatched, 'utf-8'))
+  })
+  it('should warn when patch fails to apply', () => {
+    const patchFilePath = f.find('non-applicable.patch')
+    const patchedDir = prepareDirToPatch()
+    applyPatchToDir({
+      allowFailure: true,
+      patchFilePath,
+      patchedDir,
+    })
+    expect((globalWarn as jest.Mock).mock.calls).toStrictEqual([[
+      `Could not apply patch ${patchFilePath} to ${patchedDir}`,
+    ]])
+    expect(fs.readFileSync(path.join(patchedDir, 'patch-target.txt'), 'utf-8')).toBe(fs.readFileSync(f.find('patch-target.txt'), 'utf-8'))
+  })
   it('should fail on invalid patch', () => {
     const patchFilePath = f.find('invalid.patch')
     expect(() => {
