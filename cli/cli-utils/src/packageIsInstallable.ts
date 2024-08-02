4b5c3b13aec12da73701b7dce30f365f88bb4f94
Zoltan Kochan <z@kochan.io>
2024-08-03 00:11:24 +0200

    refactor: parsing packageManager

--- cli/cli-utils/src/packageIsInstallable.ts
+++ cli/cli-utils/src/packageIsInstallable.ts
@@ -4,6 +4,17 @@ import { logger, globalWarn } from '@pnpm/logger'
 import { checkPackage, UnsupportedEngineError, type WantedEngine } from '@pnpm/package-is-installable'
 import { type SupportedArchitectures } from '@pnpm/types'
 
+export function parsePackageManager (packageManager: string): { name: string, version: string | undefined } {
+  const [name, pmReference] = packageManager.split('@')
+  // pmReference is semantic versioning, not URL
+  if (pmReference.includes(':')) return { name, version: undefined }
+  const [version] = pmReference.split('+')
+  return {
+    name,
+    version,
+  }
+}
+
 export function packageIsInstallable (
   pkgPath: string,
   pkg: {
@@ -25,26 +36,22 @@ export function packageIsInstallable (
     ? packageManager.version
     : undefined
   if (pkg.packageManager && !isExecutedByCorepack()) {
-    const [pmName, pmReference] = pkg.packageManager.split('@')
-    if (pmName && pmName !== 'pnpm') {
-      const msg = `This project is configured to use ${pmName}`
+    const pm = parsePackageManager(pkg.packageManager)
+    if (pm.name && pm.name !== 'pnpm') {
+      const msg = `This project is configured to use ${pm.name}`
       if (opts.packageManagerStrict) {
         throw new PnpmError('OTHER_PM_EXPECTED', msg)
       } else {
         globalWarn(msg)
       }
-    } else if (currentPnpmVersion && opts.packageManagerStrictVersion && !pmReference.includes(':')) {
-      // pmReference is semantic versioning, not URL
-      const [requiredPnpmVersion] = pmReference.split('+')
-      if (requiredPnpmVersion && requiredPnpmVersion !== currentPnpmVersion) {
-        const msg = `This project is configured to use v${requiredPnpmVersion} of pnpm. Your current pnpm is v${currentPnpmVersion}`
-        if (opts.packageManagerStrict) {
-          throw new PnpmError('BAD_PM_VERSION', msg, {
-            hint: 'If you want to bypass this version check, you can set the "package-manager-strict" configuration to "false" or set the "COREPACK_ENABLE_STRICT" environment variable to "0"',
-          })
-        } else {
-          globalWarn(msg)
-        }
+    } else if (currentPnpmVersion && opts.packageManagerStrictVersion && pm.version && pm.version !== currentPnpmVersion) {
+      const msg = `This project is configured to use v${pm.version} of pnpm. Your current pnpm is v${currentPnpmVersion}`
+      if (opts.packageManagerStrict) {
+        throw new PnpmError('BAD_PM_VERSION', msg, {
+          hint: 'If you want to bypass this version check, you can set the "package-manager-strict" configuration to "false" or set the "COREPACK_ENABLE_STRICT" environment variable to "0"',
+        })
+      } else {
+        globalWarn(msg)
       }
     }
   }
