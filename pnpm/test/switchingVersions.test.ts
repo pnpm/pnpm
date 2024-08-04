5b171a8eb5ebeaba51a3d0c7db2959f2ecdfad98
Zoltan Kochan <z@kochan.io>
2024-08-04 15:38:48 +0200

    test: fix

--- pnpm/test/switchingVersions.test.ts
+++ pnpm/test/switchingVersions.test.ts
@@ -1,18 +1,16 @@
 import path from 'path'
-import PATH_NAME from 'path-name'
 import fs from 'fs'
 import { prepare } from '@pnpm/prepare'
 import { sync as writeJsonFile } from 'write-json-file'
 import { execPnpmSync } from './utils'
 
-test('global installation', async () => {
+test('switch to the pnpm version specified in the packageManager field of package.json', async () => {
   prepare()
-  process.chdir('/Volumes/src/pnpm/pnpm_tmp/108_6140/1')
   const global = path.resolve('..', 'global')
   const pnpmHome = path.join(global, 'pnpm')
   fs.mkdirSync(global)
 
-  const env = { [PATH_NAME]: pnpmHome, PNPM_HOME: pnpmHome, XDG_DATA_HOME: global }
+  const env = { PNPM_HOME: pnpmHome, XDG_DATA_HOME: global }
 
   writeJsonFile('package.json', {
     packageManager: 'pnpm@9.3.0',
