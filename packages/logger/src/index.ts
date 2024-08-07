9be856ae5a7803dde87ab76464d78c0b781260cb
khai96_ <hvksmr1996@gmail.com>
2024-08-07 18:59:08 +0700

    chore: create @pnpm/logger

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,17 @@
+export {
+  type Logger,
+  logger,
+  globalInfo,
+  globalWarn,
+} from './logger'
+export {
+  type LogBaseDebug,
+  type LogBaseError,
+  type LogBaseInfo,
+  type LogBaseWarn,
+  type LogBase,
+  type LogLevel,
+  createStreamParser,
+  streamParser,
+} from './streamParser'
+export { writeToConsole } from './writeToConsole'
