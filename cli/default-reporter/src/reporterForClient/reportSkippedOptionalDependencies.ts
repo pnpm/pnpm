ddb15b1455ee966934bf00d54ba807b8467a2318
khai96_ <hvksmr1996@gmail.com>
2024-08-08 05:41:30 +0700

    fix: missing `id` field

--- cli/default-reporter/src/reporterForClient/reportSkippedOptionalDependencies.ts
+++ cli/default-reporter/src/reporterForClient/reportSkippedOptionalDependencies.ts
@@ -12,7 +12,7 @@ export function reportSkippedOptionalDependencies (
     filter((log) => Boolean(log['prefix'] === opts.cwd && log.parents && log.parents.length === 0)),
     map((log) => Rx.of({
       msg: `info: ${
-        log.package['id'] || log.package.name && (`${log.package.name}@${log.package.version}`) || log.package['pref']
+        log.package.id || log.package.name && (`${log.package.name}@${log.package.version}`) || log.package.pref
       } is an optional dependency and failed compatibility check. Excluding it from installation.`,
     }))
   )
