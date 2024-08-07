81042a07836d87387601b660344e7233d08eceb2
khai96_ <hvksmr1996@gmail.com>
2024-08-08 01:21:51 +0700

    refactor: change logger error type

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,15 +1,16 @@
 import bole from 'bole'
+import { type PnpmError } from '@pnpm/error'
 
 bole.setFastTime()
 
-export const logger = bole('pnpm') as Logger<object>
+export const logger = bole('pnpm') as Logger<object, PnpmError>
 
-export interface Logger<T> {
+export interface Logger<T, E extends Error = Error> {
   <Y>(name: string): Logger<Y>
   debug: (log?: T) => void
   info: (log: { message: string, prefix: string }) => void
-  warn: (log: { message: string, prefix: string, error?: Error }) => void
-  error: (err: Error, log?: string | Error) => void
+  warn: (log: { message: string, prefix: string, error?: E }) => void
+  error: (err: E, log?: string | E) => void
 }
 
 const globalLogger = bole('pnpm:global')
