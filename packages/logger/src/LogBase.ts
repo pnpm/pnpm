bb4c39abf64e661ab013c2405511aa19b825412c
khai96_ <hvksmr1996@gmail.com>
2024-08-08 01:20:53 +0700

    refactor: move types to their own files

---
+++ packages/logger/src/LogBase.ts
@@ -0,0 +1,29 @@
+import { LogLevel } from './LogLevel'
+
+export interface LogBaseTemplate {
+  level?: LogLevel
+  prefix?: string
+  message?: string
+}
+
+export interface LogBaseDebug extends LogBaseTemplate {
+  level: 'debug'
+}
+
+export interface LogBaseError extends LogBaseTemplate {
+  level: 'error'
+}
+
+export interface LogBaseInfo extends LogBaseTemplate {
+  level: 'info'
+  prefix: string
+  message: string
+}
+
+export interface LogBaseWarn extends LogBaseTemplate {
+  level: 'warn'
+  prefix: string
+  message: string
+}
+
+export type LogBase = LogBaseDebug | LogBaseError | LogBaseInfo | LogBaseWarn
--- packages/logger/src/streamParser.ts
+++ packages/logger/src/streamParser.ts
@@ -1,35 +1,6 @@
 import bole from 'bole'
 import ndjson from 'ndjson'
-
-export type LogLevel = 'error' | 'warn' | 'info' | 'debug'
-
-export interface LogBaseTemplate {
-  level?: LogLevel
-  prefix?: string
-  message?: string
-}
-
-export interface LogBaseDebug extends LogBaseTemplate {
-  level: 'debug'
-}
-
-export interface LogBaseError extends LogBaseTemplate {
-  level: 'error'
-}
-
-export interface LogBaseInfo extends LogBaseTemplate {
-  level: 'info'
-  prefix: string
-  message: string
-}
-
-export interface LogBaseWarn extends LogBaseTemplate {
-  level: 'warn'
-  prefix: string
-  message: string
-}
-
-export type LogBase = LogBaseDebug | LogBaseError | LogBaseInfo | LogBaseWarn
+import { LogBase } from './LogBase'
 
 export type Reporter = (logObj: LogBase) => void
 
