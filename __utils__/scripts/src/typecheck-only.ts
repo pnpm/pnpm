a6356c96a484d16b0d7821245d60702cad4088fa
khai96_ <hvksmr1996@gmail.com>
2024-08-10 05:30:54 +0700

    feat: change all configuration

--- __utils__/scripts/src/typecheck-only.ts
+++ __utils__/scripts/src/typecheck-only.ts
@@ -1,26 +1,23 @@
-import * as assert from 'assert/strict'
-import * as cp from 'child_process'
-import * as glob from 'fast-glob'
-import * as path from 'path'
-import * as pLimit from 'p-limit'
+import assert from 'assert/strict'
+import execa from 'execa'
+import fs from 'fs'
+import glob from 'fast-glob'
+import normalizePath from 'normalize-path'
+import path from 'path'
 import readYamlFile from 'read-yaml-file'
 
 const repoRoot = path.resolve(__dirname, '../../../')
+const typeCheckDir = path.resolve(__dirname, '../../../__typecheck__')
+const typingsDir = path.resolve(__dirname, '../../../__typings__')
 const workspaceFile = path.resolve(repoRoot, 'pnpm-workspace.yaml')
 
 interface Workspace {
   packages: string[]
 }
 
-interface TaskResult {
-  project: string
-  success: boolean
-  stdout: string
-  stderr: string
-}
-
-async function main(): Promise<void> {
+async function main (): Promise<void> {
   process.chdir(repoRoot)
+
   const workspace = await readYamlFile<Workspace>(workspaceFile)
   const patterns = workspace.packages
     .map(pattern => pattern.trim())
@@ -32,47 +29,38 @@ async function main(): Promise<void> {
   })
   assert.notEqual(tsconfigFiles.length, 0)
 
-  const limit = pLimit(50)
-  const promises = tsconfigFiles.map(tsconfigFile => limit(() => new Promise<TaskResult>((resolve, reject) => {
-    const project = path.dirname(tsconfigFile)
-    const child = cp.spawn('tsc', ['--noEmit', '-p', tsconfigFile], {
-      stdio: 'pipe',
-    })
-    let stdout = ''
-    child.stdout.on('data', data => {
-      stdout += String(data)
-    })
-    let stderr = ''
-    child.stderr.on('data', data => {
-      stderr += String(data)
-    })
-    child.once('close', status => {
-      if (status === null) {
-        return reject(new Error(`tsc exits without a status code (${project})`))
-      }
-      const success = status === 0
-      resolve({ project, success, stdout, stderr })
-    })
-    child.once('error', error => reject(error))
-  })))
+  const typeCheckTSConfig = {
+    extends: '@pnpm/tsconfig',
+    compilerOptions: {
+      composite: false,
+      rootDir: '.',
+      outDir: 'lib',
+      declaration: false,
+    },
+    include: [
+      `${normalizePath(path.relative(typeCheckDir, typingsDir))}/**/*.d.ts`,
+    ],
+    exclude: [
+      path.relative(typeCheckDir, repoRoot),
+    ],
+    references: tsconfigFiles
+      .filter(projectPath => !projectPath.includes('__typecheck__'))
+      .filter(projectPath => !projectPath.includes('__utils__/tsconfig'))
+      .filter(projectPath => !projectPath.startsWith('pnpm/'))
+      .map(projectPath => path.resolve(projectPath))
+      .map(projectPath => path.relative(typeCheckDir, projectPath))
+      .map(projectPath => normalizePath(projectPath))
+      .map(path => ({ path })),
+  }
+  fs.writeFileSync(
+    path.join(typeCheckDir, 'tsconfig.json'),
+    JSON.stringify(typeCheckTSConfig, undefined, 2)
+  )
 
-  const taskResults = await Promise.all(promises)
-  let failure = 0
-  for (const { project, success, stdout, stderr } of taskResults) {
-    if (success) continue
-    failure += 1
-    console.error(`PROJECT: ${project}`)
-    if (stdout.trim()) {
-      console.log(stdout)
-    }
-    if (stderr.trim()) {
-      console.log(stderr)
-    }
-  }
-  if (failure !== 0) {
-    console.error(`${failure} projects fail typecheck`)
-    process.exit(1)
-  }
+  await execa('tsc', ['--build'], {
+    cwd: typeCheckDir,
+    stdio: 'inherit',
+  })
 }
 
 main().catch((error: unknown) => {
