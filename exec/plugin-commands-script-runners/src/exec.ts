b66cf30e034d6f41db111ed51f03c7e044be88fd
khai96_ <hvksmr1996@gmail.com>
2024-08-12 23:24:37 +0700

    feat(exec): add missing support for executionEnv
    Fixes https://github.com/pnpm/pnpm/issues/8356

--- exec/plugin-commands-script-runners/src/exec.ts
+++ exec/plugin-commands-script-runners/src/exec.ts
@@ -5,6 +5,7 @@ import { type Config, types } from '@pnpm/config'
 import { makeNodeRequireOption } from '@pnpm/lifecycle'
 import { logger } from '@pnpm/logger'
 import { tryReadProjectManifest } from '@pnpm/read-project-manifest'
+import { prepareExecutionEnv } from '@pnpm/plugin-commands-env'
 import { sortPackages } from '@pnpm/sort-packages'
 import { type Project, type ProjectsGraph, type ProjectRootDir, type ProjectRootDirRealPath } from '@pnpm/types'
 import execa from 'execa'
@@ -136,7 +137,7 @@ export async function handler (
     resumeFrom?: string
     reportSummary?: boolean
     implicitlyFellbackFromRun?: boolean
-  } & Pick<Config, 'extraBinPaths' | 'extraEnv' | 'lockfileDir' | 'modulesDir' | 'dir' | 'userAgent' | 'recursive' | 'reporterHidePrefix' | 'workspaceDir' | 'nodeOptions'>,
+  } & Pick<Config, 'rawConfig' | 'bin' | 'pnpmHomeDir' | 'extraBinPaths' | 'extraEnv' | 'lockfileDir' | 'modulesDir' | 'dir' | 'userAgent' | 'recursive' | 'reporterHidePrefix' | 'workspaceDir' | 'nodeOptions'>,
   params: string[]
 ): Promise<{ exitCode: number }> {
   // For backward compatibility
@@ -183,15 +184,23 @@ export async function handler (
   const workspacePnpPath = opts.workspaceDir && existsPnp(opts.workspaceDir)
 
   let exitCode = 0
-  const prependPaths = [
-    './node_modules/.bin',
-    ...opts.extraBinPaths,
-  ]
+  const mapPrefixToPrependPathsEntries: Array<[ProjectRootDir, string[]]> = await Promise.all(chunks.flat().map(async prefix => {
+    const executionEnv = await prepareExecutionEnv(opts, {
+      extraBinPaths: opts.extraBinPaths,
+      executionEnv: opts.selectedProjectsGraph[prefix].package.manifest.pnpm?.executionEnv,
+    })
+    return [prefix, [
+      './node_modules/.bin',
+      ...executionEnv.extraBinPaths,
+    ]]
+  }))
+  const mapPrefixToPrependPaths: Record<ProjectRootDir, string[]> = Object.fromEntries(mapPrefixToPrependPathsEntries)
   const reporterShowPrefix = opts.recursive && opts.reporterHidePrefix === false
   for (const chunk of chunks) {
     // eslint-disable-next-line no-await-in-loop
     await Promise.all(chunk.map(async (prefix) =>
       limitRun(async () => {
+        const prependPaths = mapPrefixToPrependPaths[prefix]
         result[prefix].status = 'running'
         const startTime = process.hrtime()
         try {
