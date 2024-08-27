757e6be29d1916fd0c966e64d40f96ef82f79ba6
Zoltan Kochan <z@kochan.io>
2024-08-27 13:10:25 +0200

    fix: don't recursively switch to the same pnpm version (#8468)

---
+++ .changeset/lazy-fans-hunt.md
@@ -0,0 +1,5 @@
+---
+"pnpm": patch
+---
+
+Throw an exception if pnpm switches to the same version of itself.
--- env/path/test/index.ts
+++ env/path/test/index.ts
@@ -6,9 +6,21 @@ test('prependDirsToPath', () => {
   expect(prependDirsToPath(['foo'], {})).toStrictEqual({
     name: PATH,
     value: 'foo',
+    updated: true,
   })
   expect(prependDirsToPath(['foo'], { [PATH]: 'bar' })).toStrictEqual({
     name: PATH,
     value: `foo${path.delimiter}bar`,
+    updated: true,
+  })
+  expect(prependDirsToPath(['foo', 'qar'], { [PATH]: `foo${path.delimiter}qar${path.delimiter}bar` })).toStrictEqual({
+    name: PATH,
+    value: `foo${path.delimiter}qar${path.delimiter}bar`,
+    updated: false,
+  })
+  expect(prependDirsToPath(['foo', 'qar'], { [PATH]: `foo${path.delimiter}qar` })).toStrictEqual({
+    name: PATH,
+    value: `foo${path.delimiter}qar`,
+    updated: false,
   })
 })
