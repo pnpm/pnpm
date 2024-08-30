678a762c001b3819251cc24aeae0713e0f4912cb
Ian Krieger <iandkrieger@gmail.com>
2024-08-29 21:42:45 -0400

    fix: no need for changes

--- lockfile/audit/src/types.ts
+++ lockfile/audit/src/types.ts
@@ -57,7 +57,6 @@ export interface AuditAdvisory {
   access: string
   severity: AuditLevelString
   cwe: string
-  github_advisory_id: string
   metadata: {
     module_type: string
     exploitability: number
