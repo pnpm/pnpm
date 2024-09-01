7186891208970eb7c000976e986f86c92f1f1b89
pnodet <paul.nodet@gmail.com>
2024-09-01 15:01:51 +0200

    style: remove semicolons

--- __utils__/eslint-config/index.js
+++ __utils__/eslint-config/index.js
@@ -1,15 +1,15 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
-});
+})
 
 export default [...compat.extends("standard-with-typescript"), {
     files: ["**/*.ts"],
@@ -103,4 +103,4 @@ export default [...compat.extends("standard-with-typescript"), {
         "no-lone-blocks": "off",
         "space-before-function-paren": ["error", "always"],
     },
-}];
+}]
