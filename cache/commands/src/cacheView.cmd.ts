17549e2e104644213bfd8392c8ae6688b5117ad4
Zoltan Kochan <z@kochan.io>
2024-09-15 00:37:21 +0200

    test: cache view

--- cache/commands/src/cacheView.cmd.ts
+++ cache/commands/src/cacheView.cmd.ts
@@ -4,24 +4,37 @@ import fastGlob from 'fast-glob'
 import { getIndexFilePathInCafs } from '@pnpm/store.cafs'
 import { type PackageMeta } from '@pnpm/npm-resolver'
 
-type CachedVersions = Record<string, string | false>
+interface CachedVersions {
+  cachedVersions: string[]
+  nonCachedVersions: string[]
+  cachedAt?: string
+  distTags: Record<string, string>
+}
 
 export async function cacheViewCmd (opts: { cacheDir: string, storeDir: string }, packageName: string): Promise<string> {
   const metaFilePaths = (await fastGlob(`*/${packageName}.json`, {
     cwd: opts.cacheDir,
   })).sort()
   const cafsDir = path.join(opts.storeDir, 'files')
-  const metaFilesByPath: Record<string, { versions: CachedVersions }> = {}
+  const metaFilesByPath: Record<string, CachedVersions> = {}
   for (const filePath of metaFilePaths) {
     const metaObject = JSON.parse(fs.readFileSync(path.join(opts.cacheDir, filePath), 'utf8')) as PackageMeta
-    const versions: CachedVersions = {}
+    const cachedVersions: string[] = []
+    const nonCachedVersions: string[] = []
     for (const [version, manifest] of Object.entries(metaObject.versions)) {
       if (!manifest.dist.integrity) continue
       const indexFilePath = getIndexFilePathInCafs(cafsDir, manifest.dist.integrity)
-      versions[version] = fs.existsSync(indexFilePath) ? indexFilePath : false
+      if (fs.existsSync(indexFilePath)) {
+        cachedVersions.push(version)
+      } else {
+        nonCachedVersions.push(version)
+      }
     }
     metaFilesByPath[filePath] = {
-      versions,
+      cachedVersions,
+      nonCachedVersions,
+      cachedAt: metaObject.cachedAt ? new Date(metaObject.cachedAt).toString() : undefined,
+      distTags: metaObject['dist-tags'],
     }
   }
   return JSON.stringify(metaFilesByPath, null, 2)
