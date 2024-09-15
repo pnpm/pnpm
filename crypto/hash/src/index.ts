180f2e85d05b5ba6e89b586e3b5c5f4daac1d816
Zoltan Kochan <z@kochan.io>
2024-09-15 23:12:02 +0200

    feat!: use SHA256 for hashing the pnpmfile content

--- crypto/hash/src/index.ts
+++ crypto/hash/src/index.ts
@@ -1,7 +1,19 @@
 import crypto from 'crypto'
+import fs from 'fs'
 
 export function createShortHash (input: string): string {
   const hash = crypto.createHash('sha256')
   hash.update(input)
   return hash.digest('hex').substring(0, 32)
+}
+
+export function createHash (input: string): string {
+  const hash = crypto.createHash('sha256')
+  hash.update(input)
+  return `sha256-${hash.digest('base64')}`
+}
+
+export async function createHashFromFile (file: string): Promise<string> {
+  const content = await fs.promises.readFile(file, 'utf8')
+  return createHash(content.split('\r\n').join('\n'))
 }
