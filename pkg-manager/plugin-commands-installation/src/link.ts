2c16d0609a68f14377f9c3eae31db36c0dd4312e
Zoltan Kochan <z@kochan.io>
2024-10-16 02:29:03 +0200

    feat!: the link command should add overrides

--- pkg-manager/plugin-commands-installation/src/link.ts
+++ pkg-manager/plugin-commands-installation/src/link.ts
@@ -8,6 +8,7 @@ import {
 } from '@pnpm/cli-utils'
 import { UNIVERSAL_OPTIONS } from '@pnpm/common-cli-options-help'
 import { type Config, getOptionsFromRootManifest, types as allTypes } from '@pnpm/config'
+import { DEPENDENCIES_FIELDS } from '@pnpm/types'
 import { PnpmError } from '@pnpm/error'
 import { findWorkspaceDir } from '@pnpm/find-workspace-dir'
 import { arrayOfWorkspacePackagesToMap } from '@pnpm/get-context'
@@ -15,7 +16,6 @@ import { findWorkspacePackages } from '@pnpm/workspace.find-packages'
 import { type StoreController } from '@pnpm/package-store'
 import { createOrConnectStoreControllerCached, type CreateStoreControllerOptions } from '@pnpm/store-connection-manager'
 import {
-  addDependenciesToPackage,
   install,
   type InstallOptions,
   link,
@@ -146,7 +146,7 @@ export async function handler (
 
   const linkCwdDir = opts.cliOptions?.dir && opts.cliOptions?.global ? path.resolve(opts.cliOptions.dir) : cwd
 
-  // pnpm link
+  // "pnpm link -g"
   if ((params == null) || (params.length === 0)) {
     if (path.relative(linkOpts.dir, cwd) === '') {
       throw new PnpmError('LINK_BAD_PARAMS', 'You must provide a parameter')
@@ -154,45 +154,24 @@ export async function handler (
 
     await checkPeerDeps(linkCwdDir, opts)
 
-    const { manifest, writeProjectManifest } = await tryReadProjectManifest(opts.dir, opts)
-    const newManifest = await addDependenciesToPackage(
-      manifest ?? {},
-      [`link:${linkCwdDir}`],
-      linkOpts
-    )
+    const { manifest, writeProjectManifest } = await tryReadProjectManifest(linkOpts.dir, opts)
+    const newManifest = manifest ?? {}
+    newManifest.pnpm = newManifest.pnpm ?? {}
+    newManifest.pnpm.overrides = newManifest.pnpm.overrides ?? {}
+    const { manifest: linkedManifest } = await tryReadProjectManifest(linkCwdDir, opts)
+    const linkedPkgName = linkedManifest?.name ?? path.basename(linkCwdDir)
+    const linkedPkgSpec = `link:${linkCwdDir}`
+    newManifest.pnpm.overrides[linkedPkgName] = linkedPkgSpec
+    if (DEPENDENCIES_FIELDS.every((depField) => newManifest[depField]?.[linkedPkgName] == null)) {
+      newManifest.dependencies = newManifest.dependencies ?? {}
+      newManifest.dependencies[linkedPkgName] = linkedPkgSpec
+    }
     await writeProjectManifest(newManifest)
+    await install(newManifest, linkOpts)
     return
   }
 
   const [pkgPaths, pkgNames] = partition((inp) => isFilespec.test(inp), params)
-
-  await Promise.all(
-    pkgPaths.map(async (dir) => installLimit(async () => {
-      const s = await createOrConnectStoreControllerCached(storeControllerCache, opts)
-      const config = await getConfig(
-        { ...opts.cliOptions, dir },
-        {
-          excludeReporter: true,
-          rcOptionsTypes: installCommand.rcOptionsTypes(),
-          workspaceDir: await findWorkspaceDir(dir),
-        }
-      )
-      await install(
-        await readProjectManifestOnly(dir, opts), {
-          ...config,
-          ...getOptionsFromRootManifest(config.rootProjectManifestDir, config.rootProjectManifest ?? {}),
-          include: {
-            dependencies: config.production !== false,
-            devDependencies: config.dev !== false,
-            optionalDependencies: config.optional !== false,
-          },
-          storeController: s.ctrl,
-          storeDir: s.dir,
-          workspacePackages,
-        } as InstallOptions
-      )
-    }))
-  )
 
   if (pkgNames.length > 0) {
     let globalPkgNames!: string[]
@@ -220,37 +199,18 @@ export async function handler (
 
   const { manifest, writeProjectManifest } = await readProjectManifest(linkCwdDir, opts)
 
+  const overrides: Record<string, string> = {}
   await Promise.all(
     pkgPaths.map(async (dir) => {
+      const manifest = await readProjectManifestOnly(dir, opts)
+      overrides[manifest!.name!] = `link:${path.relative(linkCwdDir, dir)}`
       await checkPeerDeps(dir, opts)
     })
   )
 
-  const linkConfig = await getConfig(
-    { ...opts.cliOptions, dir: cwd },
-    {
-      excludeReporter: true,
-      rcOptionsTypes: installCommand.rcOptionsTypes(),
-      workspaceDir: await findWorkspaceDir(cwd),
-    }
-  )
-  const storeL = await createOrConnectStoreControllerCached(storeControllerCache, linkConfig)
-  const newManifest = await link(pkgPaths, path.join(linkCwdDir, 'node_modules'), {
-    ...linkConfig,
-    targetDependenciesField: linkOpts.targetDependenciesField,
-    storeController: storeL.ctrl,
-    storeDir: storeL.dir,
-    manifest,
-  } as LinkFunctionOptions)
-  if (!opts.cliOptions?.global) {
-    await writeProjectManifest(newManifest)
-  }
-
-  await Promise.all(
-    Array.from(storeControllerCache.values())
-      .map(async (storeControllerPromise) => {
-        const storeControllerHolder = await storeControllerPromise
-        await storeControllerHolder.ctrl.close()
-      })
-  )
+  const newManifest = manifest ?? {}
+  newManifest.pnpm = newManifest.pnpm ?? {}
+  newManifest.pnpm.overrides = newManifest.pnpm.overrides ?? {}
+  Object.assign(newManifest.pnpm.overrides, overrides)
+  await writeProjectManifest(newManifest)
 }
