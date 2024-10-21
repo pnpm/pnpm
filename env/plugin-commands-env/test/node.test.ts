d04f7f23206065c085b6a2d66f39790c26549078
Ardeshir Izadi <ardeshirizadinl@gmail.com>
2024-10-21 02:53:47 +0200

    fix: don't download node, when wanted node version is running (#8673)
    close #8391
    
    ---------
    
    Co-authored-by: Zoltan Kochan <z@kochan.io>

--- env/plugin-commands-env/test/node.test.ts
+++ env/plugin-commands-env/test/node.test.ts
@@ -2,7 +2,7 @@ import AdmZip from 'adm-zip'
 import { Response } from 'node-fetch'
 import path from 'path'
 import { Readable } from 'stream'
-import { getNodeDir, getNodeBinDir, getNodeVersionsBaseDir, type NvmNodeCommandOptions } from '../lib/node'
+import { getNodeDir, getNodeBinDir, getNodeVersionsBaseDir, type NvmNodeCommandOptions, prepareExecutionEnv } from '../lib/node'
 import { tempDir } from '@pnpm/prepare'
 
 const fetchMock = jest.fn(async (url: string) => {
@@ -83,4 +83,20 @@ test('get node version base dir', async () => {
   expect(typeof getNodeVersionsBaseDir).toBe('function')
   const versionDir = getNodeVersionsBaseDir(process.cwd())
   expect(versionDir).toBe(path.resolve(process.cwd(), 'nodejs'))
+})
+
+describe('prepareExecutionEnv', () => {
+  test('should not proceed to fetch Node.js if the process is already running in wanted node version', async () => {
+    fetchMock.mockImplementationOnce(() => {
+      throw new Error('prepareExecutionEnv should not proceed to fetch Node.js when wanted version is running')
+    })
+
+    await prepareExecutionEnv({
+      bin: '',
+      pnpmHomeDir: process.cwd(),
+      rawConfig: {},
+    }, {
+      executionEnv: { nodeVersion: process.versions.node },
+    })
+  })
 })
