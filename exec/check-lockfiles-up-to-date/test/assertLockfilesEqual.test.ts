2a7f0d060d6f1ac3f02d23cf6f8c616e6dc1ad25
khai96_ <hvksmr1996@gmail.com>
2024-10-22 19:40:21 +0700

    test: assertLockfilesEqual

--- exec/check-lockfiles-up-to-date/test/assertLockfilesEqual.test.ts
+++ exec/check-lockfiles-up-to-date/test/assertLockfilesEqual.test.ts
@@ -3,22 +3,80 @@ import { type Lockfile } from '@pnpm/lockfile.fs'
 import { type ProjectId } from '@pnpm/types'
 import { assertLockfilesEqual } from '../src/assertLockfilesEqual'
 
-const wantedLockfile: Lockfile = {
-  lockfileVersion: LOCKFILE_VERSION,
-  importers: {
-    ['.' as ProjectId]: {
-      specifiers: {
-        foo: '^1.0.0',
-      },
-      dependencies: {
-        foo: '1.0.1',
+test('if wantedLockfile does not have any specifier, currentLockfile is allowed to be null', () => {
+  assertLockfilesEqual(null, {
+    lockfileVersion: LOCKFILE_VERSION,
+    importers: {
+      ['.' as ProjectId]: {
+        specifiers: {},
       },
     },
-  },
-}
-
-test.skip('should not error if the currentLockfile does not exist', () => {
-  assertLockfilesEqual(null, wantedLockfile, '<LOCKFILE_DIR>')
+  }, '<LOCKFILE_DIR>')
 })
 
-test.todo('more test cases')
+test('should throw if wantedLockfile has specifiers but currentLockfile is null', () => {
+  // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+  expect(() => assertLockfilesEqual(null, {
+    lockfileVersion: LOCKFILE_VERSION,
+    importers: {
+      ['.' as ProjectId]: {
+        specifiers: {
+          foo: '^1.0.0',
+        },
+        dependencies: {
+          foo: '1.0.1',
+        },
+      },
+    },
+  }, '<LOCKFILE_DIR>')).toThrow('Project . requires dependencies but none was installed.')
+})
+
+test('should not throw if wantedLockfile and currentLockfile are equal', () => {
+  const lockfile = (): Lockfile => ({
+    lockfileVersion: LOCKFILE_VERSION,
+    importers: {
+      ['.' as ProjectId]: {
+        specifiers: {
+          foo: '^1.0.0',
+        },
+        dependencies: {
+          foo: '1.0.1',
+        },
+      },
+    },
+  })
+  assertLockfilesEqual(lockfile(), lockfile(), '<LOCKFILE_DIR>')
+})
+
+test('should throw if wantedLockfile and currentLockfile are not equal', () => {
+  // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+  expect(() => assertLockfilesEqual(
+    {
+      lockfileVersion: LOCKFILE_VERSION,
+      importers: {
+        ['.' as ProjectId]: {
+          specifiers: {
+            foo: '^1.0.0',
+          },
+          dependencies: {
+            foo: '1.0.1',
+          },
+        },
+      },
+    },
+    {
+      lockfileVersion: LOCKFILE_VERSION,
+      importers: {
+        ['.' as ProjectId]: {
+          specifiers: {
+            foo: '^1.0.0',
+          },
+          dependencies: {
+            foo: '1.1.0',
+          },
+        },
+      },
+    },
+    '<LOCKFILE_DIR>')
+  ).toThrow('The installed dependencies in the modules directory is not up-to-date with the lockfile in <LOCKFILE_DIR>.')
+})
