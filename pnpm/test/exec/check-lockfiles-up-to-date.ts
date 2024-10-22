d1a003f961dccf0a675e05de6d24151f14b4026e
khai96_ <hvksmr1996@gmail.com>
2024-10-22 18:01:21 +0700

    test: outline

---
+++ pnpm/test/exec/check-lockfiles-up-to-date.ts
@@ -0,0 +1,14 @@
+// import { prepare } from '@pnpm/prepare'
+// import { execPnpmSync } from '../utils'
+
+test.todo('should check for outdated dependencies for single project')
+
+// test.todo('should not check if env.pnpm_run_skip_deps_check is defined')
+
+test.todo('should not prevent nested `pnpm run` after having mutated the manifests')
+
+test.todo('should check for outdated dependencies for multi-project workspace before `pnpm run` on the root package')
+
+test.todo('should check for outdated dependencies for multi-project workspace before `pnpm run` on one of the package in the workspace')
+
+test.todo('should check for outdated dependencies for multi-project workspace before recursive run')
