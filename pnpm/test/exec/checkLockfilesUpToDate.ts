19df8103ac016b4a15e2dd16cfc34b5997f9097b
khai96_ <hvksmr1996@gmail.com>
2024-10-23 19:43:49 +0700

    test: create groups

--- pnpm/test/exec/checkLockfilesUpToDate.ts
+++ pnpm/test/exec/checkLockfilesUpToDate.ts
@@ -6,108 +6,112 @@ import { execPnpm, execPnpmSync } from '../utils'
 
 const CHECK_DEPS_BEFORE_RUN_SCRIPTS = '--config.check-deps-before-run-scripts=true'
 
-test('should check for outdated dependencies for single project', async () => {
-  const manifest: ProjectManifest = {
-    name: 'root',
-    private: true,
-    dependencies: {
-      '@pnpm.e2e/foo': '100.0.0',
-    },
-    scripts: {
-      start: 'echo hello from script',
-      'check-env:linux': 'echo pnpm_run_skip_deps_check is $pnpm_run_skip_deps_check',
-    },
-  }
+describe('single project workspace', () => {
+  test('single dependency', async () => {
+    const manifest: ProjectManifest = {
+      name: 'root',
+      private: true,
+      dependencies: {
+        '@pnpm.e2e/foo': '100.0.0',
+      },
+      scripts: {
+        start: 'echo hello from script',
+        'check-env:linux': 'echo pnpm_run_skip_deps_check is $pnpm_run_skip_deps_check',
+      },
+    }
 
-  const project = prepare(manifest)
+    const project = prepare(manifest)
 
-  const cacheDir = path.resolve('cache')
-  const config = [
-    CHECK_DEPS_BEFORE_RUN_SCRIPTS,
-    `--config.cache-dir=${cacheDir}`,
-  ]
+    const cacheDir = path.resolve('cache')
+    const config = [
+      CHECK_DEPS_BEFORE_RUN_SCRIPTS,
+      `--config.cache-dir=${cacheDir}`,
+    ]
 
-  // attempting to execute a script without installing dependencies should fail
-  {
-    const { status, stdout } = execPnpmSync([...config, 'start'])
-    expect(status).not.toBe(0)
-    expect(stdout.toString()).toContain('ERR_PNPM_RUN_CHECK_DEPS_LOCKFILE_NOT_FOUND')
-  }
+    // attempting to execute a script without installing dependencies should fail
+    {
+      const { status, stdout } = execPnpmSync([...config, 'start'])
+      expect(status).not.toBe(0)
+      expect(stdout.toString()).toContain('ERR_PNPM_RUN_CHECK_DEPS_LOCKFILE_NOT_FOUND')
+    }
 
-  await execPnpm([...config, 'install'])
+    await execPnpm([...config, 'install'])
 
-  // installing dependencies on a single package workspace should not create a packages list cache
-  {
-    const packagesList = await loadPackagesList({ cacheDir, workspaceDir: process.cwd() })
-    expect(packagesList).toBeUndefined()
-  }
+    // installing dependencies on a single package workspace should not create a packages list cache
+    {
+      const packagesList = await loadPackagesList({ cacheDir, workspaceDir: process.cwd() })
+      expect(packagesList).toBeUndefined()
+    }
 
-  // should be able to execute a script after dependencies have been installed
-  {
-    const { status, stdout } = execPnpmSync([...config, 'start'])
-    expect(status).toBe(0)
-    expect(stdout.toString()).toContain('hello from script')
-  }
+    // should be able to execute a script after dependencies have been installed
+    {
+      const { status, stdout } = execPnpmSync([...config, 'start'])
+      expect(status).toBe(0)
+      expect(stdout.toString()).toContain('hello from script')
+    }
 
-  project.writePackageJson({
-    ...manifest,
-    dependencies: {
-      ...manifest.dependencies,
-      '@pnpm.e2e/foo': '100.1.0',
-    },
+    project.writePackageJson({
+      ...manifest,
+      dependencies: {
+        ...manifest.dependencies,
+        '@pnpm.e2e/foo': '100.1.0',
+      },
+    })
+
+    // attempting to execute a script with outdated dependencies should fail
+    {
+      const { status, stdout } = execPnpmSync([...config, 'start'])
+      expect(status).not.toBe(0)
+      expect(stdout.toString()).toContain('ERR_PNPM_RUN_CHECK_DEPS_UNSATISFIED_PKG_MANIFEST')
+    }
+
+    await execPnpm([...config, 'install'])
+
+    // should be able to execute a script after dependencies have been updated
+    {
+      const { status, stdout } = execPnpmSync([...config, 'start'])
+      expect(status).toBe(0)
+      expect(stdout.toString()).toContain('hello from script')
+    }
+
+    project.writePackageJson({
+      ...manifest,
+      dependencies: {}, // delete all dependencies
+    })
+
+    // attempting to execute a script with redundant dependencies should fail
+    {
+      const { status, stdout } = execPnpmSync([...config, 'start'])
+      expect(status).not.toBe(0)
+      expect(stdout.toString()).toContain('ERR_PNPM_RUN_CHECK_DEPS_UNSATISFIED_PKG_MANIFEST')
+    }
+
+    await execPnpm([...config, 'install'])
+
+    // should be able to execute a script without dependencies
+    {
+      const { status, stdout } = execPnpmSync([...config, 'start'])
+      expect(status).toBe(0)
+      expect(stdout.toString()).toContain('hello from script')
+    }
+
+    // should set env.pnpm_run_skip_deps_check for the script
+    if (process.platform === 'linux') {
+      const { status, stdout } = execPnpmSync([...config, 'run', 'check-env:linux'])
+      expect(status).toBe(0)
+      expect(stdout.toString()).toContain('pnpm_run_skip_deps_check is true')
+    }
   })
 
-  // attempting to execute a script with outdated dependencies should fail
-  {
-    const { status, stdout } = execPnpmSync([...config, 'start'])
-    expect(status).not.toBe(0)
-    expect(stdout.toString()).toContain('ERR_PNPM_RUN_CHECK_DEPS_UNSATISFIED_PKG_MANIFEST')
-  }
-
-  await execPnpm([...config, 'install'])
-
-  // should be able to execute a script after dependencies have been updated
-  {
-    const { status, stdout } = execPnpmSync([...config, 'start'])
-    expect(status).toBe(0)
-    expect(stdout.toString()).toContain('hello from script')
-  }
-
-  project.writePackageJson({
-    ...manifest,
-    dependencies: {}, // delete all dependencies
-  })
-
-  // attempting to execute a script with redundant dependencies should fail
-  {
-    const { status, stdout } = execPnpmSync([...config, 'start'])
-    expect(status).not.toBe(0)
-    expect(stdout.toString()).toContain('ERR_PNPM_RUN_CHECK_DEPS_UNSATISFIED_PKG_MANIFEST')
-  }
-
-  await execPnpm([...config, 'install'])
-
-  // should be able to execute a script without dependencies
-  {
-    const { status, stdout } = execPnpmSync([...config, 'start'])
-    expect(status).toBe(0)
-    expect(stdout.toString()).toContain('hello from script')
-  }
-
-  // should set env.pnpm_run_skip_deps_check for the script
-  if (process.platform === 'linux') {
-    const { status, stdout } = execPnpmSync([...config, 'run', 'check-env:linux'])
-    expect(status).toBe(0)
-    expect(stdout.toString()).toContain('pnpm_run_skip_deps_check is true')
-  }
+  test.todo('no dependencies')
 })
 
-test.todo('single project with no dependencies')
+describe('multi-project workspace', () => {
+  test.todo('should not prevent nested `pnpm run` after having mutated the manifests')
 
-test.todo('should not prevent nested `pnpm run` after having mutated the manifests')
+  test.todo('should check for outdated dependencies before `pnpm run` on the root package')
 
-test.todo('should check for outdated dependencies for multi-project workspace before `pnpm run` on the root package')
+  test.todo('should check for outdated dependencies before `pnpm run` on one of the package in the workspace')
 
-test.todo('should check for outdated dependencies for multi-project workspace before `pnpm run` on one of the package in the workspace')
-
-test.todo('should check for outdated dependencies for multi-project workspace before recursive run')
+  test.todo('should check for outdated dependencies before recursive run')
+})
