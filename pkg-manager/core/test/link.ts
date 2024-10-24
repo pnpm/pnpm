aa50a0606fff163eee57b573b32def6583804238
Zoltan Kochan <z@kochan.io>
2024-10-24 02:16:31 +0200

    test: fix

--- pkg-manager/core/test/link.ts
+++ pkg-manager/core/test/link.ts
@@ -1,7 +1,9 @@
 import path from 'path'
-import { install } from '@pnpm/core'
+import { addDependenciesToPackage, install } from '@pnpm/core'
 import { fixtures } from '@pnpm/test-fixtures'
 import { prepareEmpty } from '@pnpm/prepare'
+import { addDistTag } from '@pnpm/registry-mock'
+import symlinkDir from 'symlink-dir'
 import { testDefaults } from './utils'
 
 const f = fixtures(__dirname)
@@ -31,6 +33,45 @@ test('relative link is linked by the name of the alias', async () => {
       version: 'link:../hello-world-js-bin',
     },
   })
+})
+
+test('relative link is not rewritten by argumentless install', async () => {
+  const project = prepareEmpty()
+
+  const linkedPkgName = 'hello-world-js-bin'
+  const linkedPkgPath = path.resolve('..', linkedPkgName)
+
+  f.copy(linkedPkgName, linkedPkgPath)
+  symlinkDir.sync(linkedPkgPath, path.resolve('node_modules/@pnpm.e2e/hello-world-js-bin'))
+
+  await install({}, testDefaults())
+
+  expect(project.requireModule('@pnpm.e2e/hello-world-js-bin/package.json').isLocal).toBeTruthy()
+})
+
+test('relative link is rewritten by named installation to regular dependency', async () => {
+  await addDistTag({ package: '@pnpm.e2e/hello-world-js-bin', version: '1.0.0', distTag: 'latest' })
+  const project = prepareEmpty()
+
+  const linkedPkgName = 'hello-world-js-bin'
+  const linkedPkgPath = path.resolve('..', linkedPkgName)
+
+  const opts = testDefaults({ fastUnpack: false })
+
+  f.copy(linkedPkgName, linkedPkgPath)
+  symlinkDir.sync(linkedPkgPath, path.resolve('node_modules/@pnpm.e2e/hello-world-js-bin'))
+
+  const manifest = await addDependenciesToPackage({}, ['@pnpm.e2e/hello-world-js-bin'], opts)
+
+  expect(manifest.dependencies).toStrictEqual({ '@pnpm.e2e/hello-world-js-bin': '^1.0.0' })
+
+  expect(project.requireModule('@pnpm.e2e/hello-world-js-bin/package.json').isLocal).toBeFalsy()
+
+  const wantedLockfile = project.readLockfile()
+  expect(wantedLockfile.importers['.'].dependencies?.['@pnpm.e2e/hello-world-js-bin'].version).toBe('1.0.0')
+
+  const currentLockfile = project.readCurrentLockfile()
+  expect(currentLockfile.importers['.'].dependencies?.['@pnpm.e2e/hello-world-js-bin'].version).toBe('1.0.0')
 })
 
 // test.skip('relative link when an external lockfile is used', async () => {
