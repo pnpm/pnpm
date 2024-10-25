2aed76dbaffcd9302f7c16d2642a16a03fc8c528
Brandon Cheng <gluxon@users.noreply.github.com>
2024-10-25 08:06:17 -0400

    fix: fork bomb when pnpm self-manages its version (#8679)
    * fix: prevent fork bombing if .tools dir had an incomplete download
    
    * refactor: factor out reusable `VersionSwitchFail` class
    
    * fix: avoid swallowing errors from spawn.sync()
    
    * test: fix

--- pnpm/test/switchingVersions.test.ts
+++ pnpm/test/switchingVersions.test.ts
@@ -1,8 +1,10 @@
 import path from 'path'
 import fs from 'fs'
 import { prepare } from '@pnpm/prepare'
+import { getToolDirPath } from '@pnpm/tools.path'
 import { sync as writeJsonFile } from 'write-json-file'
 import { execPnpmSync } from './utils'
+import isWindows from 'is-windows'
 
 test('switch to the pnpm version specified in the packageManager field of package.json, when manager-package-manager=versions is true', async () => {
   prepare()
@@ -57,4 +59,28 @@ test('do not switch to pnpm version when a range is specified', async () => {
   const { stdout } = execPnpmSync(['help'], { env })
 
   expect(stdout.toString()).toContain('Cannot switch to pnpm@^9.3.0')
+})
+
+test('throws error if pnpm tools dir is corrupt', () => {
+  prepare()
+  const pnpmHome = path.resolve('pnpm')
+  const env = { PNPM_HOME: pnpmHome }
+  const version = '9.3.0'
+  fs.writeFileSync('.npmrc', 'manage-package-manager-versions=true')
+  writeJsonFile('package.json', {
+    packageManager: `pnpm@${version}`,
+  })
+
+  // Run pnpm once to ensure the tools dir is created.
+  execPnpmSync(['help'], { env })
+
+  // Intentionally corrupt the tool dir.
+  const toolDir = getToolDirPath({ pnpmHomeDir: pnpmHome, tool: { name: 'pnpm', version } })
+  fs.rmSync(path.join(toolDir, 'bin/pnpm'))
+  if (isWindows()) {
+    fs.rmSync(path.join(toolDir, 'bin/pnpm.cmd'))
+  }
+
+  const { stderr } = execPnpmSync(['help'], { env })
+  expect(stderr.toString()).toContain('Failed to switch pnpm to v9.3.0. Looks like pnpm CLI is missing')
 })
