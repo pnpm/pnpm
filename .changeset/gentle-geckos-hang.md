9d40a4edb0e8b9502aebcf2c22c0bb987e8c894a
Zoltan Kochan <z@kochan.io>
2024-11-02 11:58:05 +0100

    feat: self-update should accept a version specifier (#8725)

---
+++ .changeset/gentle-geckos-hang.md
@@ -0,0 +1,6 @@
+---
+"@pnpm/tools.plugin-commands-self-updater": minor
+"pnpm": minor
+---
+
+The `self-update` now accepts a version specifier to install a specific version of pnpm. E.g.: `pnpm self-update 9.5.0` or `pnpm self-update next-10`.
--- tools/plugin-commands-self-updater/test/selfUpdate.test.ts
+++ tools/plugin-commands-self-updater/test/selfUpdate.test.ts
@@ -59,22 +59,24 @@ function prepareOptions (dir: string) {
   }
 }
 
-function createMetadata (latest: string, registry: string) {
+function createMetadata (latest: string, registry: string, otherVersions: string[] = []) {
+  const versions = [...otherVersions, latest]
   return {
     name: 'pnpm',
     'dist-tags': { latest },
-    versions: {
-      [latest]: {
+    versions: Object.fromEntries(versions.map((version) => [
+      version,
+      {
         name: 'pnpm',
-        version: latest,
+        version,
         dist: {
           shasum: '217063ce3fcbf44f3051666f38b810f1ddefee4a',
-          tarball: `${registry}pnpm/-/pnpm-${latest}.tgz`,
+          tarball: `${registry}pnpm/-/pnpm-${version}.tgz`,
           fileCount: 880,
           integrity: 'sha512-Z/WHmRapKT5c8FnCOFPVcb6vT3U8cH9AyyK+1fsVeMaq07bEEHzLO6CzW+AD62IaFkcayDbIe+tT+dVLtGEnJA==',
         },
       },
-    },
+    ])),
   }
 }
 
@@ -87,7 +89,32 @@ test('self-update', async () => {
     .get('/pnpm/-/pnpm-9.1.0.tgz')
     .replyWithFile(200, path.join(__dirname, 'pnpm-9.1.0.tgz'))
 
-  await selfUpdate.handler(opts)
+  await selfUpdate.handler(opts, [])
+
+  const pnpmPkgJson = JSON.parse(fs.readFileSync(path.join(opts.pnpmHomeDir, '.tools/pnpm/9.1.0/node_modules/pnpm/package.json'), 'utf8'))
+  expect(pnpmPkgJson.version).toBe('9.1.0')
+
+  const pnpmEnv = prependDirsToPath([opts.pnpmHomeDir])
+  const { status, stdout } = spawn.sync('pnpm', ['-v'], {
+    env: {
+      ...process.env,
+      [pnpmEnv.name]: pnpmEnv.value,
+    },
+  })
+  expect(status).toBe(0)
+  expect(stdout.toString().trim()).toBe('9.1.0')
+})
+
+test('self-update by exact version', async () => {
+  const opts = prepare()
+  nock(opts.registries.default)
+    .get('/pnpm')
+    .reply(200, createMetadata('9.2.0', opts.registries.default, ['9.1.0']))
+  nock(opts.registries.default)
+    .get('/pnpm/-/pnpm-9.1.0.tgz')
+    .replyWithFile(200, path.join(__dirname, 'pnpm-9.1.0.tgz'))
+
+  await selfUpdate.handler(opts, ['9.1.0'])
 
   const pnpmPkgJson = JSON.parse(fs.readFileSync(path.join(opts.pnpmHomeDir, '.tools/pnpm/9.1.0/node_modules/pnpm/package.json'), 'utf8'))
   expect(pnpmPkgJson.version).toBe('9.1.0')
@@ -109,7 +136,7 @@ test('self-update does nothing when pnpm is up to date', async () => {
     .get('/pnpm')
     .reply(200, createMetadata('9.0.0', opts.registries.default))
 
-  const output = await selfUpdate.handler(opts)
+  const output = await selfUpdate.handler(opts, [])
 
   expect(output).toBe('The currently active pnpm v9.0.0 is already "latest" and doesn\'t need an update')
 })
@@ -125,7 +152,7 @@ test('self-update links pnpm that is already present on the disk', async () => {
   fs.writeFileSync(path.join(latestPnpmDir, 'package.json'), JSON.stringify({ name: 'pnpm', bin: 'bin.js' }), 'utf8')
   fs.writeFileSync(path.join(latestPnpmDir, 'bin.js'), `#!/usr/bin/env node
 console.log('9.2.0')`, 'utf8')
-  const output = await selfUpdate.handler(opts)
+  const output = await selfUpdate.handler(opts, [])
 
   expect(output).toBe(`The latest version, v9.2.0, is already present on the system. It was activated by linking it from ${path.join(latestPnpmDir, '../..')}.`)
 
@@ -159,7 +186,7 @@ test('self-update updates the packageManager field in package.json', async () =>
     .get('/pnpm/-/pnpm-9.1.0.tgz')
     .replyWithFile(200, path.join(__dirname, 'pnpm-9.1.0.tgz'))
 
-  const output = await selfUpdate.handler(opts)
+  const output = await selfUpdate.handler(opts, [])
 
   expect(output).toBe('The current project has been updated to use pnpm v9.1.0')
 
