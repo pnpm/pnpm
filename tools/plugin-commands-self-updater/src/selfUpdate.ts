2a90fa5300b914e7fafc78d3b539e03e9c6efa58
Zoltan Kochan <z@kochan.io>
2024-11-02 02:39:31 +0100

    feat: self-update should accept a version specifier

--- tools/plugin-commands-self-updater/src/selfUpdate.ts
+++ tools/plugin-commands-self-updater/src/selfUpdate.ts
@@ -28,34 +28,41 @@ export const commandNames = ['self-update']
 
 export function help (): string {
   return renderHelp({
-    description: 'Updates pnpm to the latest version',
+    description: 'Updates pnpm to the latest version (or the one specified)',
     descriptionLists: [],
     url: docsUrl('self-update'),
-    usages: [],
+    usages: [
+      'pnpm self-update',
+      'pnpm self-update 9',
+      'pnpm self-update next-10',
+      'pnpm self-update 9.10.0',
+    ],
   })
 }
 
 export type SelfUpdateCommandOptions = InstallCommandOptions & Pick<Config, 'wantedPackageManager' | 'managePackageManagerVersions'>
 
 export async function handler (
-  opts: SelfUpdateCommandOptions
+  opts: SelfUpdateCommandOptions,
+  params: string[]
 ): Promise<undefined | string> {
   if (isExecutedByCorepack()) {
     throw new PnpmError('CANT_SELF_UPDATE_IN_COREPACK', 'You should update pnpm with corepack')
   }
   const { resolve } = createResolver({ ...opts, authConfig: opts.rawConfig })
   const pkgName = 'pnpm'
-  const resolution = await resolve({ alias: pkgName, pref: 'latest' }, {
+  const pref = params[0] ?? 'latest'
+  const resolution = await resolve({ alias: pkgName, pref }, {
     lockfileDir: opts.lockfileDir ?? opts.dir,
     preferredVersions: {},
     projectDir: opts.dir,
-    registry: pickRegistryForPackage(opts.registries, pkgName, 'latest'),
+    registry: pickRegistryForPackage(opts.registries, pkgName, pref),
   })
   if (!resolution?.manifest) {
-    throw new PnpmError('CANNOT_RESOLVE_PNPM', 'Cannot find latest version of pnpm')
+    throw new PnpmError('CANNOT_RESOLVE_PNPM', `Cannot find "${pref}" version of pnpm`)
   }
   if (resolution.manifest.version === packageManager.version) {
-    return `The currently active ${packageManager.name} v${packageManager.version} is already "latest" and doesn't need an update`
+    return `The currently active ${packageManager.name} v${packageManager.version} is already "${pref}" and doesn't need an update`
   }
 
   if (opts.wantedPackageManager?.name === packageManager.name && opts.managePackageManagerVersions) {
@@ -79,7 +86,7 @@ export async function handler (
         warn: globalWarn,
       }
     )
-    return `The latest version, v${resolution.manifest.version}, is already present on the system. It was activated by linking it from ${dir}.`
+    return `The ${pref} version, v${resolution.manifest.version}, is already present on the system. It was activated by linking it from ${dir}.`
   }
   fs.mkdirSync(dir, { recursive: true })
   fs.writeFileSync(path.join(dir, 'package.json'), '{}')
