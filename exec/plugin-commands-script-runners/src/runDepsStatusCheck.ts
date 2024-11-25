7997ed4ae2a43664ff657e387ad54aa2a95fa1e5
Zoltan Kochan <z@kochan.io>
2024-11-25 14:02:17 +0100

    fix: running install automatically before run (#8805)

--- exec/plugin-commands-script-runners/src/runDepsStatusCheck.ts
+++ exec/plugin-commands-script-runners/src/runDepsStatusCheck.ts
@@ -1,18 +1,19 @@
+import path from 'path'
+import { sync as execSync } from 'execa'
 import { type VerifyDepsBeforeRun } from '@pnpm/config'
 import { PnpmError } from '@pnpm/error'
 import { globalWarn } from '@pnpm/logger'
-import { install } from '@pnpm/plugin-commands-installation'
 import { checkDepsStatus, type CheckDepsStatusOptions } from '@pnpm/deps.status'
 import { prompt } from 'enquirer'
 
-export type RunDepsStatusCheckOptions = CheckDepsStatusOptions & { verifyDepsBeforeRun?: VerifyDepsBeforeRun }
+export type RunDepsStatusCheckOptions = CheckDepsStatusOptions & { dir: string, verifyDepsBeforeRun?: VerifyDepsBeforeRun }
 
 export async function runDepsStatusCheck (opts: RunDepsStatusCheckOptions): Promise<void> {
   const { upToDate, issue } = await checkDepsStatus(opts)
   if (upToDate) return
   switch (opts.verifyDepsBeforeRun) {
   case 'install':
-    await install.handler(opts as unknown as install.InstallCommandOptions)
+    install()
     break
   case 'prompt': {
     const confirmed = await prompt<{ runInstall: boolean }>({
@@ -24,7 +25,7 @@ Would you like to run "pnpm install" to update your "node_modules"?`,
       initial: true,
     })
     if (confirmed.runInstall) {
-      await install.handler(opts as unknown as install.InstallCommandOptions)
+      install()
     }
     break
   }
@@ -35,5 +36,19 @@ Would you like to run "pnpm install" to update your "node_modules"?`,
   case 'warn':
     globalWarn(`Your node_modules are out of sync with your lockfile. ${issue}`)
     break
+  }
+
+  function install () {
+    const execOpts = {
+      cwd: opts.dir,
+      stdio: 'inherit' as const,
+    }
+    if (path.basename(process.execPath) === 'pnpm') {
+      execSync(process.execPath, ['install'], execOpts)
+    } else if (path.basename(process.argv[1]) === 'pnpm.cjs') {
+      execSync(process.execPath, [process.argv[1], 'install'], execOpts)
+    } else {
+      execSync('pnpm', ['install'], execOpts)
+    }
   }
 }
