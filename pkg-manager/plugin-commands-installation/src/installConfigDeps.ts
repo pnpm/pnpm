b833d744ab47fa05573f052d2c6a1d98c86c9e29
Zoltan Kochan <z@kochan.io>
2024-12-28 02:49:40 +0100

    feat: update configuration dependencies

--- pkg-manager/plugin-commands-installation/src/installConfigDeps.ts
+++ pkg-manager/plugin-commands-installation/src/installConfigDeps.ts
@@ -8,6 +8,7 @@ import { pickRegistryForPackage } from '@pnpm/pick-registry-for-package'
 import { getStorePath } from '@pnpm/store-path'
 import { readModulesDir } from '@pnpm/read-modules-dir'
 import rimraf from '@zkochan/rimraf'
+import { safeReadPackageJsonFromDir } from '@pnpm/read-package-json'
 import { type InstallDepsOptions } from './installDeps'
 
 export async function installConfigDeps (configDeps: Record<string, string>, opts: InstallDepsOptions): Promise<void> {
@@ -30,16 +31,23 @@ export async function installConfigDeps (configDeps: Record<string, string>, opt
     }
   }))
   await Promise.all(Object.entries(configDeps).map(async ([pkgName, pkgSpec]) => {
+    const configDepPath = path.join(configModulesDir, pkgName)
     const sepIndex = pkgSpec.indexOf('+')
     const version = pkgSpec.substring(0, sepIndex)
     const integrity = pkgSpec.substring(sepIndex + 1)
+    if (existingConfigDeps.includes(pkgName)) {
+      const configDepPkgJson = await safeReadPackageJsonFromDir(configDepPath)
+      if (configDepPkgJson == null || configDepPkgJson.name !== pkgName || configDepPkgJson.version !== version) {
+        await rimraf(configDepPath)
+      }
+    }
     const registry = pickRegistryForPackage(opts.registries, pkgName)
     const filesIndexFile = cafs.getIndexFilePathInCafs(integrity, 'index')
     const fetchResult = await remoteTarball(cafs, {
       tarball: getNpmTarballUrl(pkgName, version, { registry }),
       integrity,
     }, { filesIndexFile, lockfileDir: opts.lockfileDir ?? opts.dir, pkg: { name: pkgName, version: version } })
-    cafs.importPackage(path.join(configModulesDir, pkgName), {
+    cafs.importPackage(configDepPath, {
       force: true,
       requiresBuild: false,
       filesResponse: {
