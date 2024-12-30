934147f2662b678ec2974d11048663a4372a5d78
btea <2356281422@qq.com>
2024-12-30 08:45:02 +0800

    fix(resolving): `pkg.pr.new` not supported HEAD request

--- resolving/tarball-resolver/src/index.ts
+++ resolving/tarball-resolver/src/index.ts
@@ -12,7 +12,7 @@ export async function resolveFromTarball (
   if (isRepository(wantedDependency.pref)) return null
 
   // If there are redirects, we want to get the final URL address
-  const { url: resolvedUrl } = await fetchFromRegistry(wantedDependency.pref, { method: 'HEAD' })
+  const { url: resolvedUrl } = await fetchFromRegistry(wantedDependency.pref, { method: 'GET' })
 
   return {
     id: resolvedUrl as PkgResolutionId,
