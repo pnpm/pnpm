4188408e466a6873e286a1b0799136461d24ea5c
khai96_ <hvksmr1996@gmail.com>
2025-01-07 16:27:51 +0700

    feat: refuse aliases

--- pkg-manager/resolve-dependencies/src/validatePeerDependencies.ts
+++ pkg-manager/resolve-dependencies/src/validatePeerDependencies.ts
@@ -4,27 +4,13 @@ import { validRange } from 'semver'
 
 export function validatePeerDependencies (peerDependencies: Dependencies | undefined): void {
   for (const name in peerDependencies) {
-    const spec = peerDependencies[name]
-    if (!isValidPeerSpec(spec)) {
-      throw new PnpmError('INVALID_PEER_DEPENDENCY_SPECIFICATION', `The peer dependency named ${name} has unacceptable specification: ${spec}`)
+    const version = peerDependencies[name]
+    if (!isValidPeerVersion(version)) {
+      throw new PnpmError('INVALID_PEER_DEPENDENCY_SPECIFICATION', `The peer dependency named ${name} has unacceptable specification: ${version}`)
     }
   }
 }
 
-function isValidPeerSpec (spec: string): boolean {
-  if (isValidPeerVersion(spec)) return true
-
-  const parseResult = parseAliasedSpec(spec)
-  return parseResult ? isValidPeerVersion(parseResult.version) : false
-}
-
 function isValidPeerVersion (version: string): boolean {
-  return typeof validRange(version) === 'string' || version.startsWith('npm:') || version.startsWith('workspace:') || version.startsWith('catalog:')
-}
-
-function parseAliasedSpec (spec: string): { version: string } | undefined {
-  const splitIndex = spec.indexOf('@', 1)
-  if (splitIndex === -1) return undefined
-  const version = spec.slice(splitIndex + '@'.length)
-  return { version }
+  return typeof validRange(version) === 'string' || version.startsWith('workspace:') || version.startsWith('catalog:')
 }
--- pkg-manager/resolve-dependencies/test/validatePeerDependencies.test.ts
+++ pkg-manager/resolve-dependencies/test/validatePeerDependencies.test.ts
@@ -3,13 +3,15 @@ import { validatePeerDependencies } from '../src/validatePeerDependencies'
 test('accepts valid specifications that make sense for peerDependencies', () => {
   validatePeerDependencies({
     'use-version-range': '>=1.2.3 || ^3.2.1',
-    'use-aliased-version-range': 'foo@>=1.2.3 || ^3.2.1',
-    'use-npm-alias': 'npm:pkg@>=1.2.3 || 3.2.1',
     'use-workspace': 'workspace:^',
-    'use-workspace-alias': 'pkg@workspace:^',
     'use-catalog': 'catalog:',
-    'use-catalog-alias': 'pkg@catalog:',
   })
+})
+
+test('forbids aliases', () => {
+  expect(validatePeerDependencies.bind(null, {
+    foo: 'bar@1.2.3',
+  })).toThrow('The peer dependency named foo has unacceptable specification: bar@1.2.3')
 })
 
 test('forbids `file:` scheme', () => {
@@ -18,20 +20,8 @@ test('forbids `file:` scheme', () => {
   })).toThrow('The peer dependency named foo has unacceptable specification: file:../foo')
 })
 
-test('forbids aliased `file:` scheme', () => {
-  expect(validatePeerDependencies.bind(null, {
-    foo: 'foo@file:../foo',
-  })).toThrow('The peer dependency named foo has unacceptable specification: foo@file:../foo')
-})
-
 test('forbids `link:` scheme', () => {
   expect(validatePeerDependencies.bind(null, {
     foo: 'link:../foo',
   })).toThrow('The peer dependency named foo has unacceptable specification: link:../foo')
-})
-
-test('forbids aliased `link:` scheme', () => {
-  expect(validatePeerDependencies.bind(null, {
-    foo: 'foo@link:../foo',
-  })).toThrow('The peer dependency named foo has unacceptable specification: foo@link:../foo')
 })
