2d94c9080a337cfe3a456c4c3c9f36a16d0e0f76
khai96_ <hvksmr1996@gmail.com>
2025-01-10 19:35:59 +0700

    test: fix `shouldRunCheck`

--- exec/plugin-commands-script-runners/test/shouldRunCheck.test.ts
+++ exec/plugin-commands-script-runners/test/shouldRunCheck.test.ts
@@ -1,20 +1,13 @@
 import { DISABLE_DEPS_CHECK_ENV, shouldRunCheck } from '../src/shouldRunCheck'
 
 test('should return true if skip env is not defined and script name is not special', () => {
-  expect(shouldRunCheck({}, 'start')).toBe(true)
+  expect(shouldRunCheck({})).toBe(true)
 })
 
 test('should return false if skip env is defined', () => {
-  expect(shouldRunCheck({ ...DISABLE_DEPS_CHECK_ENV }, 'start')).toBe(false)
+  expect(shouldRunCheck({ ...DISABLE_DEPS_CHECK_ENV })).toBe(false)
 })
 
-test.each([
-  'preinstall',
-  'install',
-  'postinstall',
-  'preuninstall',
-  'uninstall',
-  'postuninstall',
-])('should return false if script name is %p', scriptName => {
-  expect(shouldRunCheck({}, scriptName)).toBe(false)
+test('should return false if npm_lifecycle_even is defined', () => {
+  expect(shouldRunCheck({ npm_lifecycle_event: 'anything' })).toBe(false)
 })
