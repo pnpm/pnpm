fc925150a44b740ea28b7836463a10d678766593
Zoltan Kochan <z@kochan.io>
2025-02-02 15:47:42 +0100

    test: fix

--- exec/plugin-commands-script-runners/test/dlx.createCacheKey.test.ts
+++ exec/plugin-commands-script-runners/test/dlx.createCacheKey.test.ts
@@ -5,7 +5,7 @@ test('creates a hash', () => {
   const received = createCacheKey(['shx', '@foo/bar'], {
     default: 'https://registry.npmjs.com/',
     '@foo': 'https://example.com/npm-registry/foo/',
-  })
+  }, [])
   const expected = createHexHash(JSON.stringify([['@foo/bar', 'shx'], [
     ['@foo', 'https://example.com/npm-registry/foo/'],
     ['default', 'https://registry.npmjs.com/'],
@@ -15,16 +15,16 @@ test('creates a hash', () => {
 
 test('is agnostic to package order', () => {
   const registries = { default: 'https://registry.npmjs.com/' }
-  expect(createCacheKey(['a', 'c', 'b'], registries)).toBe(createCacheKey(['a', 'b', 'c'], registries))
-  expect(createCacheKey(['b', 'a', 'c'], registries)).toBe(createCacheKey(['a', 'b', 'c'], registries))
-  expect(createCacheKey(['b', 'c', 'a'], registries)).toBe(createCacheKey(['a', 'b', 'c'], registries))
-  expect(createCacheKey(['c', 'a', 'b'], registries)).toBe(createCacheKey(['a', 'b', 'c'], registries))
-  expect(createCacheKey(['c', 'b', 'a'], registries)).toBe(createCacheKey(['a', 'b', 'c'], registries))
+  expect(createCacheKey(['a', 'c', 'b'], registries, [])).toBe(createCacheKey(['a', 'b', 'c'], registries, []))
+  expect(createCacheKey(['b', 'a', 'c'], registries, [])).toBe(createCacheKey(['a', 'b', 'c'], registries, []))
+  expect(createCacheKey(['b', 'c', 'a'], registries, [])).toBe(createCacheKey(['a', 'b', 'c'], registries, []))
+  expect(createCacheKey(['c', 'a', 'b'], registries, [])).toBe(createCacheKey(['a', 'b', 'c'], registries, []))
+  expect(createCacheKey(['c', 'b', 'a'], registries, [])).toBe(createCacheKey(['a', 'b', 'c'], registries, []))
 })
 
 test('is agnostic to registry key order', () => {
   const packages = ['a', 'b', 'c']
   const foo = 'https://example.com/foo/'
   const bar = 'https://example.com/bar/'
-  expect(createCacheKey(packages, { '@foo': foo, '@bar': bar })).toBe(createCacheKey(packages, { '@bar': bar, '@foo': foo }))
+  expect(createCacheKey(packages, { '@foo': foo, '@bar': bar }, [])).toBe(createCacheKey(packages, { '@bar': bar, '@foo': foo }, []))
 })
