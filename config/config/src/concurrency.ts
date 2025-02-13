2a44b574a7d74f42381dd96354f1c318c1c423cc
btea <2356281422@qq.com>
2025-02-13 12:30:21 +0800

    chore: update

--- config/config/src/concurrency.ts
+++ config/config/src/concurrency.ts
@@ -6,7 +6,7 @@ export function getWorkspaceConcurrency (option: number | undefined): number {
   if (option <= 0) {
     // If option is <= 0, it uses the amount of cores minus the absolute of the number given
     // but always returning at least 1
-    return Math.max(1, os.availableParallelism?.() ?? os.cpus().length - Math.abs(option))
+    return Math.max(1, (os.availableParallelism?.() ?? os.cpus().length) - Math.abs(option))
   }
 
   return option
