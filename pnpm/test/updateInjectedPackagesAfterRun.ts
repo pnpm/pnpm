083aed37e555bdafe6a476886fef0a5dc076b51a
khai96_ <hvksmr1996@gmail.com>
2025-02-19 02:19:03 +0700

    test: directories and symlinks

--- pnpm/test/updateInjectedPackagesAfterRun.ts
+++ pnpm/test/updateInjectedPackagesAfterRun.ts
@@ -6,7 +6,8 @@ import { sync as writeYamlFile } from 'write-yaml-file'
 import { execPnpm } from './utils'
 
 const TEMPLATE_PACKAGE_NAMES = ['foo', 'bar', 'baz'] as const
-const TEMPLATE_FILE_NAMES = ['build1.cjs', 'build2.cjs', 'should-be-deleted-by-build1.txt', 'should-be-modified-by-build1.txt'] as const
+const TEMPLATE_SCRIPT_NAMES = ['build1.cjs', 'build2.cjs', 'build3.cjs'] as const
+const TEMPLATE_FILE_NAMES = [...TEMPLATE_SCRIPT_NAMES, 'should-be-deleted-by-build1.txt', 'should-be-modified-by-build1.txt'] as const
 
 const template = {
   manifests: (): Record<typeof TEMPLATE_PACKAGE_NAMES[number], ProjectManifest> => ({
@@ -19,6 +20,7 @@ const template = {
       scripts: {
         build1: 'node ./build1.cjs',
         build2: 'node ./build2.cjs',
+        build3: 'node ./build3.cjs',
       },
     },
     bar: {
@@ -30,6 +32,7 @@ const template = {
       scripts: {
         build1: 'node ./build1.cjs',
         build2: 'node ./build2.cjs',
+        build3: 'node ./build3.cjs',
       },
     },
     baz: {
@@ -41,6 +44,7 @@ const template = {
       scripts: {
         build1: 'node ./build1.cjs',
         build2: 'node ./build2.cjs',
+        build3: 'node ./build3.cjs',
       },
     },
   }),
@@ -54,6 +58,22 @@ const template = {
     'build2.cjs': `
       const fs = require('fs')
       fs.writeFileSync('created-by-build2.txt', __filename)
+    `,
+    'build3.cjs': `
+      const fs = require('fs')
+      console.log('Creating a tree of empty directories...')
+      fs.mkdirSync('empty-dirs/a/a/', { recursive: true })
+      fs.mkdirSync('empty-dirs/a/b/', { recursive: true })
+      fs.mkdirSync('empty-dirs/b/a/', { recursive: true })
+      fs.mkdirSync('empty-dirs/b/b/', { recursive: true })
+      console.log('Creating a tree of real files...')
+      fs.mkdirSync('files/foo/foo/', { recursive: true })
+      fs.writeFileSync('files/foo/foo/foo.txt', '')
+      fs.writeFileSync('files/foo/bar.txt', '')
+      fs.writeFileSync('files/foo_bar.txt', 'This is foo_bar')
+      console.log('Creating symlinks...')
+      fs.symlinkSync('files/foo_bar.txt', 'link-to-a-file', 'file')
+      fs.symlinkSync('files/foo', 'link-to-a-dir', 'dir')
     `,
     'should-be-deleted-by-build1.txt': '',
     'should-be-modified-by-build1.txt': 'Before modification',
@@ -285,5 +305,82 @@ test('filter scripts', async () => {
 
     // injected bar
     expect(fs.readdirSync('node_modules/.pnpm/bar@file+packages+bar/node_modules/bar')).not.toContain('created-by-build2.txt')
+  }
+})
+
+test('directories and symlinks', async () => {
+  const manifests = template.manifests()
+  preparePackages(
+    TEMPLATE_PACKAGE_NAMES.map(pkgName => ({
+      location: `packages/${pkgName}`,
+      package: manifests[pkgName],
+    }))
+  )
+
+  const files = template.files()
+  for (const pkgName of TEMPLATE_PACKAGE_NAMES) {
+    for (const fileName of TEMPLATE_FILE_NAMES) {
+      fs.writeFileSync(path.join('packages', pkgName, fileName), files[fileName])
+    }
+  }
+
+  writeYamlFile('pnpm-workspace.yaml', {
+    packages: ['packages/*'],
+  })
+
+  const npmrc = template.npmrc('update-injected-packages-after-run=true')
+  fs.writeFileSync('.npmrc', npmrc)
+
+  await execPnpm(['install'])
+  expect(fs.readdirSync('node_modules/.pnpm')).toContain('foo@file+packages+foo')
+  expect(fs.readdirSync('node_modules/.pnpm')).toContain('bar@file+packages+bar')
+  expect(fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo').sort()).toStrictEqual([
+    ...TEMPLATE_FILE_NAMES,
+    'package.json',
+  ].sort())
+  expect(fs.readdirSync('node_modules/.pnpm/bar@file+packages+bar/node_modules/bar').sort()).toStrictEqual([
+    ...TEMPLATE_FILE_NAMES,
+    'package.json',
+  ].sort())
+
+  // build3 should update the injected files
+  {
+    await execPnpm(['--filter=foo', 'run', 'build3'])
+
+    // should create empty-dirs at source
+    expect(fs.readdirSync('packages/foo/empty-dirs/a/a')).toStrictEqual([])
+    expect(fs.readdirSync('packages/foo/empty-dirs/a/b')).toStrictEqual([])
+    expect(fs.readdirSync('packages/foo/empty-dirs/b/a')).toStrictEqual([])
+    expect(fs.readdirSync('packages/foo/empty-dirs/b/b')).toStrictEqual([])
+
+    // should not create empty-dirs at the injected location
+    expect(fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo')).not.toContain('empty-dirs')
+
+    // should recreate a directories tree at the injected location
+    expect(fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo')).toContain('files')
+    expect(
+      fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo/files')
+        .sort()
+    ).toStrictEqual(['foo', 'foo_bar.txt'])
+    expect(
+      fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo/files/foo')
+        .sort()
+    ).toStrictEqual(['bar.txt', 'foo'])
+    expect(
+      fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo/files/foo/foo')
+        .sort()
+    ).toStrictEqual(['foo.txt'])
+
+    // should recreate the structure of the symlinks at the injected location
+    // NOTE: The current implementation of @pnpm/directory-fetcher would treat symlinks to dir at real dir
+    //       because it uses fs.stat instead of fs.lstat, so testing with fs.realpathSync wouldn't work.
+    expect(fs.readFileSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo/link-to-a-file', 'utf-8')).toBe('This is foo_bar')
+    expect(
+      fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo/link-to-a-dir')
+        .sort()
+    ).toStrictEqual(
+      fs.readdirSync('node_modules/.pnpm/foo@file+packages+foo/node_modules/foo/files/foo')
+        .sort()
+    )
   }
 })
