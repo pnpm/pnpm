8de26d67e0a17488a9cceeed99345883832d5882
R.P. Pedraza <rp.pedraza@gmail.com>
2025-02-26 09:50:35 +0800

    fix: updateWorkspaceManifest not writing to manifest file
    close #9168

--- workspace/manifest-writer/src/index.ts
+++ workspace/manifest-writer/src/index.ts
@@ -1,9 +1,11 @@
 import { readWorkspaceManifest, type WorkspaceManifest } from '@pnpm/workspace.read-manifest'
+import { WORKSPACE_MANIFEST_FILENAME } from '@pnpm/constants'
 import writeYamlFile from 'write-yaml-file'
+import path from 'node:path'
 
 export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>): Promise<void> {
   const manifest = await readWorkspaceManifest(dir)
-  await writeYamlFile(dir, {
+  await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), {
     ...manifest,
     updatedFields,
   })
