9365047dabb344fe1c5f858d184e3f51ba6ecfb2
Zoltan Kochan <z@kochan.io>
2025-03-04 02:48:33 +0100

    test: refactor

--- pnpm/test/config.ts
+++ pnpm/test/config.ts
@@ -1,5 +1,4 @@
 import fs from 'fs'
-import path from 'path'
 import { WANTED_LOCKFILE } from '@pnpm/constants'
 import { prepare } from '@pnpm/prepare'
 import { execPnpmSync } from './utils'
@@ -7,11 +6,6 @@ import { execPnpmSync } from './utils'
 test('read settings from pnpm-workspace.yaml', async () => {
   prepare()
   fs.writeFileSync('pnpm-workspace.yaml', 'useLockfile: false', 'utf8')
-  const { status } = execPnpmSync(['install'], {
-    env: {
-      PNPM_HOME: path.resolve('pnpm_home'),
-    },
-  })
-  expect(status).toBe(0)
+  expect(execPnpmSync(['install']).status).toBe(0)
   expect(fs.existsSync(WANTED_LOCKFILE)).toBeFalsy()
 })
