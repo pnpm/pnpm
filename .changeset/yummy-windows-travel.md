8faf28d4283b74423ecff6fa859603386399c0b2
btea <2356281422@qq.com>
2025-03-11 12:40:22 +0800

    fix: compatible with different `outputEncoding` output results are consistent

---
+++ .changeset/yummy-windows-travel.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/crypto.polyfill": patch
+---
+
+When the `outputEncoding` parameter is not passed, the results of the two methods are kept consistent.
--- crypto/polyfill/src/index.ts
+++ crypto/polyfill/src/index.ts
@@ -1,8 +1,31 @@
 import crypto from 'crypto'
 
-export type Hash = (algorithm: string, data: crypto.BinaryLike, outputEncoding: crypto.BinaryToTextEncoding) => string
+// Overload signatures:
+export function hash (
+  algorithm: string,
+  data: crypto.BinaryLike
+): Buffer
+export function hash (
+  algorithm: string,
+  data: crypto.BinaryLike,
+  outputEncoding: crypto.BinaryToTextEncoding
+): string
 
-export const hash: Hash =
+// Implementation signature:
+export function hash (
+  algorithm: string,
+  data: crypto.BinaryLike,
+  outputEncoding?: crypto.BinaryToTextEncoding
+): string | Buffer {
   // @ts-expect-error -- crypto.hash is supported in Node 21.7.0+, 20.12.0+
-  crypto.hash ??
-  ((algorithm, data, outputEncoding) => crypto.createHash(algorithm).update(data).digest(outputEncoding))
+  if (crypto.hash) {
+    // @ts-expect-error -- crypto.hash is supported in Node 21.7.0+, 20.12.0+
+    // https://nodejs.org/docs/latest/api/crypto.html#cryptohashalgorithm-data-outputencoding
+    // crypto.hash outputEncoding is optional and defaults to 'hex', we should treat it as digest without receiving parameter
+    return outputEncoding ? crypto.hash(algorithm, data, outputEncoding) : crypto.hash(algorithm, data, 'buffer')
+  }
+  // Fallback to createHash:
+  const h = crypto.createHash(algorithm)
+  h.update(data)
+  return outputEncoding ? h.digest(outputEncoding) : h.digest()
+}
