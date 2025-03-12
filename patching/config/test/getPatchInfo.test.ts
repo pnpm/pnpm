97402b84c141f45cda391daa9fbb58257da1bd83
khai96_ <hvksmr1996@gmail.com>
2025-03-12 10:41:18 +0700

    test: exact version overrides range conflict

--- patching/config/test/getPatchInfo.test.ts
+++ patching/config/test/getPatchInfo.test.ts
@@ -174,3 +174,46 @@ test('getPatchInfo(_, name, version) throws an error when name@version matches m
     hint: 'Explicitly set the exact version (foo@2.1.0) to resolve conflict',
   }))
 })
+
+test('getPatchInfo(_, name, version) does not throw an error when name@version matches an exact version patch and more than one version range patches', () => {
+  const patchedDependencies = {
+    foo: {
+      exact: {
+        '2.1.0': {
+          file: {
+            path: 'patches/foo_a.patch',
+            hash: '00000000000000000000000000000000',
+          },
+          key: 'foo@>=1.0.0 <3.0.0',
+          strict: true,
+        },
+      },
+      range: [
+        {
+          version: '>=1.0.0 <3.0.0',
+          patch: {
+            file: {
+              path: 'patches/foo_b.patch',
+              hash: '00000000000000000000000000000000',
+            },
+            key: 'foo@>=1.0.0 <3.0.0',
+            strict: true,
+          },
+        },
+        {
+          version: '>=2.0.0',
+          patch: {
+            file: {
+              path: 'patches/foo_c.patch',
+              hash: '00000000000000000000000000000000',
+            },
+            key: 'foo@>=2.0.0',
+            strict: true,
+          },
+        },
+      ],
+      all: undefined,
+    },
+  } satisfies PatchGroupRecord
+  expect(getPatchInfo(patchedDependencies, 'foo', '2.1.0')).toStrictEqual(patchedDependencies.foo.exact['2.1.0'])
+})
