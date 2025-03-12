c49563caefc9b890a86d4ed3c6acac8f71604331
khai96_ <hvksmr1996@gmail.com>
2025-03-12 10:10:45 +0700

    refactor: stop re-exporting `allPatchKeys`

--- patching/config/src/index.ts
+++ patching/config/src/index.ts
@@ -5,7 +5,6 @@ export {
   type PatchGroup,
   type PatchGroupRecord,
 } from '@pnpm/patching.types'
-export { allPatchKeys } from './allPatchKeys'
 export { groupPatchedDependencies } from './groupPatchedDependencies'
 export { getPatchInfo } from './getPatchInfo'
 export { type VerifyPatchesOptions, verifyPatches } from './verifyPatches'
