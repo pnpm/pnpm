b6e5da2e1d3dfb38ee4c10241f9c0b7a5e717490
khai96_ <hvksmr1996@gmail.com>
2025-03-12 10:24:13 +0700

    refactor: move error creation into a class

--- patching/config/src/getPatchInfo.ts
+++ patching/config/src/getPatchInfo.ts
@@ -1,6 +1,21 @@
 import { PnpmError } from '@pnpm/error'
-import { type ExtendedPatchInfo, type PatchGroupRecord } from '@pnpm/patching.types'
+import { type ExtendedPatchInfo, type PatchGroupRangeItem, type PatchGroupRecord } from '@pnpm/patching.types'
 import { satisfies } from 'semver'
+
+class PatchKeyConflictError extends PnpmError {
+  constructor (
+    pkgName: string,
+    pkgVersion: string,
+    satisfied: Array<Pick<PatchGroupRangeItem, 'version'>>
+  ) {
+    const pkgId = `${pkgName}@${pkgVersion}`
+    const satisfiedVersions = satisfied.map(({ version }) => version)
+    const message = `Unable to choose between ${satisfied.length} version ranges to patch ${pkgId}: ${satisfiedVersions.join(', ')}`
+    super('PATCH_KEY_CONFLICT', message, {
+      hint: `Explicitly set the exact version (${pkgId}) to resolve conflict`,
+    })
+  }
+}
 
 export function getPatchInfo (
   patchFileGroups: PatchGroupRecord | undefined,
@@ -14,11 +29,7 @@ export function getPatchInfo (
 
   const satisfied = patchFileGroups[pkgName].range.filter(item => satisfies(pkgVersion, item.version))
   if (satisfied.length > 1) {
-    const pkgId = `${pkgName}@${pkgVersion}`
-    const message = `Unable to choose between ${satisfied.length} version ranges to patch ${pkgId}: ${satisfied.map(x => x.version).join(', ')}`
-    throw new PnpmError('PATCH_KEY_CONFLICT', message, {
-      hint: `Explicitly set the exact version (${pkgId}) to resolve conflict`,
-    })
+    throw new PatchKeyConflictError(pkgName, pkgVersion, satisfied)
   }
   if (satisfied.length === 1) {
     return satisfied[0].patch
