72b473f33b1bf0a730eb64a1b91f1d0400bbccab
btea <2356281422@qq.com>
2025-03-22 21:56:07 +0800

    feat: config delete local should delete setting in pnpm-workspace.yaml

--- workspace/manifest-writer/src/index.ts
+++ workspace/manifest-writer/src/index.ts
@@ -1,10 +1,25 @@
 import path from 'path'
+import fs from 'fs'
 import { readWorkspaceManifest, type WorkspaceManifest } from '@pnpm/workspace.read-manifest'
 import { WORKSPACE_MANIFEST_FILENAME } from '@pnpm/constants'
 import writeYamlFile from 'write-yaml-file'
 
-export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>): Promise<void> {
+export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>, isDelete?: boolean): Promise<void> {
   const manifest = await readWorkspaceManifest(dir)
+  if (manifest && isDelete) {
+    for (const field of Object.keys(updatedFields)) {
+      delete manifest[field as keyof WorkspaceManifest]
+    }
+    if (Object.keys(manifest).length === 0) {
+      try {
+        fs.rmSync(path.join(dir, WORKSPACE_MANIFEST_FILENAME))
+      } catch (err) {
+      }
+      return
+    }
+    await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), manifest)
+    return
+  }
   await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), {
     ...manifest,
     ...updatedFields,
