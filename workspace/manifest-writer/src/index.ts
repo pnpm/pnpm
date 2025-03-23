3a90ec1f8f47576c96d8a7b2ec99dabe6685d48a
btea <2356281422@qq.com>
2025-03-23 09:28:43 +0800

    feat: config delete local should delete setting in pnpm-workspace.yaml (#9317)
    * feat: config delete local should delete setting in pnpm-workspace.yaml
    
    * refactor: removing settings from pnpm-workspace.yaml
    
    ---------
    
    Co-authored-by: Zoltan Kochan <z@kochan.io>

--- workspace/manifest-writer/src/index.ts
+++ workspace/manifest-writer/src/index.ts
@@ -1,12 +1,30 @@
+import fs from 'fs'
 import path from 'path'
 import { readWorkspaceManifest, type WorkspaceManifest } from '@pnpm/workspace.read-manifest'
 import { WORKSPACE_MANIFEST_FILENAME } from '@pnpm/constants'
 import writeYamlFile from 'write-yaml-file'
+import equals from 'ramda/src/equals'
 
 export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>): Promise<void> {
-  const manifest = await readWorkspaceManifest(dir)
-  await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), {
-    ...manifest,
-    ...updatedFields,
-  })
+  const manifest = await readWorkspaceManifest(dir) ?? {} as WorkspaceManifest
+  let shouldBeUpdated = false
+  for (const [key, value] of Object.entries(updatedFields)) {
+    if (!equals(manifest[key as keyof WorkspaceManifest], value)) {
+      shouldBeUpdated = true
+      if (value == null) {
+        delete manifest[key as keyof WorkspaceManifest]
+      } else {
+        // @ts-expect-error
+        manifest[key as keyof WorkspaceManifest] = value
+      }
+    }
+  }
+  if (!shouldBeUpdated) {
+    return
+  }
+  if (Object.keys(manifest).length === 0) {
+    await fs.promises.rm(path.join(dir, WORKSPACE_MANIFEST_FILENAME))
+    return
+  }
+  await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), manifest)
 }
