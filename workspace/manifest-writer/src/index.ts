d1cd8a5783a73de02bdaf0a827186513862bee13
Zoltan Kochan <z@kochan.io>
2025-03-23 01:48:40 +0100

    refactor: removing settings from pnpm-workspace.yaml

--- workspace/manifest-writer/src/index.ts
+++ workspace/manifest-writer/src/index.ts
@@ -1,27 +1,30 @@
-import path from 'path'
 import fs from 'fs'
+import path from 'path'
 import { readWorkspaceManifest, type WorkspaceManifest } from '@pnpm/workspace.read-manifest'
 import { WORKSPACE_MANIFEST_FILENAME } from '@pnpm/constants'
 import writeYamlFile from 'write-yaml-file'
+import equals from 'ramda/src/equals'
 
-export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>, isDelete?: boolean): Promise<void> {
-  const manifest = await readWorkspaceManifest(dir)
-  if (manifest && isDelete) {
-    for (const field of Object.keys(updatedFields)) {
-      delete manifest[field as keyof WorkspaceManifest]
-    }
-    if (Object.keys(manifest).length === 0) {
-      try {
-        fs.rmSync(path.join(dir, WORKSPACE_MANIFEST_FILENAME))
-      } catch (err) {
+export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>): Promise<void> {
+  const manifest = await readWorkspaceManifest(dir) ?? {} as WorkspaceManifest
+  let shouldBeUpdated = false
+  for (const [key, value] of Object.entries(updatedFields)) {
+    if (!equals(manifest[key as keyof WorkspaceManifest], value)) {
+      shouldBeUpdated = true
+      if (value == null) {
+        delete manifest[key as keyof WorkspaceManifest]
+      } else {
+        // @ts-expect-error
+        manifest[key as keyof WorkspaceManifest] = value
       }
-      return
     }
-    await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), manifest)
+  }
+  if (!shouldBeUpdated) {
     return
   }
-  await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), {
-    ...manifest,
-    ...updatedFields,
-  })
+  if (Object.keys(manifest).length === 0) {
+    await fs.promises.rm(path.join(dir, WORKSPACE_MANIFEST_FILENAME))
+    return
+  }
+  await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), manifest)
 }
