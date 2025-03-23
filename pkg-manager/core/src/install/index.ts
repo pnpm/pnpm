dd5b60a28e95c78764f6ccca6d5efe0982a66f5f
btea <2356281422@qq.com>
2025-03-23 20:43:09 +0800

    fix: throw error

--- pkg-manager/core/src/install/index.ts
+++ pkg-manager/core/src/install/index.ts
@@ -1192,6 +1192,9 @@ const _installInContext: InstallFunction = async (projects, ctx, opts) => {
           )
       }
       if (!opts.ignoreScripts || Object.keys(opts.patchedDependencies ?? {}).length > 0) {
+        if (ctx.modulesFile?.ignoredBuilds?.length) {
+          throw new PnpmError('IGNORED_BUILD_DEPENDENCIES', 'There are some ignored build dependencies in the lockfile. Run pnpm approve-builds to build them before running the install command.')
+        }
         // postinstall hooks
         const depPaths = Object.keys(dependenciesGraph) as DepPath[]
         const rootNodes = depPaths.filter((depPath) => dependenciesGraph[depPath].depth === 0)
@@ -1227,12 +1230,8 @@ const _installInContext: InstallFunction = async (projects, ctx, opts) => {
           unsafePerm: opts.unsafePerm,
           userAgent: opts.userAgent,
         })).ignoredBuilds
-        if (ctx.modulesFile?.ignoredBuilds?.length) {
-          if (ignoredBuilds) {
-            ignoredBuilds = [...ignoredBuilds, ...ctx.modulesFile.ignoredBuilds]
-          } else {
-            ignoredBuilds = ctx.modulesFile.ignoredBuilds
-          }
+        if (ignoredBuilds && ctx.modulesFile?.ignoredBuilds?.length) {
+          ignoredBuilds = ctx.modulesFile.ignoredBuilds
           ignoredScriptsLogger.debug({ packageNames: ignoredBuilds })
         }
       }
