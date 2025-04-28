10e07d0981e121052ca8076b74d9f79f1f8d4b3a
khai96_ <hvksmr1996@gmail.com>
2025-04-28 20:03:00 +0700

    feat(workspace/manifest-writer): addDefaultCatalog

--- workspace/manifest-writer/src/index.ts
+++ workspace/manifest-writer/src/index.ts
@@ -5,6 +5,16 @@ import { WORKSPACE_MANIFEST_FILENAME } from '@pnpm/constants'
 import writeYamlFile from 'write-yaml-file'
 import equals from 'ramda/src/equals'
 
+async function writeManifestFile (dir: string, manifest: WorkspaceManifest): Promise<void> {
+  return writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), manifest, {
+    lineWidth: -1, // This is setting line width to never wrap
+    blankLines: true,
+    noCompatMode: true,
+    noRefs: true,
+    sortKeys: false,
+  })
+}
+
 export async function updateWorkspaceManifest (dir: string, updatedFields: Partial<WorkspaceManifest>): Promise<void> {
   const manifest = await readWorkspaceManifest(dir) ?? {} as WorkspaceManifest
   let shouldBeUpdated = false
@@ -26,11 +36,21 @@ export async function updateWorkspaceManifest (dir: string, updatedFields: Parti
     await fs.promises.rm(path.join(dir, WORKSPACE_MANIFEST_FILENAME))
     return
   }
-  await writeYamlFile(path.join(dir, WORKSPACE_MANIFEST_FILENAME), manifest, {
-    lineWidth: -1, // This is setting line width to never wrap
-    blankLines: true,
-    noCompatMode: true,
-    noRefs: true,
-    sortKeys: false,
-  })
+  await writeManifestFile(dir, manifest)
+}
+
+export async function addDefaultCatalog (workspaceDir: string, name: string, spec: string): Promise<void> {
+  const manifest: WorkspaceManifest = await readWorkspaceManifest(workspaceDir) ?? {
+    packages: [],
+  }
+
+  let catalog: Record<string, string> | undefined = manifest.catalog ?? manifest.catalogs?.default
+  if (catalog == null) {
+    catalog = {}
+    manifest.catalog = catalog
+  }
+
+  catalog[name] = spec
+
+  await writeManifestFile(workspaceDir, manifest)
 }
