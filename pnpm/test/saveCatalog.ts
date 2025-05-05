051eaf8ffd4b44c604ef79ffdb9b8cd9d4a1a3ba
khai96_ <hvksmr1996@gmail.com>
2025-05-06 04:00:05 +0700

    test: single workspace package

--- pnpm/test/saveCatalog.ts
+++ pnpm/test/saveCatalog.ts
@@ -1,5 +1,5 @@
 import { type LockfileFile } from '@pnpm/lockfile.types'
-import { preparePackages } from '@pnpm/prepare'
+import { prepare, preparePackages } from '@pnpm/prepare'
 import { addDistTag } from '@pnpm/registry-mock'
 import { type ProjectManifest } from '@pnpm/types'
 import { sync as loadJsonFile } from 'load-json-file'
@@ -8,6 +8,87 @@ import { sync as writeYamlFile } from 'write-yaml-file'
 import { execPnpm } from './utils'
 
 const SAVE_CATALOG = ['--save-catalog'] as const
+
+test('--save-catalog adds catalogs to the manifest of a single package workspace', async () => {
+  const manifest: ProjectManifest = {
+    name: 'test-save-catalog',
+    version: '0.0.0',
+    private: true,
+    dependencies: {
+      '@pnpm.e2e/bar': 'catalog:',
+    },
+  }
+
+  prepare(manifest)
+
+  writeYamlFile('pnpm-workspace.yaml', {
+    catalog: {
+      '@pnpm.e2e/bar': '^100.1.0',
+    },
+  })
+
+  await addDistTag({ package: '@pnpm.e2e/foo', version: '100.1.0', distTag: 'latest' })
+  await addDistTag({ package: '@pnpm.e2e/bar', version: '100.1.0', distTag: 'latest' })
+
+  await execPnpm(['install'])
+  expect(readYamlFile('pnpm-lock.yaml')).toMatchObject({
+    catalogs: {
+      default: {
+        '@pnpm.e2e/bar': {
+          specifier: '^100.1.0',
+          version: '100.1.0',
+        },
+      },
+    },
+    importers: {
+      '.': {
+        dependencies: {
+          '@pnpm.e2e/bar': {
+            specifier: 'catalog:',
+            version: '100.1.0',
+          },
+        },
+      },
+    },
+    packages: {
+      '@pnpm.e2e/bar@100.1.0': expect.anything(),
+    },
+  } as Partial<LockfileFile>)
+
+  await execPnpm(['add', ...SAVE_CATALOG, '@pnpm.e2e/foo'])
+  expect(readYamlFile('pnpm-lock.yaml')).toMatchObject({
+    catalogs: {
+      default: {
+        '@pnpm.e2e/bar': {
+          specifier: '^100.1.0',
+          version: '100.1.0',
+        },
+        // '@pnpm.e2e/foo': {
+        //   specifier: '^100.1.0',
+        //   version: '100.1.0',
+        // },
+      },
+    },
+    importers: {
+      '.': {
+        dependencies: {
+          '@pnpm.e2e/bar': {
+            specifier: 'catalog:',
+            version: '100.1.0',
+          },
+          '@pnpm.e2e/foo': {
+            specifier: 'catalog:',
+            version: '100.1.0',
+          },
+        },
+      },
+    },
+    packages: {
+      '@pnpm.e2e/bar@100.1.0': expect.anything(),
+      '@pnpm.e2e/foo@100.1.0': expect.anything(),
+    },
+  } as Partial<LockfileFile>)
+})
 
 test('--save-catalog adds catalogs to the manifest of a shared lockfile workspace', async () => {
   const manifests: ProjectManifest[] = [
