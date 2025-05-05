285c0106e6a0269230ec99c3442f95a040083ae4
khai96_ <hvksmr1996@gmail.com>
2025-05-05 18:16:53 +0700

    test: create workspace manifest

---
+++ pkg-manager/plugin-commands-installation/test/saveCatalog.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import { add } from '@pnpm/plugin-commands-installation'
+import { prepare } from '@pnpm/prepare'
+import { sync as loadJsonFile } from 'load-json-file'
+import { sync as readYamlFile } from 'read-yaml-file'
+import { DEFAULT_OPTS } from './utils'
+
+// This must be a function because some of its values depend on CWD
+const createOptions = () => ({
+  ...DEFAULT_OPTS,
+  rawConfig: {
+    ...DEFAULT_OPTS.rawConfig,
+    'save-catalog': true,
+  },
+  registries: {
+    ...DEFAULT_OPTS.registries,
+  },
+  saveCatalog: true,
+  dir: process.cwd(),
+  cacheDir: path.resolve('cache'),
+  storeDir: path.resolve('store'),
+  workspaceDir: process.cwd(),
+  recursive: true,
+} satisfies Partial<add.AddCommandOptions>)
+
+test('saveCatalog creates new workspace manifest if there was not one', async () => {
+  const project = prepare({
+    name: 'test-save-catalog',
+    version: '0.0.0',
+    private: true,
+  })
+
+  await add.handler(createOptions(), ['@pnpm.e2e/foo'])
+
+  expect(loadJsonFile('package.json')).toMatchObject({
+    dependencies: {
+      '@pnpm.e2e/foo': 'catalog:',
+    },
+  })
+
+  expect(readYamlFile('pnpm-workspace.yaml')).toStrictEqual({
+    catalog: {
+      '@pnpm.e2e/foo': '^2.0.0',
+    },
+  })
+})
