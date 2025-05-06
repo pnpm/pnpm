4f02e3edc7aae8adb0d0085729da252c06310bad
khai96_ <hvksmr1996@gmail.com>
2025-05-06 23:28:55 +0700

    test: multi-lockfile

--- pnpm/test/saveCatalog.ts
+++ pnpm/test/saveCatalog.ts
@@ -197,6 +197,125 @@ test('--save-catalog adds catalogs to the manifest of a shared lockfile workspac
   })
 })
 
+test('--save-catalog adds catalogs to the manifest of a multi-lockfile workspace', async () => {
+  const manifests: ProjectManifest[] = [
+    {
+      name: 'project-0',
+      version: '0.0.0',
+      dependencies: {
+        '@pnpm.e2e/bar': 'catalog:',
+      },
+    },
+    {
+      name: 'project-1',
+      version: '0.0.0',
+    },
+  ]
+
+  preparePackages(manifests)
+
+  writeYamlFile('pnpm-workspace.yaml', {
+    sharedWorkspaceLockfile: false,
+    catalog: {
+      '@pnpm.e2e/bar': '^100.1.0',
+    },
+    packages: ['project-0', 'project-1'],
+  })
+
+  await addDistTag({ package: '@pnpm.e2e/foo', version: '100.1.0', distTag: 'latest' })
+  await addDistTag({ package: '@pnpm.e2e/bar', version: '100.1.0', distTag: 'latest' })
+
+  {
+    await execPnpm(['install'])
+
+    const lockfile0: LockfileFile = readYamlFile('project-0/pnpm-lock.yaml')
+    expect(lockfile0.catalogs).toStrictEqual({
+      default: {
+        '@pnpm.e2e/bar': {
+          specifier: '^100.1.0',
+          version: '100.1.0',
+        },
+      },
+    } as LockfileFile['catalogs'])
+    expect(lockfile0.importers).toStrictEqual({
+      '.': {
+        dependencies: {
+          '@pnpm.e2e/bar': {
+            specifier: 'catalog:',
+            version: '100.1.0',
+          },
+        },
+      },
+    } as LockfileFile['importers'])
+
+    const lockfile1: LockfileFile = readYamlFile('project-1/pnpm-lock.yaml')
+    expect(lockfile1.catalogs).toBeUndefined()
+    expect(lockfile1.importers).toStrictEqual({
+      '.': {},
+    } as LockfileFile['importers'])
+  }
+
+  {
+    await execPnpm(['--filter=project-1', 'add', ...SAVE_CATALOG, '@pnpm.e2e/foo'])
+
+    const lockfile0: LockfileFile = readYamlFile('project-0/pnpm-lock.yaml')
+    expect(lockfile0.catalogs).toStrictEqual({
+      default: {
+        '@pnpm.e2e/bar': {
+          specifier: '^100.1.0',
+          version: '100.1.0',
+        },
+      },
+    } as LockfileFile['catalogs'])
+    expect(lockfile0.importers).toStrictEqual({
+      '.': {
+        dependencies: {
+          '@pnpm.e2e/bar': {
+            specifier: 'catalog:',
+            version: '100.1.0',
+          },
+        },
+      },
+    } as LockfileFile['importers'])
+
+    const lockfile1: LockfileFile = readYamlFile('project-1/pnpm-lock.yaml')
+    expect(lockfile1.catalogs).toStrictEqual({
+      default: {
+        '@pnpm.e2e/foo': {
+          specifier: '^100.1.0',
+          version: '100.1.0',
+        },
+      },
+    } as LockfileFile['catalogs'])
+    expect(lockfile1.importers).toStrictEqual({
+      '.': {
+        dependencies: {
+          '@pnpm.e2e/foo': {
+            specifier: 'catalog:',
+            version: '100.1.0',
+          },
+        },
+      },
+    } as LockfileFile['importers'])
+
+    expect(readYamlFile('pnpm-workspace.yaml')).toStrictEqual({
+      catalog: {
+        '@pnpm.e2e/bar': '^100.1.0',
+        '@pnpm.e2e/foo': '^100.1.0',
+      },
+      packages: ['project-0', 'project-1'],
+      sharedWorkspaceLockfile: false,
+    })
+
+    expect(loadJsonFile('project-1/package.json')).toStrictEqual({
+      ...manifests[1],
+      dependencies: {
+        '@pnpm.e2e/foo': 'catalog:',
+      },
+    })
+  }
+})
+
 test('--save-catalog does not add local workspace dependency as a catalog', async () => {
   const manifests: ProjectManifest[] = [
     {
