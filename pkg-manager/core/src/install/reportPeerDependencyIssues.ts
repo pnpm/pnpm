751017f82397bc6ac8c73ef7736c10392c890137
Zoltan Kochan <z@kochan.io>
2025-05-09 02:01:21 +0200

    fix: don't fail with strictPeerDependencies=true if the peerDependencyRules ignore the peer issues
    close #9449

--- pkg-manager/core/src/install/reportPeerDependencyIssues.ts
+++ pkg-manager/core/src/install/reportPeerDependencyIssues.ts
@@ -1,28 +1,130 @@
 import { PnpmError } from '@pnpm/error'
+import { createMatcher } from '@pnpm/matcher'
 import { peerDependencyIssuesLogger } from '@pnpm/core-loggers'
-import { type PeerDependencyIssuesByProjects } from '@pnpm/types'
+import { type PeerDependencyIssuesByProjects, type PeerDependencyRules, type BadPeerDependencyIssue } from '@pnpm/types'
+import semver from 'semver'
 import isEmpty from 'ramda/src/isEmpty'
+import { parseOverrides, type VersionOverride } from '@pnpm/parse-overrides'
 
 export function reportPeerDependencyIssues (
   peerDependencyIssuesByProjects: PeerDependencyIssuesByProjects,
   opts: {
     lockfileDir: string
+    rules?: PeerDependencyRules
     strictPeerDependencies: boolean
   }
 ): void {
+  const ignoreMissingPatterns = [...new Set(opts?.rules?.ignoreMissing ?? [])]
+  const ignoreMissingMatcher = createMatcher(ignoreMissingPatterns)
+  const allowAnyPatterns = [...new Set(opts?.rules?.allowAny ?? [])]
+  const allowAnyMatcher = createMatcher(allowAnyPatterns)
+  const { allowedVersionsMatchAll, allowedVersionsByParentPkgName } = parseAllowedVersions(opts?.rules?.allowedVersions ?? {})
+  const newPeerDependencyIssuesByProjects: PeerDependencyIssuesByProjects = {}
+  for (const [projectId, { bad, missing, conflicts, intersections }] of Object.entries(peerDependencyIssuesByProjects)) {
+    newPeerDependencyIssuesByProjects[projectId] = { bad: {}, missing: {}, conflicts: [], intersections: {} }
+    for (const [peerName, issues] of Object.entries(missing)) {
+      if (
+        !conflicts.includes(peerName) &&
+        intersections[peerName] == null ||
+        ignoreMissingMatcher(peerName)
+      ) {
+        continue
+      }
+      newPeerDependencyIssuesByProjects[projectId].missing[peerName] = issues
+    }
+    for (const [peerName, issues] of Object.entries(bad)) {
+      if (allowAnyMatcher(peerName)) continue
+      const filteredIssues: BadPeerDependencyIssue[] = []
+      for (const issue of issues) {
+        if (allowedVersionsMatchAll[peerName]?.some((range) => semver.satisfies(issue.foundVersion, range))) continue
+        const currentParentPkg = issue.parents.at(-1)
+        if (currentParentPkg && allowedVersionsByParentPkgName[peerName]?.[currentParentPkg.name]) {
+          const allowedVersionsByParent: Record<string, string[]> = {}
+          for (const { targetPkg, parentPkg, ranges } of allowedVersionsByParentPkgName[peerName][currentParentPkg.name]) {
+            if (!parentPkg.bareSpecifier || currentParentPkg.version &&
+              (isSubRange(parentPkg.bareSpecifier, currentParentPkg.version) || semver.satisfies(currentParentPkg.version, parentPkg.bareSpecifier))) {
+              allowedVersionsByParent[targetPkg.name] = ranges
+            }
+          }
+          if (allowedVersionsByParent[peerName]?.some((range) => semver.satisfies(issue.foundVersion, range))) continue
+        }
+        filteredIssues.push(issue)
+      }
+      if (filteredIssues.length) {
+        newPeerDependencyIssuesByProjects[projectId].bad[peerName] = filteredIssues
+      }
+    }
+  }
+
   if (
-    Object.values(peerDependencyIssuesByProjects).every((peerIssuesOfProject) =>
+    Object.values(newPeerDependencyIssuesByProjects).every((peerIssuesOfProject) =>
       isEmpty(peerIssuesOfProject.bad) && (
         isEmpty(peerIssuesOfProject.missing) ||
         peerIssuesOfProject.conflicts.length === 0 && Object.keys(peerIssuesOfProject.intersections).length === 0
       ))
   ) return
   if (opts.strictPeerDependencies) {
-    throw new PeerDependencyIssuesError(peerDependencyIssuesByProjects)
+    throw new PeerDependencyIssuesError(newPeerDependencyIssuesByProjects)
   }
   peerDependencyIssuesLogger.debug({
-    issuesByProjects: peerDependencyIssuesByProjects,
+    issuesByProjects: newPeerDependencyIssuesByProjects,
   })
+}
+
+function isSubRange (superRange: string | undefined, subRange: string): boolean {
+  return !superRange ||
+  subRange === superRange ||
+  semver.validRange(subRange) != null &&
+  semver.validRange(superRange) != null &&
+  semver.subset(subRange, superRange)
+}
+
+type AllowedVersionsByParentPkgName = Record<string, Record<string, Array<Required<Pick<VersionOverride, 'parentPkg' | 'targetPkg'>> & { ranges: string[] }>>>
+
+interface ParsedAllowedVersions {
+  allowedVersionsMatchAll: Record<string, string[]>
+  allowedVersionsByParentPkgName: AllowedVersionsByParentPkgName
+}
+
+function tryParseAllowedVersions (allowedVersions: Record<string, string>): VersionOverride[] {
+  try {
+    return parseOverrides(allowedVersions ?? {})
+  } catch (err) {
+    throw new PnpmError('INVALID_ALLOWED_VERSION_SELECTOR',
+      `${(err as PnpmError).message} in pnpm.peerDependencyRules.allowedVersions`)
+  }
+}
+
+function parseAllowedVersions (allowedVersions: Record<string, string>): ParsedAllowedVersions {
+  const overrides = tryParseAllowedVersions(allowedVersions)
+  const allowedVersionsMatchAll: Record<string, string[]> = {}
+  const allowedVersionsByParentPkgName: AllowedVersionsByParentPkgName = {}
+  for (const { parentPkg, targetPkg, newBareSpecifier } of overrides) {
+    const ranges = parseVersions(newBareSpecifier)
+    if (!parentPkg) {
+      allowedVersionsMatchAll[targetPkg.name] = ranges
+      continue
+    }
+    if (!allowedVersionsByParentPkgName[targetPkg.name]) {
+      allowedVersionsByParentPkgName[targetPkg.name] = {}
+    }
+    if (!allowedVersionsByParentPkgName[targetPkg.name][parentPkg.name]) {
+      allowedVersionsByParentPkgName[targetPkg.name][parentPkg.name] = []
+    }
+    allowedVersionsByParentPkgName[targetPkg.name][parentPkg.name].push({
+      parentPkg,
+      targetPkg,
+      ranges,
+    })
+  }
+  return {
+    allowedVersionsMatchAll,
+    allowedVersionsByParentPkgName,
+  }
+}
+
+function parseVersions (versions: string): string[] {
+  return versions.split('||').map(v => v.trim())
 }
 
 export class PeerDependencyIssuesError extends PnpmError {
