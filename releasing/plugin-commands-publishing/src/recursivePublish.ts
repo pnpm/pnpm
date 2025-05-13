2b7505bc49d97f6a539284b812b1e9d5d4e426d1
Zoltan Kochan <z@kochan.io>
2025-05-13 15:14:43 +0200

    revert: concurrent publish

--- releasing/plugin-commands-publishing/src/recursivePublish.ts
+++ releasing/plugin-commands-publishing/src/recursivePublish.ts
@@ -1,13 +1,12 @@
 import path from 'path'
 import { createResolver } from '@pnpm/client'
-import { type Config, getWorkspaceConcurrency } from '@pnpm/config'
+import { type Config } from '@pnpm/config'
 import { logger } from '@pnpm/logger'
 import { pickRegistryForPackage } from '@pnpm/pick-registry-for-package'
 import { type ResolveFunction } from '@pnpm/resolver-base'
 import { sortPackages } from '@pnpm/sort-packages'
 import { type Registries, type ProjectRootDir } from '@pnpm/types'
 import pFilter from 'p-filter'
-import pLimit from 'p-limit'
 import pick from 'ramda/src/pick'
 import writeJsonFile from 'write-json-file'
 import { publish } from './publish'
@@ -21,7 +20,6 @@ export type PublishRecursiveOpts = Required<Pick<Config,
 | 'rawConfig'
 | 'registries'
 | 'workspaceDir'
-| 'workspaceConcurrency'
 >> &
 Partial<Pick<Config,
 | 'tag'
@@ -105,41 +103,38 @@ export async function recursivePublish (
       appendedArgs.push(`--otp=${opts.cliOptions['otp'] as string}`)
     }
     const chunks = sortPackages(opts.selectedProjectsGraph)
-    const limitPublish = pLimit(getWorkspaceConcurrency(opts.workspaceConcurrency))
     const tag = opts.tag ?? 'latest'
     for (const chunk of chunks) {
-      // eslint-disable-next-line no-await-in-loop
-      const publishResults = await Promise.all(chunk.map(pkgDir =>
-        limitPublish(async () => {
-          if (!publishedPkgDirs.has(pkgDir)) return
-          const pkg = opts.selectedProjectsGraph[pkgDir].package
-          const registry = pkg.manifest.publishConfig?.registry ?? pickRegistryForPackage(opts.registries, pkg.manifest.name!)
-
-          const publishResult = await publish({
-            ...opts,
-            dir: pkg.rootDir,
-            argv: {
-              original: [
-                'publish',
-                '--tag',
-                tag,
-                '--registry',
-                registry,
-                ...appendedArgs,
-              ],
-            },
-            gitChecks: false,
-            recursive: false,
-          }, [pkg.rootDir])
-          if (publishResult?.manifest != null) {
-            publishedPackages.push(pick(['name', 'version'], publishResult.manifest))
-          }
-          return publishResult
-        })
-      ))
-      const failedPublish = publishResults.find((result) => result?.exitCode)
-      if (failedPublish) {
-        return { exitCode: failedPublish.exitCode! }
+      // We can't run publish concurrently due to the npm CLI asking for OTP.
+      // NOTE: If we solve the OTP issue, we still need to limit packages concurrency.
+      // Otherwise, publishing will consume too much resources.
+      // See related issue: https://github.com/pnpm/pnpm/issues/6968
+      for (const pkgDir of chunk) {
+        if (!publishedPkgDirs.has(pkgDir)) continue
+        const pkg = opts.selectedProjectsGraph[pkgDir].package
+        const registry = pkg.manifest.publishConfig?.registry ?? pickRegistryForPackage(opts.registries, pkg.manifest.name!)
+        // eslint-disable-next-line no-await-in-loop
+        const publishResult = await publish({
+          ...opts,
+          dir: pkg.rootDir,
+          argv: {
+            original: [
+              'publish',
+              '--tag',
+              tag,
+              '--registry',
+              registry,
+              ...appendedArgs,
+            ],
+          },
+          gitChecks: false,
+          recursive: false,
+        }, [pkg.rootDir])
+        if (publishResult?.manifest != null) {
+          publishedPackages.push(pick(['name', 'version'], publishResult.manifest))
+        } else if (publishResult?.exitCode) {
+          return { exitCode: publishResult.exitCode }
+        }
       }
     }
   }
