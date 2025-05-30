be60215f32243a271cfd76aede8406292c78b30f
Zoltan Kochan <z@kochan.io>
2025-05-30 17:13:52 +0200

    test: global virtual store

--- pkg-manager/core/test/install/globalVirtualStore.ts
+++ pkg-manager/core/test/install/globalVirtualStore.ts
@@ -1,19 +1,42 @@
 import fs from 'fs'
 import path from 'path'
 import { prepareEmpty } from '@pnpm/prepare'
-import { addDependenciesToPackage } from '@pnpm/core'
+import { install } from '@pnpm/core'
+import { sync as rimraf } from '@zkochan/rimraf'
 import { testDefaults } from '../utils'
 
 test('using a global virtual store', async () => {
   prepareEmpty()
   const virtualStoreDir = path.resolve('deps')
-  await addDependenciesToPackage({}, ['@pnpm.e2e/pkg-with-1-dep@100.0.0'], testDefaults({
+  const manifest = {
+    dependencies: {
+      '@pnpm.e2e/pkg-with-1-dep': '100.0.0',
+    },
+  }
+  await install(manifest, testDefaults({
     enableGlobalVirtualStore: true,
     virtualStoreDir,
   }))
 
-  const files = fs.readdirSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0'))
-  expect(files.length).toBe(1)
-  expect(fs.existsSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0', files[0], 'node_modules/@pnpm.e2e/pkg-with-1-dep/package.json'))).toBeTruthy()
-  expect(fs.existsSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0', files[0], 'node_modules/@pnpm.e2e/dep-of-pkg-with-1-dep/package.json'))).toBeTruthy()
+  {
+    const files = fs.readdirSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0'))
+    expect(files.length).toBe(1)
+    expect(fs.existsSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0', files[0], 'node_modules/@pnpm.e2e/pkg-with-1-dep/package.json'))).toBeTruthy()
+    expect(fs.existsSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0', files[0], 'node_modules/@pnpm.e2e/dep-of-pkg-with-1-dep/package.json'))).toBeTruthy()
+  }
+
+  rimraf('node_modules')
+  rimraf(virtualStoreDir)
+  await install(manifest, testDefaults({
+    enableGlobalVirtualStore: true,
+    virtualStoreDir,
+    frozenLockfile: true,
+  }))
+
+  {
+    const files = fs.readdirSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0'))
+    expect(files.length).toBe(1)
+    expect(fs.existsSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0', files[0], 'node_modules/@pnpm.e2e/pkg-with-1-dep/package.json'))).toBeTruthy()
+    expect(fs.existsSync(path.join(virtualStoreDir, '@pnpm.e2e/pkg-with-1-dep/100.0.0', files[0], 'node_modules/@pnpm.e2e/dep-of-pkg-with-1-dep/package.json'))).toBeTruthy()
+  }
 })
