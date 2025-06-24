f209f7f1012959cfaec38c343f75034ccf69b98c
khai96_ <hvksmr1996@gmail.com>
2025-06-25 01:32:43 +0700

    test: simplify

--- pnpm/test/list.ts
+++ pnpm/test/list.ts
@@ -1,8 +1,6 @@
 import { preparePackages } from '@pnpm/prepare'
-import { type LockfileObject } from '@pnpm/lockfile.types'
 import { sync as writeYamlFile } from 'write-yaml-file'
-import { sync as readYamlFile } from 'read-yaml-file'
-import { execPnpm, execPnpmSync } from './utils'
+import { execPnpmSync } from './utils'
 
 test('ls --filter=not-exist --json should prints an empty array (#9672)', async () => {
   preparePackages([
@@ -19,13 +17,6 @@ test('ls --filter=not-exist --json should prints an empty array (#9672)', async 
   writeYamlFile('pnpm-workspace.yaml', {
     packages: ['packages/*'],
   })
-
-  await execPnpm(['install'])
-  expect(readYamlFile('pnpm-lock.yaml')).toStrictEqual(expect.objectContaining({
-    importers: {
-      'packages/foo': expect.any(Object),
-    },
-  } as Partial<LockfileObject>))
 
   const { stdout } = execPnpmSync(['ls', '--filter=project-that-does-not-exist', '--json'], { expectSuccess: true })
   expect(JSON.parse(stdout.toString())).toStrictEqual([])
