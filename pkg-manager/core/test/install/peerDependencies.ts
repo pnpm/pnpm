pnpm/pnpm

commit bd01a2a5a903d88341dece03c5cac78addc16f10

fix: crash during peers resolution (#8760)
close #8759

pkg-manager/resolve-dependencies/src/resolvePeers.ts
@@ -366,6 +366,7 @@ interface ParentPkgInfo {
 type ParentPkgsOfNode = Map<NodeId, Record<string, ParentPkgInfo>>
 
 async function resolvePeersOfNode<T extends PartialResolvedPackage> (
+  currentAlias: string,
   nodeId: NodeId,
   parentParentPkgs: ParentRefs,
   ctx: ResolvePeersContext & {
@@ -519,7 +520,7 @@ async function resolvePeersOfNode<T extends PartialResolvedPackage> (
     addDepPathToGraph(resolvedPackage.pkgIdWithPatchHash as unknown as DepPath)
   } else {
     const peerIds: PeerId[] = []
-    const pendingPeerNodeIds: NodeId[] = []
+    const pendingPeers: Array<{ alias: string, nodeId: NodeId }> = []
     for (const [alias, peerNodeId] of allResolvedPeers.entries()) {
       if (typeof peerNodeId === 'string' && peerNodeId.startsWith('link:')) {
         const linkedDir = peerNodeId.slice(5)
@@ -534,13 +535,13 @@ async function resolvePeersOfNode<T extends PartialResolvedPackage> (
         peerIds.push(peerDepPath)
         continue
       }
-      pendingPeerNodeIds.push(peerNodeId)
+      pendingPeers.push({ alias, nodeId: peerNodeId })
     }
-    if (pendingPeerNodeIds.length === 0) {
+    if (pendingPeers.length === 0) {
       const peersDirSuffix = createPeersDirSuffix(peerIds, ctx.peersSuffixMaxLength)
       addDepPathToGraph(`${resolvedPackage.pkgIdWithPatchHash}${peersDirSuffix}` as DepPath)
     } else {
-      calculateDepPathIfNeeded = calculateDepPath.bind(null, peerIds, pendingPeerNodeIds)
+      calculateDepPathIfNeeded = calculateDepPath.bind(null, peerIds, pendingPeers)
     }
   }
 
@@ -553,31 +554,28 @@ async function resolvePeersOfNode<T extends PartialResolvedPackage> (
 
   async function calculateDepPath (
     peerIds: PeerId[],
-    pendingPeerNodeIds: NodeId[],
+    pendingPeerNodes: Array<{ alias: string, nodeId: NodeId }>,
     cycles: string[][]
   ): Promise<void> {
-    const cyclicPeerNodeIds = new Set()
-    const node = ctx.dependenciesTree.get(nodeId)
-    if (node?.resolvedPackage.name != null) {
-      for (const cycle of cycles) {
-        if (cycle.includes(node.resolvedPackage.name)) {
-          for (const peerNodeId of cycle) {
-            cyclicPeerNodeIds.add(peerNodeId)
-          }
+    const cyclicPeerAliases = new Set()
+    for (const cycle of cycles) {
+      if (cycle.includes(currentAlias)) {
+        for (const peerAlias of cycle) {
+          cyclicPeerAliases.add(peerAlias)
         }
       }
     }
     const peersDirSuffix = createPeersDirSuffix([
       ...peerIds,
-      ...await Promise.all(pendingPeerNodeIds
-        .map(async (peerNodeId) => {
-          const peerNode = ctx.dependenciesTree.get(peerNodeId)?.resolvedPackage as T
-          if (peerNode && cyclicPeerNodeIds.has(peerNode.name)) {
-            const id = `${peerNode.name}@${peerNode.version}`
-            ctx.pathsByNodeIdPromises.get(peerNodeId)?.resolve(id as DepPath)
+      ...await Promise.all(pendingPeerNodes
+        .map(async (pendingPeer) => {
+          if (cyclicPeerAliases.has(pendingPeer.alias)) {
+            const { name, version } = ctx.dependenciesTree.get(pendingPeer.nodeId)?.resolvedPackage as T
+            const id = `${name}@${version}`
+            ctx.pathsByNodeIdPromises.get(pendingPeer.nodeId)?.resolve(id as DepPath)
             return id
           }
-          return ctx.pathsByNodeIdPromises.get(peerNodeId)!.promise
+          return ctx.pathsByNodeIdPromises.get(pendingPeer.nodeId)!.promise
         })
       ),
     ], ctx.peersSuffixMaxLength)
@@ -794,6 +792,7 @@ async function resolvePeersOfChildren<T extends PartialResolvedPackage> (
   // We check repeated first as the peers resolution of those probably are cached already.
   const [repeated, notRepeated] = partition(([alias]) => parentPkgs[alias] != null, Object.entries(children))
   const nodeIds = Array.from(new Set([...repeated, ...notRepeated].map(([, nodeId]) => nodeId)))
+  const aliasByNodeId = Object.fromEntries(Object.entries(children).map(([alias, nodeId]) => [nodeId, alias]))
 
   for (const nodeId of nodeIds) {
     if (!ctx.pathsByNodeIdPromises.has(nodeId)) {
@@ -822,12 +821,13 @@ async function resolvePeersOfChildren<T extends PartialResolvedPackage> (
     ctx.parentPkgsOfNode.set(childNodeId, parentDepPaths)
   }
   for (const childNodeId of nodeIds) {
+    const currentAlias = aliasByNodeId[childNodeId]
     const {
       resolvedPeers,
       missingPeers,
       calculateDepPath,
       finishing,
-    } = await resolvePeersOfNode(childNodeId, parentPkgs, ctx) // eslint-disable-line no-await-in-loop
+    } = await resolvePeersOfNode(currentAlias, childNodeId, parentPkgs, ctx) // eslint-disable-line no-await-in-loop
     if (finishing) {
       finishingList.push(finishing)
     }
@@ -838,15 +838,8 @@ async function resolvePeersOfChildren<T extends PartialResolvedPackage> (
     for (const [peerName, peerNodeId] of resolvedPeers) {
       allResolvedPeers.set(peerName, peerNodeId)
       edges.push(peerName)
-      const node = ctx.dependenciesTree.get(peerNodeId)
-      if (node?.resolvedPackage.name && node.resolvedPackage.name !== peerName) {
-        edges.push(node.resolvedPackage.name)
-      }
     }
-    const childNode = ctx.dependenciesTree.get(childNodeId)
-    if (childNode?.resolvedPackage.name != null) {
-      graph.push([childNode.resolvedPackage.name, edges])
-    }
+    graph.push([currentAlias, edges])
     for (const [missingPeer, range] of missingPeers.entries()) {
       allMissingPeers.set(missingPeer, range)
     }

pkg-manager/core/test/install/peerDependencies.ts
@@ -1892,3 +1892,18 @@ test('peer dependency cache is invalidated correctly when the peer of a peer mis
   expect(lockfile.snapshots['@pnpm.e2e/repeat-peers.d@1.0.0(@pnpm.e2e/repeat-peers.b@1.0.0(@pnpm.e2e/repeat-peers.a@1.0.0))']).toBeTruthy()
   expect(lockfile.snapshots['@pnpm.e2e/repeat-peers.d@1.0.0(@pnpm.e2e/repeat-peers.b@1.0.0(@pnpm.e2e/repeat-peers.a@2.0.0))']).toBeTruthy()
 })
+
+// Covers https://github.com/pnpm/pnpm/issues/8759
+test('detection of circular peer dependencies should not crash with aliased dependencies', async () => {
+  prepareEmpty()
+
+  await install({
+    dependencies: {
+      fastify: '5.1.0',
+      fastify4: 'npm:fastify@4.28.1',
+      'ts-jest': '29.2.5',
+    },
+  }, testDefaults())
+
+  expect(fs.existsSync(path.resolve(WANTED_LOCKFILE))).toBeTruthy()
+})

commit 260f1caff18e370b1f32324304784d17c4e03dda

fix(#9673): break mutual-wait dead-lock introduced in #8760 (#9675)
close #9673

---------

Co-authored-by: Zoltan Kochan <z@kochan.io>

pkg-manager/resolve-dependencies/src/resolvePeers.ts
@@ -807,7 +807,7 @@ async function resolvePeersOfChildren<T extends PartialResolvedPackage> (
 
   // Resolving non-repeated nodes before repeated nodes proved to be slightly faster.
   const calculateDepPaths: CalculateDepPath[] = []
-  const graph = []
+  const graph = new Map()
   const finishingList: FinishingResolutionPromise[] = []
   const parentDepPaths: Record<string, ParentPkgInfo> = {}
   for (const [name, parentPkg] of Object.entries(parentPkgs)) {
@@ -844,13 +844,27 @@ async function resolvePeersOfChildren<T extends PartialResolvedPackage> (
       allResolvedPeers.set(peerName, peerNodeId)
       edges.push(peerName)
     }
-    graph.push([currentAlias, edges])
+    addEdgesToGraph(currentAlias, edges)
+    const node = ctx.dependenciesTree.get(childNodeId)!
+    // We resolve peer dependencies via both the alias and the real name of the package.
+    // That's why we need to detect circular graphs via both the alias and the real name.
+    if (currentAlias !== node.resolvedPackage.name) {
+      addEdgesToGraph(node.resolvedPackage.name, edges)
+    }
     for (const [missingPeer, range] of missingPeers.entries()) {
       allMissingPeers.set(missingPeer, range)
     }
   }
+  function addEdgesToGraph (pkgName: string, edges: string[]) {
+    const existingEdges = graph.get(pkgName)
+    if (existingEdges == null) {
+      graph.set(pkgName, edges)
+    } else {
+      existingEdges.push(...edges)
+    }
+  }
   if (calculateDepPaths.length) {
-    const { cycles } = analyzeGraph(graph as unknown as Graph) as unknown as { cycles: string[][] }
+    const { cycles } = analyzeGraph(Array.from(graph.entries()) as unknown as Graph) as unknown as { cycles: string[][] }
     finishingList.push(...calculateDepPaths.map((calculateDepPath) => calculateDepPath(cycles)))
   }
   const finishing = Promise.all(finishingList).then(() => {})

pnpm-lock.yaml
@@ -33,6 +33,9 @@ catalogs:
     '@pnpm/log.group':
       specifier: 3.0.1
       version: 3.0.1
+    '@pnpm/logger':
+      specifier: '>=1001.0.0 <1002.0.0'
+      version: 1001.0.0
     '@pnpm/meta-updater':
       specifier: 2.0.6
       version: 2.0.6
@@ -58,8 +61,8 @@ catalogs:
       specifier: 0.0.1
       version: 0.0.1
     '@pnpm/registry-mock':
-      specifier: 4.4.0
-      version: 4.4.0
+      specifier: 4.5.0
+      version: 4.5.0
     '@pnpm/semver-diff':
       specifier: ^1.1.0
       version: 1.1.0
@@ -865,7 +868,7 @@ importers:
         version: link:../../pkg-manager/modules-yaml
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/types':
         specifier: workspace:*
         version: link:../../packages/types
@@ -899,7 +902,7 @@ importers:
     dependencies:
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/store.cafs':
         specifier: workspace:*
         version: link:../../store/cafs
@@ -974,7 +977,7 @@ importers:
     dependencies:
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/worker':
         specifier: workspace:*
         version: link:../../worker
@@ -1160,7 +1163,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@types/ramda':
         specifier: 'catalog:'
         version: 0.29.12
@@ -1648,7 +1651,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/testing.temp-store':
         specifier: workspace:*
         version: link:../../testing/temp-store
@@ -2287,7 +2290,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/types':
         specifier: workspace:*
         version: link:../../packages/types
@@ -2576,7 +2579,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -2724,7 +2727,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-ipc-server':
         specifier: workspace:*
         version: link:../../__utils__/test-ipc-server
@@ -4410,7 +4413,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -4697,7 +4700,7 @@ importers:
         version: link:../../pkg-manifest/read-package-json
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/store-path':
         specifier: workspace:*
         version: link:../../store/store-path
@@ -4970,7 +4973,7 @@ importers:
         version: link:../read-projects-context
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/store-path':
         specifier: workspace:*
         version: link:../../store/store-path
@@ -5321,7 +5324,7 @@ importers:
         version: 'link:'
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -5547,7 +5550,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -6151,7 +6154,7 @@ importers:
         version: link:../pkg-manifest/read-project-manifest
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/run-npm':
         specifier: workspace:*
         version: link:../exec/run-npm
@@ -6464,7 +6467,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -6591,7 +6594,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-ipc-server':
         specifier: workspace:*
         version: link:../../__utils__/test-ipc-server
@@ -7200,7 +7203,7 @@ importers:
         version: link:../../pkg-manifest/read-package-json
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -7264,7 +7267,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/workspace.filter-packages-from-dir':
         specifier: workspace:*
         version: link:../../workspace/filter-packages-from-dir
@@ -7349,7 +7352,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/test-fixtures':
         specifier: workspace:*
         version: link:../../__utils__/test-fixtures
@@ -7703,7 +7706,7 @@ importers:
         version: link:../../__utils__/prepare
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@types/archy':
         specifier: 'catalog:'
         version: 0.0.33
@@ -7957,7 +7960,7 @@ importers:
         version: link:../../store/package-store
       '@pnpm/registry-mock':
         specifier: 'catalog:'
-        version: 4.4.0(encoding@0.1.13)(typanion@3.14.0)
+        version: 4.5.0(encoding@0.1.13)(typanion@3.14.0)
       '@pnpm/store-controller-types':
         specifier: workspace:*
         version: link:../../store/store-controller-types
@@ -9806,8 +9809,8 @@ packages:
     resolution: {integrity: sha512-UY5ZFl8jTgWpPMp3qwVt1z455gDLGh4aAna7ufqsJP9qhI6lr9scFpnEamjpA51Y3MJMBtnML8KATmH6RY+NHQ==}
     engines: {node: '>=18.12'}
 
-  '@pnpm/registry-mock@4.4.0':
-    resolution: {integrity: sha512-OOHciXVfvqt4U70C0w+GyYnT3KrIvLG/1O1OE9fZQRji+/XdhiJd68kfhLhnTvlgi++r3mO1URlKldvi2zV/Yw==}
+  '@pnpm/registry-mock@4.5.0':
+    resolution: {integrity: sha512-xLruwgulp1Q950TJeQmbDyXzH5wIEVM5Wdm2rFHplRLlBbM+9JGaJZpcU0LiqivizD60Ow6cSIZbQ9qxOi9tnQ==}
     engines: {node: '>=18.12'}
     hasBin: true
 
@@ -16567,7 +16570,7 @@ snapshots:
 
   '@npmcli/fs@4.0.0':
     dependencies:
-      semver: 7.7.1
+      semver: 7.7.2
 
   '@pkgjs/parseargs@0.11.0':
     optional: true
@@ -17466,7 +17469,7 @@ snapshots:
       read-yaml-file: 2.1.0
       strip-bom: 4.0.0
 
-  '@pnpm/registry-mock@4.4.0(encoding@0.1.13)(typanion@3.14.0)':
+  '@pnpm/registry-mock@4.5.0(encoding@0.1.13)(typanion@3.14.0)':
     dependencies:
       anonymous-npm-registry-client: 0.3.2
       execa: 5.1.1
@@ -23902,7 +23905,7 @@ snapshots:
 
   wide-align@1.1.5:
     dependencies:
-      string-width: 1.0.2
+      string-width: 4.2.3
     optional: true
 
   widest-line@3.1.0:

pnpm-workspace.yaml
@@ -61,7 +61,7 @@ catalog:
   '@pnpm/npm-package-arg': ^1.0.0
   '@pnpm/os.env.path-extender': ^2.0.3
   '@pnpm/patch-package': 0.0.1
-  '@pnpm/registry-mock': 4.4.0
+  '@pnpm/registry-mock': 4.5.0
   '@pnpm/semver-diff': ^1.1.0
   '@pnpm/tabtab': ^0.5.4
   '@pnpm/util.lex-comparator': ^3.0.2

pkg-manager/core/test/install/peerDependencies.ts
@@ -1907,3 +1907,19 @@ test('detection of circular peer dependencies should not crash with aliased depe
 
   expect(fs.existsSync(path.resolve(WANTED_LOCKFILE))).toBeTruthy()
 })
+
+// Covers https://github.com/pnpm/pnpm/pull/9673
+test('no deadlock on circular aliased peers', async () => {
+  prepareEmpty()
+
+  await install({
+    dependencies: {
+      '@pnpm.e2e/deadlock.a': '1.0.0',
+      '@pnpm.e2e/deadlock.b': '2.0.0',
+      'alias-b': 'npm:@pnpm.e2e/deadlock.b@1.0.0',
+      'alias-c': 'npm:@pnpm.e2e/deadlock.c@1.0.0',
+    },
+  }, testDefaults())
+
+  expect(fs.existsSync(path.resolve(WANTED_LOCKFILE))).toBeTruthy()
+})