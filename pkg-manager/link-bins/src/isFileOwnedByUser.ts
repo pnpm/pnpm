665d2a141911e1e5c5eb42fde12e0d8cc931d89d
aghArdeshir <ardeshirizadinl@gmail.com>
2025-07-13 11:09:33 +0200

    fix(link-bins): replace chatGpt file owner check with a more understandable one

--- pkg-manager/link-bins/src/isFileOwnedByUser.ts
+++ pkg-manager/link-bins/src/isFileOwnedByUser.ts
@@ -1,14 +1,7 @@
 import { promises as fs } from 'fs'
 
 export async function isFileOwnedByUser (filePath: string): Promise<boolean> {
-  try {
-    const stats = await fs.stat(filePath)
-    const uid = process.getuid ? process.getuid() : process.pid // Fallback for environments without getuid
-    return stats.uid === uid
-  } catch (error) {
-    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
-      return false // File does not exist
-    }
-    throw error // Re-throw other errors
-  }
+  const { uid: fileOwnerId } = await fs.stat(filePath)
+  const userId = process.getuid?.()
+  return fileOwnerId === userId
 }
