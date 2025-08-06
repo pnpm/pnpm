84110431b1d5ed445c01e8b0e49117cf047640b1
khai96_ <hvksmr1996@gmail.com>
2025-08-06 16:08:42 +0700

    refactor: extract `output` into a function

--- config/plugin-commands-config/src/configGet.ts
+++ config/plugin-commands-config/src/configGet.ts
@@ -1,4 +1,5 @@
 import kebabCase from 'lodash.kebabcase'
+import { globalWarn } from '@pnpm/logger'
 import { runNpm } from '@pnpm/run-npm'
 import { type ConfigCommandOptions } from './ConfigCommandOptions'
 import { settingShouldFallBackToNpm } from './configSet'
@@ -9,13 +10,20 @@ export function configGet (opts: ConfigCommandOptions, key: string): { output: s
     return { output: '', exitCode: exitCode ?? 0 }
   }
   const config = opts.rawConfig[kebabCase(key)]
-  let output: string
-  if (Array.isArray(config)) {
-    output = config.join(',')
-  } else if (typeof config === 'object' && config !== null) {
-    output = JSON.stringify(config)
-  } else {
-    output = String(config)
-  }
+  const output = displayConfig(config, opts)
   return { output, exitCode: 0 }
+}
+
+type DisplayConfigOptions = Pick<ConfigCommandOptions, 'json'>
+
+function displayConfig (config: unknown, opts: DisplayConfigOptions): string {
+  if (opts.json) return JSON.stringify(config, undefined, 2)
+  if (Array.isArray(config)) {
+    globalWarn('`pnpm config get` would display an array as comma-separated list due to legacy implementation, use `--json` to print them as json')
+    return config.join(',')
+  }
+  if (typeof config === 'object') {
+    return JSON.stringify(config, undefined, 2)
+  }
+  return String(config)
 }
