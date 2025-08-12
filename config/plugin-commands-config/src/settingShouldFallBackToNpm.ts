ef59306c3f3aa9c819855ec27ae4e7f87fa4d18b
khai96_ <hvksmr1996@gmail.com>
2025-08-13 04:10:11 +0700

    fix: eslint

--- config/plugin-commands-config/src/settingShouldFallBackToNpm.ts
+++ config/plugin-commands-config/src/settingShouldFallBackToNpm.ts
@@ -3,5 +3,5 @@ export function settingShouldFallBackToNpm (key: string): boolean {
     ['registry', '_auth', '_authToken', 'username', '_password'].includes(key) ||
     key[0] === '@' ||
     key.startsWith('//')
-  );
+  )
 }
