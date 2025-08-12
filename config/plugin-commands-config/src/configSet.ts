827b3ce5efc2a826f24b792b536f5e7914c6cf93
khai96_ <hvksmr1996@gmail.com>
2025-08-13 03:00:38 +0700

    chore(git): temporary revert
    This reverts commit 66add58c4b7467062df8be2c65bbb62ade4daaaf.

--- config/plugin-commands-config/src/configSet.ts
+++ config/plugin-commands-config/src/configSet.ts
@@ -2,8 +2,6 @@ import fs from 'fs'
 import path from 'path'
 import util from 'util'
 import { types } from '@pnpm/config'
-import { PnpmError } from '@pnpm/error'
-import { getObjectValueByPropertyPath } from '@pnpm/object.property-path'
 import { runNpm } from '@pnpm/run-npm'
 import { updateWorkspaceManifest } from '@pnpm/workspace.manifest-writer'
 import camelCase from 'camelcase'
@@ -11,8 +9,6 @@ import kebabCase from 'lodash.kebabcase'
 import { readIniFile } from 'read-ini-file'
 import { writeIniFile } from 'write-ini-file'
 import { type ConfigCommandOptions } from './ConfigCommandOptions'
-import { isStrictlyKebabCase } from './isStrictlyKebabCase'
-import { parseConfigPropertyPath } from './parseConfigPropertyPath'
 
 export async function configSet (opts: ConfigCommandOptions, key: string, value: string | null): Promise<void> {
   if (opts.global && settingShouldFallBackToNpm(key)) {
@@ -24,39 +20,19 @@ export async function configSet (opts: ConfigCommandOptions, key: string, value:
     }
     return
   }
-
-  const parsedPropertyPath = isStrictlyKebabCase(key)
-    ? [key] // we don't parse kebab-case keys as property paths because it's not a valid JS syntax
-    : Array.from(parseConfigPropertyPath(key))
-  if (parsedPropertyPath.length === 0) {
-    throw new PnpmError('SET_CONFIG_BY_EMPTY_PATH', 'Cannot set config value by an empty path')
-  }
-  const prefix = parsedPropertyPath.slice(0, -1)
-  const bottomLevelKey = parsedPropertyPath.at(-1)!
-
   if (opts.global === true || fs.existsSync(path.join(opts.dir, '.npmrc'))) {
     const configPath = opts.global ? path.join(opts.configDir, 'rc') : path.join(opts.dir, '.npmrc')
-    const overallSettings = await safeReadIniFile(configPath)
-    type TargetSubSettings = string | number | null | undefined | Record<string | number, unknown>
-    const targetSubSettings = getObjectValueByPropertyPath(overallSettings, prefix) as TargetSubSettings
-    if (typeof targetSubSettings !== 'object') {
-      throw new PnpmError('SET_PROPERTY_OF_NON_OBJECT', `Cannot set property ${JSON.stringify(bottomLevelKey)} of a non-object (${JSON.stringify(targetSubSettings)})`)
-    }
+    const settings = await safeReadIniFile(configPath)
+    key = kebabCase(key)
     if (value == null) {
-      if (targetSubSettings?.[bottomLevelKey] == null) return
-      delete targetSubSettings[bottomLevelKey]
-    } else if (targetSubSettings == null) {
-      throw new PnpmError('SET_PROPERTY_OF_NULL', `Cannot set property ${JSON.stringify(bottomLevelKey)} of null`)
+      if (settings[key] == null) return
+      delete settings[key]
     } else {
-      targetSubSettings[bottomLevelKey] = value
+      settings[key] = value
     }
-    await writeIniFile(configPath, overallSettings)
+    await writeIniFile(configPath, settings)
     return
   }
-
-  // const [topLevelKey, ...middle] = prefix
-  // const
-
   key = camelCase(key)
   await updateWorkspaceManifest(opts.workspaceDir ?? opts.dir, {
     [key]: castField(value, kebabCase(key)),
