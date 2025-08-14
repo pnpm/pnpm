8b234ddb83dd27897d2e0e16bcd579f7d97fb7e8
khai96_ <hvksmr1996@gmail.com>
2025-08-14 17:01:33 +0700

    test: censorship of protected settings

--- config/plugin-commands-config/test/configList.test.ts
+++ config/plugin-commands-config/test/configList.test.ts
@@ -36,3 +36,50 @@ test('config list --json', async () => {
     'store-dir': '~/store',
   }, null, 2))
 })
+
+test('config list censors protected settings', async () => {
+  const rawConfig = {
+    'store-dir': '~/store',
+    'fetch-retries': '2',
+    'username': 'general-username',
+    '@my-org:registry': 'https://my-org.example.com/registry',
+    '//my-org.example.com:username': 'my-username-in-my-org',
+  }
+
+  const output = await config.handler({
+    dir: process.cwd(),
+    cliOptions: {},
+    configDir: process.cwd(),
+    rawConfig,
+  }, ['list'])
+
+  expect(ini.decode(getOutputString(output))).toEqual({
+    ...rawConfig,
+    '//my-org.example.com:username': '(protected)',
+    username: '(protected)',
+  })
+})
+
+test('config list --json censors protected settings', async () => {
+  const rawConfig = {
+    'store-dir': '~/store',
+    'fetch-retries': '2',
+    'username': 'general-username',
+    '@my-org:registry': 'https://my-org.example.com/registry',
+    '//my-org.example.com:username': 'my-username-in-my-org',
+  }
+
+  const output = await config.handler({
+    dir: process.cwd(),
+    json: true,
+    cliOptions: {},
+    configDir: process.cwd(),
+    rawConfig,
+  }, ['list'])
+
+  expect(JSON.parse(getOutputString(output))).toStrictEqual({
+    ...rawConfig,
+    '//my-org.example.com:username': '(protected)',
+    username: '(protected)',
+  })
+})
