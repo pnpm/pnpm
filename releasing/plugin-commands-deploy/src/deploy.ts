987d386657c2e9ce1723c74420a6e8918879a706
Zoltan Kochan <z@kochan.io>
2025-09-03 01:43:25 +0200

    fix: only print hint when there's a script named deploy

--- releasing/plugin-commands-deploy/src/deploy.ts
+++ releasing/plugin-commands-deploy/src/deploy.ts
@@ -82,9 +82,11 @@ export type DeployOptions =
 
 export async function handler (opts: DeployOptions, params: string[]): Promise<void> {
   if (!opts.workspaceDir) {
-    throw new PnpmError('CANNOT_DEPLOY', 'A deploy is only possible from inside a workspace', {
-      hint: 'Maybe you wanted to invoke "pnpm run deploy"'
-    })
+    let hint: string | undefined
+    if (opts.rootProjectManifest?.scripts?.['deploy'] != null) {
+      hint = 'Maybe you wanted to invoke "pnpm run deploy"'
+    }
+    throw new PnpmError('CANNOT_DEPLOY', 'A deploy is only possible from inside a workspace', { hint })
   }
   const selectedProjects = Object.values(opts.selectedProjectsGraph ?? {})
   if (selectedProjects.length === 0) {
