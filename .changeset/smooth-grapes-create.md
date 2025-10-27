88b7fc666d0c47112b716649caa267e282a83dc5
nissy-dev <nd.12021218@gmail.com>
2025-10-27 21:16:43 +0900

    fix: omit the major version matching restriction when filtering pkg metadata

---
+++ .changeset/smooth-grapes-create.md
@@ -0,0 +1,5 @@
+---
+"@pnpm/registry.pkg-metadata-filter": patch
+---
+
+fix: omit the major version matching restriction when filtering pkg metadata
--- registry/pkg-metadata-filter/test/index.ts
+++ registry/pkg-metadata-filter/test/index.ts
@@ -38,4 +38,27 @@ test('filterPkgMetadataByPublishDate', () => {
       '3.2.0': '2020-05-01T00:00:00.000Z',
     },
   }, cutoff)).toMatchSnapshot()
+
+  expect(filterPkgMetadataByPublishDate({
+    name,
+    versions: {
+      '3.0.0': {
+        name,
+        version: '3.0.0',
+        dist: { tarball: `https://registry.npmjs.org/${name}/-/${name}-3.0.0.tgz`, shasum: '' },
+      },
+      '2.9.9': {
+        name,
+        version: '2.9.9',
+        dist: { tarball: `https://registry.npmjs.org/${name}/-/${name}-2.9.9.tgz`, shasum: '' },
+      },
+    },
+    'dist-tags': {
+      latest: '2.9.9',
+    },
+    time: {
+      '2.9.9': '2020-03-01T00:00:00.000Z',
+      '3.0.0': '2020-05-01T00:00:00.000Z',
+    },
+  }, cutoff)).toMatchSnapshot()
 })
