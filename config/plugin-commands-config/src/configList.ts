e18037ed62dfa3092c7a90ef5fc8ba7721c27eb1
khai96_ <hvksmr1996@gmail.com>
2025-10-30 16:26:44 +0700

    refactor: only use necessary fields

--- config/plugin-commands-config/src/configList.ts
+++ config/plugin-commands-config/src/configList.ts
@@ -1,7 +1,7 @@
 import { processConfig } from './processConfig.js'
 import { type ConfigCommandOptions } from './ConfigCommandOptions.js'
 
-export type ConfigListOptions = Omit<ConfigCommandOptions, 'json'>
+export type ConfigListOptions = Pick<ConfigCommandOptions, 'rawConfig'>
 
 export async function configList (opts: ConfigListOptions): Promise<string> {
   const processedConfig = processConfig(opts.rawConfig)
