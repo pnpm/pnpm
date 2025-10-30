e2295a6f204d599c80154b0d4653817614730b8e
khai96_ <hvksmr1996@gmail.com>
2025-10-30 16:19:21 +0700

    refactor: actually use `ConfigListOptions`

--- config/plugin-commands-config/src/configList.ts
+++ config/plugin-commands-config/src/configList.ts
@@ -3,7 +3,7 @@ import { type ConfigCommandOptions } from './ConfigCommandOptions.js'
 
 export type ConfigListOptions = Omit<ConfigCommandOptions, 'json'>
 
-export async function configList (opts: ConfigCommandOptions): Promise<string> {
+export async function configList (opts: ConfigListOptions): Promise<string> {
   const processedConfig = processConfig(opts.rawConfig)
   return JSON.stringify(processedConfig, undefined, 2)
 }
