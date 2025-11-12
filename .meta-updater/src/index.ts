575528e09d0fdb534f2a595107e74388db938d2e
Zoltan Kochan <z@kochan.io>
2025-11-12 15:04:15 +0100

    chore: update pnpm

--- .meta-updater/src/index.ts
+++ .meta-updater/src/index.ts
@@ -378,9 +378,7 @@ async function updateManifest (workspaceDir: string, manifest: ProjectManifest, 
     bugs: {
       url: 'https://github.com/pnpm/pnpm/issues',
     },
-    engines: manifest.engines?.runtime != null
-      ? manifest.engines
-      : { node: '>=20.19' },
+    engines: { node: '>=20.19' },
     files,
     funding: 'https://opencollective.com/pnpm',
     homepage,
