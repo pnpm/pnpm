26c0aa3cab9ad08d1cc88f913b4d3ebc8c2fa138
VR <101288516+VillainsRule@users.noreply.github.com>
2025-11-21 18:26:33 -0500

    fix issue raised by @zkochan

--- resolving/tarball-resolver/src/index.ts
+++ resolving/tarball-resolver/src/index.ts
@@ -17,11 +17,21 @@ export async function resolveFromTarball (
 
   if (isRepository(wantedDependency.bareSpecifier)) return null
 
+  let resolvedUrl: string
+
+  // If there are redirects and the response is immutable, we want to get the final URL address
+  const response = await fetchFromRegistry(wantedDependency.bareSpecifier, { method: 'HEAD' })
+  if (response?.headers?.get('cache-control')?.includes('immutable')) {
+    resolvedUrl = response.url
+  } else {
+    resolvedUrl = wantedDependency.bareSpecifier
+  }
+
   return {
     id: wantedDependency.bareSpecifier as PkgResolutionId,
     normalizedBareSpecifier: wantedDependency.bareSpecifier,
     resolution: {
-      tarball: wantedDependency.bareSpecifier,
+      tarball: resolvedUrl,
     },
     resolvedVia: 'url',
   }
