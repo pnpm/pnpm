ae37f70211be9c610197f79d49ac801a94daa16f
Trevor Burnham <trevorburnham@gmail.com>
2025-11-27 22:06:32 -0500

    chore: fix tsc errors

--- hooks/pnpmfile/src/Hooks.ts
+++ hooks/pnpmfile/src/Hooks.ts
@@ -1,8 +1,5 @@
-import {
-  type PreResolutionHook,
-} from '@pnpm/hooks.types'
+import { type PreResolutionHook } from '@pnpm/hooks.types'
 import { type LockfileObject } from '@pnpm/lockfile.types'
-import { type BaseManifest } from '@pnpm/types'
 import { type Log } from '@pnpm/core-loggers'
 import { type CustomFetchers } from '@pnpm/fetcher-base'
 import { type ImportIndexedPackageAsync } from '@pnpm/store-controller-types'
@@ -11,10 +8,9 @@ export interface HookContext {
   log: (message: string) => void
 }
 
-export type ReadPackageHookFunction = <Pkg extends BaseManifest>(pkg: Pkg, context: HookContext) => Pkg | Promise<Pkg>
-
 export interface Hooks {
-  readPackage?: ReadPackageHookFunction
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Flexible hook signature for any package manifest
+  readPackage?: (pkg: any, context: HookContext) => any
   preResolution?: PreResolutionHook
   afterAllResolved?: (lockfile: LockfileObject, context: HookContext) => LockfileObject | Promise<LockfileObject>
   filterLog?: (log: Log) => boolean
@@ -23,15 +19,6 @@ export interface Hooks {
    * @deprecated Use top-level `fetchers` export instead. This will be removed in a future version.
    */
   fetchers?: CustomFetchers
-  /**
-   * Hook to modify pnpm configuration.
-   *
-   * Note: The config parameter is actually the Config type from @pnpm/config,
-   * but we use `any` here to avoid circular dependencies. Hook implementations
-   * can safely cast it to the full Config type.
-   *
-   * @param config - The pnpm configuration object
-   * @returns The modified configuration object
-   */
-  updateConfig?: (config: any) => any // eslint-disable-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Flexible hook signature for any config object
+  updateConfig?: (config: any) => any
 }
