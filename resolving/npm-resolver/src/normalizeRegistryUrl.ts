1dfb85f5da9c74f2961359738f0627e07e1a2940
Zoltan Kochan <z@kochan.io>
2025-12-11 01:12:50 +0100

    feat: refactor, add test and changeset

--- resolving/npm-resolver/src/normalizeRegistryUrl.ts
+++ resolving/npm-resolver/src/normalizeRegistryUrl.ts
@@ -1,12 +1,9 @@
+/**
+ * Remove default ports (80 for HTTP, 443 for HTTPS) to ensure consistency
+ */
 export function normalizeRegistryUrl (urlString: string): string {
-  // Remove default ports (80 for HTTP, 443 for HTTPS) to ensure consistency
   try {
-    const urlObj = new URL(urlString)
-    if ((urlObj.protocol === 'https:' && urlObj.port === '443') ||
-        (urlObj.protocol === 'http:' && urlObj.port === '80')) {
-      urlObj.port = ''
-    }
-    return urlObj.toString()
+    return new URL(urlString).toString()
   } catch {
     return urlString
   }
