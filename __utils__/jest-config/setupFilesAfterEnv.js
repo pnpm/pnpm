215daa539a36b97da843505f8a9923b20ab5fa07
Zoltan Kochan <z@kochan.io>
2025-12-14 21:03:43 +0100

    test: fix

--- __utils__/jest-config/setupFilesAfterEnv.js
+++ __utils__/jest-config/setupFilesAfterEnv.js
@@ -1,4 +1,8 @@
+import path from 'path'
 import { finishWorkers } from '@pnpm/worker'
+
+const pnpmBinDir = path.join(import.meta.dirname, 'node_modules/.bin')
+process.env.PATH = `${pnpmBinDir}${path.delimiter}${process.env.PATH}`
 
 afterAll(async () => {
   await finishWorkers()
