d068fcf281a9abfd5f53db810d57446fb9566681
Zoltan Kochan <z@kochan.io>
2025-12-17 11:24:32 +0100

    fix: the add command should not fail, when blockExoticSubdeps is true (#10327)
    close #10324

--- pkg-manager/resolve-dependencies/src/resolveDependencies.ts
+++ pkg-manager/resolve-dependencies/src/resolveDependencies.ts
@@ -1392,7 +1392,8 @@ async function resolveDependency (
   if (
     ctx.blockExoticSubdeps &&
     options.currentDepth > 0 &&
-    !isNonExoticDep(pkgResponse.body.resolvedVia)
+    pkgResponse.body.resolvedVia != null && // This is already coming from the lockfile, we skip the check in this case for now. Should be fixed later.
+    isExoticDep(pkgResponse.body.resolvedVia)
   ) {
     const error = new PnpmError(
       'EXOTIC_SUBDEP',
@@ -1809,6 +1810,6 @@ const NON_EXOTIC_RESOLVED_VIA = new Set([
   'workspace',
 ])
 
-function isNonExoticDep (resolvedVia: string | undefined): boolean {
-  return resolvedVia != null && NON_EXOTIC_RESOLVED_VIA.has(resolvedVia)
+function isExoticDep (resolvedVia: string): boolean {
+  return !NON_EXOTIC_RESOLVED_VIA.has(resolvedVia)
 }
