e1c59ef51f9b4eed091883be119107d28c916c00
Brandon Cheng <gluxon@users.noreply.github.com>
2025-12-25 20:31:39 -0500

    refactor: remove class property access modifiers in constructor
    ```
    ../dedupe/check/src/DedupeCheckIssuesError.ts:5:16 - error TS1294: This syntax is not allowed when 'erasableSyntaxOnly' is enabled.
    
    5   constructor (public dedupeCheckIssues: DedupeCheckIssues) {
                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    ../reviewing/dependencies-hierarchy/src/getTree.ts:243:24 - error TS1294: This syntax is not allowed when 'erasableSyntaxOnly' is enabled.
    
    243   private constructor (private readonly keypath: readonly string[]) {}
                               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```

--- dedupe/check/src/DedupeCheckIssuesError.ts
+++ dedupe/check/src/DedupeCheckIssuesError.ts
@@ -2,7 +2,10 @@ import { type DedupeCheckIssues } from '@pnpm/dedupe.types'
 import { PnpmError } from '@pnpm/error'
 
 export class DedupeCheckIssuesError extends PnpmError {
-  constructor (public dedupeCheckIssues: DedupeCheckIssues) {
+  public dedupeCheckIssues: DedupeCheckIssues
+
+  constructor (dedupeCheckIssues: DedupeCheckIssues) {
     super('DEDUPE_CHECK_ISSUES', 'Dedupe --check found changes to the lockfile')
+    this.dedupeCheckIssues = dedupeCheckIssues
   }
 }
