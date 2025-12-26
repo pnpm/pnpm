88fa457b9da2939a8551ded1e211be5b177cc557
Brandon Cheng <gluxon@users.noreply.github.com>
2025-12-25 23:54:00 -0500

    refactor: factor out a `getRealNameAndSpec` function

--- pkg-manager/resolve-dependencies/src/toResolveImporter.ts
+++ pkg-manager/resolve-dependencies/src/toResolveImporter.ts
@@ -10,6 +10,7 @@ import { type ImporterToResolve } from './index.js'
 import { getWantedDependencies, type WantedDependency } from './getWantedDependencies.js'
 import { type ImporterToResolveGeneric } from './resolveDependencyTree.js'
 import { safeIsInnerLink } from './safeIsInnerLink.js'
+import { unwrapPackageName } from './unwrapPackageName.js'
 import { validatePeerDependencies } from './validatePeerDependencies.js'
 
 export interface ResolveImporter extends ImporterToResolve, ImporterToResolveGeneric<{ isNew?: boolean }> {
@@ -149,22 +150,18 @@ function getVersionSpecsByRealNames (deps: Dependencies): VersionSpecsByRealName
   const acc: VersionSpecsByRealNames = {}
   for (const depName in deps) {
     const currentBareSpecifier = deps[depName]
-    if (currentBareSpecifier.startsWith('npm:')) {
-      const bareSpecifier = currentBareSpecifier.slice(4)
-      const index = bareSpecifier.lastIndexOf('@')
-      const spec = bareSpecifier.slice(index + 1)
-      const selector = getVerSelType(spec)
-      if (selector != null) {
-        const pkgName = bareSpecifier.substring(0, index)
-        acc[pkgName] = acc[pkgName] || {}
-        acc[pkgName][selector.normalized] = selector.type
-      }
-    } else if (!currentBareSpecifier.includes(':')) { // we really care only about semver specs
-      const selector = getVerSelType(currentBareSpecifier)
-      if (selector != null) {
-        acc[depName] = acc[depName] || {}
-        acc[depName][selector.normalized] = selector.type
-      }
+
+    const { pkgName, bareSpecifier } = unwrapPackageName(depName, currentBareSpecifier)
+
+    // we really care only about semver specs
+    if (bareSpecifier.includes(':')) {
+      continue
+    }
+
+    const selector = getVerSelType(bareSpecifier)
+    if (selector != null) {
+      acc[pkgName] = acc[pkgName] || {}
+      acc[pkgName][selector.normalized] = selector.type
     }
   }
   return acc
