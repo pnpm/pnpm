e225d534ee3aa14856579ceb136cd4a4752ca9d0
Zoltan Kochan <z@kochan.io>
2026-01-14 21:49:07 +0100

    refactor: revert not needed change

--- pkg-manager/core/src/install/checkCustomResolverForceResolve.ts
+++ pkg-manager/core/src/install/checkCustomResolverForceResolve.ts
@@ -1,6 +1,6 @@
 import { parse as parseDepPath } from '@pnpm/dependency-path'
-import { type CustomResolver, type WantedDependency, checkCustomResolverCanResolve } from '@pnpm/hooks.types'
 import { type LockfileObject } from '@pnpm/lockfile.types'
+import { type CustomResolver, type WantedDependency, checkCustomResolverCanResolve } from '@pnpm/hooks.types'
 
 // Sentinel for Promise.any rejections (not an error condition)
 const SKIP = new Error('skip')
