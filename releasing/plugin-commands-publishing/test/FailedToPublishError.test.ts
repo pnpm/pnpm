bc3e02e61a70db78d5a0b01a1a49f783eb36f432
khai96_ <hvksmr1996@gmail.com>
2026-01-15 04:16:04 +0700

    test: FailedToPublishError

---
+++ releasing/plugin-commands-publishing/test/FailedToPublishError.test.ts
@@ -0,0 +1,101 @@
+import { type PackResult } from '../src/pack.js'
+import { FailedToPublishError } from '../src/FailedToPublishError.js'
+
+const pack = (): PackResult => ({
+  contents: ['index.js', 'bin.js'],
+  publishedManifest: {
+    name: 'example-pack',
+    version: '0.1.2',
+  },
+  tarballPath: 'example-pack.tgz',
+})
+
+describe('createFailedToPublishError', () => {
+  test('without details', async () => {
+    expect(await FailedToPublishError.createFailedToPublishError(pack(), {
+      status: 401,
+      statusText: 'Unauthorized',
+      text: () => '',
+    })).toMatchObject({
+      code: 'ERR_PNPM_FAILED_TO_PUBLISH',
+      message: 'Failed to publish package example-pack@0.1.2 (status 401 Unauthorized)',
+      status: 401,
+      statusText: 'Unauthorized',
+      text: '',
+      pack: pack(),
+    } as Partial<FailedToPublishError>)
+  })
+
+  test('failed to get details text', async () => {
+    expect(await FailedToPublishError.createFailedToPublishError(pack(), {
+      status: 401,
+      statusText: 'Unauthorized',
+      text () {
+        throw new Error('No details')
+      },
+    })).toMatchObject({
+      code: 'ERR_PNPM_FAILED_TO_PUBLISH',
+      message: 'Failed to publish package example-pack@0.1.2 (status 401 Unauthorized)',
+      status: 401,
+      statusText: 'Unauthorized',
+      text: '',
+      pack: pack(),
+    } as Partial<FailedToPublishError>)
+  })
+
+  test('with single-line details', async () => {
+    const text = 'Failed to authenticate'
+    expect(await FailedToPublishError.createFailedToPublishError(pack(), {
+      status: 401,
+      statusText: 'Unauthorized',
+      text: () => text,
+    })).toMatchObject({
+      code: 'ERR_PNPM_FAILED_TO_PUBLISH',
+      message: 'Failed to publish package example-pack@0.1.2 (status 401 Unauthorized): Failed to authenticate',
+      status: 401,
+      statusText: 'Unauthorized',
+      text,
+      pack: pack(),
+    } as Partial<FailedToPublishError>)
+  })
+
+  test('with multi-line details', async () => {
+    const text = [
+      'Failed to authenticate',
+      'No token provided',
+    ].join('\n')
+    expect(await FailedToPublishError.createFailedToPublishError(pack(), {
+      status: 401,
+      statusText: 'Unauthorized',
+      text: () => text,
+    })).toMatchObject({
+      code: 'ERR_PNPM_FAILED_TO_PUBLISH',
+      message: [
+        'Failed to publish package example-pack@0.1.2 (status 401 Unauthorized)',
+        'Details:',
+        '    Failed to authenticate',
+        '    No token provided',
+        '',
+      ].join('\n'),
+      status: 401,
+      statusText: 'Unauthorized',
+      text,
+      pack: pack(),
+    } as Partial<FailedToPublishError>)
+  })
+
+  test('with an empty statusText', async () => {
+    expect(await FailedToPublishError.createFailedToPublishError(pack(), {
+      status: 499,
+      statusText: '',
+      text: () => '',
+    })).toMatchObject({
+      code: 'ERR_PNPM_FAILED_TO_PUBLISH',
+      message: 'Failed to publish package example-pack@0.1.2 (status 499)',
+      status: 499,
+      statusText: '',
+      text: '',
+      pack: pack(),
+    } as Partial<FailedToPublishError>)
+  })
+})
