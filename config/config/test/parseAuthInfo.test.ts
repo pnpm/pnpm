515b2ba45bbf388706cfaf7f4ea4964f3897327e
khai96_ <hvksmr1996@gmail.com>
2026-01-21 18:19:43 +0700

    feat(config/getNetworkConfigs): load auth info
    In order to resolve merge conflicts ahead of time
    for https://github.com/pnpm/pnpm/pull/10385

---
+++ config/config/test/parseAuthInfo.test.ts
@@ -0,0 +1,97 @@
+import { type AuthInfo, TokenHelperUnsupportedCharacterError, parseAuthInfo } from '../src/parseAuthInfo.js'
+
+describe('parseAuthInfo', () => {
+  test('empty object', () => {
+    expect(parseAuthInfo({})).toBeUndefined()
+  })
+
+  test('authToken', () => {
+    expect(parseAuthInfo({
+      authToken: 'example auth token',
+    })).toStrictEqual({
+      authToken: 'example auth token',
+    } as AuthInfo)
+  })
+
+  test('authPairBase64', () => {
+    expect(parseAuthInfo({
+      authPairBase64: btoa('foo:bar'),
+    })).toStrictEqual({
+      authUserPass: {
+        username: 'foo',
+        password: 'bar',
+      },
+    } as AuthInfo)
+
+    expect(parseAuthInfo({
+      authPairBase64: btoa('foo:bar:baz'),
+    })).toStrictEqual({
+      authUserPass: {
+        username: 'foo',
+        password: 'bar:baz',
+      },
+    } as AuthInfo)
+  })
+
+  test('authUsername and authPassword', () => {
+    expect(parseAuthInfo({
+      authUsername: 'foo',
+      authPassword: 'bar',
+    })).toStrictEqual({
+      authUserPass: {
+        username: 'foo',
+        password: 'bar',
+      },
+    } as AuthInfo)
+
+    expect(parseAuthInfo({
+      authUsername: 'foo',
+    })).toBeUndefined()
+
+    expect(parseAuthInfo({
+      authPassword: 'bar',
+    })).toBeUndefined()
+  })
+
+  test('tokenHelper', () => {
+    expect(parseAuthInfo({
+      tokenHelper: 'example-token-helper --foo --bar baz',
+    })).toStrictEqual({
+      tokenHelper: ['example-token-helper', '--foo', '--bar', 'baz'],
+    } as AuthInfo)
+
+    expect(parseAuthInfo({
+      tokenHelper: './example-token-helper.sh --foo --bar baz',
+    })).toStrictEqual({
+      tokenHelper: ['./example-token-helper.sh', '--foo', '--bar', 'baz'],
+    } as AuthInfo)
+
+    expect(parseAuthInfo({
+      tokenHelper: 'node ./example-token-helper.js --foo --bar baz',
+    })).toStrictEqual({
+      tokenHelper: ['node', './example-token-helper.js', '--foo', '--bar', 'baz'],
+    } as AuthInfo)
+
+    expect(parseAuthInfo({
+      tokenHelper: './example-token-helper.sh',
+    })).toStrictEqual({
+      tokenHelper: ['./example-token-helper.sh'],
+    } as AuthInfo)
+  })
+
+  test('tokenHelper does not support environment variable', () => {
+    expect(() => parseAuthInfo({
+      tokenHelper: 'example-token-helper $MY_VAR',
+    })).toThrow(new TokenHelperUnsupportedCharacterError('$'))
+  })
+
+  test('tokenHelper does not support quotations', () => {
+    expect(() => parseAuthInfo({
+      tokenHelper: 'example-token-helper "hello world"',
+    })).toThrow(new TokenHelperUnsupportedCharacterError('"'))
+
+    expect(() => parseAuthInfo({
+      tokenHelper: "example-token-helper 'hello world'",
+    })).toThrow(new TokenHelperUnsupportedCharacterError("'"))
+  })
+})
