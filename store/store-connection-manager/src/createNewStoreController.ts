1cb5e0a43465e3bd31ffd9c62577b1d926305832
Brandon Cheng <gluxon@users.noreply.github.com>
2026-01-22 17:51:38 -0500

    fix: pass storeDir to createClient to fix resolution skipping (#10502)

--- store/store-connection-manager/src/createNewStoreController.ts
+++ store/store-connection-manager/src/createNewStoreController.ts
@@ -71,6 +71,7 @@ export async function createNewStoreController (
     authConfig: opts.rawConfig,
     ca: opts.ca,
     cacheDir: opts.cacheDir,
+    storeDir: opts.storeDir,
     cert: opts.cert,
     fetchWarnTimeoutMs: opts.fetchWarnTimeoutMs,
     fetchMinSpeedKiBps: opts.fetchMinSpeedKiBps,
