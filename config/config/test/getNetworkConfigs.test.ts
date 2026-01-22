d019a7c7e7f501960f89cd9849f31623a048cc66
Khải <hvksmr1996@gmail.com>
2026-01-22 20:40:30 +0700

    feat(config/getNetworkConfigs): load auth info (#10491)
    * feat(config/getNetworkConfigs): load auth info
    
    In order to resolve merge conflicts ahead of time
    for https://github.com/pnpm/pnpm/pull/10385
    
    * fix: seperator of `_auth`
    
    * fix: pedantic
    
    * fix: spelling

--- config/config/test/getNetworkConfigs.test.ts
+++ config/config/test/getNetworkConfigs.test.ts
@@ -83,3 +83,84 @@ test('with files', () => {
     },
   } as NetworkConfigs)
 })
+
+test('auth infos', () => {
+  expect(getNetworkConfigs({
+    '@foo:registry': 'https://example.com/foo',
+    '//example.com/foo:_authToken': 'example auth token',
+  })).toStrictEqual({
+    registries: {
+      '@foo': 'https://example.com/foo',
+    },
+    authInfos: {
+      '//example.com/foo': {
+        authToken: 'example auth token',
+      },
+    },
+    sslConfigs: {},
+  } as NetworkConfigs)
+
+  expect(getNetworkConfigs({
+    '@foo:registry': 'https://example.com/foo',
+    '//example.com/foo:_auth': btoa('foo:bar'),
+  })).toStrictEqual({
+    registries: {
+      '@foo': 'https://example.com/foo',
+    },
+    authInfos: {
+      '//example.com/foo': {
+        authUserPass: {
+          username: 'foo',
+          password: 'bar',
+        },
+      },
+    },
+    sslConfigs: {},
+  } as NetworkConfigs)
+
+  expect(getNetworkConfigs({
+    '@foo:registry': 'https://example.com/foo',
+    '//example.com/foo:username': 'foo',
+    '//example.com/foo:_password': 'bar',
+  })).toStrictEqual({
+    registries: {
+      '@foo': 'https://example.com/foo',
+    },
+    authInfos: {
+      '//example.com/foo': {
+        authUserPass: {
+          username: 'foo',
+          password: 'bar',
+        },
+      },
+    },
+    sslConfigs: {},
+  } as NetworkConfigs)
+
+  expect(getNetworkConfigs({
+    '@foo:registry': 'https://example.com/foo',
+    '//example.com/foo:tokenHelper': 'node ./my-token-helper.cjs',
+  })).toStrictEqual({
+    registries: {
+      '@foo': 'https://example.com/foo',
+    },
+    authInfos: {
+      '//example.com/foo': {
+        tokenHelper: ['node', './my-token-helper.cjs'],
+      },
+    },
+    sslConfigs: {},
+  } as NetworkConfigs)
+})
+
+test('unsupported key', () => {
+  expect(getNetworkConfigs({
+    '@foo:registry': 'https://example.com/foo',
+    '//example.com/foo:someUnsupportedKey': 'hello world',
+  })).toStrictEqual({
+    registries: {
+      '@foo': 'https://example.com/foo',
+    },
+    sslConfigs: {},
+  } as NetworkConfigs)
+})
