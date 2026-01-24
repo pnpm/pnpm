d621b007861f8c7fb145b5c6ed9a6a8e2fbbdb63
Brandon Cheng <gluxon@users.noreply.github.com>
2026-01-24 13:36:04 -0500

    chore: remove copied `pnpm.overrides` for publish-packed

--- .meta-updater/src/index.ts
+++ .meta-updater/src/index.ts
@@ -4,7 +4,6 @@ import { readWantedLockfile, type LockfileObject } from '@pnpm/lockfile.fs'
 import { type ProjectId, type ProjectManifest } from '@pnpm/types'
 import { createUpdateOptions, type FormatPluginFnOptions } from '@pnpm/meta-updater'
 import { sortDirectKeys, sortKeysByPriority } from '@pnpm/object.key-sorting'
-import { parsePkgAndParentSelector } from '@pnpm/parse-overrides'
 import { findWorkspacePackagesNoCheck } from '@pnpm/workspace.find-packages'
 import { readWorkspaceManifest } from '@pnpm/workspace.read-manifest'
 import isSubdir from 'is-subdir'
@@ -69,14 +68,6 @@ export default async (workspaceDir: string) => { // eslint-disable-line
           }
         }
       } else {
-        manifest.pnpm = manifest.pnpm ?? {}
-        manifest.pnpm.overrides = { ...workspaceManifest!.overrides }
-        for (const selector in manifest.pnpm.overrides) {
-          if (manifest.pnpm.overrides[selector] === 'catalog:') {
-            const { targetPkg } = parsePkgAndParentSelector(selector)
-            manifest.pnpm.overrides[selector] = workspaceManifest!.catalog![targetPkg.name]
-          }
-        }
         for (const depType of ['devDependencies'] as const) {
           if (!manifest[depType]) continue
           for (const depName of Object.keys(manifest[depType] ?? {})) {
