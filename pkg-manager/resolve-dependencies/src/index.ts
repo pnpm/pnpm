b92e4ad17193336c3f8392c6e1d52c9932f4a486
Alessio Attilio <attilio.alessio@protonmail.com>
2026-01-25 21:59:40 +0100

    fix: prevent catalog: from leaking into pnpm-workspace.yaml (fixes #10176)

--- pkg-manager/resolve-dependencies/src/index.ts
+++ pkg-manager/resolve-dependencies/src/index.ts
@@ -288,9 +288,12 @@ export async function resolveDependencies (
       if (project.wantedDependencies[i]?.updateSpec == null) continue
       const dep = resolvedImporter.directDependencies[i]
       if (dep.catalogLookup == null) continue
+      // If normalizedBareSpecifier isn't defined, this catalog entry was resolved from cache.
+      // Avoid updating the updatedCatalogs map since it is likely unchanged.
+      if (dep.normalizedBareSpecifier == null) continue
       updatedCatalogs ??= {}
       updatedCatalogs[dep.catalogLookup.catalogName] ??= {}
-      updatedCatalogs[dep.catalogLookup.catalogName][dep.alias] = dep.normalizedBareSpecifier ?? dep.catalogLookup.userSpecifiedBareSpecifier
+      updatedCatalogs[dep.catalogLookup.catalogName][dep.alias] = dep.normalizedBareSpecifier
     }
   }
 
