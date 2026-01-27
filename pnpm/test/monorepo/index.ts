25ed567b66ee492b785700ebbf5d53996a03f31d
btea <2356281422@qq.com>
2026-01-28 00:28:14 +0800

    feat: support check more invalid workspace file (#10319)
    close #10313
    
    ---------
    
    Co-authored-by: Zoltan Kochan <z@kochan.io>

--- pnpm/test/monorepo/index.ts
+++ pnpm/test/monorepo/index.ts
@@ -61,19 +61,31 @@ test('no projects found', async () => {
   }
 })
 
-test('incorrect workspace manifest', async () => {
-  preparePackages([
-    {
-      name: 'project',
-      version: '1.0.0',
-    },
-  ])
+const invalidWorkspaceManifests = [
+  'pnpm-workspaces.yaml',
+  'pnpm-workspaces.yml',
+  'pnpm-workspace.yml',
+  '.pnpm-workspaces.yaml',
+  '.pnpm-workspaces.yml',
+  '.pnpm-workspace.yml',
+  '.pnpm-workspace.yaml',
+]
 
-  writeYamlFile('pnpm-workspace.yml', { packages: ['**', '!store/**'] })
+invalidWorkspaceManifests.forEach((filename) => {
+  test('incorrect workspace manifest ' + filename, async () => {
+    preparePackages([
+      {
+        name: 'project',
+        version: '1.0.0',
+      },
+    ])
 
-  const { status, stderr } = execPnpmSync(['install'])
-  expect(stderr.toString()).toMatch(/The workspace manifest file should be named "pnpm-workspace.yaml"/)
-  expect(status).toBe(1)
+    writeYamlFile(filename, { packages: ['**', '!store/**'] })
+
+    const { status, stderr } = execPnpmSync(['install'])
+    expect(stderr.toString()).toMatch(/The workspace manifest file should be named "pnpm-workspace.yaml"/)
+    expect(status).toBe(1)
+  })
 })
 
 test('linking a package inside a monorepo with --link-workspace-packages when installing new dependencies', async () => {
