c814edeb903b0d62dc8eece22f02b5161f188618
Brandon Cheng <gluxon@users.noreply.github.com>
2026-02-09 00:04:00 -0500

    test: ensure prerelease weighting is correct

---
+++ installing/deps-installer/test/install/prereleaseWeights.ts
@@ -0,0 +1,133 @@
+import path from 'node:path'
+
+import { type MutatedProject, mutateModules, type MutateModulesOptions, type ProjectOptions } from '@pnpm/installing.deps-installer'
+import { prepareEmpty } from '@pnpm/prepare'
+import type { PackageMeta } from '@pnpm/resolving.registry.types'
+import type { ProjectId, ProjectManifest, ProjectRootDir } from '@pnpm/types'
+import nock from 'nock'
+
+import { testDefaults } from '../utils/index.js'
+
+beforeEach(() => {
+  nock.disableNetConnect()
+})
+
+afterEach(() => {
+  // https://github.com/nock/nock?tab=readme-ov-file#resetting-netconnect
+  nock.cleanAll()
+  nock.enableNetConnect()
+})
+
+// Regression test for https://github.com/pnpm/pnpm/issues/10626
+test('prerelease specifiers do not cause not-yet-used version to be resolved', async () => {
+  const rootProject = prepareEmpty()
+  const lockfileDir = rootProject.dir()
+
+  const name = '@pnpm.e2e/prerelease'
+
+  const projects: Record<ProjectId, ProjectManifest> = {
+    ['a' as ProjectId]: {
+      name: 'a',
+      dependencies: {
+        [name]: '^1.1.0-beta',
+      },
+    },
+    ['b' as ProjectId]: {
+      name: 'b',
+      dependencies: {
+        [name]: '^1.2.0-beta',
+      },
+    },
+    ['c' as ProjectId]: {
+      name: 'c',
+    },
+  }
+  const allProjects: ProjectOptions[] = Object.entries(projects)
+    .map(([id, manifest]) => ({
+      buildIndex: 0,
+      manifest,
+      rootDir: path.resolve(id) as ProjectRootDir,
+    }))
+  const options = {
+    ...testDefaults(
+      { allProjects },
+      { retry: { retries: 0 } }
+    ),
+    lockfileDir,
+    lockfileOnly: true,
+    resolutionMode: 'highest',
+  } satisfies MutateModulesOptions
+
+  const installProjects: MutatedProject[] = Object.entries(projects)
+    .map(([id, manifest]) => ({
+      mutation: 'install',
+      id,
+      manifest,
+      rootDir: path.resolve(id) as ProjectRootDir,
+    }))
+
+  const meta: PackageMeta = {
+    name,
+    versions: {
+      '1.1.0-beta': {
+        name,
+        version: '1.1.0-beta',
+        // Generated locally through: echo '1.1.0-beta' | sha1sum
+        dist: { shasum: '7957736c00bc1e5a875e5e4f8f48d8f5a3830866', tarball: `${options.registries.default}/${name}-1.1.0-beta.tgz` },
+      },
+      '1.2.0-beta': {
+        name,
+        version: '1.2.0-beta',
+        dist: { shasum: '50c0586b05b59205f39610d63cc38ea04954182c', tarball: `${options.registries.default}/${name}-1.2.0-beta.tgz` },
+      },
+    },
+    'dist-tags': {
+      latest: '1.2.0-beta',
+    },
+  }
+
+  nock(options.registries.default)
+    // cspell:disable-next-line
+    .get('/@pnpm.e2e%2Fprerelease')
+    .reply(200, meta)
+
+  await mutateModules(installProjects, options)
+
+  {
+    const lockfile = rootProject.readLockfile()
+    expect(lockfile.importers['a' as ProjectId].dependencies?.[name]).toEqual({
+      specifier: '^1.1.0-beta',
+      version: '1.1.0-beta',
+    })
+    expect(lockfile.importers['b' as ProjectId].dependencies?.[name]).toEqual({
+      specifier: '^1.2.0-beta',
+      version: '1.2.0-beta',
+    })
+  }
+
+  // Simulate publishing a new 1.2.0 version.
+  meta.versions['1.2.0'] = {
+    name,
+    version: '1.2.0',
+    dist: { shasum: 'f95c23882c82328c872ac94af630c49ae57f37bb', tarball: `${options.registries.default}/${name}-1.2.0.tgz` },
+  }
+  meta['dist-tags'].latest = '1.2.0'
+
+  nock(options.registries.default)
+    // cspell:disable-next-line
+    .get('/@pnpm.e2e%2Fprerelease')
+    .reply(200, meta)
+
+  options.storeController.clearResolutionCache()
+
+  projects['c' as ProjectId].dependencies = { [name]: '^1.2.0-beta' }
+  await mutateModules(installProjects, options)
+
+  const lockfile = rootProject.readLockfile()
+  expect(lockfile.importers['c' as ProjectId].dependencies?.[name]).toEqual({
+    specifier: '^1.2.0-beta',
+    version: '1.2.0-beta',
+  })
+
+  expect(nock.isDone()).toBeTruthy()
+})
