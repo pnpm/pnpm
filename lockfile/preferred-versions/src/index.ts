5128757126f7ff3b596ae05a73ecd9ebaf95f1c7
Brandon Cheng <gluxon@users.noreply.github.com>
2026-02-14 23:47:11 -0500

    fix: use higher weight for package versions already in lockfile

--- lockfile/preferred-versions/src/index.ts
+++ lockfile/preferred-versions/src/index.ts
@@ -1,6 +1,12 @@
 import { nameVerFromPkgSnapshot, type PackageSnapshots } from '@pnpm/lockfile.utils'
 import { getAllDependenciesFromManifest } from '@pnpm/pkg-manifest.utils'
-import { DIRECT_DEP_SELECTOR_WEIGHT, type PreferredVersions } from '@pnpm/resolving.resolver-base'
+import {
+  DIRECT_DEP_SELECTOR_WEIGHT,
+  EXISTING_VERSION_SELECTOR_WEIGHT,
+  type PreferredVersions,
+  type VersionSelectorType,
+  type VersionSelectorWithWeight,
+} from '@pnpm/resolving.resolver-base'
 import type { DependencyManifest, ProjectManifest } from '@pnpm/types'
 import getVersionSelectorType from 'version-selector-type'
 
@@ -27,12 +33,53 @@ export function getPreferredVersionsFromLockfileAndManifests (
 }
 
 function addPreferredVersionsFromLockfile (snapshots: PackageSnapshots, preferredVersions: PreferredVersions): void {
+  // The snapshots object can contain multiple entries with the same package
+  // name and version. This is because a dependency can appear multiple times
+  // with the same version in the lockfile due to peer dependency resolution. To
+  // avoid inflating the weight of package versions that appear multiple times,
+  // generate a map with only the unique set to iterate over.
+  const uniqueNameVersions: Record<string, Set<string>> = {}
   for (const [depPath, snapshot] of Object.entries(snapshots)) {
     const { name, version } = nameVerFromPkgSnapshot(depPath, snapshot)
-    if (!preferredVersions[name]) {
-      preferredVersions[name] = { [version]: 'version' }
-    } else if (!preferredVersions[name][version]) {
-      preferredVersions[name][version] = 'version'
+    uniqueNameVersions[name] ??= new Set()
+    uniqueNameVersions[name].add(version)
+  }
+
+  for (const [name, versions] of Object.entries(uniqueNameVersions)) {
+    for (const version of versions) {
+      preferredVersions[name] ??= {}
+
+      const existingSelector = preferredVersions[name][version]
+      if (existingSelector == null) {
+        preferredVersions[name][version] = { selectorType: 'version', weight: EXISTING_VERSION_SELECTOR_WEIGHT }
+        continue
+      }
+
+      // The lookup for this selector was for an exact version and not a range
+      // or tag. If there's an existing selector and it's not for a version,
+      // that's unexpected and our program state is corrupted.
+      const existingSelectorType = typeof existingSelector === 'string'
+        ? existingSelector
+        : existingSelector.selectorType
+      if (existingSelectorType !== 'version') {
+        throw new Error(`Encountered unexpected version selector '${existingSelectorType}' for dependency '${name}@${version}'`)
+      }
+
+      // There might be an existing selector on this exact version from a direct
+      // dependency. If so, we should increase its weight. This allows a version
+      // present in the lockfile that's also used by a direct dependency to be
+      // considered at a higher priority than a package with only one of the two
+      // criteria.
+      preferredVersions[name][version] = addWeightToVersionSelector(existingSelector, EXISTING_VERSION_SELECTOR_WEIGHT)
     }
   }
+}
+
+function addWeightToVersionSelector (
+  selector: VersionSelectorWithWeight | VersionSelectorType,
+  weight: number
+): VersionSelectorWithWeight {
+  return typeof selector === 'string'
+    ? { selectorType: selector, weight: weight + 1 }
+    : { selectorType: selector.selectorType, weight: selector.weight + weight }
 }
