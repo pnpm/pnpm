a2c8bd0875e01d8371c423223ca67301e2517312
Zoltan Kochan <z@kochan.io>
2026-02-16 23:51:25 +0100

    fix: eslint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 import eslintConfig from "@pnpm/eslint-config";
 import * as regexpPlugin from "eslint-plugin-regexp";
 
-export default [{
-    ignores: ["**/fixtures", "**/__fixtures__", "**/node_modules", "**/lib"],
-}, ...eslintConfig, regexpPlugin.configs['flat/recommended']];
+export default [
+    {
+        ignores: ["**/fixtures", "**/__fixtures__", "**/node_modules", "**/lib"],
+    },
+    ...eslintConfig,
+    regexpPlugin.configs['flat/recommended'],
+    {
+        files: ["pnpm/src/**/*.ts"],
+        rules: {
+            "import-x/no-extraneous-dependencies": "off",
+        },
+    },
+]
