6fc3c51da0e6be5a23497345abeff749a75442dd
Jason Paulos <jasonpaulos@users.noreply.github.com>
2026-02-18 23:38:48 -0500

    fix: import jest in setupFilesAfterEnv & reduce retries to 2

--- __utils__/jest-config/setupFilesAfterEnv.js
+++ __utils__/jest-config/setupFilesAfterEnv.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { finishWorkers } from '@pnpm/worker'
 import { isCI } from 'ci-info'
+import { jest } from '@jest/globals';
 
 const pnpmBinDir = path.join(import.meta.dirname, 'node_modules/.bin')
 process.env.PATH = `${pnpmBinDir}${path.delimiter}${process.env.PATH}`
@@ -10,5 +11,6 @@ afterAll(async () => {
 })
 
 if (isCI) {
-  jest.retryTimes(3, { logErrorsBeforeRetry: true })
+  // In CI, retry failed tests up to 2 times to mitigate flakiness, and log errors before retrying for better debugging
+  jest.retryTimes(2, { logErrorsBeforeRetry: true })
 }
