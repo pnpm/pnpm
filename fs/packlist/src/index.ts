84075f96bfae328ce6c711435be14905c0c1d120
Zoltan Kochan <z@kochan.io>
2026-02-25 11:33:52 +0100

    fix: update npm-packlist (#10658)

--- fs/packlist/src/index.ts
+++ fs/packlist/src/index.ts
@@ -1,16 +1,53 @@
+import fs from 'fs/promises'
+import path from 'path'
+import util from 'util'
 import npmPacklist from 'npm-packlist'
 
 export async function packlist (pkgDir: string, opts?: {
-  packageJsonCache?: Record<string, Record<string, unknown>>
+  manifest?: Record<string, unknown>
 }): Promise<string[]> {
-  const packageJsonCacheMap = opts?.packageJsonCache
-    ? new Map(Object.entries(opts.packageJsonCache))
-    : undefined
-  const files = await npmPacklist({ path: pkgDir, packageJsonCache: packageJsonCacheMap })
-  // There's a bug in the npm-packlist version that we use,
-  // it sometimes returns duplicates.
-  // Related issue: https://github.com/pnpm/pnpm/issues/6997
-  // Unfortunately, we cannot upgrade the library
-  // newer versions of npm-packlist are very slow.
-  return Array.from(new Set(files.map((file) => file.replace(/^\.[/\\]/, ''))))
+  const pkg = opts?.manifest ?? await readPackageJson(pkgDir)
+  const tree = {
+    path: pkgDir,
+    package: normalizePackage(pkg),
+    isProjectRoot: true,
+    edgesOut: new Map(),
+  }
+  const files = await npmPacklist(tree)
+  return files.map((file) => file.replace(/^\.[/\\]/, ''))
+}
+
+async function readPackageJson (dir: string): Promise<Record<string, unknown>> {
+  try {
+    return JSON.parse(await fs.readFile(path.join(dir, 'package.json'), 'utf8'))
+  } catch (err: unknown) {
+    if (util.types.isNativeError(err) && 'code' in err && err.code === 'ENOENT') {
+      return {}
+    }
+    throw err
+  }
+}
+
+function stripDotSlash (p: string): string {
+  return p.replace(/^\.[/\\]/, '')
+}
+
+function normalizePackage (pkg: Record<string, unknown>): Record<string, unknown> {
+  const normalized = { ...pkg }
+  if (typeof normalized.main === 'string') {
+    normalized.main = stripDotSlash(normalized.main)
+  }
+  if (typeof normalized.browser === 'string') {
+    normalized.browser = stripDotSlash(normalized.browser)
+  }
+  if (typeof normalized.bin === 'string') {
+    normalized.bin = stripDotSlash(normalized.bin)
+  } else if (normalized.bin != null && typeof normalized.bin === 'object') {
+    const bin: Record<string, string> = {}
+    for (const [key, value] of Object.entries(normalized.bin as Record<string, string>)) {
+      bin[key] = stripDotSlash(value)
+    }
+    normalized.bin = bin
+  }
+  return normalized
 }
