33131113144511c1e558af4183924ad14a7c95dc
Zoltan Kochan <z@kochan.io>
2026-03-06 11:00:49 +0100

    revert: "chore: remove forceExit"
    This reverts commit 5a96bf1445afd7d47e3b2732d9db260d5d937025.

--- __utils__/jest-config/with-registry/jest-preset.js
+++ __utils__/jest-config/with-registry/jest-preset.js
@@ -7,6 +7,10 @@ export default {
   // Unfortunately, this means that if two such tests will run at the same time,
   // they may break each other.
   maxWorkers: 1,
+  // Force Jest to exit after globalTeardown completes.  The Verdaccio server
+  // and lifecycle child-processes spawned during tests may leave ref'd handles
+  // that prevent the process from exiting on its own.
+  forceExit: true,
   globalSetup: path.join(import.meta.dirname, 'globalSetup.js'),
   globalTeardown: path.join(import.meta.dirname, 'globalTeardown.js'),
 }
