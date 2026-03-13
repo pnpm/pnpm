24e0815621e4f1507eb3266fb23ba43bed3c80fa
khai96_ <hvksmr1996@gmail.com>
2026-03-13 07:12:56 +0700

    docs: use hint

--- releasing/plugin-commands-publishing/src/otp.ts
+++ releasing/plugin-commands-publishing/src/otp.ts
@@ -286,6 +286,8 @@ export class OtpNonInteractiveError extends PnpmError {
 
 export class OtpSecondChallengeError extends PnpmError {
   constructor () {
-    super('OTP_SECOND_CHALLENGE', 'The registry requested a one-time password (OTP) a second time after one was already provided. This is unexpected behavior from the registry.')
+    super('OTP_SECOND_CHALLENGE', 'The registry requested a one-time password (OTP) a second time after one was already provided', {
+      hint: 'This is unexpected behavior from the registry, you can do nothing about it, sorry',
+    })
   }
 }
