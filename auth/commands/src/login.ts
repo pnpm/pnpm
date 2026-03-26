7c0fdbe0afbd41ce73684f53412efa4c3effc1d7
khai96_ <hvksmr1996@gmail.com>
2026-03-26 09:03:21 +0700

    docs: remove redundant comments

--- auth/commands/src/login.ts
+++ auth/commands/src/login.ts
@@ -202,11 +202,6 @@ async function webLogin (
     headers: {
       'content-type': 'application/json',
       accept: 'application/json',
-      // Q: Why does pnpm send this header unconditionally?
-      // A: This header doesn't say "I prefer web-based authentication";
-      //    it only says "I am capable of web-based authentication".
-      //    The npm CLI does the same:
-      //    <https://github.com/npm/npm-registry-fetch/blob/844230f/lib/index.js#L196-L198>
       'npm-auth-type': 'web',
     },
     body: JSON.stringify({}),
@@ -263,11 +258,6 @@ async function classicLogin (
       const headers: LoginFetchOptions['headers'] = {
         'content-type': 'application/json',
         accept: 'application/json',
-        // Q: Why does pnpm send this header unconditionally?
-        // A: This header doesn't say "I prefer web-based authentication";
-        //    it only says "I am capable of web-based authentication".
-        //    The npm CLI does the same:
-        //    <https://github.com/npm/npm-registry-fetch/blob/844230f/lib/index.js#L197-L199>
         'npm-auth-type': 'web',
       }
       if (otp) {
