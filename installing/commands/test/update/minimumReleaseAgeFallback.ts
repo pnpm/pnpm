3bb58c52cb8ec1b46fe6167760b9d5c7ede16aea
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-28 01:23:51 +0530

    test: cover version-qualified minimumReleaseAgeExclude retries

--- installing/commands/test/update/minimumReleaseAgeFallback.ts
+++ installing/commands/test/update/minimumReleaseAgeFallback.ts
@@ -40,6 +40,30 @@ test('retries latest updates without direct minimumReleaseAgeExclude matches aft
   expect(installDeps.mock.calls[1][0].minimumReleaseAgeExclude).toEqual(['@pnpm.e2e/bar@1.0.0'])
 })
 
+test('retries latest updates without scoped version-qualified direct exclusions after maturity failure', async () => {
+  prepare({
+    dependencies: {
+      '@pnpm.e2e/foo': '^1.0.0',
+    },
+  })
+
+  const err = Object.assign(new Error('maturity failure'), {
+    code: 'ERR_PNPM_NO_MATURE_MATCHING_VERSION',
+  })
+  installDeps.mockRejectedValueOnce(err)
+  installDeps.mockResolvedValueOnce(undefined)
+
+  await handler({
+    ...DEFAULT_OPTS,
+    dir: process.cwd(),
+    latest: true,
+    minimumReleaseAgeExclude: ['@pnpm.e2e/foo@1.0.0'],
+  }, ['@pnpm.e2e/foo'])
+
+  expect(installDeps).toHaveBeenCalledTimes(2)
+  expect(installDeps.mock.calls[1][0].minimumReleaseAgeExclude).toEqual([])
+})
+
 test('full latest updates keep exclusions for ignored direct dependencies on retry', async () => {
   prepare({
     dependencies: {
