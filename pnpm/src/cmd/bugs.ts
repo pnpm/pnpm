c0ba3cab0f6b22d01cf56c98bf2229d8bb6adc86
Alessio Attilio <attilio.alessio@protonmail.com>
2026-04-28 14:40:09 +0200

    fix(bugs): support package name arguments like npm bugs
    According to npm help, the usage is:
    Usage: npm bugs [<pkgname> [<pkgname> ...]]
    
    This change adds support for passing package names to the bugs command,
    which will open the bugs URL for that package from the registry.

--- pnpm/src/cmd/bugs.ts
+++ pnpm/src/cmd/bugs.ts
@@ -22,34 +22,76 @@ export function help (): string {
       },
     ],
     url: docsUrl('bugs'),
-    usages: ['pnpm bugs'],
+    usages: ['pnpm bugs', 'pnpm bugs [<pkgname> [<pkgname> ...]]'],
   })
 }
 
 export async function handler (
   opts: {
     dir: string
-  }
+    registries?: {
+      default?: string
+      [key: string]: string | undefined
+    }
+  },
+  params: string[]
 ): Promise<void> {
-  const { manifest } = await tryReadProjectManifest(opts.dir, {})
-  let bugsUrl: string | undefined
+  const pkgNames = params
 
+  if (pkgNames.length === 0) {
+    const { manifest } = await tryReadProjectManifest(opts.dir, {})
+    const bugsUrl = getBugsUrlFromManifest(manifest)
+
+    if (!bugsUrl) {
+      throw new PnpmError('NO_BUGS_URL', 'The package.json does not have a bugs URL. Add a "bugs" field or a "repository" field to your package.json.')
+    }
+
+    await openUrl(bugsUrl)
+  } else {
+    const getBugsUrlFromRegistry = async (pkgName: string): Promise<string> => {
+      const registry = opts.registries?.default ?? 'https://registry.npmjs.org'
+      const url = `${registry.replace(/\/$/, '')}/${pkgName}`
+      const response = await fetch(url, {
+        headers: {
+          'accept': 'application/json',
+        },
+      })
+      if (!response.ok) {
+        throw new PnpmError('PACKAGE_NOT_FOUND', `Could not fetch package "${pkgName}" from registry: ${response.status} ${response.statusText}`)
+      }
+      const data: Record<string, unknown> = await response.json()
+      const bugsUrl = getBugsUrlFromManifest(data as PackageManifest)
+      if (!bugsUrl) {
+        throw new PnpmError('NO_BUGS_URL', `The package "${pkgName}" does not have a bugs URL.`)
+      }
+      return bugsUrl
+    }
+
+    const bugsUrls = await Promise.all(pkgNames.map(getBugsUrlFromRegistry))
+    await Promise.all(bugsUrls.map(openUrl))
+  }
+}
+
+type PackageManifest = {
+  bugs?: { url?: string } | string
+  repository?: { url?: string } | string
+} | null
+
+function getBugsUrlFromManifest (manifest: PackageManifest): string | undefined {
   if (manifest?.bugs && typeof manifest.bugs !== 'string' && manifest.bugs.url) {
-    bugsUrl = manifest.bugs.url
-  } else if (typeof manifest?.bugs === 'string') {
-    bugsUrl = manifest.bugs
-  } else if (typeof manifest?.repository === 'object' && manifest.repository.url) {
-    bugsUrl = `${manifest.repository.url.replace(/\.git$/, '')}/issues`
-  } else if (typeof manifest?.repository === 'string') {
+    return manifest.bugs.url
+  }
+  if (typeof manifest?.bugs === 'string') {
+    return manifest.bugs
+  }
+  if (typeof manifest?.repository === 'object' && manifest.repository.url) {
+    return `${manifest.repository.url.replace(/\.git$/, '')}/issues`
+  }
+  if (typeof manifest?.repository === 'string') {
     const repoUrl = manifest.repository.replace(/\.git$/, '')
-    bugsUrl = `${repoUrl}/issues`
+    return `${repoUrl}/issues`
   }
-
-  if (!bugsUrl) {
-    throw new PnpmError('NO_BUGS_URL', 'The package.json does not have a bugs URL. Add a "bugs" field or a "repository" field to your package.json.')
-  }
-
-  await openUrl(bugsUrl)
+  return undefined
 }
 
 async function openUrl (url: string): Promise<void> {
