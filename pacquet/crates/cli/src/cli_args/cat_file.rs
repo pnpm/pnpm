ae5b9aba3bd0d22fa56321ee2cffdd35e82df287
ALESSIO ATTTILIO <ATTILIO.ALESSIO@PROTONMAIL.COM>
2026-06-21 11:59:36 +0200

    fix(pacquet): fix import granularity for dylint

--- pacquet/crates/cli/src/cli_args/cat_file.rs
+++ pacquet/crates/cli/src/cli_args/cat_file.rs
@@ -3,8 +3,7 @@ use clap::Args;
 use miette::{Context, IntoDiagnostic};
 use pacquet_config::Config;
 use pacquet_store_dir::StoreDir;
-use std::fs::File;
-use std::io::Write;
+use std::{fs::File, io::Write};
 
 #[derive(Debug, Args)]
 pub struct CatFileArgs {
