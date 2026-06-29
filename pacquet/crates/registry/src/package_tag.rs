82fa5d4f572a6bcca12c7b9973b75008b8786753
ALESSIO ATTTILIO <ATTILIO.ALESSIO@PROTONMAIL.COM>
2026-06-29 16:33:58 +0200

    fix(pacquet): fix package tag parsing and display format

--- pacquet/crates/registry/src/package_tag.rs
+++ pacquet/crates/registry/src/package_tag.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use derive_more::{Display, From, TryInto};
-use node_semver::Version;
+use node_semver::{Range, SemverError, Version};
 
 /// Version or tag that is attachable to a registry URL.
 #[derive(Debug, Display, Clone, From, TryInto)]
@@ -10,22 +10,24 @@ pub enum PackageTag {
     #[display("latest")]
     Latest,
     /// Pinned version.
+    #[display("{}", _0)]
     Version(Version),
     /// A custom tag (e.g. `beta`, `next`).
-    #[display("{_0}")]
+    #[display("{}", _0)]
     Tag(String),
 }
 
 impl FromStr for PackageTag {
-    type Err = std::convert::Infallible;
+    type Err = SemverError;
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         if value == "latest" {
             Ok(PackageTag::Latest)
+        } else if let Ok(version) = value.parse::<Version>() {
+            Ok(PackageTag::Version(version))
+        } else if Range::parse(value).is_ok() {
+            value.parse::<Version>().map(PackageTag::Version)
         } else {
-            match value.parse::<Version>() {
-                Ok(version) => Ok(PackageTag::Version(version)),
-                Err(_) => Ok(PackageTag::Tag(value.to_owned())),
-            }
+            Ok(PackageTag::Tag(value.to_owned()))
         }
     }
 }
