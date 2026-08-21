82916164eacfaa825abc318bf9f890b18f842d96
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-08-21 11:22:56 +0100

    test: add recursive test

--- pnpm11/pnpm/test/update.ts
+++ pnpm11/pnpm/test/update.ts
@@ -164,6 +164,41 @@ test('recursive update --no-save', async () => {
   expect(pkg.dependencies?.['@pnpm.e2e/foo']).toBe('^100.0.0')
 })
 
+test('recursive update <pkg>@<version> --lockfile-only --no-save does not leak across major lines for transitive targets', async () => {
+  preparePackages([
+    {
+      name: 'project-1',
+      version: '1.0.0',
+      dependencies: {
+        '@pnpm.e2e/pkg-with-good-optional': '1.0.0',
+      },
+    },
+    {
+      name: 'project-2',
+      version: '1.0.0',
+      dependencies: {
+        '@pnpm.e2e/dep-of-pkg-with-1-dep': '101.0.0',
+      },
+    },
+  ])
+
+  writeYamlFileSync('pnpm-workspace.yaml', { packages: ['**', '!store/**'] })
+  await execPnpm(['recursive', 'install', '--lockfile-only'])
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const lockfileBefore = readYamlFileSync<any>('pnpm-lock.yaml')
+  const project2VersionBefore = lockfileBefore.importers['project-2'].dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version
+
+  await execPnpm(['recursive', 'update', '--lockfile-only', '--no-save', '@pnpm.e2e/dep-of-pkg-with-1-dep@100.1.0'])
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const lockfile = readYamlFileSync<any>('pnpm-lock.yaml')
+  const depKeys = Object.keys(lockfile.packages ?? {}).filter((key) => key.startsWith('@pnpm.e2e/dep-of-pkg-with-1-dep@'))
+
+  expect(lockfile.importers['project-2'].dependencies['@pnpm.e2e/dep-of-pkg-with-1-dep'].version).toBe(project2VersionBefore)
+  expect(depKeys.filter((key) => key.startsWith('@pnpm.e2e/dep-of-pkg-with-1-dep@101.'))).toStrictEqual([`@pnpm.e2e/dep-of-pkg-with-1-dep@${project2VersionBefore}`])
+})
+
 test('recursive update', async () => {
   await addDistTag('@pnpm.e2e/foo', '100.1.0', 'latest')
   preparePackages([
