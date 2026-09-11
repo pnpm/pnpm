ed550da880397cdf81e7d83fafcdaecc8aa8631b
Trevor Burnham <trevorburnham@gmail.com>
2026-09-11 18:56:47 -0400

    style(deps-restorer): let rustfmt reorder the test imports

--- pnpm/crates/deps-restorer/src/link_file/tests/links.rs
+++ pnpm/crates/deps-restorer/src/link_file/tests/links.rs
@@ -1,5 +1,9 @@
 #[cfg(target_os = "linux")]
 use super::super::AUTO_FIRST_TIER;
+use super::{
+    super::{Host, LINK_STATE_HARDLINK, LinkFileError, auto_link, link_file, try_import},
+    OutOfLinks, write_source,
+};
 #[cfg(unix)]
 use super::{
     super::{
@@ -7,10 +11,6 @@ use super::{
         is_link_permission_error, recover_from_concurrent_import,
     },
     EaccesHardLink, EaccesLinks, EpermHardLink, EpermReflink, inode,
-};
-use super::{
-    super::{Host, LINK_STATE_HARDLINK, LinkFileError, auto_link, link_file, try_import},
-    OutOfLinks, write_source,
 };
 use pnpm_config::PackageImportMethod;
 use pnpm_reporter::SilentReporter;
