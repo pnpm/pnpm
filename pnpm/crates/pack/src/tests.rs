918165690f749acc7b781c9c332f12456ecbeab0
btea <2356281422@qq.com>
2026-09-11 19:53:22 +0800

    fix: update

--- pnpm/crates/pack/src/tests.rs
+++ pnpm/crates/pack/src/tests.rs
@@ -160,7 +160,7 @@ fn tarball_entries_are_written_in_compression_order() {
             "package/B.txt".into(),
             "package/mmm.txt".into(),
             "package/zzz.txt".into(),
-        ]
+        ],
     );
 }
 
@@ -189,7 +189,7 @@ fn duplicate_named_files_are_adjacent_for_compression() {
             "package/package.json".into(),
             "package/template-a/hero.png".into(),
             "package/template-b/hero.png".into(),
-        ]
+        ],
     );
 }
 
